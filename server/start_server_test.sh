f3f668bd1abea07e99596bc2475d0bd3c92a3b0b
Srinidhi Viswanathan <srinidhi@mit.edu>
2016-10-29 20:56:15 -0400

    add new server.sh and reference-test.conf for end-to-end tests

---
+++ server/start_server_test.sh
@@ -0,0 +1,2 @@
+../scripts/gen_thrift_file.py java '../thrift/ModelDB.thrift' './src/main/thrift/' 
+mvn clean compile && mvn exec:java -Dexec.mainClass="edu.mit.csail.db.ml.main.Main" -Dexec.args="-conf ./src/main/resources/reference-test.conf"
