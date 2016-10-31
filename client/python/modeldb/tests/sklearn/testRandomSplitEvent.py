6923f8d7d1b2a4606789feb978fb1ccc2837b23c
Srinidhi Viswanathan <srinidhi@mit.edu>
2016-10-31 18:21:34 -0400

    add slight change to random split event, for row number range.

--- client/python/modeldb/tests/sklearn/testRandomSplitEvent.py
+++ client/python/modeldb/tests/sklearn/testRandomSplitEvent.py
@@ -28,6 +28,7 @@ class TestRandomSplitEvent(unittest.TestCase):
         X_set, y_set = SyncableRandomSplit.randomSplit(X, [0.7, 0.3], seed, y)
         events = SyncerTest.instance.sync()
         self.randomSplitEvent = events[0]
+        print self.randomSplitEvent
 
     def test_random_split_event(self):
         utils.validate_random_split_event_struct(self.randomSplitEvent, self)
@@ -57,8 +58,8 @@ class TestRandomSplitEvent(unittest.TestCase):
         utils.validate_dataframe_struct(dataframe2, self)
 
         # Check if dataframes are split according to weights (within some margin of error)
-        self.assertIn(dataframe1.numRows, range(60,80))
-        self.assertIn(dataframe2.numRows, range(20,40))
+        self.assertIn(dataframe1.numRows, range(65,75))
+        self.assertIn(dataframe2.numRows, range(25,35))
         self.assertEquals(dataframe1.numRows + dataframe2.numRows, 100)
 
 if __name__ == '__main__':
