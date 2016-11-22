15e71b66aac4b21c8ebc44b81b9d0c73d0b190b6
Harihar Subramanyam <hahasubbu@gmail.com>
2016-11-21 20:57:22 -0500

    Revert "Update gen_sqlite so that it checks for dependencies before trying to…"

--- server/codegen/gen_sqlite.sh
+++ server/codegen/gen_sqlite.sh
@@ -1,21 +1,11 @@
 #!/usr/bin/env bash
+rm -rf jars/
+rm sqlite/*jar
 mkdir -p jars
-mkdir -p sqlite
-
-dependencies=(jars/jooq-3.8.4.jar jars/jooq-meta-3.8.4.jar jars/jooq-codegen-3.8.4.jar sqlite/sqlite-jdbc-3.8.11.2.jar)
-sources=(http://central.maven.org/maven2/org/jooq/jooq/3.8.4/jooq-3.8.4.jar http://central.maven.org/maven2/org/jooq/jooq-meta/3.8.4/jooq-meta-3.8.4.jar http://central.maven.org/maven2/org/jooq/jooq-codegen/3.8.4/jooq-codegen-3.8.4.jar http://central.maven.org/maven2/org/xerial/sqlite-jdbc/3.8.11.2/sqlite-jdbc-3.8.11.2.jar)
-
-for ind in {0..3}
-do
-	if [ -f ${dependencies[$ind]} ];
-	then
-		 echo "File ${dependencies[$ind]} exists."
-	else
-		 echo "File ${dependencies[$ind]} does not exist. Fetching from Internet."
-     wget ${sources[$ind]} -O ${dependencies[$ind]}
-	fi
-done
-
+wget http://central.maven.org/maven2/org/jooq/jooq/3.8.4/jooq-3.8.4.jar -O jars/jooq-3.8.4.jar
+wget http://central.maven.org/maven2/org/jooq/jooq-meta/3.8.4/jooq-meta-3.8.4.jar -O jars/jooq-meta-3.8.4.jar
+wget http://central.maven.org/maven2/org/jooq/jooq-codegen/3.8.4/jooq-codegen-3.8.4.jar -O jars/jooq-codegen-3.8.4.jar
+wget http://central.maven.org/maven2/org/xerial/sqlite-jdbc/3.8.11.2/sqlite-jdbc-3.8.11.2.jar -O sqlite/sqlite-jdbc-3.8.11.2.jar
 cat ./sqlite/createDb.sql | sqlite3 modeldb.db  &&
 cat ./sqlite/createDb.sql | sqlite3 modeldb_test.db  &&
 java -classpath jars/jooq-3.8.4.jar:jars/jooq-meta-3.8.4.jar:jars/jooq-codegen-3.8.4.jar:sqlite/sqlite-jdbc-3.8.11.2.jar:. org.jooq.util.GenerationTool sqlite/library.xml && 
