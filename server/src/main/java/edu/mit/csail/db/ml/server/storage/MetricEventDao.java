3b07d7b20fa2c053216e7da1617cf4a602299ca6
Harihar Subramanyam <hahasubbu@gmail.com>
2017-01-24 13:48:08 -0500

    Commented MetricEventDao

--- server/src/main/java/edu/mit/csail/db/ml/server/storage/MetricEventDao.java
+++ server/src/main/java/edu/mit/csail/db/ml/server/storage/MetricEventDao.java
@@ -9,12 +9,24 @@ import modeldb.MetricEvent;
 import modeldb.MetricEventResponse;
 import org.jooq.DSLContext;
 
+/**
+ * This class contains logic for storing and reading metric events.
+ */
 public class MetricEventDao {
+  /**
+   * Store a MetricEvent in the database.
+   * @param me - The metric event.
+   * @param ctx - The database context.
+   * @return A response indicating that the MetricEvent has been stored.
+   */
   public static MetricEventResponse store(MetricEvent me, DSLContext ctx) {
+    // Store the DataFrame being evaluated.
     DataframeRecord df = DataFrameDao.store(me.df, me.experimentRunId, ctx);
+
+    // Store the Transformer being evaluated.
     TransformerRecord t = TransformerDao.store(me.model, me.experimentRunId, ctx);
 
-
+    // Store an entry in the MetricEvent table.
     MetriceventRecord meRec = ctx.newRecord(Tables.METRICEVENT);
     meRec.setId(null);
     meRec.setTransformer(t.getId());
@@ -24,8 +36,9 @@ public class MetricEventDao {
     meRec.setExperimentrun(me.experimentRunId);
     meRec.store();
 
+    // Store an entry in the Event table.
     EventRecord ev = EventDao.store(meRec.getId(), "metric", me.experimentRunId, ctx);
-
+    
     return new MetricEventResponse(t.getId(), df.getId(), ev.getId(), meRec.getId());
   }
 }
