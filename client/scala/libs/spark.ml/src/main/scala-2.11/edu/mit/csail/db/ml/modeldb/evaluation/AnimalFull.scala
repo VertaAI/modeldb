7a958da522f439754dff3e4f3ec3a741248fce56
Harihar Subramanyam <hahasubbu@gmail.com>
2017-01-31 17:45:22 -0500

    Comment AnimalFull

--- client/scala/libs/spark.ml/src/main/scala-2.11/edu/mit/csail/db/ml/modeldb/evaluation/AnimalFull.scala
+++ client/scala/libs/spark.ml/src/main/scala-2.11/edu/mit/csail/db/ml/modeldb/evaluation/AnimalFull.scala
@@ -5,15 +5,22 @@ import org.apache.spark.ml.classification.{LogisticRegression, OneVsRest}
 import org.apache.spark.ml.evaluation.MulticlassClassificationEvaluator
 import edu.mit.csail.db.ml.modeldb.client.ModelDbSyncer._
 import org.apache.spark.ml.tuning.{CrossValidator, ParamGridBuilder}
-
+/**
+  * This workflow runs a preprocessing pipeline, trains a cross-validated one vs. rest logistic regression model on
+  * the animal shelter data, and evaluates it. It measures the time required to perform various operations.
+  */
 object AnimalFull {
   def run(config: Config): Unit = {
+    // Create the session and read the dataset.
     val spark = Common.makeSession()
     val df = Common.ensureMinSize(Common.readAnimalShelter(config.pathToData, spark), config.minNumRows)
+
+    // Start timing operations and create the syncer if necessary.
     Timer.activate()
 
     if (config.syncer) Common.makeSyncer()
 
+    // Build a preprocessing pipeline.
     val categoricalCols = Array[String]("AnimalType", "SexuponOutcome",
       "SimpleBreed", "SimpleColor")
     val numericalCol = "AgeInYears"
@@ -33,8 +40,10 @@ object AnimalFull {
     )(spark.sqlContext)
     val labelConverter = labelConverterOpt.get
 
+    // Split the data.
     val Array(train, test) = preprocessedData.randomSplitSync(Array(0.7, 0.3))
 
+    // Create a cross-validated model.
     val lr = new LogisticRegression()
       .setMaxIter(20)
       .setLabelCol(FeatureVectorizer.indexed(labelCol))
@@ -64,6 +73,8 @@ object AnimalFull {
       .setNumFolds(3)
 
     val model = cv.fitSync(train)
+
+    // Evaluate the model.
     val predictions = model.transformSync(test)
     model.saveSync("animal_full_model")
 
