27a80c75d3c37174668b646c6ddf1b04e6dd81c3
Wei-En Lee <weienlee@mit.edu>
2017-02-07 03:03:13 -0500

    added model types to projects page

--- frontend/public/js/projects.js
+++ frontend/public/js/projects.js
@@ -1,4 +1,8 @@
+var category10 = ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd',
+                  '#8c564b', '#e377c2', '#7f7f7f', '#bcbd22', '#17becf'];
+
 $(function() {
+  $('#search').appendTo($('nav'));
   $('#search').focus();
   $('#search').hideseek({
     highlight: true
@@ -36,6 +40,8 @@ $(function() {
           for (var j=0; j<model.metrics.length; j++) {
             var metric = model.metrics[j];
             if (metrics.hasOwnProperty(metric.key)) {
+              metrics[metric.key].total += metric.val;
+              metrics[metric.key].count += 1;
               if (metric.val < metrics[metric.key].min) {
                 metrics[metric.key].min_id = model.id;
                 metrics[metric.key].min = metric.val;
@@ -49,7 +55,9 @@ $(function() {
                 'min_id': model.id,
                 'max_id': model.id,
                 'min': metric.val,
-                'max': metric.val
+                'max': metric.val,
+                'total': metric.val,
+                'count': 1
               };
             }
           }
@@ -72,24 +80,45 @@ $(function() {
         project.find('.project-metric-names').append($('<h2>Metrics</h2>'));
         project.find('.project-min-models').append($('<h2>min</h2>'));
         project.find('.project-max-models').append($('<h2>max</h2>'));
+        project.find('.project-avg-models').append($('<h2>average</h2>'));
 
         var hasMetrics = false;
         for (var key in metrics) {
           if (metrics.hasOwnProperty(key)) {
             hasMetrics = true;
+            var avg = (metrics[key].total/metrics[key].count).toFixed(2)
             project.find('.project-metric-names').append($('<div class="project-metric-name">' + key + '</div>'));
             project.find('.project-min-models').append($('<div class="project-min-model">' + metrics[key].min + '</div>'));
             project.find('.project-max-models').append($('<div class="project-max-model">' + metrics[key].max + '</div>'));
+            project.find('.project-avg-models').append($('<div class="project-avg-model">' + avg + '</div>'));
           }
         }
 
         if (!hasMetrics) {
           project.find('.project-metric-names').append($('<div class="project-metric-name">' + 'N/A' + '</div>'));
-          project.find('.project-min-models').append($('<div class="project-metric-name">' + 'N/A' + '</div>'));
-          project.find('.project-max-models').append($('<div class="project-metric-name">' + 'N/A' + '</div>'));
+          project.find('.project-min-models').append($('<div class="project-min-model">' + 'N/A' + '</div>'));
+          project.find('.project-max-models').append($('<div class="project-max-model">' + 'N/A' + '</div>'));
+          project.find('.project-avg-models').append($('<div class="project-avg-model">' + 'N/A' + '</div>'));
         }
-        console.log(types);
-        console.log(metrics);
+
+        if (!jQuery.isEmptyObject(types)) {
+          var cursor = 0;
+          var bar = project.find('.project-model-types-bar');
+          $('<h2>Project Model Types</h2>').insertBefore(bar);
+
+          for (var key in types) {
+            if (types.hasOwnProperty(key)) {
+              var block = $('<div class="project-model-type"></div>');
+              var width = 100 * types[key] / response.length;
+              block.css({
+                'background-color': category10[cursor],
+                'width': width + '%'
+              });
+              cursor += 1;
+              bar.append(block);
+            }
+          }
+        }
       }
     });
   };
