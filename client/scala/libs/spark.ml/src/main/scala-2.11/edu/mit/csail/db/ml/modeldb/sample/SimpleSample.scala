45ae07a0eba0fe2054db33b1e5624b4335b42098
mpvartak <mpvartak@gmail.com>
2017-02-08 12:19:30 -0500

    Update SimpleSample.scala

--- client/scala/libs/spark.ml/src/main/scala-2.11/edu/mit/csail/db/ml/modeldb/sample/SimpleSample.scala
+++ client/scala/libs/spark.ml/src/main/scala-2.11/edu/mit/csail/db/ml/modeldb/sample/SimpleSample.scala
@@ -3,7 +3,7 @@ package edu.mit.csail.db.ml.modeldb.sample
 // // modeldb start
 // import edu.mit.csail.db.ml.modeldb.client.ModelDbSyncer._
 // import edu.mit.csail.db.ml.modeldb.client.{SyncerConfig, DefaultExperiment, ModelDbSyncer, NewExperimentRun, NewOrExistingProject}
-// modeldb end
+// // modeldb end
 
 import org.apache.log4j.{Level, Logger}
 import org.apache.spark.sql.SparkSession
