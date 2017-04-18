2e62123d7d46bd8a3816ffff280031f5b0e852fc
Justin Anderson <jander@mit.edu>
2017-04-18 15:34:11 -0400

    Take mongo hostname from command line in wait script.

--- dockerbuild/wait_for_mongodb.sh
+++ dockerbuild/wait_for_mongodb.sh
@@ -7,14 +7,20 @@ host="$1"
 shift
 cmd="$@"
 
-until mongo mongo/foo --eval "db.getCollectionNames()" > /dev/null 2>&1; do
+# 'foo' doesn't matter here. Just needs something so that $host is interpreted
+# as a hostname and not a database name.
+until mongo "$host"/foo --eval "db.getCollectionNames()" > /dev/null 2>&1; do
     >&2 echo "MongoDB is unavailable - sleeping"
     sleep 1
 done
 
 >&2 echo "MongoDB is up - executing command"
 
-pwd
-echo $cmd
+# Substitute the desired MongoDB hostname in the built project's conf file.
+before="$PWD"
+cd /modeldb/server
+cp src/main/resources/reference-docker.conf target/classes/reference.conf
+sed -i "s/MONGODB_HOST/$host/" target/classes/reference.conf
+cd "$before"
 
 exec $cmd
