81027dc077cc0a7d7574ce3e4c193324d923f64a
mcslao <mcslao@mit.edu>
2017-04-19 18:51:51 -0400

    Update start server test script

--- server/start_server_test.sh
+++ server/start_server_test.sh
@@ -1,2 +1,7 @@
 ../scripts/gen_thrift_file.py java '../thrift/ModelDB.thrift' './src/main/thrift/' 
-mvn clean compile && mvn exec:java -Dexec.mainClass="edu.mit.csail.db.ml.main.Main" -Dexec.args="-conf ./src/main/resources/reference-test.conf"
+
+# check if thrift version specified and pass that on
+if [ -n "$1" ]; then
+    THRIFT_VERSION="-Dthrift_version=$1"    
+fi
+mvn clean compile $THRIFT_VERSION && mvn exec:java -Dexec.mainClass="edu.mit.csail.db.ml.main.Main" -Dexec.args="-conf ./src/main/resources/reference-test.conf" $THRIFT_VERSION
