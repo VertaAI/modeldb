14b7ea83a4ff2432845e3aaf1f8babb087f0c12b
Albert Carter <albert.r.carter@gmail.com>
2017-04-21 14:00:45 -0400

    remove tests. thaw scikit-learn req

--- client/python/setup.py
+++ client/python/setup.py
@@ -23,7 +23,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.0.0.dev1',
+    version='0.0.0.dev5',
 
     description='A system to manage machine learning models',
     long_description=long_description,
@@ -68,7 +68,7 @@ setup(
 
     # You can just specify the packages manually here if your project is
     # simple. Or you can use find_packages().
-    packages=find_packages(),
+    packages=find_packages(exclude=['*.tests.*', '*.tests']),
 
     # Alternatively, if you want to distribute just a my_module.py, uncomment
     # this:
@@ -80,7 +80,7 @@ setup(
     # https://packaging.python.org/en/latest/requirements.html
     install_requires=[
         'numpy', 'pandas', 'statsmodels', 'matplotlib', 'patsy',
-        'scikit-learn==0.17.1', 'sklearn', 'thrift', 'pyyaml', 'requests'],
+        'scikit-learn', 'sklearn', 'thrift', 'pyyaml', 'requests'],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
@@ -102,7 +102,7 @@ setup(
     # need to place data files outside of your packages. See:
     # http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files # noqa
     # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
-    data_files=[('config', ['../syncer.json'])],
+    data_files=[('config', ['syncer.json'])],
 
     # To provide executable scripts, use entry points in preference to the
     # "scripts" keyword. Entry points provide cross-platform support and allow
