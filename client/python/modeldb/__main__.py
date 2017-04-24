989c4a9ee5739d2391e4fdf0a858769242864b26
Albert Carter <albert.r.carter@gmail.com>
2017-04-24 17:41:12 -0400

    create duplciate syncer.json on request

---
+++ client/python/modeldb/__main__.py
@@ -0,0 +1,174 @@
+from sys import argv, exit, stderr
+from runpy import run_path
+
+import os
+import json
+
+
+def to_bool(value):
+    valid = {'true': True, 't': True, '1': True,
+             'false': False, 'f': False, '0': False,
+             }
+
+    if isinstance(value, bool):
+        return value
+
+    if not isinstance(value, basestring):
+        raise ValueError('invalid literal for boolean. Not a string.')
+
+    lower_value = value.lower()
+    if lower_value in valid:
+        return valid[lower_value]
+    else:
+        raise ValueError('invalid literal for boolean: "%s"' % value)
+
+
+if __name__ == '__main__':
+    execution_dir = os.getcwd()
+    file_dir = os.path.dirname(__file__)
+    filename = file_dir + '/syncer.json'
+    config = json.load(file(filename, 'r'))
+    new_config = json.load(file(filename, 'r'))
+
+    prompt = '> '
+
+    print(
+        'This utility helps you create a modeldb configuration file.\n'
+        'The file is used by the modeldb python and/or scala clients to '
+        'connect to the modeldb server.\n\n'
+        'Press <ENTER> to use defaults')
+
+    raw_input('OK')
+
+    print('\n== Thrift ==')
+
+    prompt = 'host (default=%s): ' % config['thrift']['host']
+    thrift_host = raw_input(prompt)
+    if thrift_host == '':
+        thrift_host = config['thrift']['host']
+    new_config['thrift']['host'] = thrift_host
+
+    prompt = 'port (default=%s): ' % (config['thrift']['port'])
+    thrift_port = raw_input(prompt)
+    if thrift_port == '':
+        thrift_port = config['thrift']['port']
+    thrift_port = int(thrift_port)
+    new_config['thrift']['port'] = thrift_port
+
+    print('\n== Syncing Strategy =='
+          'eager/lazy')
+
+    prompt = 'syncing strategy (default=%s): ' % config['syncingStrategy']
+    syncing_strategy = raw_input(prompt)
+    if syncing_strategy == '':
+        syncing_strategy = config['syncingStrategy']
+    new_config['syncingStrategy'] = syncing_strategy
+
+    print('\n== Project ==')
+    prompt = 'name (default=%s): ' % (config['project']['name'])
+    project_name = raw_input(prompt)
+    if project_name == '':
+        project_name = config['project']['name']
+    new_config['project']['name'] = project_name
+
+    prompt = 'author (default=%s): ' % (config['project']['author'])
+    project_author = raw_input(prompt)
+    if project_author == '':
+        project_author = config['project']['author']
+    new_config['project']['author'] = project_author
+
+    prompt = 'description (default=%s): ' % (config['project']['description'])
+    project_description = raw_input(prompt)
+    if project_description == '':
+        project_description = config['project']['description']
+    new_config['project']['description'] = project_description
+
+    print('\n== Experiment ==')
+    prompt = 'name (default=%s): ' % (config['experiment']['name'])
+    experiment_name = raw_input(prompt)
+    if experiment_name == '':
+        experiment_name = config['experiment']['name']
+    new_config['experiment']['name'] = experiment_name
+
+    prompt = 'description (default=%s): ' % (
+        config['experiment']['description'])
+    experiment_description = raw_input(prompt)
+    if experiment_description == '':
+        experiment_description = config['experiment']['description']
+    new_config['experiment']['description'] = experiment_description
+
+    print('\n== Experiment Run Description ==')
+    prompt = 'experiment run description (default=%s): ' % (
+        config['experimentRun']['description'])
+    experiment_run_description = raw_input(prompt)
+    if experiment_run_description == '':
+        experiment_run_description = config['experimentRun']
+    new_config['experimentRun'] = experiment_run_description
+
+    print('\n== ShouldCountRows ==')
+    prompt = 'Should Count Rows (default=%s): ' % (config['shouldCountRows'])
+    should_count_rows = raw_input(prompt)
+    if should_count_rows == '':
+        should_count_rows = config['shouldCountRows']
+    should_count_rows = to_bool(should_count_rows)
+    new_config['shouldCountRows'] = should_count_rows
+
+    print('\n== shouldStoreGSCVE ==')
+    prompt = 'Should store GSCVE (default=%s): ' % (config['shouldStoreGSCVE'])
+    should_store_gscve = raw_input(prompt)
+    if should_store_gscve == '':
+        should_store_gscve = config['shouldStoreGSCVE']
+    should_store_gscve = to_bool(should_store_gscve)
+    new_config['shouldStoreGSCVE'] = should_store_gscve
+
+    print('\n== shouldStoreSpecificModels ==')
+    prompt = ('Should store specific models (default=%s): '
+              % (config['shouldStoreSpecificModels']))
+    should_store_specific_models = raw_input(prompt)
+    if should_store_specific_models == '':
+        should_store_specific_models = config['shouldStoreSpecificModels']
+    should_store_specific_models = to_bool(should_store_specific_models)
+    new_config[
+        'shouldStoreSpecificModels'] = should_store_specific_models
+
+    print('\n== Git ==')
+    prompt = 'version_code (default=%s): ' % (config['git']['versionCode'])
+    git_version_code = raw_input(prompt)
+    if git_version_code == '':
+        git_version_code = config['git']['versionCode']
+    new_config['git']['versionCode'] = git_version_code
+
+    prompt = 'username (default=%s): ' % (config['git']['username'])
+    git_username = raw_input(prompt)
+    if git_username == '':
+        git_username = config['git']['username']
+    new_config['git']['username'] = git_username
+
+    prompt = 'access token (default=%s): ' % (
+        str(config['git']['accessToken']))
+    git_access_token = raw_input(prompt)
+    if git_access_token == '':
+        git_access_token = config['git']['accessToken']
+    new_config['git']['accessToken'] = git_access_token
+
+    prompt = 'repo (default=%s): ' % (str(config['git']['repo']))
+    git_repo = raw_input(prompt)
+    if git_repo == '':
+        git_repo = config['git']['repo']
+    new_config['git']['repo'] = git_repo
+
+    prompt = 'export directory (default=%s): ' % (
+        str(config['git']['exptDir']))
+    git_export_dir = raw_input(prompt)
+    if git_export_dir == '':
+        git_export_dir = config['git']['exptDir']
+    new_config['git']['exptDir'] = git_export_dir
+
+    prompt = 'repo directory (default=%s): ' % (str(config['git']['repoDir']))
+    git_repo_dir = raw_input(prompt)
+    if git_repo_dir == '':
+        git_repo_dir = config['git']['repoDir']
+    new_config['git']['repoDir'] = git_repo_dir
+
+    import pdb
+    pdb.set_trace()
--- client/python/setup.py
+++ client/python/setup.py
@@ -23,8 +23,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.0.1.a3',
-
+    version='0.0.1.a4',
     description='A system to manage machine learning models',
     long_description=long_description,
 
@@ -94,15 +93,15 @@ setup(
     # If there are data files included in your packages that need to be
     # installed, specify them here.  If using Python 2.6 or less, then these
     # have to be included in MANIFEST.in as well.
-    # package_data={
-    #     'config': ['..syncer.json'],
-    # },
+    package_data={
+        '': ['syncer.json'],
+    },
 
     # Although 'package_data' is the preferred approach, in some case you may
     # need to place data files outside of your packages. See:
     # http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files # noqa
     # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
-    data_files=[('config', ['syncer.json'])],
+    # data_files=[('config', ['syncer.json'])],
 
     # To provide executable scripts, use entry points in preference to the
     # "scripts" keyword. Entry points provide cross-platform support and allow
