ac8afde7a03c030b2c056cfb707d271827c50522
Albert Carter <albert.r.carter@gmail.com>
2017-05-01 12:38:04 -0400

    add missing requirement. Send updated version to pypi

--- client/python/setup.py
+++ client/python/setup.py
@@ -23,7 +23,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.0.1.a5',
+    version='0.0.1.a6',
     description='A system to manage machine learning models',
     long_description=long_description,
 
@@ -79,7 +79,7 @@ setup(
     # https://packaging.python.org/en/latest/requirements.html
     install_requires=[
         'numpy', 'pandas', 'statsmodels', 'matplotlib', 'patsy',
-        'scikit-learn', 'sklearn', 'thrift', 'pyyaml', 'requests'],
+        'scikit-learn', 'sklearn', 'thrift', 'pyyaml', 'requests', 'dpath'],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
