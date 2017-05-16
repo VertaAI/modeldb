81af2eb0f664f011a15c16f9bfa40b548d19ea38
Albert Carter <albert.r.carter@gmail.com>
2017-05-16 13:27:08 -0400

    note support for python 3.5.1. Upload to PyPi

--- client/python/setup.py
+++ client/python/setup.py
@@ -59,7 +59,7 @@ setup(
         # 'Programming Language :: Python :: 3',
         # 'Programming Language :: Python :: 3.3',
         # 'Programming Language :: Python :: 3.4',
-        # 'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.5',
     ],
 
     # What does your project relate to?
