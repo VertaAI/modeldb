dcba3270ad7eb24ea199b6d62ac252ac47b539bc
Manasi Vartak <mpvartak@gmail.com>
2018-10-29 21:22:01 -0700

    persistent store

--- dockerbuild/create_sqlite_external.sh
+++ dockerbuild/create_sqlite_external.sh
@@ -2,5 +2,6 @@
 
 if [ ! -f /db/modeldb.db ]; then
     cat /sqlite_scripts/createDb.sql | sqlite3 /db/modeldb.db
+    cat /sqlite_scripts/createDb.sql | sqlite3 /db/modeldb_test.db
     echo "created DB"
 fi
