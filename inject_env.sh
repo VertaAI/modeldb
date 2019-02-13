31e28adef879c0db4d48beb8ce4e95316276c690
Aparna Palle <aparnapallae@gmail.com>
2019-02-12 16:30:26 -0800

    Dockerfile - Remove unused code  from script
    Dockerfile - Remove unused code  from script

--- inject_env.sh
+++ inject_env.sh
@@ -21,23 +21,9 @@ set -xe
 set -xe
 : "${REACT_APP_AUTH_CALLBACK_URL?$REACT_APP_AUTH_CALLBACK_URL}"
 
-
-#sed -i "s/REACT_APP_AUTH_DOMAIN/$REACT_APP_AUTH_DOMAIN/g" /usr/share/nginx/html/static/js/main*chunk.js
-#sed -i "s/REACT_APP_AUTH_CLIENT_ID/$REACT_APP_AUTH_CLIENT_ID/g" /usr/share/nginx/html/static/js/main*chunk.js
-
-#echo "{$REACT_APP_AUTH_CALLBACK_URL//\//\\/}"
-
-#sed -i "s/REACT_APP_AUTH_CALLBACK_URL/$REACT_APP_AUTH_CALLBACK_URL/g" /usr/share/nginx/html/static/js/main*chunk.js
-
-REACT_APP_PROTOCOL='http'
-REACT_APP_DOMAIN='localhost'
-REACT_APP_PORT='8080'
-
-#sed -i "s/REACT_APP_PROTOCOL/$REACT_APP_PROTOCOL/g" /usr/share/nginx/html/static/js/main*chunk.js
-#sed -i "s/REACT_APP_DOMAIN/$REACT_APP_DOMAIN/g" /usr/share/nginx/html/static/js/main*chunk.js
-#sed -i "s/REACT_APP_PORT/$REACT_APP_PORT/g" /usr/share/nginx/html/static/js/main*chunk.js
-
-
+# REACT_APP_PROTOCOL='http'
+# REACT_APP_DOMAIN='localhost'
+# REACT_APP_PORT='8080'
 
 # Run application
 exec "$@"
