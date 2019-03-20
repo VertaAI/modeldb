572e0134a742631c70c37150e0417a6f5a927de3
kirill.gulyaev <kirill.gulyaev@quantumsoft.ru>
2019-03-20 10:57:40 +0700

    Removed console.log.

--- server.js
+++ server.js
@@ -50,12 +50,10 @@ var strategy = new Auth0Strategy(
 passport.use(strategy);
 
 passport.serializeUser(function (user, done) {
-  console.log('serializeUser');
   done(null, user);
 });
 
 passport.deserializeUser(function (user, done) {
-  console.log('deserializeUser');
   done(null, user);
 });
 
@@ -89,20 +87,17 @@ app.use('/api/auth/', auth);
 
 app.get('/api/getUser',
   (req, res, next) => {
-    if (req.user) {
-      console.log('user is auth');
+    if (req) {
       next();
     } else {
       res.send(401, 'user is not authorized');
     }
   },
   (req, res) => {
-    console.log('QQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQ');
     res.json({ user: req.user });
   }
 );
 
-console.log("hello");
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
 app.get('*', (req, res) => {
@@ -112,17 +107,14 @@ app.get('*', (req, res) => {
     tmpPath = '/index.html';
   }
   if (req.user) {
-    console.log('got user in session');
     res.cookie('verta', req.user, {maxAge: 900000, httpOnly: true});
   } else {
-    console.log('no req.user');
   }
 
   res.header("Cache-Control", "no-cache, no-store, must-revalidate");
   res.header("Pragma", "no-cache");
   res.header("Expires", 0);
 
-  // console.log(req);
   res.sendFile(path.join(__dirname+'/client/build' + tmpPath));
 });
 
@@ -130,5 +122,3 @@ app.disable('etags');
 
 const port = process.env.PORT || 3000;
 app.listen(port);
-
-console.log(`ModelDB server listening on ${port}`);
