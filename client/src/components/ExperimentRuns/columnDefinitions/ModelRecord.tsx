deb36475504a8c91450d9bb4190d95d317a6ff09
Santhosh <sandyorbit@gmail.com>
2019-03-23 16:24:38 -0700

    model record updated styles

--- client/src/components/ExperimentRuns/columnDefinitions/ModelRecord.tsx
+++ client/src/components/ExperimentRuns/columnDefinitions/ModelRecord.tsx
@@ -1,27 +1,35 @@
-import * as React from 'react';
-import { Link } from 'react-router-dom';
-
-import routes from 'routes';
-
-import styles from './ColumnDefs.module.css';
+import * as React from "react";
+import { Link } from "react-router-dom";
+import routes from "routes";
+import styles from "./ColumnDefs.module.css";
 
 class ModelRecordColDef extends React.Component<any> {
   public render() {
     const { id, projectId, experimentId } = this.props.data;
     return (
       <div className={styles.param_cell}>
-        <Link className={styles.model_link} to={routes.modelRecord.getRedirectPath({ projectId, modelRecordId: id })}>
-          <strong>Model ID</strong> {' : '}
+        <Link
+          className={styles.model_link}
+          to={routes.modelRecord.getRedirectPath({
+            projectId,
+            modelRecordId: id
+          })}
+        >
+          <strong>Model ID</strong> {" : "}
           {`${id.slice(0, 4)}...${id.slice(-4)}`}
         </Link>
-        <div className={styles.experiment_link}>{`Project ID: ${projectId.slice(0, 4)}...`}</div>
-        <div className={styles.experiment_link}>{`Experiment ID: ${experimentId.slice(0, 4)}...`}</div>
-        {/* <Draggable
-          type="filter"
-          data={{ type: PropertyType.METRIC, name: 'expID', value: modelRecord.expId, comparisonType: ComparisonType.EQUALS }}
-        >
-          <div className={styles.experiment_link}>Experiment ID</div>
-        </Draggable> */}
+        <div className={styles.experiment_link}>
+          <span className={styles.parma_link_label}> Project ID:</span>
+          <span className={styles.parma_link_value}>
+            {projectId.slice(0, 4) + ".."}
+          </span>
+        </div>
+        <div className={styles.experiment_link}>
+          <span className={styles.parma_link_label}> Experiment ID:</span>
+          <span className={styles.parma_link_value}>
+            {experimentId.slice(0, 4) + ".."}
+          </span>
+        </div>
       </div>
     );
   }
