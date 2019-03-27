862d2e3c261d2e9383d30a3f09e8f7eeace0ae0a
Santhosh <sandyorbit@gmail.com>
2019-03-26 20:27:43 -0700

    print error message in case of error

--- src/services/ProjectDataService.ts
+++ src/services/ProjectDataService.ts
@@ -73,6 +73,7 @@ export class ProjectDataService extends BaseDataService implements IProjectDataS
             }
             return projects;
           } catch (error) {
+            console.log(error);
             return data;
           }
         }
