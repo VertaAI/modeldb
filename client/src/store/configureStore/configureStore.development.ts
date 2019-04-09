43d3d29684b166edf729c7d1ad48dd5d84ad13ff
kirill.gulyaev <kirill.gulyaev@quantumsoft.ru>
2019-04-09 14:38:19 +0700

    communications. Moved configureStore.development, configureStore.production in configureStore folder.

--- client/src/store/configureStore/configureStore.development.ts
+++ client/src/store/configureStore/configureStore.development.ts
@@ -1,20 +1,26 @@
 import { routerMiddleware } from 'connected-react-router';
 import { History } from 'history';
 import { applyMiddleware, createStore, Store } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+// import createLogger from 'redux-logger';
 import reduxThunk, { ThunkMiddleware } from 'redux-thunk';
 
-import { createRootReducer, IApplicationState } from './store';
+import { createRootReducer, IApplicationState } from '../store';
 
 export default function configureStore(
   history: History,
   initialState: IApplicationState
 ): Store<IApplicationState> {
+  const composeEnhancers = composeWithDevTools({});
+
   const store = createStore(
     createRootReducer(history),
     initialState,
-    applyMiddleware(routerMiddleware(history), reduxThunk as ThunkMiddleware<
-      IApplicationState
-    >)
+    composeEnhancers(
+      applyMiddleware(routerMiddleware(history), reduxThunk as ThunkMiddleware<
+        IApplicationState
+      >)
+    )
   );
 
   return store;
