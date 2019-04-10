9e37b20070ef5fd5829dab95a97eb858f6d714d2
kirill.gulyaev <kirill.gulyaev@quantumsoft.ru>
2019-04-10 13:31:05 +0700

    Fixed bug with communications and redux thunk extraArguments.

--- client/src/store/configureStore/configureStore.production.ts
+++ client/src/store/configureStore/configureStore.production.ts
@@ -1,19 +1,35 @@
 import { routerMiddleware } from 'connected-react-router';
 import { History } from 'history';
-import { applyMiddleware, createStore, Store } from 'redux';
+import { applyMiddleware, createStore, Store, AnyAction } from 'redux';
 import reduxThunk, { ThunkMiddleware } from 'redux-thunk';
 
-import { createRootReducer, IApplicationState } from '../store';
+import { FilterContextPool } from 'models/FilterContextPool';
+import ServiceFactory from 'services/ServiceFactory';
+
+import {
+  createRootReducer,
+  IApplicationState,
+  IThunkActionDependencies,
+} from '../store';
 
 export default function configureStore(
   history: History,
   initialState: IApplicationState
 ): Store<IApplicationState> {
+  const reduxThunkExtraArgument: IThunkActionDependencies = {
+    FilterContextPool: FilterContextPool,
+    ServiceFactory: ServiceFactory,
+  };
+
   const store = createStore(
     createRootReducer(history),
     initialState,
-    applyMiddleware(routerMiddleware(history), reduxThunk as ThunkMiddleware<
-      IApplicationState
+    applyMiddleware(routerMiddleware(history), reduxThunk.withExtraArgument(
+      reduxThunkExtraArgument
+    ) as ThunkMiddleware<
+      IApplicationState,
+      AnyAction,
+      IThunkActionDependencies
     >)
   );
 
