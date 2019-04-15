7686dc817c0fe1585b5bb1ca8d2012d14077aa6f
Santhosh <sandyorbit@gmail.com>
2019-04-15 14:39:56 -0700

    refactored model name/ids on the exp-runs tables

--- client/src/components/ExperimentRuns/columnDefinitions/Summary.tsx
+++ client/src/components/ExperimentRuns/columnDefinitions/Summary.tsx
@@ -1,21 +1,28 @@
 import * as React from 'react';
+import { Link } from 'react-router-dom';
 
 import Tag from 'components/TagBlock/Tag';
 import tag_styles from 'components/TagBlock/TagBlock.module.css';
+import routes from 'routes';
 
 import styles from './ColumnDefs.module.css';
 
 class SummaryColDef extends React.Component<any> {
   public render() {
     const modelRecord = this.props.data;
+    console.log(modelRecord);
     return (
       <div className={styles.summary_cell}>
-        <div style={{ marginBottom: '10px' }}>
-          Name:{' '}
-          <div className={styles.highlight_summary}>{modelRecord.name}</div>
-        </div>
+        {modelRecord.experimentId && (
+          <div>
+            <this.parmaLink
+              label="Experiment:"
+              value={modelRecord.experimentId.slice(0, 6)}
+            />
+          </div>
+        )}
         {modelRecord.codeVersion && (
-          <div style={{ marginBottom: '10px' }}>
+          <div className={styles.codeVersion_block}>
             Code Version:{' '}
             <div className={styles.highlight_summary}>
               {modelRecord.codeVersion}
@@ -39,6 +46,16 @@ class SummaryColDef extends React.Component<any> {
       </div>
     );
   }
+
+  public parmaLink = (props: { label: string; value: string; link?: any }) => {
+    const { label, value } = props;
+    return (
+      <div className={styles.experiment_link}>
+        <span className={styles.parma_link_label}>{label}</span>{' '}
+        <span className={styles.parma_link_value}>{value.slice(0, 6)}</span>
+      </div>
+    );
+  };
 }
 
 export default SummaryColDef;
