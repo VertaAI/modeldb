85decb8046f4d5bdd914d0605a52f46a82424076
Santhosh <sandyorbit@gmail.com>
2019-04-16 01:44:39 -0700

    used number formatter function to return 4 digits

--- client/src/components/FilterSelect/MetricFilterEditor/MetricFilterEditor.tsx
+++ client/src/components/FilterSelect/MetricFilterEditor/MetricFilterEditor.tsx
@@ -2,6 +2,7 @@ import { bind } from 'decko';
 import * as React from 'react';
 
 import { ComparisonType, IMetricFilterData } from 'models/Filters';
+import { numberTo4Decimal } from 'utils/MapperConverters/NumberFormatter';
 
 import styles from './MetricFilterEditor.module.css';
 
@@ -23,9 +24,7 @@ export default class MetricFilterEditor extends React.Component<ILocalProps> {
           <option value={ComparisonType.LESS}>&lt;</option>
         </select>
         <input
-          defaultValue={(
-            Math.round(this.props.data.value * 10000) / 10000
-          ).toString()}
+          defaultValue={numberTo4Decimal(this.props.data.value).toString()}
           onBlur={this.onBlur}
           onKeyUp={this.onSubmit}
         />
--- client/src/components/FilterSelect/NumberFilterEditor/NumberFilterEditor.tsx
+++ client/src/components/FilterSelect/NumberFilterEditor/NumberFilterEditor.tsx
@@ -2,6 +2,7 @@ import { bind } from 'decko';
 import * as React from 'react';
 
 import { INumberFilterData } from 'models/Filters';
+import { numberTo4Decimal } from 'utils/MapperConverters/NumberFormatter';
 
 import styles from './NumberFilterEditor.module.css';
 
@@ -17,9 +18,7 @@ export default class NumberFilterEditor extends React.Component<ILocalProps> {
         <div className={styles.input}>
           <input
             type="number"
-            defaultValue={(
-              Math.round(this.props.data.value * 10000) / 10000
-            ).toString()}
+            defaultValue={numberTo4Decimal(this.props.data.value).toString()}
             onBlur={this.onBlur}
             onKeyUp={this.onSubmit}
           />
