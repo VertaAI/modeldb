68b120d0f576bd26f3eaf42f19d1d76d95199cb0
kirill.gulyaev <kirill.gulyaev@quantumsoft.ru>
2020-03-16 19:13:07 +0700

    Added data versioning pages.

---
+++ webapp/client/src/pages/authorized/RepositoriesPages/RepositoryDetailsPages/CommitPage/CommitPage.tsx
@@ -0,0 +1,25 @@
+import * as React from 'react';
+import { useParams } from 'react-router';
+
+import { Commit } from 'core/features/viewCommit';
+import { IRepository } from 'core/shared/models/Repository/Repository';
+import routes, { GetRouteParams } from 'routes';
+
+import RepositoryDetailsPagesLayout from '../shared/RepositoryDetailsPagesLayout/RepositoryDetailsPagesLayout';
+import styles from './CommitPage.module.css';
+
+interface ILocalProps {
+  repository: IRepository;
+}
+
+const CommitPage = ({ repository }: ILocalProps) => {
+  const params = useParams<GetRouteParams<typeof routes.repositoryCommit>>();
+
+  return (
+    <RepositoryDetailsPagesLayout repository={repository}>
+      <Commit repository={repository} commitSha={params.commitSha} />
+    </RepositoryDetailsPagesLayout>
+  );
+};
+
+export default CommitPage;
