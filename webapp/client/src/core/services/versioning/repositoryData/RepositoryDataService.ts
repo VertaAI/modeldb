d7faeb806c95c8da701dbecf7a19fd0cf7b826a5
maryann <tendersalmon@mail.ru>
2020-04-01 20:48:43 +0700

    Fixed endpoints for loading commits.

--- webapp/client/src/core/services/versioning/repositoryData/RepositoryDataService.ts
+++ webapp/client/src/core/services/versioning/repositoryData/RepositoryDataService.ts
@@ -151,7 +151,7 @@ export default class RepositoryDataService extends BaseDataService {
     const response = await this.get({
       url: DataLocation.addAsLocationPrefixQueryParams(
         location,
-        `/v1/modeldb/versioning/repositories/${repositoryId}/branches/${branch}/commits`
+        `/v1/modeldb/versioning/repositories/${repositoryId}/branches/${branch}/log`
       ),
       config: {
         params: {
@@ -282,7 +282,7 @@ export default class RepositoryDataService extends BaseDataService {
     branch: Branch
   ): Promise<IHydratedCommit> {
     const response = await this.get({
-      url: `/v1/modeldb/versioning/repositories/${repositoryId}/branches/${branch}/commits`,
+      url: `/v1/modeldb/versioning/repositories/${repositoryId}/branches/${branch}/log`,
       config: {
         params: convertClientPaginationToNamespacedServerPagination({
           currentPage: 0,
