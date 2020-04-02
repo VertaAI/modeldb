e47cbf5471287966f92db2b4bf32634ddf623a50
Evgenii Lezhnin <eugene.lezhnin@quantumsoft.ru>
2020-04-02 13:36:11 +0700

    VR-3754 delete workspace role bindings common function

--- backend/src/main/java/ai/verta/modeldb/versioning/RepositoryDAORdbImpl.java
+++ backend/src/main/java/ai/verta/modeldb/versioning/RepositoryDAORdbImpl.java
@@ -32,6 +32,7 @@ import java.util.HashSet;
 import java.util.LinkedHashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
