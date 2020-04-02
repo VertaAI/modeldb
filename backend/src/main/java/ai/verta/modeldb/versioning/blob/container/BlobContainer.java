bb0fae2be63bfae96587eafeda985f01bf716815
evgeniilezhnin <eugene.lezhnin@quantumsoft.ru>
2020-04-02 11:26:46 +0700

    Feature/vr 3616 validation refactoring (#412)
    * VR-3616 validation refactoring to use visitors
    
    * VR-3616 use post instead of pre
    
    Co-authored-by: Conrado Miranda <conrado@verta.ai>
    Co-authored-by: Ravi Shetye <ravi@verta.ai>
    Co-authored-by: conradoverta <53195685+conradoverta@users.noreply.github.com>

--- backend/src/main/java/ai/verta/modeldb/versioning/blob/container/BlobContainer.java
+++ backend/src/main/java/ai/verta/modeldb/versioning/blob/container/BlobContainer.java
@@ -3,8 +3,6 @@ package ai.verta.modeldb.versioning.blob.container;
 import ai.verta.modeldb.ModelDBException;
 import ai.verta.modeldb.versioning.BlobExpanded;
 import ai.verta.modeldb.versioning.FileHasher;
-import ai.verta.modeldb.versioning.PathDatasetBlob;
-import ai.verta.modeldb.versioning.PathDatasetComponentBlob;
 import ai.verta.modeldb.versioning.TreeElem;
 import io.grpc.Status.Code;
 import java.security.NoSuchAlgorithmException;
@@ -38,8 +36,6 @@ public abstract class BlobContainer {
     }
   }
 
-  public abstract void validate() throws ModelDBException;
-
   /**
    * get location list used in in tree representation
    *
@@ -62,16 +58,4 @@ public abstract class BlobContainer {
   public abstract void process(
       Session session, TreeElem rootTree, FileHasher fileHasher, Set<String> blobHashes)
       throws NoSuchAlgorithmException, ModelDBException;
-
-  protected void validate(PathDatasetBlob path) throws ModelDBException {
-    for (PathDatasetComponentBlob component : path.getComponentsList()) {
-      validate(component);
-    }
-  }
-
-  protected void validate(PathDatasetComponentBlob path) throws ModelDBException {
-    if (path.getPath().isEmpty()) {
-      throw new ModelDBException("Dataset path is empty", Code.INVALID_ARGUMENT);
-    }
-  }
 }
