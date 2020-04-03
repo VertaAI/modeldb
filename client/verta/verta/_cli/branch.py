3859e47737676c71236f34df45742ee78ce85fae
Michael Liu <miliu@protonmail.com>
2020-04-02 20:31:58 -0700

    Outline verta checkout

--- client/verta/verta/_cli/branch.py
+++ client/verta/verta/_cli/branch.py
@@ -7,4 +7,15 @@ import click
 @click.option('--repo', help="Repository name")  # TODO: default, get from state
 def branch(repo):
     """List available branches"""
-    click.echo("You have no branches because this is a demo")
+    click.echo("you have no branches because this is a demo")
+
+@click.command()
+@click.option('-b', is_flag=True, help="Create and checkout a new branch")
+@click.argument('branch')
+@click.option('--repo', help="Repository name")  # TODO: default, get from state
+def checkout(b, branch, repo):
+    """Checkout a branch to work on"""
+    msg = "checking out {} from repo {}".format(branch, repo)
+    if b:
+        msg = "creating and "+msg
+    click.echo(msg)
--- client/verta/verta/_cli/remote.py
+++ client/verta/verta/_cli/remote.py
@@ -14,11 +14,11 @@ def remote():
 @click.argument('url')
 def add(name, url):
     """Add a remote repository"""
-    click.echo("Registered repo {} at url {}".format(name, url))
+    click.echo("registered repo {} at url {}".format(name, url))
 
 
 @remote.command()
 @click.argument('name')
 def use(name):
     """Set a remote repository as the active one"""
-    click.echo("Now using repo {}".format(name))
+    click.echo("now using repo {}".format(name))
