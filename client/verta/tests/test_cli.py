231cc32d488ea953b5f7aea6f1cc89c39d06f324
Michael Liu <miliu@protonmail.com>
2020-04-17 17:14:37 -0700

    Add tests

--- client/verta/tests/test_cli.py
+++ client/verta/tests/test_cli.py
@@ -1,5 +1,7 @@
 import os
 
+import yaml
+
 import pytest
 from click.testing import CliRunner
 from verta._cli import cli
@@ -19,3 +21,37 @@ class TestInit:
             result = runner.invoke(cli, ['init'])
             assert not result.exception
             assert result.output.startswith("found existing config file ")
+
+
+class TestRemote:
+    def test_add(self):
+        # TODO: create a new repo and use it
+        name = "banana"
+        url = "https://www.verta.ai/"
+
+        runner = CliRunner()
+        with runner.isolated_filesystem():
+            runner.invoke(cli, ['init'])
+
+            result = runner.invoke(cli, ['remote', 'add', name, url])
+            assert not result.exception
+            with _config_utils.read_config() as config:
+                remotes = config['remotes']
+                remote = remotes[name]
+                assert remote['url'] == url
+                assert remote['branch'] == "master"
+
+    def test_use(self):
+        # TODO: create a new repo and use it
+        name = "banana"
+        url = "https://www.verta.ai/"
+
+        runner = CliRunner()
+        with runner.isolated_filesystem():
+            runner.invoke(cli, ['init'])
+            runner.invoke(cli, ['remote', 'add', name, url])
+
+            result = runner.invoke(cli, ['remote', 'use', name])
+            assert not result.exception
+            with _config_utils.read_config() as config:
+                assert config['current-remote'] == name
