47f3c2b75dea6a8a08ea3c8fc6eb4a4ac7a355f6
Michael Liu <miliu@protonmail.com>
2020-04-21 15:54:26 -0700

    Consolidate into one fn

--- client/verta/verta/_internal_utils/_data_utils.py
+++ client/verta/verta/_internal_utils/_data_utils.py
@@ -1,53 +1,38 @@
 # -*- coding: utf-8 -*-
 
-def calculate_bin_boundaries(data, num_bins=10):
+def calculate_float_histogram(data, num_bins=10):
     """
-    Calculates boundaries for `num_bins` equally-spaced histogram bins.
+    Calculates a histogram for continuous `data`.
 
     Parameters
     ----------
-    data : sequence of numbers
+    data : list-like of float
         Continuous data to be binned.
     num_bins : int, default 10
         Number of bins to use.
 
     Returns
     -------
-    bin_boundaries : list of float with length `num_bins`+1
-        Boundaries of histogram bins.
+    histogram : dict
 
     """
+    # calculate bin boundaries
     start, stop = min(data), max(data)
     space = (stop - start)/num_bins
-    return [start + space*i for i in range(num_bins+1)]
+    bin_boundaries = [start + space*i for i in range(num_bins+1)]
 
-
-def calculate_reference_counts(data, bin_boundaries):
-    """
-    Fits `data` into the histogram bins defined by `bin_boundaries`.
-
-    Parameters
-    ----------
-    data : sequence of numbers
-        Numerical data to be binned.
-    bin_boundaries : list of float of length N+1
-        Boundaries for a histogram's N bins.
-
-    Returns
-    -------
-    list of float with length N
-        Counts of `data` values in each bin defined by `bin_boundaries`.
-
-    Raises
-    ------
-    ValueError
-        If `data` contains a value outside the range defined by `bin_boundaries`.
-
-    """
-    # TODO: there is definitely a faster way to do this
+    # fit `data` into bins
     reference_counts = []
     for l, r in zip(bin_boundaries[:-1], bin_boundaries[1:]):
         count = len([datum for datum in data if l <= datum < r])
         reference_counts.append(count)
 
-    return reference_counts
+    return {
+        'histogram': {
+            'float': {
+                'bucket_limits': bin_boundaries,
+                'count': reference_counts,
+            },
+        },
+        'type': "float",
+    }
