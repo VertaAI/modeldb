b75fe2d940f692a86bd613477c6e04147d57547b
Michael Liu <miliu@protonmail.com>
2020-04-23 16:52:11 -0700

    Impl discrete histogram

--- client/verta/verta/_internal_utils/_histogram_utils.py
+++ client/verta/verta/_internal_utils/_histogram_utils.py
@@ -2,23 +2,76 @@
 
 def calculate_histogram(data):
     try:  # binary
-        pass#return calculate_binary_histogram(col)
+        pass#return calculate_binary_histogram(data)
     except HistogramError:
         pass
 
-    try:  # discrete/categorical
-        pass#return calculate_discrete_histogram(col)
+    try:  # discrete
+        return calculate_discrete_histogram(data)
     except HistogramError:
         pass
 
     # continuous
     return calculate_float_histogram(data)
 
-def calculate_binary_histogram(data):
-    raise NotImplementedError
-
 def calculate_discrete_histogram(data):
-    raise NotImplementedError
+    """
+    Calculates a histogram for discrete `data`.
+
+    Parameters
+    ----------
+    data : pandas.Series of int
+        Discrete data to be binned.
+
+    Returns
+    -------
+    histogram : dict
+
+    Raises
+    ------
+    HistogramError
+
+    """
+    value_counts = data.value_counts().sort_index()
+    values = value_counts.index.tolist()
+    counts = value_counts.values.tolist()
+
+    # reject non-numbers
+    try:
+        values = list(map(float, values))
+    except ValueError:
+        raise HistogramError(
+            "values must be castable to numbers"
+        )
+
+    # reject non-integral floats
+    if not all(value.is_integer() for value in values):
+        raise HistogramError(
+            "values must be integers"
+        )
+    values = list(map(int, values))
+
+    # heuristic: reject if too many values
+    if len(values) > 10:
+        raise HistogramError(
+            "got {} possible discrete values but heuristic says the maximum is 10".format(len(values))
+        )
+
+    # heuristic: reject if counts don't seem high enough
+    if value_counts.mean() < 10:
+        raise HistogramError(
+            "heuristic says that each discrete value should average at least 10 appearances"
+        )
+
+    return {
+        'histogram': {
+            'discrete': {
+                'bucket_values': values,
+                'count': counts,
+            },
+        },
+        'type': "discrete",
+    }
 
 def calculate_float_histogram(data, num_bins=10):
     """
@@ -26,7 +79,7 @@ def calculate_float_histogram(data, num_bins=10):
 
     Parameters
     ----------
-    data : list-like of float
+    data : pandas.Series of float
         Continuous data to be binned.
     num_bins : int, default 10
         Number of bins to use.
