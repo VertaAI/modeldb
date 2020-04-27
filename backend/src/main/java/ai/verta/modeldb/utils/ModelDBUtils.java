005a7435d28e0bf5e656c80bc1b378ca8440c00c
evgeniilezhnin <eugene.lezhnin@quantumsoft.ru>
2020-04-27 12:31:40 +0700

    stacktrace changes (#570)

--- backend/src/main/java/ai/verta/modeldb/utils/ModelDBUtils.java
+++ backend/src/main/java/ai/verta/modeldb/utils/ModelDBUtils.java
@@ -63,6 +63,7 @@ import org.yaml.snakeyaml.Yaml;
 public class ModelDBUtils {
 
   private static final Logger LOGGER = LogManager.getLogger(ModelDBUtils.class);
+  private static final int STACKTRACE_LENGTH = 4;
 
   private ModelDBUtils() {}
 
@@ -448,18 +449,26 @@ public class ModelDBUtils {
                 .addDetails(Any.pack(defaultInstance))
                 .build();
       } else {
-        LOGGER.error("Exception occurred, message: {} class: {}", e.getMessage(), e.getClass());
-        StackTraceElement[] stack = e.getStackTrace();
-        int maxLines = (stack.length > 4) ? 5 : stack.length;
-        for (int n = 0; n < maxLines; n++) {
-          LOGGER.error(stack[n].toString());
-        }
         status =
             Status.newBuilder()
                 .setCode(Code.INTERNAL_VALUE)
                 .setMessage(ModelDBConstants.INTERNAL_ERROR)
                 .addDetails(Any.pack(defaultInstance))
                 .build();
+      }
+      StackTraceElement[] stack = e.getStackTrace();
+      LOGGER.error("Stacktrace with {} elements for {}", stack.length, e.getMessage());
+      int n = 0;
+      boolean isLongStack = stack.length > STACKTRACE_LENGTH;
+      if (isLongStack) {
+        for (; n < STACKTRACE_LENGTH + 1; ++n) {
+          LOGGER.warn("{}: {}", n, stack[n].toString());
+        }
+      }
+      for (; n < stack.length; ++n) {
+        if (stack[n].getClassName().startsWith("ai.verta") || !isLongStack) {
+          LOGGER.warn("{}: {}", n, stack[n].toString());
+        }
       }
       statusRuntimeException = StatusProto.toStatusRuntimeException(status);
     }
