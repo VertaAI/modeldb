21e1924799d3107fbdf9cff46c0a5c9cc2cba516
Ravi Shetye <ravi@verta.ai>
2020-04-27 21:50:18 -0700

    delte attribute (#581)

--- protos/gen/go/protos/public/modeldb/DatasetVersionService.pb.gw.go
+++ protos/gen/go/protos/public/modeldb/DatasetVersionService.pb.gw.go
@@ -502,18 +502,15 @@ func local_request_DatasetVersionService_GetDatasetVersionAttributes_0(ctx conte
 
 }
 
-var (
-	filter_DatasetVersionService_DeleteDatasetVersionAttributes_0 = &utilities.DoubleArray{Encoding: map[string]int{}, Base: []int(nil), Check: []int(nil)}
-)
-
 func request_DatasetVersionService_DeleteDatasetVersionAttributes_0(ctx context.Context, marshaler runtime.Marshaler, client DatasetVersionServiceClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
 	var protoReq DeleteDatasetVersionAttributes
 	var metadata runtime.ServerMetadata
 
-	if err := req.ParseForm(); err != nil {
-		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	newReader, berr := utilities.IOReaderFactory(req.Body)
+	if berr != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", berr)
 	}
-	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_DatasetVersionService_DeleteDatasetVersionAttributes_0); err != nil {
+	if err := marshaler.NewDecoder(newReader()).Decode(&protoReq); err != nil && err != io.EOF {
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
 	}
 
@@ -526,7 +523,11 @@ func local_request_DatasetVersionService_DeleteDatasetVersionAttributes_0(ctx co
 	var protoReq DeleteDatasetVersionAttributes
 	var metadata runtime.ServerMetadata
 
-	if err := runtime.PopulateQueryParameters(&protoReq, req.URL.Query(), filter_DatasetVersionService_DeleteDatasetVersionAttributes_0); err != nil {
+	newReader, berr := utilities.IOReaderFactory(req.Body)
+	if berr != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", berr)
+	}
+	if err := marshaler.NewDecoder(newReader()).Decode(&protoReq); err != nil && err != io.EOF {
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
 	}
 
