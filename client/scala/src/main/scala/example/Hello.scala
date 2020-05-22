048ae2df520c976d5d1a302ce8c6293c1c8d1d6a
nhatsmrt <nhatsmrt@uw.edu>
2020-05-21 21:59:15 -0400

    add a call to get repository in hello.scala example.

--- client/scala/src/main/scala/example/Hello.scala
+++ client/scala/src/main/scala/example/Hello.scala
@@ -10,6 +10,7 @@ object Hello extends App {
 
   val client = new Client(ClientConnection.fromEnvironment())
   try {
+    println(client.getOrCreateRepository("New Repo"))
     println(client.getOrCreateProject("scala test")
       .flatMap(_.getOrCreateExperiment("experiment"))
       .flatMap(_.getOrCreateExperimentRun())
