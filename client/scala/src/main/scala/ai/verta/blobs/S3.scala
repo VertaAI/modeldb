6b1b2e8bcac2c3ec46577c64b6a1c99ad07dc3c8
nhatsmrt <nhatsmrt@uw.edu>
2020-05-26 18:42:28 -0400

    Implement a class to represent S3 location (and a small test)

--- client/scala/src/main/scala/ai/verta/blobs/S3.scala
+++ client/scala/src/main/scala/ai/verta/blobs/S3.scala
@@ -1,6 +1,7 @@
 package ai.verta.blobs
 
 import ai.verta.swagger._public.modeldb.versioning.model._
+import java.net.{URL, URI}
 
 /** Captures metadata about S3 objects
  *  TODO: implement S3.location()
@@ -8,3 +9,20 @@ import ai.verta.swagger._public.modeldb.versioning.model._
 // case class S3(val paths: List[String]) extends Dataset {
 //
 // }
+
+/** A location in S3
+ *  @param url S3 URL of the form "s3://<bucketName>" or "s3://<bucketName>/<key>"
+ */
+class S3Location(val path: String, val versionID: Option[String] = None) {
+  val uri = new URI(path)
+  if (uri.getScheme() != "s3") throw new IllegalArgumentException("Illegal path")
+
+  val bucketName = uri.getAuthority()
+  val key: Option[String] = obtainKey(uri.getPath())
+
+  private def obtainKey(rawPath: String): Option[String] = {
+    if (rawPath.length() == 0) None
+    else if (rawPath.charAt(0) == '/') obtainKey(rawPath.substring(1))
+    else Some(rawPath)
+  }
+}
---
+++ client/scala/src/test/scala/ai/verta/blobs/TestS3.scala
@@ -0,0 +1,30 @@
+package ai.verta.blobs
+
+
+import ai.verta.client._
+import ai.verta.blobs._
+import ai.verta.swagger.client.HttpException
+import ai.verta.swagger._public.modeldb.versioning.model.VersioningSetTagRequestResponse
+
+import scala.concurrent.ExecutionContext
+import scala.language.reflectiveCalls
+import scala.util.{Try, Success, Failure}
+
+import org.scalatest.FunSuite
+import org.scalatest.Assertions._
+
+import scala.collection.mutable.HashSet
+
+
+class TestS3 extends FunSuite {
+  test("S3Location should correctly determine bucket name and key") {
+    val s31 = new S3Location(path = "s3://verta-starter/census-train.csv")
+
+    assert(s31.bucketName.equals("verta-starter"))
+    assert(s31.key.get.equals("census-train.csv"))
+
+    val s32 = new S3Location(path = "s3://verta-starter")
+    assert(s32.bucketName.equals("verta-starter"))
+    assert(s32.key.isEmpty)
+  }
+}
