1db3c5ee6d9233922fe3668323a995eac83a9888
nhatsmrt <nhatsmrt@uw.edu>
2020-06-02 19:20:16 -0700

    Add implementation of tag and log.

--- client/scala/src/main/scala/ai/verta/repository/Commit.scala
+++ client/scala/src/main/scala/ai/verta/repository/Commit.scala
@@ -6,13 +6,17 @@ import ai.verta.swagger._public.modeldb.versioning.model._
 import scala.concurrent.ExecutionContext
 import scala.util.{Failure, Success, Try}
 
+import java.net.URLEncoder
+
 /** Commit within a ModelDB Repository
  *  There should not be a need to instantiate this class directly; please use Repository.getCommit methods
  */
 class Commit(
   private val clientSet: ClientSet, private val repo: VersioningRepository,
-  private val commit: VersioningCommit, private val commitBranch: Option[String] = None
+  private val commit: VersioningCommit, private var commitBranch: Option[String] = None
 ) {
+  private var saved = true // whether the commit instance is saved to database
+
 
   /** Return the id of the commit */
   def getId() = commit.commit_sha.get
@@ -21,4 +25,36 @@ class Commit(
     case other: Commit => commit.commit_sha.get == other.commit.commit_sha.get
     case _ => false
   }
+
+  /** Assigns a tag to this Commit
+   *  @param tag tag
+   */
+  def tag(tag: String)(implicit ec: ExecutionContext) = {
+    if (!saved)
+      Failure(new IllegalStateException("Commit must be saved before it can be tagged"))
+    else clientSet.versioningService.SetTag2(
+        body = "\"" + commit.commit_sha.get + "\"",
+        repository_id_repo_id = repo.id.get,
+        tag = urlEncode(tag)
+    ).map(_ => ())
+  }
+
+  /** Set the commit of named branch to current commit
+   *  @param branch branch
+   */
+  private def setBranch(branch: String)(implicit ec: ExecutionContext) = {
+    clientSet.versioningService.SetBranch2(
+      body = "\"" + commit.commit_sha.get + "\"",
+      branch = branch,
+      repository_id_repo_id = repo.id.get
+    ) match {
+      case Success(_) => {
+        commitBranch = Some(branch)
+        Success(())
+      }
+      case Failure(e) => Failure(e)
+    }
+  }
+
+  private def urlEncode(input: String): String = URLEncoder.encode(input, "UTF-8").replaceAll("\\+", "%20")
 }
