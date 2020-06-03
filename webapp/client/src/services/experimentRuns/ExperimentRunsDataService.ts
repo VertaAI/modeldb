570bd5044949fcf0b49039d6617e26da730a00d3
kirillGulyaev <kirill.gulyaev@quantumsoft.ru>
2020-06-03 17:55:32 +0700

    Updated ExperimentRunsDataService

--- webapp/client/src/services/experimentRuns/ExperimentRunsDataService.ts
+++ webapp/client/src/services/experimentRuns/ExperimentRunsDataService.ts
@@ -1,6 +1,7 @@
 import { JsonConvert } from 'json2typescript';
 import * as R from 'ramda';
 
+import * as Comments from 'features/comments';
 import { BaseDataService } from 'core/services/BaseDataService';
 import { IArtifact } from 'core/shared/models/Artifact';
 import {
@@ -10,18 +11,27 @@ import {
 } from 'core/features/filter/Model';
 import { IPagination, DataWithPagination } from 'core/shared/models/Pagination';
 import { ISorting } from 'core/shared/models/Sorting';
-import * as Comments from 'features/comments';
 import { ShortExperiment } from 'models/Experiment';
-import ModelRecord, { LoadExperimentRunErrorType } from 'models/ModelRecord';
-import { convertServerCodeVersion } from 'services/serverModel/CodeVersion/converters';
+import ModelRecord, {
+  LoadExperimentRunErrorType,
+  IVersionedInputs,
+} from 'models/ModelRecord';
+import User from 'models/User';
+import {
+  convertServerCodeVersion,
+  convertServerCodeVersionsFromBlob,
+} from 'services/serverModel/CodeVersion/converters';
 import { convertServerEntityWithLoggedDates } from 'services/serverModel/Common/converters';
 
-import makeLoadExperimentRunsRequest from './responseRequest/makeLoadExperimentRunsRequest';
+import { convertServerUser } from '../../core/services/serverModel/User/converters';
+import makeLoadExperimentRunsRequest, {
+} from './responseRequest/makeLoadExperimentRunsRequest';
 import {
   ILoadExperimentRunsResult,
   ILoadModelRecordResult,
   ILazyLoadChartData,
 } from './types';
+import { IWorkspace } from 'models/Workspace';
 import { RepositoriesDataService } from 'core/services/versioning/repositories';
 
 export const chartsPageSettings = {
@@ -50,8 +60,11 @@ export default class ExperimentRunsDataService extends BaseDataService {
       url: '/v1/modeldb/hydratedData/findHydratedExperimentRuns',
       data: request,
     });
+
+    const data = await this.convertExperimentRuns(response.data);
+
     const res: ILoadExperimentRunsResult = {
-      data: this.convertExperimentRuns(response.data),
+      data,
       totalCount: Number(response.data.total_records || 0),
     };
     return res;
@@ -71,9 +84,10 @@ export default class ExperimentRunsDataService extends BaseDataService {
         experiment_run_ids: experimentRunsIds,
       },
     });
-    return this.convertExperimentRuns(response.data).map(
-      ({ experimentRun }) => experimentRun
-    );
+
+    const data = await this.convertExperimentRuns(response.data);
+
+    return data.map(({ experimentRun }) => experimentRun);
   }
 
   public async lazyLoadChartData(
@@ -124,58 +138,9 @@ export default class ExperimentRunsDataService extends BaseDataService {
       },
     });
 
-    const hydrated_experiment_run = response.data.hydrated_experiment_run;
-    const jsonConvert = new JsonConvert();
-    const modelRecord = jsonConvert.deserializeObject(
-      hydrated_experiment_run.experiment_run,
-      ModelRecord
+    return await convertServerExperimentRun(
+      response.data.hydrated_experiment_run
     );
-    modelRecord.shortExperiment = (() => {
-      const shortExperiment = jsonConvert.deserializeObject(
-        hydrated_experiment_run.experiment,
-        ShortExperiment
-      ) as ShortExperiment;
-      shortExperiment.id = modelRecord.experimentId;
-      return shortExperiment;
-    })();
-    modelRecord.codeVersion = convertServerCodeVersion(
-      hydrated_experiment_run.experiment_run.code_version_snapshot
-    );
-
-    const dates = convertServerEntityWithLoggedDates(
-      hydrated_experiment_run.experiment_run
-    );
-    modelRecord.dateCreated = dates.dateCreated;
-    modelRecord.dateUpdated = dates.dateUpdated;
-
-    if (hydrated_experiment_run.experiment_run.versioned_inputs) {
-      const versionedInputs = await (async () => {
-        try {
-          const serverVersionedInputs =
-            hydrated_experiment_run.experiment_run.versioned_inputs;
-          const repositoryName = await new RepositoriesDataService().loadRepositoryName(
-            serverVersionedInputs.repository_id
-          );
-          return {
-            commitSha: serverVersionedInputs.commit,
-            repositoryId: serverVersionedInputs.repository_id,
-            repositoryName: repositoryName,
-            keyLocationMap: serverVersionedInputs.key_location_map,
-          };
-        } catch (e) {
-          return undefined;
-        }
-      })();
-      modelRecord.versionedInputs = versionedInputs;
-    }
-
-    const result: ILoadModelRecordResult = {
-      experimentRun: modelRecord,
-      comments: (hydrated_experiment_run.comments || []).map((comment: any) =>
-        Comments.convertServerComment(comment)
-      ),
-    };
-    return result;
   }
 
   public async deleteExperimentRun(id: string): Promise<void> {
@@ -224,12 +189,13 @@ export default class ExperimentRunsDataService extends BaseDataService {
   }
 
   public async loadExperimentRunsByDatasetVersionId(
+    workspaceName: IWorkspace['name'],
     datasetVersionId: string
   ): Promise<DataWithPagination<ModelRecord>> {
     const {
       data: { projects: serverProjects },
     } = await this.get<{ projects: Array<{ id: string }> }>({
-      url: `/v1/modeldb/project/getProjects`,
+      url: `/v1/modeldb/project/getProjects?workspace_name=${workspaceName}`,
     });
     const experimentRunsByProjects = await Promise.all(
       (serverProjects || []).map(({ id }) =>
@@ -261,51 +227,91 @@ export default class ExperimentRunsDataService extends BaseDataService {
     return res;
   }
 
-  private convertExperimentRuns(
-    data: any
-  ): Array<{
-    experimentRun: ModelRecord;
-    comments: Comments.Model.IComment[];
-  }> {
+  private async convertExperimentRuns(
+    data: any,
+    withLoadingVersionedInputs: boolean = true
+  ) {
     if (!data.hydrated_experiment_runs) {
       return [];
     }
-
-    const jsonConvert = new JsonConvert();
-    const experimentRunsWithComments: Array<{
-      experimentRun: ModelRecord;
-      comments: Comments.Model.IComment[];
-    }> = data.hydrated_experiment_runs.map((serverData: any) => {
-      const experimentRun = jsonConvert.deserializeObject(
-        serverData.experiment_run,
-        ModelRecord
-      ) as ModelRecord;
-      experimentRun.shortExperiment = (() => {
-        const shortExperiment = jsonConvert.deserializeObject(
-          serverData.experiment,
-          ShortExperiment
-        ) as ShortExperiment;
-        shortExperiment.id = experimentRun.experimentId;
-        return shortExperiment;
-      })();
-      experimentRun.codeVersion = convertServerCodeVersion(
-        serverData.experiment_run.code_version_snapshot
-      );
-
-      const dates = convertServerEntityWithLoggedDates(
-        serverData.experiment_run
-      );
-      experimentRun.dateCreated = dates.dateCreated;
-      experimentRun.dateUpdated = dates.dateUpdated;
-
-      return {
-        experimentRun,
-        comments: (serverData.comments || []).map((comment: any) =>
-          Comments.convertServerComment(comment)
-        ),
-      };
-    });
-
-    return experimentRunsWithComments;
+    return await Promise.all(
+      (data.hydrated_experiment_runs as any[]).map(x =>
+        convertServerExperimentRun(x, withLoadingVersionedInputs)
+      )
+    );
   }
 }
+
+const convertServerExperimentRun = async (
+  hydrated_experiment_run: any,
+  withLoadingVersionedInputs: boolean = true
+) => {
+  const jsonConvert = new JsonConvert();
+  const modelRecord = jsonConvert.deserializeObject(
+    hydrated_experiment_run.experiment_run,
+    ModelRecord
+  );
+  modelRecord.shortExperiment = (() => {
+    const shortExperiment = jsonConvert.deserializeObject(
+      hydrated_experiment_run.experiment,
+      ShortExperiment
+    ) as ShortExperiment;
+    shortExperiment.id = modelRecord.experimentId;
+    return shortExperiment;
+  })();
+  modelRecord.owner = convertServerUser(
+    hydrated_experiment_run.owner_user_info
+  );
+
+  modelRecord.codeVersion = convertServerCodeVersion(
+    hydrated_experiment_run.experiment_run.code_version_snapshot
+  );
+  modelRecord.codeVersionsFromBlob = hydrated_experiment_run.experiment_run
+    .code_version_from_blob
+    ? convertServerCodeVersionsFromBlob(
+        hydrated_experiment_run.experiment_run.code_version_from_blob
+      )
+    : undefined;
+
+  if (
+    withLoadingVersionedInputs &&
+    hydrated_experiment_run.experiment_run.versioned_inputs
+  ) {
+    const versionedInputs = await (async () => {
+      try {
+        const serverVersionedInputs =
+          hydrated_experiment_run.experiment_run.versioned_inputs;
+        const repositoryName = await new RepositoriesDataService().loadRepositoryName(
+          serverVersionedInputs.repository_id
+        );
+        return {
+          commitSha: serverVersionedInputs.commit,
+          repositoryId: serverVersionedInputs.repository_id,
+          repositoryName: repositoryName,
+          keyLocationMap: serverVersionedInputs.key_location_map,
+        };
+      } catch (e) {
+        return undefined;
+      }
+    })();
+    modelRecord.versionedInputs = versionedInputs;
+  }
+
+  modelRecord.allowedActions = [];
+
+  const dates = convertServerEntityWithLoggedDates(
+    hydrated_experiment_run.experiment_run
+  );
+  modelRecord.dateCreated = dates.dateCreated;
+  modelRecord.dateUpdated = dates.dateUpdated;
+
+  const result: ILoadModelRecordResult = {
+    experimentRun: modelRecord,
+    comments: (hydrated_experiment_run.comments || []).map((comment: any) =>
+      Comments.convertServerComment(
+        comment,
+      )
+    ),
+  };
+  return result;
+};
