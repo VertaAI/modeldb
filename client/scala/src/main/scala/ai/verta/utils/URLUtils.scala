f9272a6fc7eebbe1899801d7a59dd38714ad7ece
nhatsmrt <nhatsmrt@uw.edu>
2020-06-03 14:23:36 -0700

    Add a comment describing the method.

--- client/scala/src/main/scala/ai/verta/utils/URLUtils.scala
+++ client/scala/src/main/scala/ai/verta/utils/URLUtils.scala
@@ -3,5 +3,6 @@ package ai.verta.utils
 import java.net.URLEncoder
 
 object URLUtils {
+  /** Utility method for HTML form encoding  */
   def urlEncode(input: String): String = URLEncoder.encode(input, "UTF-8").replaceAll("\\+", "%20")
 }
