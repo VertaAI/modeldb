5a91238aa85af923f6e4f39331ac2fd6a7c27fc6
nhatsmrt <nhatsmrt@uw.edu>
2020-06-03 14:24:13 -0700

    Add param description.

--- client/scala/src/main/scala/ai/verta/utils/URLUtils.scala
+++ client/scala/src/main/scala/ai/verta/utils/URLUtils.scala
@@ -3,6 +3,8 @@ package ai.verta.utils
 import java.net.URLEncoder
 
 object URLUtils {
-  /** Utility method for HTML form encoding  */
+  /** Utility method for HTML form encoding
+   *  @param input input
+   */
   def urlEncode(input: String): String = URLEncoder.encode(input, "UTF-8").replaceAll("\\+", "%20")
 }
