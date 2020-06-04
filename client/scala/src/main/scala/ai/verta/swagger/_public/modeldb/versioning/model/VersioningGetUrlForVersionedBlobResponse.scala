64a78bce2da272b17f46f7354ba16055294727fd
Anand Jakhaniya <anand.jakhaniya@gmail.com>
2020-06-04 10:11:48 +0530

    protos for the get versioned blob presigned url (#702)
    * protos for the get versioned blob presigned url
    
    * resolved review comment
    
    * Added endpoints for VersionedBlobArtifact multipart upload
    
    * proto gen
    
    Co-authored-by: Ravi Shetye <ravi@verta.ai>

---
+++ client/scala/src/main/scala/ai/verta/swagger/_public/modeldb/versioning/model/VersioningGetUrlForVersionedBlobResponse.scala
@@ -0,0 +1,48 @@
+// THIS FILE IS AUTO-GENERATED. DO NOT EDIT
+package ai.verta.swagger._public.modeldb.versioning.model
+
+import scala.util.Try
+
+import net.liftweb.json._
+
+import ai.verta.swagger._public.modeldb.versioning.model.ArtifactTypeEnumArtifactType._
+import ai.verta.swagger._public.modeldb.versioning.model.DiffStatusEnumDiffStatus._
+import ai.verta.swagger._public.modeldb.versioning.model.OperatorEnumOperator._
+import ai.verta.swagger._public.modeldb.versioning.model.ProtobufNullValue._
+import ai.verta.swagger._public.modeldb.versioning.model.RepositoryVisibilityEnumRepositoryVisibility._
+import ai.verta.swagger._public.modeldb.versioning.model.TernaryEnumTernary._
+import ai.verta.swagger._public.modeldb.versioning.model.ValueTypeEnumValueType._
+import ai.verta.swagger._public.modeldb.versioning.model.VersioningBlobType._
+import ai.verta.swagger._public.modeldb.versioning.model.WorkspaceTypeEnumWorkspaceType._
+import ai.verta.swagger.client.objects._
+
+case class VersioningGetUrlForVersionedBlobResponse (
+  url: Option[String] = None
+) extends BaseSwagger {
+  def toJson(): JValue = VersioningGetUrlForVersionedBlobResponse.toJson(this)
+}
+
+object VersioningGetUrlForVersionedBlobResponse {
+  def toJson(obj: VersioningGetUrlForVersionedBlobResponse): JObject = {
+    new JObject(
+      List[Option[JField]](
+        obj.url.map(x => JField("url", JString(x)))
+      ).flatMap(x => x match {
+        case Some(y) => List(y)
+        case None => Nil
+      })
+    )
+  }
+
+  def fromJson(value: JValue): VersioningGetUrlForVersionedBlobResponse =
+    value match {
+      case JObject(fields) => {
+        val fieldsMap = fields.map(f => (f.name, f.value)).toMap
+        VersioningGetUrlForVersionedBlobResponse(
+          // TODO: handle required
+          url = fieldsMap.get("url").map(JsonConverter.fromJsonString)
+        )
+      }
+      case _ => throw new IllegalArgumentException(s"unknown type ${value.getClass.toString}")
+    }
+}
