89ca311a2e23c968bc6b5f7356234642a08ff247
Anand Jakhaniya <anand.jakhaniya@gmail.com>
2020-06-04 22:20:13 +0530

    Fix bug

--- backend/src/main/java/ai/verta/modeldb/utils/RdbmsUtils.java
+++ backend/src/main/java/ai/verta/modeldb/utils/RdbmsUtils.java
@@ -1715,29 +1715,39 @@ public class RdbmsUtils {
         builder.equal(elementMappingEntityRoot.get(ModelDBConstants.NAME), names[names.length - 1]);
     configBlobEntityRootPredicates.add(keyPredicate);
 
-    Predicate intValuePredicate =
-        getValuePredicate(
-            builder,
-            ModelDBConstants.HYPERPARAMETERS,
-            elementMappingEntityRoot.get("int_value"),
-            predicate);
-
-    Predicate floatValuePredicate =
-        getValuePredicate(
-            builder,
-            ModelDBConstants.HYPERPARAMETERS,
-            elementMappingEntityRoot.get("float_value"),
-            predicate);
-
+    List<Predicate> orPredicates = new ArrayList<>();
+    try{
+      Predicate intValuePredicate = getValuePredicate(
+              builder,
+              ModelDBConstants.HYPERPARAMETERS,
+              elementMappingEntityRoot.get("int_value"),
+              predicate);
+      orPredicates.add(intValuePredicate);
+    }
+    catch(Exception e) {
+      LOGGER.debug("Value could not be cast to int");
+    }
+    try{
+      Predicate floatValuePredicate =
+              getValuePredicate(
+                      builder,
+                      ModelDBConstants.HYPERPARAMETERS,
+                      elementMappingEntityRoot.get("float_value"),
+                      predicate);
+      orPredicates.add(floatValuePredicate);
+    }
+    catch(Exception e) {
+      LOGGER.debug("Value could not be cast to float");
+    }
     Predicate stringValuePredicate =
-        getValuePredicate(
-            builder,
-            ModelDBConstants.HYPERPARAMETERS,
-            elementMappingEntityRoot.get("string_value"),
-            predicate);
-
+            getValuePredicate(
+                    builder,
+                    ModelDBConstants.HYPERPARAMETERS,
+                    elementMappingEntityRoot.get("string_value"),
+                    predicate);
+    orPredicates.add(stringValuePredicate);
     configBlobEntityRootPredicates.add(
-        builder.or(intValuePredicate, floatValuePredicate, stringValuePredicate));
+            builder.or(orPredicates.toArray(new Predicate[0])));
 
     Predicate[] hyprElemmappingRootPredicatesOne =
         new Predicate[configBlobEntityRootPredicates.size()];
