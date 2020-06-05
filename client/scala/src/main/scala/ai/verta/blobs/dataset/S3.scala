abf101b92a6e76cb6b8a756ac80f3a85e3c4b098
nhatsmrt <nhatsmrt@uw.edu>
2020-06-05 14:32:31 -0700

    Port S3 implementation.

---
+++ client/scala/src/main/scala/ai/verta/blobs/dataset/S3.scala
@@ -0,0 +1,172 @@
+package ai.verta.blobs.dataset
+
+import ai.verta.swagger._public.modeldb.versioning.model._
+import java.net.{URL, URI}
+
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3._
+import com.amazonaws.services.s3.model._
+
+import collection.JavaConverters._
+
+import scala.collection.mutable.HashMap
+import scala.util.{Failure, Success, Try}
+
+/** Captures metadata about S3 objects
+ */
+case class S3(private val paths: List[S3Location]) extends Dataset {
+  private val s3: AmazonS3 = AmazonS3ClientBuilder.standard().withRegion(Regions.DEFAULT_REGION).build()
+  private var versionMap = new HashMap[String, String]() // keep track of the version of each object
+  paths.map(getS3LocMetadata _)
+
+  /** Get the version id of a file
+   *  @param path: S3 URL of a file in the form "s3://<bucketName>/<key>"
+   *  @return the version id of the file
+   */
+  def getVersionId(path: String) = versionMap.get(path)
+
+  override def equals(other: Any) = other match {
+    case other: S3 => contents.equals(other.contents) && versionMap.equals(other.versionMap)
+    case _ => false
+  }
+}
+
+/** Companion object to handle interaction with versioning blob */
+object S3 {
+  /** Constructor that user should use:
+   */
+  // def apply(paths: List[S3Location]): Try[S3] = {
+  //   paths.map(getS3LocMetadata) match {
+  //     case Failure(e) => Failure(e)
+  //     case Success(list) =>
+  //   }
+  // }
+  
+  /** Factory method to convert a versioning blob instance
+   *  @param s3VersioningBlob the versioning blob to convert
+   */
+  def apply(s3VersioningBlob: VersioningS3DatasetBlob) {
+    var s3 = new S3(List())
+
+    s3VersioningBlob.components.get.map(
+      comp => {
+        val s3Path = comp.path.get.path.get
+
+        s3.contents.put(s3Path, s3.toMetadata(comp.path.get))
+        if (comp.s3_version_id.isDefined)
+          s3.versionMap.put(s3Path, comp.s3_version_id.get)
+      }
+    )
+    s3
+  }
+
+  /** Convert a S3 instance to a VersioningBlob
+   *  @param blob S3 instance
+   *  @return equivalent VersioningBlob instance
+   */
+  def toVersioningBlob(blob: S3) = {
+    val s3Components = blob.components.map(comp => VersioningS3DatasetComponentBlob(
+      Some(comp),
+      blob.versionMap.get(comp.path.get)
+    ))
+
+    VersioningBlob(
+      dataset = Some(VersioningDatasetBlob(
+        s3 = Some(VersioningS3DatasetBlob(Some(s3Components)))
+      ))
+    )
+  }
+
+  /** Helper function to query metadata of S3 Location
+   */
+  private def getS3LocMetadata(loc: S3Location) = {
+    if (loc.key.isEmpty || loc.key.get.charAt(loc.key.get.length - 1) == '/') {
+      // no key, or is a folder
+      // null or empty string?
+      val versionListing =
+        if (loc.key.isEmpty) s3.listVersions(loc.bucketName, null)
+        else s3.listVersions(loc.bucketName, loc.key.get)
+
+      handleVersionListing(versionListing, List())
+    }
+    else {
+      val request =
+        if (loc.versionID.isDefined) new GetObjectMetadataRequest(loc.bucketName, loc.key.get, loc.versionID.get)
+        else new GetObjectMetadataRequest(loc.bucketName, loc.key.get)
+
+      getObjectMetadata(s3.getObjectMetadata(request), loc.bucketName, loc.key.get).map(List(_))
+    }
+  }
+
+  /** Helper function to deal with VersionListing when key is not provided
+   */
+  private def handleVersionListing(versionListing: VersionListing, acc: List[Tuple2[FileMetadata, String]]) = {
+    val batchAttempt = Try(versionListing.getVersionSummaries().asScala.toList
+    .filter((version: S3VersionSummary) => version.getKey().charAt(version.getKey().length() - 1) != '/') // not a folder
+    .filter(_.isLatest())
+    .map(getVersionMetadata _) // List[Try]
+    .map(_.get))
+
+    batchAttempt match {
+      case Failure(e) => Failure(e)
+      case Success(batch) =>
+        if (versionListing.isTruncated()) handleVersionListing(s3.listNextBatchOfVersions(versionListing), batch ::: acc)
+        else Success(acc)
+    }
+  }
+
+  /** Helper function to extract metadata from the return object
+   */
+  private def getObjectMetadata(obj: ObjectMetadata, bucketName: String, key: String) = Try {
+    val objPath = getPath(bucketName, key)
+    val metadata = new FileMetadata(
+      BigInt(obj.getLastModified().getTime()), // convert time to UNIX timestamp (ms)
+      obj.getETag(),
+      objPath,
+      BigInt(obj.getContentLength())
+    )
+
+    if (obj.getVersionId() != null)
+      Tuple2(metadata, Some(obj.getVersionId()))
+    else
+      Tuple2(metadata, None)
+  }
+
+  /** Helper function to extract metadata from a version summary
+   */
+  private def getVersionMetadata(version: S3VersionSummary): Try {
+    val versionPath = getPath(version.getBucketName(), version.getKey())
+    val metadata = new FileMetadata(
+      BigInt(version.getLastModified().getTime()),
+      version.getETag(),
+      versionPath,
+      BigInt(version.getSize())
+    )
+
+    if (version.getVersionId() != null)
+      Tuple2(metadata, Some(version.getVersionId()))
+    else
+      Tuple2(metadata, None)
+  }
+
+  /** Helper function to construct path from bucket name and key */
+  private def getPath(bucketName: String, key: String) = f"s3://${bucketName}/${key}"
+}
+
+/** A location in S3
+ *  @param path S3 URL of the form "s3://<bucketName>" or "s3://<bucketName>/<key>"
+ *  @param versionID: Version of the S3 file. Only relevant if path is to a file. If not set, latest version will be retrieved.
+ */
+class S3Location(val path: String, val versionID: Option[String] = None) {
+  private val uri = new URI(path)
+  if (uri.getScheme() != "s3") throw new IllegalArgumentException("Illegal path")
+
+  val bucketName = uri.getAuthority()
+  val key = obtainKey(uri.getPath())
+
+  private def obtainKey(rawPath: String): Option[String] = {
+    if (rawPath.length() == 0 || (rawPath.charAt(0) == '/' && rawPath.length() == 1)) None
+    else if (rawPath.charAt(0) == '/') Some(rawPath.substring(1))
+    else Some(rawPath)
+  }
+}
---
+++ client/scala/src/test/scala/ai/verta/blobs/dataset/TestS3.scala
@@ -0,0 +1,136 @@
+package ai.verta.blobs.dataset
+
+import ai.verta.client._
+import ai.verta.blobs._
+import ai.verta.swagger.client.HttpException
+import ai.verta.swagger._public.modeldb.versioning.model.VersioningSetTagRequestResponse
+
+import scala.concurrent.ExecutionContext
+import scala.language.reflectiveCalls
+import scala.util.{Try, Success, Failure}
+
+import org.scalatest.FunSuite
+import org.scalatest.Assertions._
+
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3._
+import com.amazonaws.services.s3.model._
+import collection.JavaConverters._
+
+import scala.collection.mutable.HashSet
+
+class TestS3 extends FunSuite {
+  /** Verify that a FileMetadata has correct path and does not have weird parameter
+   *  @param metadata file's metadata
+   *  @param path file's path (to be checked)
+   */
+  def assertMetadata(metadata: FileMetadata, path: String) = {
+    assert(metadata.path.equals(path))
+    assert(metadata.size > 0)
+    assert(metadata.lastModified > 0)
+    assert(metadata.md5.length > 0)
+  }
+
+  def fixture =
+    new {
+      val testfilePath = "s3://verta-scala-test/testdir/testfile"
+      val testfileLoc = new S3Location(testfilePath)
+
+      val testfilePath2 = "s3://verta-scala-test/testdir/testsubdir/testfile2"
+      val testfileLoc2 = new S3Location(testfilePath2)
+
+      val testdirPath = "s3://verta-scala-test/testdir/"
+      val testdirLoc = new S3Location(testdirPath)
+
+      val testsubdirPath = "s3://verta-scala-test/testdir/testsubdir"
+      val testsubdirLoc = new S3Location(testsubdirPath)
+
+      val bucketLoc = new S3Location("s3://verta-scala-test")
+    }
+
+  test("S3Location should correctly determine bucket name and key") {
+    val f = fixture
+    assert(f.testfileLoc.bucketName.equals("verta-scala-test"))
+    assert(f.testfileLoc.key.get.equals("testdir/testfile"))
+
+    assert(f.bucketLoc.bucketName.equals("verta-scala-test"))
+    assert(f.bucketLoc.key.isEmpty)
+  }
+
+  test("S3Location should throw an exception when encountered non-S3 path") {
+    assertThrows[IllegalArgumentException] {
+      val s3 = new S3Location("http://verta-starter/census-train.csv")
+    }
+  }
+
+  test("S3 blob should retrieve the file (i.e with key) correctly") {
+    val f = fixture
+    val s3Blob = S3(List(f.testfileLoc))
+
+    val s3File = s3Blob.getMetadata(f.testfilePath).get
+    assertMetadata(s3Blob.getMetadata(f.testfilePath).get, f.testfilePath)
+  }
+
+  test("S3 blob should retrieve the entire bucket correctly") {
+    val f = fixture
+    val s3Blob = S3(List(f.bucketLoc))
+
+    assertMetadata(s3Blob.getMetadata(f.testfilePath).get, f.testfilePath)
+    assertMetadata(s3Blob.getMetadata(f.testfilePath2).get, f.testfilePath2)
+  }
+
+  // testdir/testfile has two versions
+  test("S3 should retrieve the correct version") {
+    val f = fixture
+    val s3: AmazonS3 = AmazonS3ClientBuilder.standard().withRegion(Regions.DEFAULT_REGION).build()
+    var versionListing = s3.listVersions("verta-scala-test", "testdir/testfile")
+
+    // get latest version
+    val newVersionId = versionListing.getVersionSummaries().asScala.toList
+    .filter((version: S3VersionSummary) => version.getKey().charAt(version.getKey().length() - 1) != '/') // not a folder
+    .filter(_.isLatest()).head.getVersionId()
+
+    // default should be the latest version
+    val s3BlobDefault = S3(List(f.testfileLoc))
+    var s3BlobLatest = S3(List(new S3Location(f.testfilePath, Some(newVersionId))))
+
+    assert(s3BlobDefault equals s3BlobLatest)
+    assert(s3BlobLatest.getVersionId(f.testfilePath).get equals newVersionId)
+
+    // get older version
+    val oldVersionId = versionListing.getVersionSummaries().asScala.toList
+    .filter((version: S3VersionSummary) => version.getKey().charAt(version.getKey().length() - 1) != '/') // not a folder
+    .filter(!_.isLatest()).head.getVersionId()
+    val s3BlobOld = S3(List(new S3Location(f.testfilePath, Some(oldVersionId))))
+
+    assert(!s3BlobDefault.equals(s3BlobOld))
+    assert(s3BlobOld.getVersionId(f.testfilePath).get equals oldVersionId)
+  }
+
+  test("S3 should retrieve multiple keys correctly") {
+    val f = fixture
+    val s3Blob = S3(List(f.testfileLoc, f.testfileLoc2))
+
+    assertMetadata(s3Blob.getMetadata(f.testfilePath).get, f.testfilePath)
+    assertMetadata(s3Blob.getMetadata(f.testfilePath2).get, f.testfilePath2)
+  }
+
+  test("S3 should not have duplicate paths") {
+    val f = fixture
+    val s3Blob1 = S3(List(f.testfileLoc, f.testfileLoc, f.testdirLoc, f.testsubdirLoc, f.testfileLoc2))
+
+    val s3Blob2 = S3(List(f.testfileLoc, f.testfileLoc2))
+    assert(s3Blob1 equals s3Blob2)
+  }
+
+  test("S3 blob should discard invalid paths, but still process valid paths") {
+    val f = fixture
+    val invalidPath = "s3://verta-scala-test/testdir/no-such-file"
+    val s3LocInvalid = new S3Location(invalidPath)
+
+    val s3Blob = S3(List(s3LocInvalid, f.testfileLoc))
+
+    assert(s3Blob.getMetadata(f.testfilePath).isDefined)
+    assert(s3Blob.getMetadata(invalidPath).isEmpty)
+  }
+}
