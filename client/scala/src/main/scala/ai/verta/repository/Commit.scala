c7f7d280be0ad5a4eccad1d5745cddbeda4fb4ba
Nhat Pham <nhatsmrt@uw.edu>
2020-06-08 09:23:11 -0700

    Implement Commit's tag and branch operations for Scala client (#712)
    * Update the API so that the query map accept list of string instead.
    
    * Implement Repository functionality
    
    * getPersonalWorkspace just return personal for now
    
    * Remove all nulls
    
    * Remove a null from getCommitByTag
    
    * Make the query string builder a bit clearer.
    
    * Execute test sequentially
    
    * Fix some formatting issues
    
    * Delete an extraneous line.
    
    * Remove empty package file.
    
    * Fix variable naming. Edit some tests.
    
    * URL encode the branch name as well.
    
    * Make some tests more specific.
    
    * Delete some extra lines.
    
    * Change return type of some methods
    
    * Replace two pattern matchings with simpler maps.
    
    * Make the test for getting commit by branch with no argument passed a bit stronger.
    
    * Change the name of the test so that the intention is clearer and behavior driven.
    
    * Make commit field a val
    
    * Making the classes more encapsulated by providing getters and making the fields immutable + private.
    
    * Add/remove spaces.
    
    * Fix a typo
    
    * Add implementation of tag and log.
    
    * Remove an extra space.
    
    * Remove more extra lines.
    
    * Finish implementation of tag and setBranch. Add two more tests.
    
    * Remove an extra line.
    
    * Make test name clearer.
    
    * Fix format.
    
    * Edit a wrong scaladoc.
    
    * Add 2 tests of failure case.
    
    * Add curly braces for consistency
    
    * Modify description of saved field.
    
    * Refactor code.
    
    * Add a comment describing the method.
    
    * Add param description.
    
    * Fix some formatting issue.
    
    * Remove an extra import.
    
    * Move URLUtils to Client package.
    
    * newBranch now return a commit instead of doing in-place mutating. Fix some formatting and tests.
    
    * Use JSON formatting to code.
    
    * Move the formatting inside HttpClient instead.
    
    * Move the jsonFormat inside HttpClient.
    
    * Move url encoding inside request.
    
    * Fix a style issue.

--- client/scala/src/main/scala/ai/verta/repository/Commit.scala
+++ client/scala/src/main/scala/ai/verta/repository/Commit.scala
@@ -10,9 +10,10 @@ import scala.util.{Failure, Success, Try}
  *  There should not be a need to instantiate this class directly; please use Repository.getCommit methods
  */
 class Commit(
-  private val clientSet: ClientSet, private val repo: VersioningRepository,
+  private val clientSet: ClientSet, private val repo: Repository,
   private val commit: VersioningCommit, private val commitBranch: Option[String] = None
 ) {
+  private var saved = true // whether the commit instance is saved to database, or is currently being modified.
 
   /** Return the id of the commit */
   def id = commit.commit_sha.get
@@ -21,4 +22,40 @@ class Commit(
     case other: Commit => commit.commit_sha.get == other.commit.commit_sha.get
     case _ => false
   }
+
+  /** Creates a branch at this Commit and returns the checked-out branch
+   *  If the branch already exists, it will be moved to this commit.
+   *  @param branch branch name
+   *  @return if not saved, a failure; otherwise, this commit as the head of `branch`
+   */
+  def newBranch(branch: String)(implicit ec: ExecutionContext) = {
+    if (!saved)
+      Failure(new IllegalStateException("Commit must be saved before it can be attached to a branch"))
+    else setBranch(branch).flatMap(_ => repo.getCommitByBranch(branch))
+  }
+
+  /** Assigns a tag to this Commit
+   *  @param tag tag
+   */
+  def tag(tag: String)(implicit ec: ExecutionContext) = {
+    if (!saved)
+      Failure(new IllegalStateException("Commit must be saved before it can be tagged"))
+    else clientSet.versioningService.SetTag2(
+        body = commit.commit_sha.get,
+        repository_id_repo_id = repo.id,
+        tag = tag
+    ).map(_ => ())
+  }
+
+  /** Set the commit of named branch to current commit
+   *  @param branch branch
+   */
+  private def setBranch(branch: String)(implicit ec: ExecutionContext) = {
+    clientSet.versioningService.SetBranch2(
+      body = commit.commit_sha.get,
+      branch = branch,
+      repository_id_repo_id = repo.id
+    ).map(_ => ())
+  }
+
 }
