be696f9c9a17cdf87441e41c3089a5cf498fb66e
nhatsmrt <nhatsmrt@uw.edu>
2020-06-08 12:54:56 -0700

    Add tests for commit

--- client/scala/src/test/scala/ai/verta/repository/TestCommit.scala
+++ client/scala/src/test/scala/ai/verta/repository/TestCommit.scala
@@ -19,9 +19,14 @@ class TestCommit extends FunSuite {
         val client = new Client(ClientConnection.fromEnvironment())
         val repo = client.getOrCreateRepository("My Repo").get
         val commit = repo.getCommitByBranch().get
+        val pathBlob = PathBlob(List(
+          f"${System.getProperty("user.dir")}/src/test/scala/ai/verta/blobs/testdir"
+        )).get
     }
 
-  def cleanup(f: AnyRef{val client: Client; val repo: Repository; val commit: Commit}) = {
+  def cleanup(
+    f: AnyRef{val client: Client; val repo: Repository; val commit: Commit; val pathBlob: PathBlob}
+  ) = {
     f.client.deleteRepository(f.repo.id)
     f.client.close()
   }
@@ -30,10 +35,7 @@ class TestCommit extends FunSuite {
     val f = fixture
 
     try {
-      val workingDir = System.getProperty("user.dir")
-      val testDir = workingDir + "/src/test/scala/ai/verta/blobs/testdir"
-      val pathBlob = PathBlob(List(testDir)).get
-      f.commit.update("abc/def", pathBlob)
+      f.commit.update("abc/def", f.pathBlob)
 
       val getAttempt = f.commit.get("abc/def").get
 
@@ -41,11 +43,41 @@ class TestCommit extends FunSuite {
       val pathBlob2 = getAttempt match {
         case blob: PathBlob => blob
       }
-      assert(pathBlob2 equals pathBlob)
+      assert(pathBlob2 equals f.pathBlob)
 
       val getAttempt2 = f.commit.get("xyz/tuv")
       assert(getAttempt2.isFailure)
       assert(getAttempt2 match {case Failure(e) => e.getMessage contains "No blob was stored at this path."})
+    } finally {
+      cleanup(f)
+    }
+  }
+
+  test("Tagging unsaved commit should fail") {
+    val f = fixture
+
+    try {
+      f.commit.update("abc/def", f.pathBlob)
+      val tagAttempt = f.commit.tag("Some tag")
+      assert(tagAttempt.isFailure)
+      assert(tagAttempt match {
+        case Failure(e) => e.getMessage contains "Commit must be saved before it can be tagged"
+      })
+    } finally {
+      cleanup(f)
+    }
+  }
+
+  test("newBranch unsaved commit should fail") {
+    val f = fixture
+    
+    try {
+      f.commit.update("abc/def", f.pathBlob)
+      val newBranchAttempt = f.commit.newBranch("some-branch")
+      assert(newBranchAttempt.isFailure)
+      assert(newBranchAttempt match {
+        case Failure(e) => e.getMessage contains "Commit must be saved before it can be attached to a branch"
+      })
     } finally {
       cleanup(f)
     }
