7c7bedb8a049eb7aac75cad5ce1a63dbe5a33f54
Michael Liu <miliu@protonmail.com>
2020-06-09 12:49:02 -0700

    VR-4525: Retry part uploads on ConnectionErrors (#729)
    * Try again on ConnectionError
    
    * Add a sleep
    
    * Add comment
    
    * Decrease backoff for connection errors

--- client/verta/verta/client.py
+++ client/verta/verta/client.py
@@ -2116,7 +2116,15 @@ class ExperimentRun(_ModelDBEntity):
                 part_stream = six.BytesIO(file_part)
 
                 # upload part
-                response = _utils.make_request("PUT", url, self._conn, data=part_stream)
+                #     Retry connection errors, to make large multipart uploads more robust.
+                for _ in range(3):
+                    try:
+                        response = _utils.make_request("PUT", url, self._conn, data=part_stream)
+                    except requests.ConnectionError:  # e.g. broken pipe
+                        time.sleep(1)
+                        continue  # try again
+                    else:
+                        break
                 response.raise_for_status()
 
                 # commit part
