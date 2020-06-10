f51cd1dd7527c28e179775a4192ba29a439b70ba
Evgenii Lezhnin <eugene.lezhnin@quantumsoft.ru>
2020-06-10 17:40:24 +0700

    VR-4592 new dao methods

--- backend/src/main/java/ai/verta/modeldb/entities/metadata/LabelsMappingEntityBase.java
+++
@@ -1,5 +0,0 @@
-package ai.verta.modeldb.entities.metadata;
-
-public interface LabelsMappingEntityBase {
-  String getLabel();
-}
--- backend/src/main/java/ai/verta/modeldb/metadata/MetadataDAO.java
+++ backend/src/main/java/ai/verta/modeldb/metadata/MetadataDAO.java
@@ -1,11 +1,22 @@
 package ai.verta.modeldb.metadata;
 
 import java.util.List;
+import org.hibernate.Session;
 
 public interface MetadataDAO {
   boolean addLabels(IdentificationType id, List<String> labels);
 
+  void addParameter(Session session, IdentificationType id, String key, String value);
+
+  void addLabels(Session session, IdentificationType id, List<String> labels);
+
+  boolean addParameter(IdentificationType id, String key, String value);
+
   List<String> getLabels(IdentificationType id);
 
+  String getParameter(IdentificationType id, String key);
+
   boolean deleteLabels(IdentificationType id, List<String> labels);
+
+  boolean deleteParameter(IdentificationType id, String key);
 }
