26b92d4231bbe03ba2e6c3c21c68d6709c35922f
nhatsmrt <nhatsmrt@uw.edu>
2020-06-12 11:33:34 -0700

    Remove some bug fixing code.

--- client/scala/src/main/scala/ai/verta/repository/Diff.scala
+++ client/scala/src/main/scala/ai/verta/repository/Diff.scala
@@ -5,6 +5,4 @@ import ai.verta.swagger._public.modeldb.versioning.model._
 /** Represent the diff between two commits. This should not be instantiated by user.
  *  Please use Commit's diffFrom method to get instances
  */
-class Diff(private[repository] val blobDiffs: Option[List[VersioningBlobDiff]]) {
-  val diffs = blobDiffs
-}
+class Diff(private[repository] val blobDiffs: Option[List[VersioningBlobDiff]]) {}
