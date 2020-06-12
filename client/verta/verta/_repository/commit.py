7fc6151fb15ab5274137e53013b82ef1125bd6bd
Michael Liu <miliu@protonmail.com>
2020-06-11 22:50:59 -0700

    Define _component_blobs property

--- client/verta/verta/_repository/commit.py
+++ client/verta/verta/_repository/commit.py
@@ -448,9 +448,13 @@ class Commit(object):
         self._lazy_load_blobs()
 
         try:
-            return self._blobs[path]
+            blob = self._blobs[path]
         except KeyError:
             self._raise_lookup_error(path)
+
+        if isinstance(blob, dataset._Dataset):
+            # for dataset_blob.download()
+            blob._commit = self
 
     def remove(self, path):
         """
--- client/verta/verta/dataset/_dataset.py
+++ client/verta/verta/dataset/_dataset.py
@@ -29,6 +29,11 @@ class _Dataset(blob.Blob):
         """This shall be implemented by subclasses, but shouldn't halt execution if called."""
         return []
 
+    @property
+    def _component_blobs(self):
+        """This shall be implemented by subclasses, but shouldn't halt execution if called."""
+        return []
+
     @staticmethod
     def _path_component_to_repr_lines(path_component_msg):
         """
