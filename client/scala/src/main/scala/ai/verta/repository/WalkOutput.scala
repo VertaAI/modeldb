d2f077504fd4709f0a583e07a8addcf5de10a5e7
nhatsmrt <nhatsmrt@uw.edu>
2020-06-15 11:36:24 -0700

    Add doc.

--- client/scala/src/main/scala/ai/verta/repository/WalkOutput.scala
+++ client/scala/src/main/scala/ai/verta/repository/WalkOutput.scala
@@ -1,6 +1,7 @@
 package ai.verta.repository
 
-/** A class to represent the output of Commit's walk method
+/** A class to represent the output of Commit's walk method.
+ *  User should not instantiate this.
  *  @param folderPath path to current folder
  *  @param folderNames names of subfolders in current folder
  *  @param blobNames names of blobs in current folder
