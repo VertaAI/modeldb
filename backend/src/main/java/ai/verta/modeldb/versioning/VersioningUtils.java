98c16a71e508303ed2f247b3ba9e53b377a6125d
Evgenii Lezhnin <eugene.lezhnin@quantumsoft.ru>
2020-06-19 13:13:52 +0700

    VR-4760 clean up

--- backend/src/main/java/ai/verta/modeldb/versioning/VersioningUtils.java
+++ backend/src/main/java/ai/verta/modeldb/versioning/VersioningUtils.java
@@ -7,7 +7,6 @@ import java.security.NoSuchAlgorithmException;
 import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
 import org.hibernate.Session;
