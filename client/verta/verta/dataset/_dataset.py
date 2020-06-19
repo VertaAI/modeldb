6f4d8f2782f9c2d136043accf9b55f2581b5c29b
Michael Liu <miliu@protonmail.com>
2020-06-18 23:17:52 -0700

    Move TODO again

--- client/verta/verta/dataset/_dataset.py
+++ client/verta/verta/dataset/_dataset.py
@@ -168,7 +168,6 @@ class _Dataset(blob.Blob):
             raise KeyError("no components found for path {}".format(component_path))
 
         # determine the root path everything is being downloaded to
-        # TODO: handle if a `local_path` is at system root or a parent dir
         local_paths = map(os.path.relpath, components_to_download.values())  # relative to cwd
         root_dirnames = set(local_path.split('/')[0] for local_path in local_paths)
         if len(root_dirnames) == 1:
@@ -176,6 +175,7 @@ class _Dataset(blob.Blob):
             downloaded_to_path = root_dirnames.pop()
         else:
             # everything is relative to cwd, so cwd is our root
+            # TODO: handle if a `local_path` was actually at system root or a parent dir
             downloaded_to_path = os.curdir
 
         return (components_to_download, downloaded_to_path)
