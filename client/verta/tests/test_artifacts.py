25a797601a0438b4436a28edd6a898e05ab3413c
Michael Liu <miliu@protonmail.com>
2020-06-21 21:51:18 -0700

    Improve test

--- client/verta/tests/test_artifacts.py
+++ client/verta/tests/test_artifacts.py
@@ -121,42 +121,42 @@ class TestArtifacts:
 
     def test_clientside_storage(self, experiment_run, strs, in_tempdir):
         key = strs[0]
+        filename = strs[1]
         FILE_CONTENTS = os.urandom(2**16)
 
-        # TODO: use existing env var in case tester intentionally set
+        # TODO: be able to use existing env var for debugging
+        # NOTE: there is an assertion of `== 1` artifact that would need to be changed
         VERTA_ARTIFACT_DIR_KEY = 'VERTA_ARTIFACT_DIR'
-        VERTA_ARTIFACT_DIR = os.environ.pop(VERTA_ARTIFACT_DIR_KEY, None)
+        PREV_VERTA_ARTIFACT_DIR = os.environ.pop(VERTA_ARTIFACT_DIR_KEY, None)
         try:
             tempdir = tempfile.mkdtemp()
             try:
-                os.environ[VERTA_ARTIFACT_DIR_KEY] = os.path.join(tempdir, "artifact-store")
+                VERTA_ARTIFACT_DIR = os.path.join(tempdir, "artifact-store")
+                os.environ[VERTA_ARTIFACT_DIR_KEY] = VERTA_ARTIFACT_DIR
 
-                # create file and upload as artifact
-                with tempfile.NamedTemporaryFile() as tempf:
-                    tempf.write(FILE_CONTENTS)
-                    tempf.flush()  # flush object buffer
-                    os.fsync(tempf.fileno())  # flush OS buffer
-                    tempf.seek(0)
-
-                    experiment_run.log_artifact(key, tempf)
-                assert True  # TODO: new file in artifact dir
+                # create file
+                with open(filename, 'wb') as f:
+                    f.write(FILE_CONTENTS)
+                # log artifact and delete file
+                experiment_run.log_artifact(key, filename)
+                os.remove(filename)
+                # and then there was one
+                assert len(os.listdir(VERTA_ARTIFACT_DIR)) == 1
 
                 # artifact retrievable
                 artifact = experiment_run.get_artifact(key)
                 assert artifact.read() == FILE_CONTENTS
 
                 # artifact downloadable
-                filename = strs[1]
                 filepath = experiment_run.download_artifact(key, filename)
-                assert filepath == os.path.abspath(filename)
-                with open(filename, 'rb') as f:
+                with open(filepath, 'rb') as f:
                     assert f.read() == FILE_CONTENTS
 
             finally:
                 shutil.rmtree(tempdir)
         finally:
-            if VERTA_ARTIFACT_DIR is not None:
-                os.environ[VERTA_ARTIFACT_DIR_KEY] = VERTA_ARTIFACT_DIR
+            if PREV_VERTA_ARTIFACT_DIR is not None:
+                os.environ[VERTA_ARTIFACT_DIR_KEY] = PREV_VERTA_ARTIFACT_DIR
             else:
                 del os.environ[VERTA_ARTIFACT_DIR_KEY]
 
