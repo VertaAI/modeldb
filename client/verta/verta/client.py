adafef8b462158cba7bd9b860a933bfda1e38005
Michael Liu <miliu@protonmail.com>
2020-06-22 14:38:02 -0700

    Implement

--- client/verta/verta/client.py
+++ client/verta/verta/client.py
@@ -1868,16 +1868,7 @@ class ExperimentRun(_ModelDBEntity):
         super(ExperimentRun, self).__init__(conn, conf, _ExperimentRunService, "experiment-run", expt_run.id)
 
     def __repr__(self):
-        Message = _ExperimentRunService.GetExperimentRunById
-        msg = Message(id=self.id)
-        data = _utils.proto_to_json(msg)
-        response = _utils.make_request("GET",
-                                       "{}://{}/api/v1/modeldb/experiment-run/getExperimentRunById".format(self._conn.scheme, self._conn.socket),
-                                       self._conn, params=data)
-        _utils.raise_for_http_error(response)
-
-        response_msg = _utils.json_to_proto(_utils.body_to_json(response), Message.Response)
-        run_msg = response_msg.experiment_run
+        run_msg = self._get_self_as_msg()
         return '\n'.join((
             "name: {}".format(run_msg.name),
             "url: {}://{}/{}/projects/{}/exp-runs/{}".format(self._conn.scheme, self._conn.socket, self.workspace, run_msg.project_id, self.id),
@@ -2015,6 +2006,27 @@ class ExperimentRun(_ModelDBEntity):
             return response_msg.experiment_run
         else:
             _utils.raise_for_http_error(response)
+
+    # TODO: use this throughout `ExperimentRun`
+    def _get_self_as_msg(self):
+        """
+        Gets the full protobuf message representation of this Experiment Run.
+
+        Returns
+        -------
+        run_msg : ExperimentRun protobuf message
+
+        """
+        Message = _ExperimentRunService.GetExperimentRunById
+        msg = Message(id=self.id)
+        data = _utils.proto_to_json(msg)
+        response = _utils.make_request("GET",
+                                       "{}://{}/api/v1/modeldb/experiment-run/getExperimentRunById".format(self._conn.scheme, self._conn.socket),
+                                       self._conn, params=data)
+        _utils.raise_for_http_error(response)
+
+        response_msg = _utils.json_to_proto(_utils.body_to_json(response), Message.Response)
+        return response_msg.experiment_run
 
     def _log_artifact(self, key, artifact, artifact_type, extension=None, method=None, overwrite=False):
         """
@@ -2385,27 +2397,29 @@ class ExperimentRun(_ModelDBEntity):
 
     def get_date_created(self):
         """
-        Gets a timestamp representing the time this Experiment Run was created.
+        Gets a timestamp representing the time (in UTC) this Experiment Run was created.
 
         Returns
         -------
         timestamp : int
-            Unix timestamp (in UTC).
+            Unix timestamp in milliseconds.
 
         """
-        pass
+        run_msg = self._get_self_as_msg()
+        return int(run_msg.date_created)
 
     def get_date_updated(self):
         """
-        Gets a timestamp representing the time this Experiment Run was updated.
+        Gets a timestamp representing the time (in UTC) this Experiment Run was updated.
 
         Returns
         -------
         timestamp : int
-            Unix timestamp (in UTC).
+            Unix timestamp in milliseconds.
 
         """
-        pass
+        run_msg = self._get_self_as_msg()
+        return int(run_msg.date_updated)
 
     def log_tag(self, tag):
         """
