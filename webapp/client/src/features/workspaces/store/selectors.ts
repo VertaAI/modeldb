c901fedf58c058dbc6a6b6508a53579ef3a6e926
andrey.kuznetsov.qs <andrey.kuznetsov@quantumsoft.ru>
2020-06-22 13:45:12 +0700

    returned default workspace

--- webapp/client/src/features/workspaces/store/selectors.ts
+++ webapp/client/src/features/workspaces/store/selectors.ts
@@ -8,7 +8,7 @@ export const selectUserWorkspace = (
 ): Workspace.IUserWorkspace => {
   return {
     type: 'user',
-    name: 'andrey_kuznetsov' as Workspace.IUserWorkspace['name'],
+    name: 'personal' as Workspace.IUserWorkspace['name'],
     id: 'id' as Workspace.IUserWorkspace['id'],
   };
 };
