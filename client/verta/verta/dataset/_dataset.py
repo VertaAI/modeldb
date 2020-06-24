6cc97a352f0ef5332ee7bef492b684615fc85f38
Michael Liu <miliu@protonmail.com>
2020-06-24 15:45:04 -0700

    Remove s3: from dl_to_path

--- client/verta/verta/dataset/_dataset.py
+++ client/verta/verta/dataset/_dataset.py
@@ -165,10 +165,9 @@ class _Dataset(blob.Blob):
         if component_path is None:
             # download all
             for path in self.list_paths():
-                # TODO: removeprefix "s3://"
                 local_path = os.path.join(
                     downloaded_to_path,
-                    path,
+                    _file_utils.remove_prefix_dir(path, "s3:"),
                 )
 
                 components_to_download[path] = local_path
