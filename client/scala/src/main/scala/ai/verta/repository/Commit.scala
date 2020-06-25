1c608589c6da11c77abb37a7ace906c716287473
nhatsmrt <nhatsmrt@uw.edu>
2020-06-25 15:58:03 -0700

    Propagate the failure out.

--- client/scala/src/main/scala/ai/verta/repository/Commit.scala
+++ client/scala/src/main/scala/ai/verta/repository/Commit.scala
@@ -540,7 +540,7 @@ class Commit(
           Map[String, List[String]](),
           Map[String, String](),
           inputStream
-        ), Duration.Inf).map(_ => ())
+        ), Duration.Inf).map(_ => ()).get // propagate the failure out
       } finally {
         inputStream.close()
       }
