907f0f88cf478fba0e22488b3cb00eeab8015819
Michael Liu <miliu@protonmail.com>
2020-06-25 13:35:35 -0700

    Remove base path

--- client/verta/verta/dataset/_path.py
+++ client/verta/verta/dataset/_path.py
@@ -2,6 +2,7 @@
 
 from __future__ import print_function
 
+import collections
 import hashlib
 import os
 
@@ -30,6 +31,8 @@ class Path(_dataset._Dataset):
         List of filepaths or directory paths.
     enable_mdb_versioning : bool, default False
         Whether to upload the data itself to ModelDB to enable managed data versioning.
+    base_path : str, optional
+        Hey Michael, replace this before shipping.
 
     Examples
     --------
@@ -45,7 +48,7 @@ class Path(_dataset._Dataset):
         ])
 
     """
-    def __init__(self, paths, enable_mdb_versioning=False):
+    def __init__(self, paths, enable_mdb_versioning=False, base_path=None):
         if isinstance(paths, six.string_types):
             paths = [paths]
         paths = map(os.path.expanduser, paths)
@@ -55,7 +58,24 @@ class Path(_dataset._Dataset):
         filepaths = _file_utils.flatten_file_trees(paths)
         components = map(self._get_file_metadata, filepaths)
 
+        # remove `base_path` from the beginning of component paths
+        base_path_to_component_paths = collections.defaultdict(set)
+        for component in components:
+            path = _file_utils.remove_prefix_dir(component.path, prefix_dir=base_path)
+            if path == component.path:  # no change
+                raise ValueError("path {} does not begin with `base_path` {}".format(
+                    component.path,
+                    base_path,
+                ))
+
+            # update component with modified path
+            component.path = path
+
+            # remember base + path to reconstruct original in _prepare_components_to_upload()
+            base_path_to_component_paths[base_path].add(component.path)
+
         self._msg.path.components.extend(components)
+        self._base_path_to_component_paths = base_path_to_component_paths
 
     def __repr__(self):
         # TODO: consolidate with S3 since they're almost identical now
