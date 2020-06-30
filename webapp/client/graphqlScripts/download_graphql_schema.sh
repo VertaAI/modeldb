d66251eb68ac36105dea9c78819235556884266c
John Dean <john.holmes.dean@gmail.com>
2020-06-30 11:36:10 -0500

    Remove jd-dev from schema gen

--- webapp/client/graphqlScripts/download_graphql_schema.sh
+++ webapp/client/graphqlScripts/download_graphql_schema.sh
@@ -2,4 +2,4 @@
 
 set -e
 
-yarn run apollo client:download-schema  schema.graphql --endpoint=https://jd.dev.verta.ai/api/v1/graphql/query    --header="Grpc-Metadata-developer_key: $VERTA_DEV_KEY" --header="Grpc-Metadata-email: $VERTA_EMAIL" --header='Grpc-Metadata-source: PythonClient'
+yarn run apollo client:download-schema  schema.graphql --endpoint=https://dev.verta.ai/api/v1/graphql/query    --header="Grpc-Metadata-developer_key: $VERTA_DEV_KEY" --header="Grpc-Metadata-email: $VERTA_EMAIL" --header='Grpc-Metadata-source: PythonClient'
