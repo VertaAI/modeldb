094380226deb356e4e0c7ddcdfe0a42819502c1a
nhatsmrt <nhatsmrt@uw.edu>
2020-07-07 15:33:10 -0700

    Fix a line in doc.

--- client/scala/src/main/scala/ai/verta/blobs/dataset/ComponentToLocalPath.scala
+++ client/scala/src/main/scala/ai/verta/blobs/dataset/ComponentToLocalPath.scala
@@ -1,7 +1,7 @@
 package ai.verta.blobs.dataset
 
 /** Output of Dataset's determineComponentAndLocalPaths method
- *  @param componentToLocalPath a map from component path to local path to download to
+ *  @param componentToLocalPathMap a map from component path to local path to download to
  *  @param absoluteLocalPath absolute local path to the downloaded file(s)
  */
 private[dataset] case class ComponentToLocalPath(
