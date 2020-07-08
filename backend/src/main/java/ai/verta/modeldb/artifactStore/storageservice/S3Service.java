80827e64827e1e719bc09b498cd8175595ae99f7
evgeniilezhnin <eugene.lezhnin@quantumsoft.ru>
2020-07-08 09:51:17 +0700

    VR-5110 s3 region setting (#945)
    * VR-5110 s3 region setting

--- backend/src/main/java/ai/verta/modeldb/artifactStore/storageservice/S3Service.java
+++ backend/src/main/java/ai/verta/modeldb/artifactStore/storageservice/S3Service.java
@@ -39,12 +39,13 @@ public class S3Service implements ArtifactStoreService {
     String cloudAccessKey = app.getCloudAccessKey();
     String cloudSecretKey = app.getCloudSecretKey();
     String minioEndpoint = app.getMinioEndpoint();
+    final Regions awsRegion = Regions.fromName(app.getAwsRegion());
     if (cloudAccessKey != null && cloudSecretKey != null) {
       if (minioEndpoint == null) {
         BasicAWSCredentials awsCreds = new BasicAWSCredentials(cloudAccessKey, cloudSecretKey);
         this.s3Client =
             AmazonS3ClientBuilder.standard()
-                .withRegion(Regions.US_EAST_1)
+                .withRegion(awsRegion)
                 .withCredentials(new AWSStaticCredentialsProvider(awsCreds))
                 .build();
       } else {
@@ -57,7 +58,7 @@ public class S3Service implements ArtifactStoreService {
             AmazonS3ClientBuilder.standard()
                 .withEndpointConfiguration(
                     new AwsClientBuilder.EndpointConfiguration(
-                        minioEndpoint, Regions.US_EAST_1.name()))
+                        minioEndpoint, app.getAwsRegion()))
                 .withPathStyleAccessEnabled(true)
                 .withClientConfiguration(clientConfiguration)
                 .withCredentials(new AWSStaticCredentialsProvider(awsCreds))
@@ -65,7 +66,7 @@ public class S3Service implements ArtifactStoreService {
       }
     } else {
       // reads credential from OS Environment
-      s3Client = AmazonS3ClientBuilder.standard().withRegion(Regions.US_EAST_1).build();
+      s3Client = AmazonS3ClientBuilder.standard().withRegion(awsRegion).build();
     }
 
     this.bucketName = cloudBucketName;
