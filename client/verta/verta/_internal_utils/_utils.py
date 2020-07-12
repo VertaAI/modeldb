9891eed5f65464bac28a494225c20084531dda4d
Conrado Miranda <conrado@verta.ai>
2020-07-12 14:52:29 -0700

    Fix finding experiment runs

--- client/verta/verta/_internal_utils/_utils.py
+++ client/verta/verta/_internal_utils/_utils.py
@@ -197,9 +197,8 @@ class LazyList(object):
             if (not records
                     and msg.page_number > response_msg.total_records):  # pylint: disable=no-member
                 raise IndexError("index out of range")
-            id_ = records[0].id
 
-            return self._create_element(id_)
+            return self._create_element(records[0])
         else:
             raise TypeError("index must be integer, not {}".format(type(index)))
 
@@ -219,15 +218,15 @@ class LazyList(object):
 
             total_records = response_msg.total_records
 
-            ids = self._get_ids(response_msg)
-            for id_ in ids:
+            records = self._get_records(response_msg)
+            for rec in records:
                 # skip if we've seen the ID before
-                if id_ in seen_ids:
+                if rec.id in seen_ids:
                     continue
                 else:
-                    seen_ids.add(id_)
+                    seen_ids.add(rec.id)
 
-                yield self._create_element(id_)
+                yield self._create_element(rec)
 
     def __len__(self):
         # copy msg to avoid mutating `self`'s state
@@ -259,14 +258,11 @@ class LazyList(object):
         response_msg = json_to_proto(body_to_json(response), msg.Response)
         return response_msg
 
-    def _get_ids(self, response_msg):
-        return (record.id for record in self._get_records(response_msg))
-
     def _get_records(self, response_msg):
         """Get the attribute of `response_msg` that is not `total_records`."""
         raise NotImplementedError
 
-    def _create_element(self, id_):
+    def _create_element(self, msg):
         """Instantiate element to return to user."""
         raise NotImplementedError
 
