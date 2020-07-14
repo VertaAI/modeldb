2b4441f590b63c1fae0ea85b5eebbab896cb9124
Conrado Miranda <conrado@verta.ai>
2020-07-13 18:29:06 -0700

    Add initial registry skeleton

---
+++ client/verta/verta/_registry/model.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function
+
+from .._tracking.entity import _ModelDBEntity
+
+
+class RegisteredModel(_ModelDBEntity):
+    def __init__(self, conn, conf, msg):
+        raise NotImplementedError
+
+    def __repr__(self):
+        raise NotImplementedError
+
+    @property
+    def name(self):
+        self._refresh_cache()
+        return self._msg.name
+
+    def get_or_create_version(name=None, desc=None, tags=None, attrs=None, id=None):
+        raise NotImplementedError
+
+    def get_version(name=None, id=None):
+        raise NotImplementedError
+
+    @property
+    def versions(self):
+        return ModelVersions(self._conn, self._conf).with_model(self)
+
+    @classmethod
+    def _generate_default_name(cls):
+        return "Model {}".format(_utils.generate_default_name())
+
+    @classmethod
+    def _get_proto_by_id(cls, conn, id):
+        raise NotImplementedError
+
+    @classmethod
+    def _get_proto_by_name(cls, conn, name, workspace):
+        raise NotImplementedError
+
+    @classmethod
+    def _create_proto_internal(cls, conn, ctx, name, desc=None, tags=None, attrs=None, date_created=None, public_within_org=None):
+        raise NotImplementedError
