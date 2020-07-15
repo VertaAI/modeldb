7730dbf77125970f202fee097b564fa469763ef0
nhatsmrt <nhatsmrt@uw.edu>
2020-07-14 20:14:07 -0700

    Add a test for get model version by id.

--- client/verta/tests/test_model_registry/test_model_version.py
+++ client/verta/tests/test_model_registry/test_model_version.py
@@ -10,3 +10,7 @@ class TestModelVersion:
     def test_get_by_name(self, client, model_version):
         retrieved_model_version = client.set_model_version(model_version.name)
         assert retrieved_model_version.id == model_version.id
+
+    def test_get_by_id(self, client, model_version):
+        retrieved_model_version = client.get_or_create_repository(id=model_version.id)
+        assert model_version.id == retrieved_model_version.id
