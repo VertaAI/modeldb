2adc2556b244f28daddf132946f493f9101aab53
nhatsmrt <nhatsmrt@uw.edu>
2020-07-14 22:13:01 -0700

    Temporary fix.

--- client/verta/tests/test_model_registry/test_model_version.py
+++ client/verta/tests/test_model_registry/test_model_version.py
@@ -7,13 +7,12 @@ import verta.environment
 
 
 class TestModelVersion:
-    # def test_get_by_name(self, client, registered_model):
-    #     model_version = registered_model.get_or_create_version()
-    #     retrieved_model_version = registered_model.set_model_version(model_version.name)
-    #     assert retrieved_model_version.id == model_version.id
+    def test_get_by_name(self, registered_model):
+        model_version = registered_model.get_or_create_version()
+        retrieved_model_version = registered_model.get_version(name=model_version.name)
+        assert retrieved_model_version.id == model_version.id
 
     def test_get_by_id(self, registered_model):
         model_version = registered_model.get_or_create_version()
-        print(model_version.id)
-        retrieved_model_version = registered_model.get_or_create_version(id=model_version.id)
+        retrieved_model_version = registered_model.get_version(id=model_version.id)
         assert model_version.id == retrieved_model_version.id
--- client/verta/verta/_registry/modelversion.py
+++ client/verta/verta/_registry/modelversion.py
@@ -36,29 +36,29 @@ class RegisteredModelVersion(_ModelDBEntity):
 
     @classmethod
     def _get_proto_by_name(cls, conn, name, registered_model_id):
-        # return None # Not implemented yet!
+        return None # WIP
 
-        Message = _ModelVersionService.FindModelVersionRequest
-        RegisteredModelIDMessage = _ModelVersionService.RegisteredModelIdentification
-
-        predicates = [
-            _CommonCommonService.KeyValueQuery(key="version",
-                                               value=_utils.python_to_val_proto(name),
-                                               operator=_CommonCommonService.OperatorEnum.EQ)
-        ]
-        endpoint = "/api/v1/registry/{}/versions".format(registered_model_id)
-        msg = Message(id=RegisteredModelIDMessage(registered_model_id=registered_model_id), predicates=predicates)
-
-        proto_response = conn.make_proto_request("POST", endpoint, body=msg)
-        response = conn.maybe_proto_response(proto_response, Message.Response)
-
-        if not response.model_versions:
-            return None
-
-        return response.model_versions[0] # should only have 1 entry here, as name/version is unique
+        # Message = _ModelVersionService.FindModelVersionRequest
+        #
+        # predicates = [
+        #     _CommonCommonService.KeyValueQuery(key="version",
+        #                                        value=_utils.python_to_val_proto(name),
+        #                                        operator=_CommonCommonService.OperatorEnum.EQ)
+        # ]
+        # endpoint = "/api/v1/registry/{}/versions".format(registered_model_id)
+        # msg = Message(predicates=predicates)
+        #
+        # proto_response = conn.make_proto_request("POST", endpoint, body=msg)
+        # response = conn.maybe_proto_response(proto_response, Message.Response).model_versions[0]
+        #
+        # if not response.model_versions:
+        #     return None
+        #
+        # # should only have 1 entry here, as name/version is unique
+        # return response.model_versions[0]
 
     @classmethod
-    def _create_proto_internal(cls, conn, ctx, name, desc=None, tags=None, labels=None, attrs=None, date_created=None, registered_model_id=None, registered_model_name=None):
+    def _create_proto_internal(cls, conn, ctx, name, desc=None, tags=None, labels=None, attrs=None, date_created=None, registered_model_id=None):
         # ctx is always None here
         ModelVersionMessage = _ModelVersionService.ModelVersion
         SetModelVersionMessage = _ModelVersionService.SetModelVersion
@@ -69,7 +69,7 @@ class RegisteredModelVersion(_ModelDBEntity):
         response = conn.make_proto_request("POST", url_endpoint, body=model_version_msg)
         model_version = conn.must_proto_response(response, SetModelVersionMessage.Response).model_version
 
-        print("Created new ModelVersion: {}".format(model_version.name))
+        print("Created new ModelVersion: {}".format(model_version.version))
         return model_version
 
     def set_model(self, model, overwrite=False):
