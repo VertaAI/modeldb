49a9a76f1e3909d7a5efebaddc6eadf8493ebd10
Michael Liu <miliu@protonmail.com>
2020-07-14 17:31:52 -0700

    Add fuzzy equality operator

--- client/verta/verta/_internal_utils/_utils.py
+++ client/verta/verta/_internal_utils/_utils.py
@@ -138,7 +138,8 @@ class LazyList(object):
     # number of items to fetch per back end call in __iter__()
     _ITER_PAGE_LIMIT = 100
 
-    _OP_MAP = {'==': _CommonCommonService.OperatorEnum.EQ,
+    _OP_MAP = {'~=': _CommonCommonService.OperatorEnum.CONTAIN,
+               '==': _CommonCommonService.OperatorEnum.EQ,
                '!=': _CommonCommonService.OperatorEnum.NE,
                '>':  _CommonCommonService.OperatorEnum.GT,
                '>=': _CommonCommonService.OperatorEnum.GTE,
