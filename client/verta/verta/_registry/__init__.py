738a8ada676af648bcbed3de2b4467d681ab0c74
nhatsmrt <nhatsmrt@uw.edu>
2020-07-14 20:21:15 -0700

    Uncomment an import.

--- client/verta/verta/_registry/__init__.py
+++ client/verta/verta/_registry/__init__.py
@@ -3,4 +3,4 @@
 from .model import RegisteredModel
 from .models import RegisteredModels
 from .modelversion import RegisteredModelVersion
-# from .modelversions import RegisteredModelVersions
+from .modelversions import RegisteredModelVersions
