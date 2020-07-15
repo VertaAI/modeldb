11537c2f685ccba4fbacefb8b26d9008ab781fe5
nhatsmrt <nhatsmrt@uw.edu>
2020-07-15 08:32:33 -0700

    Change the name of a parameter.

--- client/verta/verta/_registry/modelversion.py
+++ client/verta/verta/_registry/modelversion.py
@@ -29,8 +29,8 @@ class RegisteredModelVersion(_ModelDBEntity):
     @classmethod
     def _get_proto_by_id(cls, conn, id):
         Message = _ModelVersionService.GetModelVersionRequest
-        url_endpoint = "/api/v1/registry/registered_model_versions/{}".format(id)
-        response = conn.make_proto_request("GET", url_endpoint)
+        endpoint = "/api/v1/registry/registered_model_versions/{}".format(id)
+        response = conn.make_proto_request("GET", endpoint)
 
         return conn.maybe_proto_response(response, Message.Response).model_version
 
@@ -65,8 +65,8 @@ class RegisteredModelVersion(_ModelDBEntity):
 
         model_version_msg = ModelVersionMessage(version=name, description=desc, registered_model_id=registered_model_id,
                                                 labels=labels, time_created=date_created, time_updated=date_created)
-        url_endpoint = "/api/v1/registry/{}/versions".format(registered_model_id)
-        response = conn.make_proto_request("POST", url_endpoint, body=model_version_msg)
+        endpoint = "/api/v1/registry/{}/versions".format(registered_model_id)
+        response = conn.make_proto_request("POST", endpoint, body=model_version_msg)
         model_version = conn.must_proto_response(response, SetModelVersionMessage.Response).model_version
 
         print("Created new ModelVersion: {}".format(model_version.version))
