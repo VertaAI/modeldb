9b267854e0fc61d8d95c4317ec6e2aace51f7cfd
Anand Jakhaniya <anand.jakhaniya@gmail.com>
2020-07-15 10:15:49 +0530

    Added endpoint for fetch label ids for registry find calls (#994)
    * Added updateLabels endpoint and added deleteAll field in deleteLabels
    
    * Added endpoint for fetch label ids for registry find calls

--- backend/src/main/java/ai/verta/modeldb/metadata/MetadataServiceImpl.java
+++ backend/src/main/java/ai/verta/modeldb/metadata/MetadataServiceImpl.java
@@ -1,6 +1,7 @@
 package ai.verta.modeldb.metadata;
 
 import ai.verta.modeldb.ModelDBAuthInterceptor;
+import ai.verta.modeldb.ModelDBException;
 import ai.verta.modeldb.metadata.MetadataServiceGrpc.MetadataServiceImplBase;
 import ai.verta.modeldb.monitoring.QPSCountResource;
 import ai.verta.modeldb.monitoring.RequestLatencyResource;
@@ -83,6 +84,61 @@ public class MetadataServiceImpl extends MetadataServiceImplBase {
     } catch (Exception e) {
       ModelDBUtils.observeError(
           responseObserver, e, AddLabelsRequest.Response.getDefaultInstance());
+    }
+  }
+
+  @Override
+  public void updateLabels(
+      AddLabelsRequest request, StreamObserver<AddLabelsRequest.Response> responseObserver) {
+    QPSCountResource.inc();
+    try (RequestLatencyResource latencyResource =
+        new RequestLatencyResource(ModelDBAuthInterceptor.METHOD_NAME.get())) {
+      String errorMessage = null;
+      if (request.getId() == null
+          || (request.getId().getIntId() == 0 && request.getId().getStringId().isEmpty())) {
+        errorMessage = "Invalid parameter set in AddLabelsRequest.Id";
+      } else if (request.getLabelsList().isEmpty()) {
+        errorMessage = "labels not found in AddLabelsRequest request";
+      }
+
+      if (errorMessage != null) {
+        LOGGER.info(errorMessage);
+        Status status =
+            Status.newBuilder()
+                .setCode(Code.INVALID_ARGUMENT_VALUE)
+                .setMessage(errorMessage)
+                .addDetails(Any.pack(AddLabelsRequest.Response.getDefaultInstance()))
+                .build();
+        throw StatusProto.toStatusRuntimeException(status);
+      }
+
+      boolean status = metadataDAO.updateLabels(request.getId(), request.getLabelsList());
+      responseObserver.onNext(AddLabelsRequest.Response.newBuilder().setStatus(status).build());
+      responseObserver.onCompleted();
+    } catch (Exception e) {
+      ModelDBUtils.observeError(
+          responseObserver, e, AddLabelsRequest.Response.getDefaultInstance());
+    }
+  }
+
+  @Override
+  public void getLabelIds(
+      GetLabelIdsRequest request, StreamObserver<GetLabelIdsRequest.Response> responseObserver) {
+    QPSCountResource.inc();
+    try (RequestLatencyResource latencyResource =
+        new RequestLatencyResource(ModelDBAuthInterceptor.METHOD_NAME.get())) {
+
+      if (request.getLabelsList().isEmpty()) {
+        throw new ModelDBException(
+            "Labels not found in GetLabelIdsRequest", io.grpc.Status.Code.INVALID_ARGUMENT);
+      }
+
+      List<IdentificationType> labelIds = metadataDAO.getLabelIds(request.getLabelsList());
+      responseObserver.onNext(GetLabelIdsRequest.Response.newBuilder().addAllIds(labelIds).build());
+      responseObserver.onCompleted();
+    } catch (Exception e) {
+      ModelDBUtils.observeError(
+          responseObserver, e, GetLabelIdsRequest.Response.getDefaultInstance());
     }
   }
 
