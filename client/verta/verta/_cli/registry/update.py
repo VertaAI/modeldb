be2b05fdac88ad143a3c9451271855270b8bfdfc
Michael Liu <miliu@protonmail.com>
2020-07-15 18:28:46 -0700

    Change tag to label

--- client/verta/verta/_cli/registry/update.py
+++ client/verta/verta/_cli/registry/update.py
@@ -12,9 +12,9 @@ def update():
 
 @update.command(name="registeredmodel")
 @click.argument("model_name", nargs=1, required=True)
-@click.option("--tag", "-t", multiple=True, help="Tags to be associated with the object.")
+@click.option("--label", "-t", multiple=True, help="Label to be associated with the object.")
 @click.option("--workspace", "-w", help="Workspace to use.")
-def update_model(name, tag, visibility, workspace):
+def update_model(name, label, visibility, workspace):
     """Create a new registeredmodel entry.
     """
     pass
@@ -22,12 +22,12 @@ def update_model(name, tag, visibility, workspace):
 @update.command(name="registeredmodelversion")
 @click.argument("model_name", nargs=1, required=True)
 @click.argument("version_name", nargs=1, required=True)
-@click.option("--tag", "-t", multiple=True, help="Tags to be associated with the object.")
+@click.option("--label", "-t", multiple=True, help="Label to be associated with the object.")
 @click.option("--model", help="Path to the model.")
 @click.option("--asset", multiple=True, help="Path to the asset required for the model. The format if asset_name=path_to_asset.")
 @click.option("--workspace", "-w", help="Workspace to use.")
 # TODO: add environment
-def update_model_version(type, model_name, tag):
+def update_model_version(type, model_name, label):
     """Create a new registeredmodelversion entry.
     """
     pass
