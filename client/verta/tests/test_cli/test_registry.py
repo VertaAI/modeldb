cc5776eb44cf1cc23cfdcc4ccbd8b764382f7856
Michael Liu <miliu@protonmail.com>
2020-07-15 21:31:49 -0700

    Add test_registry file

---
+++ client/verta/tests/test_cli/test_registry.py
@@ -0,0 +1,22 @@
+import json
+
+import pytest
+from click.testing import CliRunner
+
+from verta._cli import cli
+
+
+class TestCreate:
+    pass
+
+
+class TestGet:
+    pass
+
+
+class TestList:
+    pass
+
+
+class TestUpdate:
+    pass
