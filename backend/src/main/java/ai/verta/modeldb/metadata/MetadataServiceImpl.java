f5337095188b816586007462513b5738972f128d
Anand Jakhaniya <anand.jakhaniya@gmail.com>
2020-07-16 09:08:31 +0530

    Fixed update labels api issue (#1012)

--- backend/src/main/java/ai/verta/modeldb/metadata/MetadataServiceImpl.java
+++ backend/src/main/java/ai/verta/modeldb/metadata/MetadataServiceImpl.java
@@ -93,23 +93,10 @@ public class MetadataServiceImpl extends MetadataServiceImplBase {
     QPSCountResource.inc();
     try (RequestLatencyResource latencyResource =
         new RequestLatencyResource(ModelDBAuthInterceptor.METHOD_NAME.get())) {
-      String errorMessage = null;
       if (request.getId() == null
           || (request.getId().getIntId() == 0 && request.getId().getStringId().isEmpty())) {
-        errorMessage = "Invalid parameter set in AddLabelsRequest.Id";
-      } else if (request.getLabelsList().isEmpty()) {
-        errorMessage = "labels not found in AddLabelsRequest request";
-      }
-
-      if (errorMessage != null) {
-        LOGGER.info(errorMessage);
-        Status status =
-            Status.newBuilder()
-                .setCode(Code.INVALID_ARGUMENT_VALUE)
-                .setMessage(errorMessage)
-                .addDetails(Any.pack(AddLabelsRequest.Response.getDefaultInstance()))
-                .build();
-        throw StatusProto.toStatusRuntimeException(status);
+        throw new ModelDBException(
+            "Invalid parameter set in AddLabelsRequest.Id", io.grpc.Status.Code.INVALID_ARGUMENT);
       }
 
       boolean status = metadataDAO.updateLabels(request.getId(), request.getLabelsList());
