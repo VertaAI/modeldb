2180f852622f5ca764499970d6215f99727608e6
Michael Liu <miliu@protonmail.com>
2020-07-15 18:31:34 -0700

    Change tag to label (#1018)

--- client/verta/verta/_cli/registry/create.py
+++ client/verta/verta/_cli/registry/create.py
@@ -16,10 +16,10 @@ def create():
 
 @create.command(name="registeredmodel")
 @click.argument("model_name", nargs=1, required=True)
-@click.option("--tag", "-t", multiple=True, help="Tags to be associated with the object.")
+@click.option("--label", "-t", multiple=True, help="Labels to be associated with the object.")
 @click.option("--visibility", "-v", default="private", show_default=True, type=click.Choice(["private", "org"], case_sensitive=False), help="Visibility level of the object.")
 @click.option("--workspace", "-w", help="Workspace to use.")
-def create_model(model_name, tag, visibility, workspace):
+def create_model(model_name, label, visibility, workspace):
     """Create a new registeredmodel entry.
     """
     pass
@@ -27,11 +27,11 @@ def create_model(model_name, tag, visibility, workspace):
 @create.command(name="registeredmodelversion")
 @click.argument("model_name", nargs=1, required=True)
 @click.argument("version_name", nargs=1, required=True)
-@click.option("--tag", "-t", multiple=True, help="Tags to be associated with the object.")
+@click.option("--label", "-t", multiple=True, help="Labels to be associated with the object.")
 @click.option("--model", help="Path to the model.")
 @click.option("--asset", multiple=True, help="Path to the asset required for the model. The format if asset_name=path_to_asset.")
 @click.option("--workspace", "-w", help="Workspace to use.")
-def create_model_version(model_name, tag):
+def create_model_version(model_name, label):
     """Create a new registeredmodelversion entry.
     """
     pass
