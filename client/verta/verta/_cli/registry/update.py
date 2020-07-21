5c81cf6d34f2fa4cd639dc68c0823da22ea99155
nhatsmrt <nhatsmrt@uw.edu>
2020-07-20 18:13:20 -0700

    Fix a small issue in key checking.

--- client/verta/verta/_cli/registry/update.py
+++ client/verta/verta/_cli/registry/update.py
@@ -34,7 +34,7 @@ def update_model_version(model_name, version_name, label, model, artifact, works
     """
     client = Client()
 
-    if artifact is not None and len(artifact) > len(set(artifact)):
+    if artifact is not None and len(artifact) > len(set(map(lambda pair: pair[0], artifact))):
         raise click.BadParameter("cannot have duplicate artifact keys")
 
     try:
