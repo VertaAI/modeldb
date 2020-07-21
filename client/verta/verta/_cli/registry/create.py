14a5e2c2b563db2eb2379665824d44ee698af2b6
nhatsmrt <nhatsmrt@uw.edu>
2020-07-20 17:10:30 -0700

    Port implementation.

--- client/verta/verta/_cli/registry/create.py
+++ client/verta/verta/_cli/registry/create.py
@@ -3,6 +3,8 @@
 import click
 
 from .registry import registry
+from ... import Client
+
 
 @registry.group(name="create")
 def create():
@@ -39,4 +41,23 @@ def create_model(model_name, label, visibility, workspace):
 def create_model_version(model_name, version_name, label, model, artifact, workspace):
     """Create a new registeredmodelversion entry.
     """
-    pass
+    client = Client()
+
+    try:
+        registered_model = client.get_registered_model(name=model_name, workspace=workspace)
+    except ValueError:
+        raise click.BadParameter("model {} not found".format(model_name))
+
+    model_version = registered_model.get_or_create_version(name=version_name, labels=list(label))
+
+    if model is not None:
+        model_version.log_model(model, True)
+
+    if artifact is not None:
+        for (key, _) in artifact:
+            if key == "model":
+                raise click.BadParameter("the key \"model\" is reserved for model")
+
+        for (key, path) in artifact:
+            model_version.log_artifact(key, path, True)
+
