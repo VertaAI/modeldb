f3199a0c8e964192d28d34ff32b61aeb87f70c27
nhatsmrt <nhatsmrt@uw.edu>
2020-07-22 08:21:20 -0700

    Remove duplicate tests.

--- client/verta/tests/test_model_registry/test_model_version.py
+++ client/verta/tests/test_model_registry/test_model_version.py
@@ -119,27 +119,6 @@ class TestModelVersion:
         model_version.log_environment(env)
 
         model_version = registered_model.get_version(id=model_version.id)
-        assert(str(env) == str(Python._from_proto(model_version._msg)))
-
-    def test_del_environment(self, registered_model):
-        model_version = registered_model.get_or_create_version(name="my version")
-
-        reqs = Python.read_pip_environment()
-        env = Python(requirements=reqs)
-        model_version.log_environment(env)
-        model_version.del_environment()
-
-        model_version = registered_model.get_version(id=model_version.id)
-        assert not model_version.has_environment
-
-    def test_log_environment(self, registered_model):
-        model_version = registered_model.get_or_create_version(name="my version")
-
-        reqs = Python.read_pip_environment()
-        env = Python(requirements=reqs)
-        model_version.log_environment(env)
-
-        model_version = registered_model.get_version(id=model_version.id)
         assert str(env) == str(model_version.get_environment())
 
     def test_del_environment(self, registered_model):
