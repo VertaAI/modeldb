1eff5bd09e4f15de16c25f560bb7a17c04ae4652
nhatsmrt <nhatsmrt@uw.edu>
2020-07-22 13:33:42 -0700

    Add a small test.

---
+++ client/verta/tests/test_endpoint.py
@@ -0,0 +1,20 @@
+from verta._deployment import Endpoint
+from verta.deployment.strategies import DirectUpdateStrategy
+
+import time
+
+
+class TestEndpoint:
+    def test_update(self, experiment_run, model_for_deployment):
+        experiment_run.log_model_for_deployment(**model_for_deployment)
+        # TODO: remove hardcoding
+        endpoint = Endpoint(experiment_run._conn, experiment_run._conf, "Nhat_Pham", 449)
+        endpoint._path = "/string"
+
+        original_status = endpoint.get_status()
+        endpoint.update(experiment_run, DirectUpdateStrategy)
+
+        time.sleep(2) # wait for updating to complete
+        updated_status = endpoint.get_status()
+
+        assert original_status["date_updated"] != updated_status["date_updated"]
