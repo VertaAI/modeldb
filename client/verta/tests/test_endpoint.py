d737edac2b8358e92658b9db9126e6b8205eaf24
nhatsmrt <nhatsmrt@uw.edu>
2020-07-23 12:09:46 -0700

    Update test.

--- client/verta/tests/test_endpoint.py
+++ client/verta/tests/test_endpoint.py
@@ -1,11 +1,11 @@
 from verta._deployment import Endpoint
-from verta.deployment.strategies import DirectUpdateStrategy
+from verta.deployment.strategies import DirectUpdateStrategy, CanaryUpdateStrategy
 
 import time
 
 
 class TestEndpoint:
-    def test_update(self, experiment_run, model_for_deployment):
+    def test_update(self, experiment_run, model_for_deployment, experiment_run_2, model_for_deployment_2):
         experiment_run.log_model_for_deployment(**model_for_deployment)
         # TODO: remove hardcoding
         endpoint = Endpoint(experiment_run._conn, experiment_run._conf, "Nhat_Pham", 449)
@@ -17,3 +17,6 @@ class TestEndpoint:
         updated_status = endpoint.get_status()
 
         assert original_status["date_updated"] != updated_status["date_updated"]
+
+        experiment_run_2.log_model_for_deployment(**model_for_deployment_2)
+        endpoint.update(experiment_run_2, CanaryUpdateStrategy())
