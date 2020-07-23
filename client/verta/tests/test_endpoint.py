9c686047c460a60f17f1eb2c7385475a0277e882
nhatsmrt <nhatsmrt@uw.edu>
2020-07-23 14:13:15 -0700

    Remove hardcoding.

--- client/verta/tests/test_endpoint.py
+++ client/verta/tests/test_endpoint.py
@@ -8,10 +8,9 @@ import time
 
 
 class TestEndpoint:
-    def test_update(self, experiment_run, model_for_deployment):
+    def test_update(self, client, strs, experiment_run, model_for_deployment):
         experiment_run.log_model_for_deployment(**model_for_deployment)
-        # TODO: remove hardcoding
-        endpoint = Endpoint(experiment_run._conn, experiment_run._conf, "Nhat_Pham", 449)
+        endpoint = client.get_or_create_endpoint(path=strs[0])
 
         original_status = endpoint.get_status()
         endpoint.update(experiment_run, DirectUpdateStrategy())
