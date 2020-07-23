ba6d4eb93e6260f2dfe5ccc545525d5930509864
nhatsmrt <nhatsmrt@uw.edu>
2020-07-23 14:08:18 -0700

    Modify get stage

--- client/verta/verta/_deployment/endpoint.py
+++ client/verta/verta/_deployment/endpoint.py
@@ -83,7 +83,7 @@ class Endpoint(object):
         if not issubclass(type(strategy), _UpdateStrategy):
             raise TypeError("strategy must be an object from verta.deployment.strategies")
 
-        stage_id = self._get_or_create_active_stage()
+        stage_id = self._get_or_create_stage()
 
         # Create new build:
         url = "{}://{}/api/v1/deployment/workspace/{}/builds".format(
@@ -106,35 +106,36 @@ class Endpoint(object):
         response = _utils.make_request("PUT", url, self._conn, json=strategy._as_build_update_req_body(build_id))
         _utils.raise_for_http_error(response)
 
-    def _get_or_create_active_stage(self):
-        # Check if a stage exists:
+    def _get_or_create_stage(self, name="production"):
+        if name == "production":
+            # Check if a stage exists:
+            url = "{}://{}/api/v1/deployment/workspace/{}/endpoints/{}/stages".format(
+                self._conn.scheme,
+                self._conn.socket,
+                self.workspace,
+                self.id
+            )
+            response = _utils.make_request("GET", url, self._conn, params={})
+
+            _utils.raise_for_http_error(response)
+            response_json = response.json()
+
+            if response_json["stages"]:
+                return response_json["stages"][0]["id"]
+
+            # no stage found:
+            return self._create_stage("production")
+        else:
+            raise NotImplementedError # currently not supported other stages
+
+    def _create_stage(self, name="production"):
         url = "{}://{}/api/v1/deployment/workspace/{}/endpoints/{}/stages".format(
             self._conn.scheme,
             self._conn.socket,
             self.workspace,
             self.id
         )
-        response = _utils.make_request("GET", url, self._conn, params={})
-
-        _utils.raise_for_http_error(response)
-        response_json = response.json()
-
-        for stage in response_json["stages"]:
-            if stage["status"] == "active":
-                # found active stage
-                return stage["id"]
-
-        # no active stage found:
-        return self._create_stage()
-
-    def _create_stage(self):
-        url = "{}://{}/api/v1/deployment/workspace/{}/endpoints/{}/stages".format(
-            self._conn.scheme,
-            self._conn.socket,
-            self.workspace,
-            self.id
-        )
-        response = _utils.make_request("POST", url, self._conn, json={"name": "production"})
+        response = _utils.make_request("POST", url, self._conn, json={"name": name})
         _utils.raise_for_http_error(response)
         return response.json()["id"]
 
