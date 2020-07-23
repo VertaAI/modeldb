1d71857db4fe16ccfd5480f73db46e859a93951c
nhatsmrt <nhatsmrt@uw.edu>
2020-07-23 13:09:55 -0700

    Make a test stronger.

--- client/verta/tests/test_endpoint.py
+++ client/verta/tests/test_endpoint.py
@@ -1,3 +1,5 @@
+import pytest
+
 from verta._deployment import Endpoint
 from verta.deployment.strategies import DirectUpdateStrategy, CanaryUpdateStrategy
 from verta.deployment.update_rules import AverageLatencyThreshold
@@ -20,6 +22,12 @@ class TestEndpoint:
         assert original_status["date_updated"] != updated_status["date_updated"]
 
         strategy = CanaryUpdateStrategy(interval=1, step=0.5)
+        
+        with pytest.raises(RuntimeError) as excinfo:
+            endpoint.update(experiment_run, strategy)
+
+        assert "canary update strategy must have at least one rule" in str(excinfo.value)
+
         strategy.add_rule(AverageLatencyThreshold(0.8))
         endpoint.update(experiment_run, strategy)
         time.sleep(2) # wait for updating to complete
