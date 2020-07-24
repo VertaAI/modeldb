06b1e8b0080bdb90424e7c51471cad0701407469
Evgenii Lezhnin <eugene.lezhnin@quantumsoft.ru>
2020-07-24 15:22:48 +0700

    VR-5457 list endpoints

--- client/verta/verta/_cli/deployment/list.py
+++ client/verta/verta/_cli/deployment/list.py
@@ -21,7 +21,7 @@ def lst_endpoint(workspace):
     client = Client()
 
     endpoints = client.endpoints(workspace)
-    table_data = [['MODEL NAME', 'ID', 'DATE UPDATED']] + list(map(lambda endpoint: endpoint._get_info_list_by_id(), endpoints))
+    table_data = [['PATH', 'ID', 'DATE UPDATED']] + list(map(lambda endpoint: endpoint._get_info_list_by_id(), endpoints))
     table = AsciiTable(table_data)
     print(table.table)
     # TODO: call client.endpoints, display PATH, ID, and DATE UPDATED
