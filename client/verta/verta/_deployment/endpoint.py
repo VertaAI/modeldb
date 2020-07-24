b855ebb1f8ee2a24f217c4a460b2973cc8506591
Nhat Pham <nhatsmrt@uw.edu>
2020-07-24 09:37:16 -0700

    VR-5404 Endpoint.update() (#1096)
    * Work on endpoint update.
    
    * Initial implementation.
    
    * Rename endpoint to url
    
    * Fix stage getting.
    
    * Fix some small issues. Add a hard-coded test.
    
    * Reduce code in test with fixture.
    
    * Minor fixes.
    
    * Remove test.
    
    * Remove hard coding.
    
    * Update update.
    
    * Small fix.
    
    * Fix a bug in typechecking.
    
    * Change name of created stage.
    
    * Modify get stage
    
    * Small fix.

--- client/verta/verta/_deployment/endpoint.py
+++ client/verta/verta/_deployment/endpoint.py
@@ -2,9 +2,8 @@
 
 from __future__ import print_function
 
+from ..deployment.strategies import _UpdateStrategy
 from .._internal_utils import _utils
-
-from .. import deployment
 from .._tracking import experimentrun
 
 
@@ -87,9 +86,67 @@ class Endpoint(object):
         # TODO: iterate through response.json().get('endpoints', []) to find matching creator_request.path
 
     def update(self, run, strategy):
-        raise NotImplementedError
-        # TODO: check if isinstance(run, experimentrun.ExperimentRun)
-        # TODO: check if isinstance(strategy, deployment._UpdateStrategy)
+        if not isinstance(run, experimentrun.ExperimentRun):
+            raise TypeError("run must be an ExperimentRun")
+
+        if not isinstance(strategy, _UpdateStrategy):
+            raise TypeError("strategy must be an object from verta.deployment.strategies")
+
+        stage_id = self._get_or_create_stage()
+
+        # Create new build:
+        url = "{}://{}/api/v1/deployment/workspace/{}/builds".format(
+            self._conn.scheme,
+            self._conn.socket,
+            self.workspace
+        )
+        response = _utils.make_request("POST", url, self._conn, json={"run_id": run.id})
+        _utils.raise_for_http_error(response)
+        build_id = response.json()["id"]
+
+        # Update stages with new build
+        url = "{}://{}/api/v1/deployment/workspace/{}/endpoints/{}/stages/{}/update".format(
+            self._conn.scheme,
+            self._conn.socket,
+            self.workspace,
+            self.id,
+            stage_id
+        )
+        response = _utils.make_request("PUT", url, self._conn, json=strategy._as_build_update_req_body(build_id))
+        _utils.raise_for_http_error(response)
+
+    def _get_or_create_stage(self, name="production"):
+        if name == "production":
+            # Check if a stage exists:
+            url = "{}://{}/api/v1/deployment/workspace/{}/endpoints/{}/stages".format(
+                self._conn.scheme,
+                self._conn.socket,
+                self.workspace,
+                self.id
+            )
+            response = _utils.make_request("GET", url, self._conn, params={})
+
+            _utils.raise_for_http_error(response)
+            response_json = response.json()
+
+            if response_json["stages"]:
+                return response_json["stages"][0]["id"]
+
+            # no stage found:
+            return self._create_stage("production")
+        else:
+            raise NotImplementedError("currently not supported other stages")
+
+    def _create_stage(self, name="production"):
+        url = "{}://{}/api/v1/deployment/workspace/{}/endpoints/{}/stages".format(
+            self._conn.scheme,
+            self._conn.socket,
+            self.workspace,
+            self.id
+        )
+        response = _utils.make_request("POST", url, self._conn, json={"name": name})
+        _utils.raise_for_http_error(response)
+        return response.json()["id"]
 
     def update_from_config(self, filepath):
         raise NotImplementedError
