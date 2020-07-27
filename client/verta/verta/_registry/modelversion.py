f885d808be9a936c2cccb8e83d41296e380556e9
nhatsmrt <nhatsmrt@uw.edu>
2020-07-27 11:23:41 -0700

    Check if key is model in del_artifact

--- client/verta/verta/_registry/modelversion.py
+++ client/verta/verta/_registry/modelversion.py
@@ -208,6 +208,9 @@ class RegisteredModelVersion(_ModelDBEntity):
         return artifact_stream
 
     def del_artifact(self, key):
+        if key == "model":
+            raise ValueError("the key \"model\" is reserved for model; consider using del_model() instead")
+
         self._fetch_with_no_cache()
 
         ind = -1
