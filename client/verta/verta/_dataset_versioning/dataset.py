d0eb7b0b97bec9e8885db06c8453bd9a3f0ac528
Michael Liu <miliu@protonmail.com>
2020-07-27 11:30:03 -0700

    Create initial interface for objects

---
+++ client/verta/verta/_dataset_versioning/dataset.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function
+
+from .._tracking import entity
+
+from .._protos.public.modeldb import DatasetService_pb2 as _DatasetService
+
+from ..external import six
+
+from .._internal_utils import (
+    _utils,
+)
+
+
+class Dataset(entity._ModelDBEntity):
+    def __init__(self, conn, conf, msg):
+        super(Dataset, self).__init__(conn, conf, _DatasetService, "dataset", msg)
+
+    def __repr__(self):
+        raise NotImplementedError
+
+    @property
+    def name(self):
+        self._refresh_cache()
+        return self._msg.name
+
+    @property
+    def versions(self):
+        raise NotImplementedError
+
+    @classmethod
+    def _generate_default_name(cls):
+        return "Dataset {}".format(_utils.generate_default_name())
+
+    @classmethod
+    def _get_proto_by_id(cls, conn, id):
+        raise NotImplementedError
+
+    @classmethod
+    def _get_proto_by_name(cls, conn, name, workspace):
+        raise NotImplementedError
+
+    @classmethod
+    def _create_proto_internal(cls, conn, ctx, name, desc=None, tags=None, attrs=None, date_created=None, public_within_org=None):
+        raise NotImplementedError
+
+    def create_s3_version(self):  # TODO: same params as S3.__init__()
+        raise NotImplementedError
+        # TODO: create S3 blob
+        # TODO: hijack DatasetVersion creation flow to inject S3 blob??
+
+    def create_path_version(self):  # TODO: same params as Path.__init__()
+        raise NotImplementedError
---
+++ client/verta/verta/_dataset_versioning/datasetversion.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function
+
+import abc
+
+from .._tracking import entity
+
+from .._protos.public.modeldb import DatasetVersionService_pb2 as _DatasetVersionService
+
+from ..external import six
+
+from .._internal_utils import (
+    _utils,
+)
+
+
+@six.add_metaclass(abc.ABCMeta)
+class DatasetVersion(entity._ModelDBEntity):
+    def __init__(self, conn, conf, msg):
+        super(DatasetVersion, self).__init__(conn, conf, _DatasetVersionService, "dataset-version", msg)
+
+    def __repr__(self):
+        raise NotImplementedError
+
+    @property
+    def name(self):
+        self._refresh_cache()
+        return self._msg.name
+
+    @classmethod
+    def _generate_default_name(cls):
+        return "Proj {}".format(_utils.generate_default_name())
+
+    @classmethod
+    def _get_proto_by_id(cls, conn, id):
+        raise NotImplementedError
+
+    @classmethod
+    def _get_proto_by_name(cls, conn, name, workspace):
+        raise NotImplementedError
+
+    @classmethod
+    def _create_proto_internal(cls, conn, ctx, name, desc=None, tags=None, attrs=None, date_created=None, public_within_org=None):
+        raise NotImplementedError
