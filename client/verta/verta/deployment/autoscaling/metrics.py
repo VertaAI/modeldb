70cc76c1a2ef071e6fadaf6a726e1263876ade8f
Michael Liu <miliu@protonmail.com>
2020-07-28 12:47:43 -0700

    Don't bother with base class __init__

--- client/verta/verta/deployment/autoscaling/metrics.py
+++ client/verta/verta/deployment/autoscaling/metrics.py
@@ -7,8 +7,5 @@ from ...external import six
 
 @six.add_metaclass(abc.ABCMeta)
 class _AutoscalingMetric(object):
-    def __init__(self):
-        raise NotImplementedError
-
     def _as_json(self):
         raise NotImplementedError
