b5d7c7e3f5c3beaf54cf9b2f6f59dd58e0180af8
nhatsmrt <nhatsmrt@uw.edu>
2020-07-31 11:30:28 -0700

    Add a test for repr.

---
+++ client/verta/tests/test_dataset_versioning/test_dataset.py
@@ -0,0 +1,18 @@
+import verta
+import pytest
+
+
+class TestDataset:
+    def test_repr(self, client, created_datasets):
+        description = "this is a cool dataset"
+        tags = ["tag1, tag2"]
+        dataset = client.set_dataset2(desc=description, tags=tags)
+        created_datasets.append(dataset)
+
+        str_repr = repr(dataset)
+        assert "name: {}".format(dataset.name) in str_repr
+        assert "id: {}".format(dataset.id) in str_repr
+        assert "date created" in str_repr
+        assert "date updated" in str_repr
+        assert "description: {}".format(description) in str_repr
+        assert "tags: {}".format(tags) in str_repr
