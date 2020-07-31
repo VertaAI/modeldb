e72f4c1e69a757ec441226d6436ae0df0aca1108
Nhat Pham <nhatsmrt@uw.edu>
2020-07-31 13:39:33 -0700

    VR-5478 Add URLs to_ModelDBEntity.__repr__()s (#1160)
    * Initial work on url
    
    * Fix cyclic import.
    
    * Fix a small bug.
    
    * Fix a bug in getting workspace.
    
    * Do not check has field.
    
    * Fix some small typos.
    
    * URL of project now points to summary.
    
    * Remove experiment's url.

--- client/verta/verta/_registry/model.py
+++ client/verta/verta/_registry/model.py
@@ -3,7 +3,7 @@
 from __future__ import print_function
 
 from .._internal_utils._utils import NoneProtoResponse
-from .._tracking.entity import _ModelDBEntity
+from .._tracking.entity import _ModelDBEntity, _OSS_DEFAULT_WORKSPACE
 from .._tracking.context import _Context
 from .._internal_utils import _utils
 
@@ -40,6 +40,7 @@ class RegisteredModel(_ModelDBEntity):
 
         return '\n'.join((
             "name: {}".format(msg.name),
+            "url: {}://{}/{}/registry/{}".format(self._conn.scheme, self._conn.socket, self.workspace, self.id),
             "time created: {}".format(_utils.timestamp_to_str(int(msg.time_created))),
             "time updated: {}".format(_utils.timestamp_to_str(int(msg.time_updated))),
             "description: {}".format(msg.description),
@@ -51,6 +52,15 @@ class RegisteredModel(_ModelDBEntity):
     def name(self):
         self._refresh_cache()
         return self._msg.name
+
+    @property
+    def workspace(self):
+        self._refresh_cache()
+
+        if self._msg.workspace_id:
+            return self._get_workspace_name_by_id(self._msg.workspace_id)
+        else:
+            return _OSS_DEFAULT_WORKSPACE
 
     def get_or_create_version(self, name=None, desc=None, labels=None, id=None, time_created=None):
         """
