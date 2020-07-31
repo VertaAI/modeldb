2e0a507594d0b245764e3b29c3f251f12ed1cae0
nhatsmrt <nhatsmrt@uw.edu>
2020-07-30 17:42:32 -0700

    Add cleaning up.

--- client/verta/tests/test_dataset_versioning/test_dataset.py
+++ client/verta/tests/test_dataset_versioning/test_dataset.py
@@ -6,13 +6,15 @@ from verta._dataset_versioning.dataset import Dataset
 
 
 class TestDataset:
-    def test_create(self, client):
+    def test_create(self, client, created_datasets):
         dataset = client.set_dataset2()
         assert dataset
+        created_datasets.append(dataset)
 
         name = verta._internal_utils._utils.generate_default_name()
         dataset = client.create_dataset2(name)
         assert dataset
+        created_datasets.append(dataset)
 
         # TODO: Dataset can have duplicate names. Uncomment these checks when this issue is fixed:
         # with pytest.raises(requests.HTTPError) as excinfo:
@@ -21,13 +23,14 @@ class TestDataset:
         # assert "409" in excinfo_value
         # assert "already exists" in excinfo_value
 
-    def test_get(self, client):
+    def test_get(self, client, created_datasets):
         name = verta._internal_utils._utils.generate_default_name()
 
         with pytest.raises(ValueError):
             client.get_dataset2(name)
 
         dataset = client.set_dataset2(name)
+        created_datasets.append(dataset)
 
         assert dataset.id == client.get_dataset2(dataset.name).id
         assert dataset.id == client.get_dataset2(id=dataset.id).id
