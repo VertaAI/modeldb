a97e9e266a2139633c9b3e376a8ef67b2c11b96c
Nhat Pham <nhatsmrt@uw.edu>
2020-08-04 15:06:55 -0700

    VR-5591 Implement Dataset.__repr__ (#1196)
    * Implement repr.
    
    * Add a test for repr.
    
    * Switch date to time.
    
    * Small fix.
    
    * Fix a small redundant constant issue.
    
    * Fix a redundant deletion.

--- client/verta/tests/test_dataset_versioning/test_dataset.py
+++ client/verta/tests/test_dataset_versioning/test_dataset.py
@@ -31,3 +31,18 @@ class TestDataset:
 
         assert dataset.id == client.get_dataset2(dataset.name).id
         assert dataset.id == client.get_dataset2(id=dataset.id).id
+
+    def test_repr(self, client, created_datasets):
+        description = "this is a cool dataset"
+        tags = [u"tag1", u"tag2"]
+        dataset = client.set_dataset2(desc=description, tags=tags)
+        created_datasets.append(dataset)
+
+        str_repr = repr(dataset)
+
+        assert "name: {}".format(dataset.name) in str_repr
+        assert "id: {}".format(dataset.id) in str_repr
+        assert "time created" in str_repr
+        assert "time updated" in str_repr
+        assert "description: {}".format(description) in str_repr
+        assert "tags: {}".format(tags) in str_repr
