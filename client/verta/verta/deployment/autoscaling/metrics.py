7a9e8882825250e216c345db4e4abe5de84643dc
nhatsmrt <nhatsmrt@uw.edu>
2020-08-04 10:01:50 -0700

    Fix a metric.

--- client/verta/verta/deployment/autoscaling/metrics.py
+++ client/verta/verta/deployment/autoscaling/metrics.py
@@ -10,21 +10,33 @@ class _AutoscalingMetric(object):
     _METRIC_ID = 0
     _NAME = ""
 
-    def __init__(self, value):
-        self._value = value
-
-    def _as_json(self):
+    def _as_dict(self):
         return {
             "metric_id": self._METRIC_ID,
-            "parameters": [
-                {
-                    "name": self._NAME,
-                    "value": self._value
-                }
-            ]
+            "name": self._NAME,
+            "parameters": self._parameters_dict()
         }
+
+    def _parameters_dict(self):
+        raise NotImplementedError
 
 
 class CpuUtilization(_AutoscalingMetric):
     _METRIC_ID = 1001
     _NAME = "cpu_utilization"
+
+    def __init__(self, cpu_target, cpu_threshold):
+        self._cpu_target = cpu_target
+        self._cpu_threshold = cpu_threshold
+
+    def _parameters_dict(self):
+        return [
+            {
+                "name": "cpu_target",
+                "value": self._cpu_target
+            },
+            {
+                "name": "cpu_threshold",
+                "value": self._cpu_threshold
+            }
+        ]
