241604089017f21ff74c41ae3788a7fd16e69c6f
nhatsmrt <nhatsmrt@uw.edu>
2020-08-05 14:15:18 -0700

    Fix 2 tests.

--- client/verta/tests/test_endpoint/test_endpoint.py
+++ client/verta/tests/test_endpoint/test_endpoint.py
@@ -183,12 +183,12 @@ class TestEndpoint:
                 "progress_step": 0.05,
                 "progress_interval_seconds": 30,
                 "rules": [
-                    {"parent_name": "latency",
+                    {"rule": "latency",
                      "rule_parameters": [
                          {"name": "latency_avg",
                           "value": "0.1"}
                     ]},
-                    {"parent_name": "error_rate",
+                    {"rule": "error_rate",
                      "rule_parameters": [
                         {"name": "error_rate",
                          "value": "1"}
@@ -225,12 +225,12 @@ class TestEndpoint:
                 "progress_step": 0.05,
                 "progress_interval_seconds": 30,
                 "rules": [
-                    {"parent_name": "latency",
+                    {"rule": "latency",
                      "rule_parameters": [
                          {"name": "latency_avg",
                           "value": "0.1"}
                     ]},
-                    {"parent_name": "error_rate",
+                    {"rule": "error_rate",
                      "rule_parameters": [
                         {"name": "error_rate",
                          "value": "1"}
