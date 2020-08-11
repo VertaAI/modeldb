0662c3ccc0ddc3bfec18fe4afc3f23d8bf594bd4
Ravi Shetye <ravi@verta.ai>
2020-08-10 18:03:36 -0700

    protos from private (#1263)
    workspace_id in Organization & VertaInfo
    patch api for registry
    audit-service
    default_registered_model_collaborator_type

---
+++ client/scala/src/main/scala/ai/verta/swagger/_public/uac/audit/model/ProtobufAny.scala
@@ -0,0 +1,42 @@
+// THIS FILE IS AUTO-GENERATED. DO NOT EDIT
+package ai.verta.swagger._public.uac.audit.model
+
+import scala.util.Try
+
+import net.liftweb.json._
+
+import ai.verta.swagger.client.objects._
+
+case class ProtobufAny (
+  type_url: Option[String] = None,
+  value: Option[String] = None
+) extends BaseSwagger {
+  def toJson(): JValue = ProtobufAny.toJson(this)
+}
+
+object ProtobufAny {
+  def toJson(obj: ProtobufAny): JObject = {
+    new JObject(
+      List[Option[JField]](
+        obj.type_url.map(x => JField("type_url", JString(x))),
+        obj.value.map(x => JField("value", JString(x)))
+      ).flatMap(x => x match {
+        case Some(y) => List(y)
+        case None => Nil
+      })
+    )
+  }
+
+  def fromJson(value: JValue): ProtobufAny =
+    value match {
+      case JObject(fields) => {
+        val fieldsMap = fields.map(f => (f.name, f.value)).toMap
+        ProtobufAny(
+          // TODO: handle required
+          type_url = fieldsMap.get("type_url").map(JsonConverter.fromJsonString),
+          value = fieldsMap.get("value").map(JsonConverter.fromJsonString)
+        )
+      }
+      case _ => throw new IllegalArgumentException(s"unknown type ${value.getClass.toString}")
+    }
+}
--- client/scala/src/main/scala/ai/verta/swagger/_public/uac/model/UacOrganization.scala
+++ client/scala/src/main/scala/ai/verta/swagger/_public/uac/model/UacOrganization.scala
@@ -13,6 +13,7 @@ case class UacOrganization (
   created_timestamp: Option[BigInt] = None,
   default_dataset_collaborator_type: Option[CollaboratorTypeEnumCollaboratorType] = None,
   default_endpoint_collaborator_type: Option[CollaboratorTypeEnumCollaboratorType] = None,
+  default_registered_model_collaborator_type: Option[CollaboratorTypeEnumCollaboratorType] = None,
   default_repo_collaborator_type: Option[CollaboratorTypeEnumCollaboratorType] = None,
   description: Option[String] = None,
   global_can_deploy: Option[TernaryEnumTernary] = None,
@@ -21,7 +22,8 @@ case class UacOrganization (
   name: Option[String] = None,
   owner_id: Option[String] = None,
   short_name: Option[String] = None,
-  updated_timestamp: Option[BigInt] = None
+  updated_timestamp: Option[BigInt] = None,
+  workspace_id: Option[String] = None
 ) extends BaseSwagger {
   def toJson(): JValue = UacOrganization.toJson(this)
 }
@@ -33,6 +35,7 @@ object UacOrganization {
         obj.created_timestamp.map(x => JField("created_timestamp", JInt(x))),
         obj.default_dataset_collaborator_type.map(x => JField("default_dataset_collaborator_type", ((x: CollaboratorTypeEnumCollaboratorType) => CollaboratorTypeEnumCollaboratorType.toJson(x))(x))),
         obj.default_endpoint_collaborator_type.map(x => JField("default_endpoint_collaborator_type", ((x: CollaboratorTypeEnumCollaboratorType) => CollaboratorTypeEnumCollaboratorType.toJson(x))(x))),
+        obj.default_registered_model_collaborator_type.map(x => JField("default_registered_model_collaborator_type", ((x: CollaboratorTypeEnumCollaboratorType) => CollaboratorTypeEnumCollaboratorType.toJson(x))(x))),
         obj.default_repo_collaborator_type.map(x => JField("default_repo_collaborator_type", ((x: CollaboratorTypeEnumCollaboratorType) => CollaboratorTypeEnumCollaboratorType.toJson(x))(x))),
         obj.description.map(x => JField("description", JString(x))),
         obj.global_can_deploy.map(x => JField("global_can_deploy", ((x: TernaryEnumTernary) => TernaryEnumTernary.toJson(x))(x))),
@@ -41,7 +44,8 @@ object UacOrganization {
         obj.name.map(x => JField("name", JString(x))),
         obj.owner_id.map(x => JField("owner_id", JString(x))),
         obj.short_name.map(x => JField("short_name", JString(x))),
-        obj.updated_timestamp.map(x => JField("updated_timestamp", JInt(x)))
+        obj.updated_timestamp.map(x => JField("updated_timestamp", JInt(x))),
+        obj.workspace_id.map(x => JField("workspace_id", JString(x)))
       ).flatMap(x => x match {
         case Some(y) => List(y)
         case None => Nil
@@ -58,6 +62,7 @@ object UacOrganization {
           created_timestamp = fieldsMap.get("created_timestamp").map(JsonConverter.fromJsonInteger),
           default_dataset_collaborator_type = fieldsMap.get("default_dataset_collaborator_type").map(CollaboratorTypeEnumCollaboratorType.fromJson),
           default_endpoint_collaborator_type = fieldsMap.get("default_endpoint_collaborator_type").map(CollaboratorTypeEnumCollaboratorType.fromJson),
+          default_registered_model_collaborator_type = fieldsMap.get("default_registered_model_collaborator_type").map(CollaboratorTypeEnumCollaboratorType.fromJson),
           default_repo_collaborator_type = fieldsMap.get("default_repo_collaborator_type").map(CollaboratorTypeEnumCollaboratorType.fromJson),
           description = fieldsMap.get("description").map(JsonConverter.fromJsonString),
           global_can_deploy = fieldsMap.get("global_can_deploy").map(TernaryEnumTernary.fromJson),
@@ -66,7 +71,8 @@ object UacOrganization {
           name = fieldsMap.get("name").map(JsonConverter.fromJsonString),
           owner_id = fieldsMap.get("owner_id").map(JsonConverter.fromJsonString),
           short_name = fieldsMap.get("short_name").map(JsonConverter.fromJsonString),
-          updated_timestamp = fieldsMap.get("updated_timestamp").map(JsonConverter.fromJsonInteger)
+          updated_timestamp = fieldsMap.get("updated_timestamp").map(JsonConverter.fromJsonInteger),
+          workspace_id = fieldsMap.get("workspace_id").map(JsonConverter.fromJsonString)
         )
       }
       case _ => throw new IllegalArgumentException(s"unknown type ${value.getClass.toString}")
