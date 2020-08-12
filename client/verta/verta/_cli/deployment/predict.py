81c5f8b74dba54ee67323a13d310d8e132fef411
nhatsmrt <nhatsmrt@uw.edu>
2020-08-12 13:33:45 -0700

    Implement CLI endpoint predict and add test.

---
+++ client/verta/verta/_cli/deployment/predict.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+import click
+import json
+
+from .deployment import deployment
+from ... import Client
+
+
+@deployment.group()
+def predict():
+    pass
+
+
+@predict.command(name="endpoint")
+@click.argument("path", nargs=1, required=True)
+@click.option("--data", "-d", help="Input for prediction. Must be a valid JSON string.")
+@click.option("--workspace", "-w", help="Workspace to use.")
+def predict_endpoint(path, data, workspace):
+    if not data:
+        raise click.BadParameter("--data must be provided.")  # maybe make this an argument instead?
+
+    client = Client()
+
+    try:
+        endpoint = client.get_endpoint(path=path, workspace=workspace)
+    except ValueError:
+        raise click.BadParameter("endpoint with path {} not found".format(path))
+
+    deployed_model = endpoint.get_deployed_model()
+    click.echo(deployed_model.predict(json.loads(data)))
