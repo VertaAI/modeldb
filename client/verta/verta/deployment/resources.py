ec61238badd274668eda57cc701ec9203f404738
nhatsmrt <nhatsmrt@uw.edu>
2020-08-13 09:59:06 -0700

    Fix docstring.

--- client/verta/verta/deployment/resources.py
+++ client/verta/verta/deployment/resources.py
@@ -54,8 +54,10 @@ class Memory(_Resource):
     """
     Amount of memory allowed for Endpoint.
 
-    Must be a string representing a plain integer or a fixed-point integer with suffixes
-    "E, P, T, G, M, K, Ei, Pi, Ti, Gi, Mi, Ki, for example: 128974848, 129e6, 129M, 123Mi"
+    Must be a string representing a plain integer or a fixed-point integer with one of the following suffixes:
+    "E, P, T, G, M, K, Ei, Pi, Ti, Gi, Mi, Ki."
+
+    For example: "128974848", "129e6", "129M", "123Mi"
 
     """
     memory_err_msg = "`memory` must be a string representing a plain integer or a fixed-point integer with suffixes " \
