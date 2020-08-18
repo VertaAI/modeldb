2250b52bfef4fabcc16ca60c25e3b6df15da3ff3
nhatsmrt <nhatsmrt@uw.edu>
2020-08-18 15:13:51 -0700

    Add test for org public project.

--- client/verta/tests/test_sharing.py
+++ client/verta/tests/test_sharing.py
@@ -9,3 +9,13 @@ class TestSharing:
 
         assert client_2.get_project(id=project.id)
         assert client_2.get_project(name=project.name)
+
+    def test_share_org_public_project(self, client, organization, client_2, email_2):
+        project_name = _utils.generate_default_name()
+        project = client.create_project(project_name, workspace=organization.name, public_within_org=True)
+
+        organization.add_member(email_2)
+        project._add_collaborator(email=email_2)
+
+        assert client_2.get_project(id=project.id)
+        assert client_2.get_project(name=project.name)
