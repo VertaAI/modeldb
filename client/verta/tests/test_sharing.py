f8a5c412863700c6305f28a809c4c0c16fde6f36
nhatsmrt <nhatsmrt@uw.edu>
2020-08-18 15:32:46 -0700

    Fix + add more tests.

--- client/verta/tests/test_sharing.py
+++ client/verta/tests/test_sharing.py
@@ -1,6 +1,7 @@
 import pytest
 import requests
 
+
 from verta._internal_utils import _utils
 
 
@@ -13,26 +14,36 @@ class TestSharing:
         assert client_2.get_project(id=project.id)
         assert client_2.get_project(name=project.name)
 
-    def test_share_org_public_project(self, client, organization, client_2, email_2):
+    def test_org_public_project(self, client, organization, client_2, email_2):
         project_name = _utils.generate_default_name()
         project = client.create_project(project_name, workspace=organization.name, public_within_org=True)
 
         organization.add_member(email_2)
-        project._add_collaborator(email=email_2)
 
         assert client_2.get_project(id=project.id)
         assert client_2.get_project(name=project.name, workspace=organization.name)
 
-    def test_share_org_project_not_public_error(self, client, organization, client_2, email_2):
+    def test_non_org_public_project_access_error(self, client, organization, client_2, email_2):
+        project_name = _utils.generate_default_name()
+        project = client.create_project(project_name, workspace=organization.name, public_within_org=False)
+
+        organization.add_member(email_2)
+
+        # Shouldn't be able to access:
+        with pytest.raises(requests.HTTPError) as excinfo:
+            client_2.get_project(id=project.id)
+
+        excinfo_value = str(excinfo.value).strip()
+        assert "403" in excinfo_value
+        assert "Access Denied" in excinfo_value
+
+
+    def test_share_org_projectr(self, client, organization, client_2, email_2):
         project_name = _utils.generate_default_name()
         project = client.create_project(project_name, workspace=organization.name, public_within_org=False)
 
         organization.add_member(email_2)
         project._add_collaborator(email=email_2)
 
-        with pytest.raises(requests.HTTPError) as excinfo:
-            project._add_collaborator(email=email_2)
-
-        excinfo_value = str(excinfo.value).strip()
-        assert "403" in excinfo_value
-        assert "Access Denied" in excinfo_value
+        assert client_2.get_project(id=project.id)
+        assert client_2.get_project(name=project.name, workspace=organization.name)
