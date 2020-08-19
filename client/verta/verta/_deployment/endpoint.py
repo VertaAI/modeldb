f53fe72fc87582d34cb565645d3450a31f9e8e87
evgeniilezhnin <eugene.lezhnin@quantumsoft.ru>
2020-08-19 20:54:45 +0700

    Revert "VR-6046 url endpoint (#1355)"
    This reverts commit 4ee4daa3afcce91d6ab2624f11b72806be4cebfd.

--- client/verta/verta/_deployment/endpoint.py
+++ client/verta/verta/_deployment/endpoint.py
@@ -58,8 +58,6 @@ class Endpoint(object):
 
         return '\n'.join((
             "path: {}".format(data['creator_request']['path']),
-            "url: {}://{}/{}/endpoints/{}/summary".format(self._conn.scheme, self._conn.socket,
-                                                          self.workspace, self.id),
             "id: {}".format(self.id),
             "curl: {}".format(curl),
             "status: {}".format(status["status"]),
