b3bfcb395b31ca9b65118f2d6564e089d840346c
Michael Liu <miliu@protonmail.com>
2020-08-19 20:19:58 -0700

    VR-6031: Implement ModelVersion.fetch_artifacts() (#1352)
    * Add test
    
    * Speed up test
    
    * Move _cache(), _get_cached() to _DeployableEntity
    
    * Move fetch_artifacts() to _DeployableEntity
    
    * Set default artifact_type to match signatures
    
    * Remove dependence on ExperimentRun
    
    * Handle differing returns for _get_artifact()
    
    * Fix remaining import
    
    * Remove unused imports
    
    * Remove excess newlines
    
    * Expand comment about return handling
    
    * Reorganize test functions

--- client/verta/verta/_tracking/deployable_entity.py
+++ client/verta/verta/_tracking/deployable_entity.py
@@ -2,10 +2,14 @@
 
 from __future__ import print_function
 
+import abc
 import copy
 import glob
 import os
+import shutil
 import sys
+import tarfile
+import tempfile
 import zipfile
 
 import requests
@@ -25,7 +29,15 @@ from ..external import six
 # location in DeploymentService model container
 _CUSTOM_MODULES_DIR = os.environ.get('VERTA_CUSTOM_MODULES_DIR', "/app/custom_modules/")
 
+# for caching files
+_CACHE_DIR = os.path.join(
+    os.path.expanduser("~"),
+    ".verta",
+    "cache",
+)
 
+
+@six.add_metaclass(abc.ABCMeta)
 class _DeployableEntity(_ModelDBEntity):
     @property
     def _histogram_endpoint(self):
@@ -34,6 +46,170 @@ class _DeployableEntity(_ModelDBEntity):
             self._conn.socket,
             self.id,
         )
+
+    def _cache_file(self, filename, contents):
+        """
+        Caches `contents` to `filename` within ``_CACHE_DIR``.
+
+        If `contents` represents a ZIP file, then it will be unzipped, and the path to the target
+        directory will be returned.
+
+        Parameters
+        ----------
+        filename : str
+            Filename within ``_CACHE_DIR`` to write to.
+        contents : bytes
+            Contents to be cached.
+
+        Returns
+        -------
+        str
+            Full path to cached contents.
+
+        """
+        # write contents to temporary file
+        with tempfile.NamedTemporaryFile(delete=False) as tempf:
+            tempf.write(contents)
+            tempf.flush()  # flush object buffer
+            os.fsync(tempf.fileno())  # flush OS buffer
+
+        name, extension = os.path.splitext(filename)
+        if extension == '.zip':
+            temp_path = tempfile.mkdtemp()
+
+            with zipfile.ZipFile(tempf.name, 'r') as zipf:
+                zipf.extractall(temp_path)
+            os.remove(tempf.name)
+        elif extension == '.tgz':
+            temp_path = tempfile.mkdtemp()
+
+            with tarfile.open(tempf.name, 'r:gz') as tarf:
+                tarf.extractall(temp_path)
+            os.remove(tempf.name)
+        elif extension == '.tar':
+            temp_path = tempfile.mkdtemp()
+
+            with tarfile.open(tempf.name, 'r') as tarf:
+                tarf.extractall(temp_path)
+            os.remove(tempf.name)
+        elif extension == '.gz' and os.path.splitext(name)[1] == '.tar':
+            name = os.path.splitext(name)[0]
+
+            temp_path = tempfile.mkdtemp()
+
+            with tarfile.open(tempf.name, 'r:gz') as tarf:
+                tarf.extractall(temp_path)
+            os.remove(tempf.name)
+        else:
+            name = filename
+            temp_path = tempf.name
+
+        path = os.path.join(_CACHE_DIR, name)
+
+        # create intermediate dirs
+        try:
+            os.makedirs(os.path.dirname(path))
+        except OSError:  # already exists
+            pass
+
+        # move written contents to cache location
+        shutil.move(temp_path, path)
+
+        return path
+
+    def _get_cached_file(self, filename):
+        name, extension = os.path.splitext(filename)
+        if extension == '.zip':
+            pass
+        elif extension == '.tgz':
+            pass
+        elif extension == '.tar':
+            pass
+        elif extension == '.gz' and os.path.splitext(name)[1] == '.tar':
+            name = os.path.splitext(name)[0]
+        else:
+            name = filename
+
+        path = os.path.join(_CACHE_DIR, name)
+        return path if os.path.exists(path) else None
+
+    @abc.abstractmethod
+    def _get_artifact(self, key):
+        raise NotImplementedError
+
+    def fetch_artifacts(self, keys):
+        """
+        Downloads artifacts that are associated with a class model.
+
+        Parameters
+        ----------
+        keys : list of str
+            Keys of artifacts to download.
+
+        Returns
+        -------
+        dict of str to str
+            Map of artifacts' keys to their cache filepaths—for use as the ``artifacts`` parameter
+            to a Verta class model.
+
+        Examples
+        --------
+        .. code-block:: python
+
+            run.log_artifact("weights", open("weights.npz", 'rb'))
+            # upload complete (weights)
+            run.log_artifact("text_embeddings", open("embedding.csv", 'rb'))
+            # upload complete (text_embeddings)
+            artifact_keys = ["weights", "text_embeddings"]
+            artifacts = run.fetch_artifacts(artifact_keys)
+            artifacts
+            # {'weights': '/Users/convoliution/.verta/cache/artifacts/50a9726b3666d99aea8af006cf224a7637d0c0b5febb3b0051192ce1e8615f47/weights.npz',
+            #  'text_embeddings': '/Users/convoliution/.verta/cache/artifacts/2d2d1d809e9bce229f0a766126ae75df14cadd1e8f182561ceae5ad5457a3c38/embedding.csv'}
+            ModelClass(artifacts=artifacts).predict(["Good book.", "Bad book!"])
+            # [0.955998517288053, 0.09809996313422353]
+            run.log_model(ModelClass, artifacts=artifact_keys)
+            # upload complete (custom_modules.zip)
+            # upload complete (model.pkl)
+            # upload complete (model_api.json)
+
+        """
+        if not (isinstance(keys, list)
+                and all(isinstance(key, six.string_types) for key in keys)):
+            raise TypeError("`keys` must be list of str, not {}".format(type(keys)))
+
+        # validate that `keys` are actually logged
+        self._refresh_cache()
+        existing_artifact_keys = {artifact.key for artifact in self._msg.artifacts}
+        unlogged_artifact_keys = set(keys) - existing_artifact_keys
+        if unlogged_artifact_keys:
+            raise ValueError("`keys` contains keys that have not been logged: {}".format(sorted(unlogged_artifact_keys)))
+
+        # get artifact checksums
+        paths = {artifact.key: artifact.path
+                 for artifact in self._msg.artifacts}
+
+        artifacts = dict()
+        for key in keys:
+            filename = os.path.join("artifacts", paths[key])
+
+            # check cache, otherwise write to cache
+            #     "try-get-then-create" can lead multiple threads trying to write to the cache
+            #     simultaneously, but artifacts being cached at a particular location should be
+            #     identical, so multiple writes would be idempotent.
+            path = self._get_cached_file(filename)
+            if path is None:
+                contents = self._get_artifact(key)
+                if isinstance(contents, tuple):
+                    # ExperimentRun._get_artifact() returns two values (contents, path_only)
+                    # whereas ModelVersion._get_artifact() returns one (contents), so until
+                    # their implementations are unified, this check is to handle the difference.
+                    contents, _ = contents  # TODO: raise error if path_only
+
+                path = self._cache_file(filename, contents)
+
+            artifacts.update({key: path})
+
+        return artifacts
 
     def _custom_modules_as_artifact(self, paths=None):
         if isinstance(paths, six.string_types):
