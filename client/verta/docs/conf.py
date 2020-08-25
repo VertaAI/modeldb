751e20facd0f7ad3029bb8cadcb7a582af5f32db
Michael Liu <miliu@protonmail.com>
2020-08-25 16:58:48 -0700

    Fix typo

--- client/verta/docs/conf.py
+++ client/verta/docs/conf.py
@@ -134,7 +134,7 @@ html_theme_options = {
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
-html_favicon = '_status/images/favicon.ico'
+html_favicon = '_static/images/favicon.ico'
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
