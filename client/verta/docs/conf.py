28db38a2d1430c04006ecdba0296477d146053fe
Michael Liu <miliu@protonmail.com>
2020-08-25 16:45:36 -0700

    Add favicon

--- client/verta/docs/conf.py
+++ client/verta/docs/conf.py
@@ -134,7 +134,7 @@ html_theme_options = {
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
-html_favicon = '../../../webapp/client/public/favicon.svg'
+html_favicon = 'images/favicon.ico'
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
