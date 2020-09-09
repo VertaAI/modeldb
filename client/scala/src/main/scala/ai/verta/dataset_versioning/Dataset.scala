302b640f3003b77cf0e163db8c75b0d13fe00aea
nhatsmrt <nhatsmrt@uw.edu>
2020-09-09 10:48:06 -0700

    Implement creating version.

--- client/scala/src/main/scala/ai/verta/dataset_versioning/Dataset.scala
+++ client/scala/src/main/scala/ai/verta/dataset_versioning/Dataset.scala
@@ -1,7 +1,13 @@
 package ai.verta.dataset_versioning
 
+import scala.util.{Try, Success, Failure}
+import scala.concurrent.ExecutionContext
+
+import net.liftweb.json._
+
+import ai.verta.blobs.dataset.{PathBlob, S3, S3Location}
 import ai.verta.swagger._public.modeldb.model._
-import ai.verta.swagger._public.modeldb.versioning.model._
+// import ai.verta.swagger._public.modeldb.versioning.model._
 import ai.verta.swagger.client.ClientSet
 
 /** Represents a ModelDB dataset.
@@ -12,4 +18,42 @@ class Dataset(private val clientSet: ClientSet, private val dataset: ModeldbData
 
   /** Name of the dataset. */
   def name: String = dataset.name.get
+
+  private def convertModel(
+    datasetMessage: ai.verta.swagger._public.modeldb.versioning.model.VersioningDatasetBlob
+  ): VersioningDatasetBlob = modelFromJson(modelToJson(datasetMessage))
+
+  private def modelToJson: ai.verta.swagger._public.modeldb.versioning.model.VersioningDatasetBlob => JObject =
+    ai.verta.swagger._public.modeldb.versioning.model.VersioningDatasetBlob.toJson _
+
+  private def modelFromJson: JObject => VersioningDatasetBlob =
+    VersioningDatasetBlob.fromJson _
+
+  /** Creates path dataset version.
+   *  @param paths Dataset version paths.
+   *  @return Dataset version from paths.
+   */
+  def createPathVersion(paths: List[String])(implicit ec: ExecutionContext): Try[DatasetVersion] = {
+    for (
+      pathBlob <- PathBlob(paths);
+      response <- clientSet.datasetVersionService.DatasetVersionService_createDatasetVersion(ModeldbCreateDatasetVersion(
+        dataset_id = Some(id),
+        dataset_blob = Some(convertModel(PathBlob.toVersioningBlob(pathBlob).dataset.get))
+      ))
+    ) yield new DatasetVersion(clientSet, this, response.dataset_version.get)
+  }
+
+  /** Creates S3 dataset version.
+   *  @param paths S3 locations.
+   *  @return Dataset version from S3 locations.
+   */
+  def createS3Version(locations: List[S3Location])(implicit ec: ExecutionContext): Try[DatasetVersion] = {
+    for (
+      s3Blob <- S3(locations);
+      response <- clientSet.datasetVersionService.DatasetVersionService_createDatasetVersion(ModeldbCreateDatasetVersion(
+        dataset_id = Some(id),
+        dataset_blob = Some(convertModel(S3.toVersioningBlob(s3Blob).dataset.get))
+      ))
+    ) yield new DatasetVersion(clientSet, this, response.dataset_version.get)
+  }
 }
---
+++ client/scala/src/main/scala/ai/verta/dataset_versioning/DatasetVersion.scala
@@ -0,0 +1,15 @@
+package ai.verta.dataset_versioning
+
+import ai.verta.blobs.dataset.PathBlob
+import ai.verta.swagger._public.modeldb.model._
+import ai.verta.swagger._public.modeldb.versioning.model._
+import ai.verta.swagger.client.ClientSet
+
+/** Represents a ModelDB dataset version from path.
+ */
+class DatasetVersion(
+  private val clientSet: ClientSet,
+  private val dataset: Dataset,
+  private val datasetVersion: ModeldbDatasetVersion
+) {
+}
