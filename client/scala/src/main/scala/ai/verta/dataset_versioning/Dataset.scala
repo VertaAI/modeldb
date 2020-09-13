246947f6f2f872b77875f7309c04561440c20e69
Nhat Pham <nhatsmrt@uw.edu>
2020-09-12 17:41:57 -0700

    VR-6550  Enable Dataset CRD for Scala Client (#1463)
    * Implement CRD methods for dataset.
    
    * Add documentation.
    
    * Remove redundant import.
    
    * Small change.
    
    * Use new workspace resolution.
    
    * Update client/scala/src/main/scala/ai/verta/client/Client.scala
    
    Co-authored-by: conradoverta <53195685+conradoverta@users.noreply.github.com>

---
+++ client/scala/src/main/scala/ai/verta/dataset_versioning/Dataset.scala
@@ -0,0 +1,14 @@
+package ai.verta.dataset_versioning
+
+import ai.verta.swagger._public.modeldb.model._
+import ai.verta.swagger.client.ClientSet
+
+/** Represents a ModelDB dataset.
+ */
+class Dataset(private val clientSet: ClientSet, private val dataset: ModeldbDataset) {
+  /** ID of the dataset. */
+  def id: String = dataset.id.get
+
+  /** Name of the dataset. */
+  def name: String = dataset.name.get
+}
---
+++ client/scala/src/test/scala/ai/verta/dataset_versioning/TestDataset.scala
@@ -0,0 +1,64 @@
+package ai.verta.dataset_versioning
+
+import ai.verta.client._
+import ai.verta.dataset_versioning._
+
+import scala.concurrent.ExecutionContext
+import scala.language.reflectiveCalls
+import scala.util.{Try, Success, Failure}
+
+import org.scalatest.FunSuite
+import org.scalatest.Assertions._
+
+class TestDataset extends FunSuite {
+  implicit val ec = ExecutionContext.global
+
+  def fixture =
+    new {
+        val client = new Client(ClientConnection.fromEnvironment())
+        val dataset = client.getOrCreateDataset("my dataset").get
+    }
+
+  def cleanup(
+    f: AnyRef{val client: Client; val dataset: Dataset}
+  ) = {
+    f.client.deleteDataset(f.dataset.id)
+    f.client.close()
+  }
+
+  test("retrieve dataset by id") {
+    val f = fixture
+
+    try {
+      assert(f.client.getDatasetById(f.dataset.id).get.id == f.dataset.id)
+    } finally {
+      cleanup(f)
+    }
+  }
+
+  test("retrieve dataset by name") {
+    val f = fixture
+
+    try {
+      assert(f.client.getDatasetByName(f.dataset.name).get.id == f.dataset.id)
+    } finally {
+      cleanup(f)
+    }
+  }
+
+  test("retrieve dataset with wrong name or id should fail") {
+    val f = fixture
+
+    try {
+      val getByNameAttempt = f.client.getDatasetByName("wrong-name")
+      assert(getByNameAttempt match {
+        case Failure(e) => e.getMessage contains "not found"
+      })
+
+      val getByIdAttempt = f.client.getDatasetById("wrong-id")
+      assert(getByIdAttempt.isFailure) // message differs in OSS and dev setup.
+    } finally {
+      cleanup(f)
+    }
+  }
+}
