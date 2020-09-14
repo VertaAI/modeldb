58a4b08104bd8c2db914626c7932613f5cd5d052
nhatsmrt <nhatsmrt@uw.edu>
2020-09-14 13:34:44 -0700

    Extract entity.

--- client/scala/src/main/scala/ai/verta/blobs/dataset/AtlasHiveDatasetBlob.scala
+++ client/scala/src/main/scala/ai/verta/blobs/dataset/AtlasHiveDatasetBlob.scala
@@ -52,11 +52,22 @@ object AtlasHiveDatasetBlob {
       case JObject(fields) => {
         val fieldsMap = fields.map(f => (f.name, f.value)).toMap
 
-        val attributesMap = fieldsMap.get("attributes") match {
+        val entity = fieldsMap.get("entities").map(
+          (x: JValue) x match {
+            case JArray(elements) => elements.get(0)
+            case _ => throw new IllegalArgumentException(s"unknown type ${x.getClass.toString}")
+          }
+        )
+
+        val entityMap = entity.match {
+          case JObject(entityFields) => entityFields.map(f => (f.name, f.value)).toMap
+        }
+
+        val attributesMap = entityMap.get("attributes") match {
           case Some(JObject(attributes)) => attributes.map(f => (f.name, f.value)).toMap
         }
 
-        val relationshipAttributesMap = fieldsMap.get("relationshipAttributes") match {
+        val relationshipAttributesMap = entityMap.get("relationshipAttributes") match {
           case Some(JObject(relationshipAttributes)) => relationshipAttributes.map(f => (f.name, f.value)).toMap
         }
         val tableName: String = attributesMap.get("name").map(JsonConverter.fromJsonString).get
