f46f8aabbb412c08d7206bb7d0164846ab98f897
nhatsmrt <nhatsmrt@uw.edu>
2020-09-14 22:40:05 -0700

    Update test.

--- client/scala/src/test/scala/ai/verta/blobs/dataset/TestAtlasHiveDatasetBlob.scala
+++ client/scala/src/test/scala/ai/verta/blobs/dataset/TestAtlasHiveDatasetBlob.scala
@@ -15,14 +15,23 @@ class TestAtlasHiveDatasetBlob extends FunSuite {
   implicit val ec = ExecutionContext.global
 
   test("Atlas hive blob should save the correct query and connection") {
-    val guid: String = sys.env.get("GUID").get
+    val guid = sys.env.get("GUID").get
     val expectedNumRecords: BigInt = 8279779
-    val expectedDatabaseName: String = "default"
-    val expectedTableName: String = "trip_details_by_zone"
-    val expectedQuery: String = f"select * from ${expectedDatabaseName}.${expectedTableName}"
+    val expectedDatabaseName = "default"
+    val expectedTableName = "trip_details_by_zone"
+    val expectedType = "hive_table"
+    val expectedQuery = f"select * from ${expectedDatabaseName}.${expectedTableName}"
 
     val atlasHiveBlob = AtlasHiveDatasetBlob(guid).get
+
     assert(atlasHiveBlob.query.get == expectedQuery)
     assert(atlasHiveBlob.numRecords.get == expectedNumRecords)
+
+    val attributes = atlasHiveBlob.attributes
+    assert(attributes.get("table_name").get.asString.get == expectedTableName)
+    assert(attributes.get("database_name").get.asString.get == expectedDatabaseName)
+    assert(attributes.get("type").get.asString.get == expectedType)
+    assert(attributes.get("created_time").isDefined)
+    assert(attributes.get("updated_time").isDefined)
   }
 }
