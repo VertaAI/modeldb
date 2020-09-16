ba31ae7fdf98d74aa40fc0815eb7c05d68e5f8d5
Nhat Pham <nhatsmrt@uw.edu>
2020-09-16 11:28:45 -0700

    VR-6717 Handle the case of empty tags for Dataset and DatasetVersion in Scala (#1505)
    * Fix a small bug of empty tags list.
    
    * Make test cleaner.

--- client/scala/src/test/scala/ai/verta/dataset_versioning/TestDataset.scala
+++ client/scala/src/test/scala/ai/verta/dataset_versioning/TestDataset.scala
@@ -38,6 +38,7 @@ class TestDataset extends FunSuite {
       val testDir = workingDir + "/src/test/scala/ai/verta/blobs/testdir"
       val version = f.dataset.createPathVersion(List(testDir)).get
 
+      assert(version.getTags().get.isEmpty)
       version.addTag("tag-1")
       version.addTags(List("tag-2", "tag-3"))
 
@@ -54,6 +55,7 @@ class TestDataset extends FunSuite {
     val f = fixture
 
     try {
+      assert(f.dataset.getTags().get.isEmpty)
       f.dataset.addTag("tag-1")
       f.dataset.addTags(List("tag-2", "tag-3"))
 
@@ -75,6 +77,7 @@ class TestDataset extends FunSuite {
       val testDir = workingDir + "/src/test/scala/ai/verta/blobs/testdir"
       val version = f.dataset.createPathVersion(List(testDir)).get
 
+      assert(version.getAttributes().get.isEmpty)
       version.addAttribute("some", 0.5)
       version.addAttribute("int", 4)
       version.addAttributes(Map("other" -> 0.3, "string" -> "desc"))
@@ -96,6 +99,8 @@ class TestDataset extends FunSuite {
     val f = fixture
 
     try {
+      assert(f.dataset.getAttributes().get.isEmpty)
+
       f.dataset.addAttribute("some", 0.5)
       f.dataset.addAttribute("int", 4)
       f.dataset.addAttributes(Map("other" -> 0.3, "string" -> "desc"))
