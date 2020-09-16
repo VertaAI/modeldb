be27ecf3ead89b8544f0e02c6594ed4673b196ab
Anand Jakhaniya <anand.jakhaniya@gmail.com>
2020-09-16 16:45:33 +0530

    Health check via REST endpoint

--- backend/src/main/java/ai/verta/modeldb/ModelDBConstants.java
+++ backend/src/main/java/ai/verta/modeldb/ModelDBConstants.java
@@ -158,6 +158,8 @@ public interface ModelDBConstants {
   String EMAIL = "email";
   String USERNAME = "username";
   String GRPC_HEALTH_CHECK_METHOD_NAME = "grpc.health.v1.Health/Check";
+  String SPRING_HEALTH_CHECK_METHOD_NAME = "check";
+  String HEALTH_CHECK_SERVICE_FIELD = "service";
   String DELETED = "deleted";
   String DEV_KEY = "devKey";
   String REQUEST_TIMEOUT = "requestTimeout";
--- backend/src/main/java/ai/verta/modeldb/health/HealthServiceImpl.java
+++ backend/src/main/java/ai/verta/modeldb/health/HealthServiceImpl.java
@@ -19,6 +19,9 @@
 
 package ai.verta.modeldb.health;
 
+import ai.verta.modeldb.ModelDBConstants;
+import ai.verta.modeldb.monitoring.QPSCountResource;
+import ai.verta.modeldb.monitoring.RequestLatencyResource;
 import ai.verta.modeldb.utils.ModelDBHibernateUtil;
 import io.grpc.Status;
 import io.grpc.StatusException;
@@ -30,17 +33,40 @@ import io.grpc.stub.StreamObserver;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import javax.annotation.Nullable;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
+@RestController
 public class HealthServiceImpl extends HealthGrpc.HealthImplBase {
 
   /* Due to the latency of rpc calls, synchronization of the map does not help with consistency.
    * However, need use ConcurrentHashMap to prevent the possible race condition of concurrently
    * putting two keys with a colliding hashCode into the map.*/
   private final Map<String, ServingStatus> statusMap = new ConcurrentHashMap<>();
+  private static final Logger LOGGER = LogManager.getLogger(HealthServiceImpl.class);
 
   @Override
   public void check(
       HealthCheckRequest request, StreamObserver<HealthCheckResponse> responseObserver) {
+    ServingStatus status = getServingStatus(request);
+    if (status == null) {
+      responseObserver.onError(
+          new StatusException(
+              Status.NOT_FOUND.withDescription("unknown service " + request.getService())));
+    } else {
+      HealthCheckResponse response = HealthCheckResponse.newBuilder().setStatus(status).build();
+      responseObserver.onNext(response);
+      responseObserver.onCompleted();
+    }
+  }
+
+  private ServingStatus getServingStatus(HealthCheckRequest request) {
     ServingStatus globalStatus;
     if (!request.getService().equals("")) {
       globalStatus = getStatus("");
@@ -52,16 +78,7 @@ public class HealthServiceImpl extends HealthGrpc.HealthImplBase {
         setStatus("live", ModelDBHibernateUtil.checkLive());
       }
     }
-    ServingStatus status = getStatus(request.getService());
-    if (status == null) {
-      responseObserver.onError(
-          new StatusException(
-              Status.NOT_FOUND.withDescription("unknown service " + request.getService())));
-    } else {
-      HealthCheckResponse response = HealthCheckResponse.newBuilder().setStatus(status).build();
-      responseObserver.onNext(response);
-      responseObserver.onCompleted();
-    }
+    return getStatus(request.getService());
   }
 
   void setStatus(String service, ServingStatus status) {
@@ -75,5 +92,27 @@ public class HealthServiceImpl extends HealthGrpc.HealthImplBase {
 
   void clearStatus(String service) {
     statusMap.remove(service);
+  }
+
+  @GetMapping(value = {ModelDBConstants.SPRING_HEALTH_CHECK_METHOD_NAME})
+  public ResponseEntity<String> check(
+      @RequestParam(ModelDBConstants.HEALTH_CHECK_SERVICE_FIELD) String service) {
+    LOGGER.trace("Spring custom health check called");
+    QPSCountResource.inc();
+    try (RequestLatencyResource latencyResource =
+        new RequestLatencyResource(ModelDBConstants.SPRING_HEALTH_CHECK_METHOD_NAME)) {
+      HealthCheckRequest request = HealthCheckRequest.newBuilder().setService(service).build();
+      ServingStatus status = getServingStatus(request);
+      if (status == null) {
+        LOGGER.trace("Spring custom health check returned with unknown service");
+        throw new ResponseStatusException(
+            HttpStatus.NOT_FOUND,
+            "Unknown service found on health check request : '" + request.getService() + "'");
+      } else {
+        LOGGER.trace("Spring custom health check returned");
+        HealthCheckResponse response = HealthCheckResponse.newBuilder().setStatus(status).build();
+        return ResponseEntity.ok(response.toString());
+      }
+    }
   }
 }
