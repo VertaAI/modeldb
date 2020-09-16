cdfcc125fc445038da2d98efc1f8b48c31f2b177
nhatsmrt <nhatsmrt@uw.edu>
2020-09-16 01:14:48 -0700

    Add list value types and some tests.

--- client/scala/src/main/scala/ai/verta/client/entities/utils/ValueType.scala
+++ client/scala/src/main/scala/ai/verta/client/entities/utils/ValueType.scala
@@ -11,6 +11,7 @@ sealed trait ValueType {
   def asBigInt: Option[BigInt] = None
   def asString: Option[String] = None
   def asDouble: Option[Double] = None
+  def asList: Option[List[ValueType]] = None
 }
 
 /** Represent (Big) integer observations, metrics, hyperparameters, etc.
@@ -32,6 +33,13 @@ case class StringValueType(private val s: String) extends ValueType {
  */
 case class DoubleValueType(private val d: Double) extends ValueType {
   override def asDouble = Some(d)
+}
+
+/** Represent list values of attributes.
+ *  User should not instantiate this class themselves but rely on implicit conversion
+ */
+case class ListValueType(private val l: List[ValueType]) extends ValueType {
+  override def asList: Option[List[ValueType]] = Some(l)
 }
 
 object ValueType {
@@ -58,4 +66,10 @@ object ValueType {
    *  @return the equivalent ValueType instance
    */
   implicit def fromDouble(d: Double): ValueType = DoubleValueType(d)
+
+  /** Implicit conversion method to convert a list to ValueType
+   *  @param l the list
+   *  @return the equivalent ValueType instance
+   */
+  implicit def fromList(l: List[ValueType]): ValueType = ListValueType(l)
 }
