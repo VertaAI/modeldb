f749901fdc24cf6f878d667c6869cb42009dcc11
Michael Liu <miliu@protonmail.com>
2020-09-17 09:24:13 -0700

    Separate S3 DatasetVersion test into separate case (#1508)
    * Separate S3 DataVer test
    
    * Do a proper get_latest() check
    
    * Simplify diff

--- client/verta/tests/test_dataset_versioning/test_dataset_version.py
+++ client/verta/tests/test_dataset_versioning/test_dataset_version.py
@@ -1,17 +1,29 @@
+import pytest
+
 import verta
 
+
 class TestDatasetVersion:
-    def test_create_get(self, client, created_datasets):
+    def test_create_get_path(self, client, created_datasets):
         name = verta._internal_utils._utils.generate_default_name()
         dataset = client._set_dataset2(name)
         created_datasets.append(dataset)
-        dataset_version = dataset.create_s3_version(paths=["s3://verta-starter/", "s3://verta-starter/census-test.csv"])
-        dataset_version2 = dataset.create_path_version(paths=["modelapi_hypothesis/",
-                                                              "modelapi_hypothesis/api_generator.py"])
+        dataset_version = dataset.create_path_version(paths=["modelapi_hypothesis/"])
+        dataset_version2 = dataset.create_path_version(paths=["modelapi_hypothesis/api_generator.py"])
 
         assert dataset_version.id == client._get_dataset_version2(dataset_version.id).id
         assert dataset_version2.id == client._get_dataset_version2(id=dataset_version2.id).id
         assert dataset_version2.id == dataset.get_latest_version().id
+
+    def test_create_get_s3(self, client, created_datasets):
+        pytest.importorskip("boto3")
+
+        name = verta._internal_utils._utils.generate_default_name()
+        dataset = client._set_dataset2(name)
+        created_datasets.append(dataset)
+        dataset_version = dataset.create_s3_version(paths=["s3://verta-starter/", "s3://verta-starter/census-test.csv"])
+
+        assert dataset_version.id == client._get_dataset_version2(dataset_version.id).id
 
     def test_description(self, client, created_datasets):
         name = verta._internal_utils._utils.generate_default_name()
