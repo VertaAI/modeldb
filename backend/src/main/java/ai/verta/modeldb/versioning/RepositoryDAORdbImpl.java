4e1b825eef62ad7cd3840c72263a57cf1eb6fb06
Anand Jakhaniya <anand.jakhaniya@gmail.com>
2020-09-23 11:41:25 +0530

    Fix review comment

--- backend/src/main/java/ai/verta/modeldb/versioning/RepositoryDAORdbImpl.java
+++ backend/src/main/java/ai/verta/modeldb/versioning/RepositoryDAORdbImpl.java
@@ -389,9 +389,6 @@ public class RepositoryDAORdbImpl implements RepositoryDAO {
       boolean create,
       RepositoryTypeEnum repositoryType)
       throws ModelDBException, NoSuchAlgorithmException, InvalidProtocolBufferException {
-    if (repositoryType.equals(RepositoryTypeEnum.REGULAR)) {
-      ModelDBUtils.validateEntityNameWithColonAndSlash(repository.getName(), false);
-    }
     RepositoryEntity repositoryEntity;
     if (create) {
       if (workspaceDTO == null) {
@@ -943,7 +940,7 @@ public class RepositoryDAORdbImpl implements RepositoryDAO {
   private void saveBranch(
       Session session, String commitSHA, String branch, RepositoryEntity repository)
       throws ModelDBException {
-    ModelDBUtils.validateEntityNameWithColonAndSlash(branch, true);
+    ModelDBUtils.validateEntityNameWithColonAndSlash(branch);
     boolean exists =
         VersioningUtils.commitRepositoryMappingExists(session, commitSHA, repository.getId());
     if (!exists) {
