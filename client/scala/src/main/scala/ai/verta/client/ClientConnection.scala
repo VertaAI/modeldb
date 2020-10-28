2c19c2f1e813e25a6c9b3237a30b3194209de031
Nhat Pham <nphamcs@gmail.com>
2020-10-27 17:00:41 -0700

    Add default scheme if not provided for VERTA_HOST.

--- client/scala/src/main/scala/ai/verta/client/ClientConnection.scala
+++ client/scala/src/main/scala/ai/verta/client/ClientConnection.scala
@@ -1,7 +1,16 @@
 package ai.verta.client
 
+import java.net.URI
+
 case class ClientConnection(host: String, ignoreConnErr: Boolean = false, maxRetries: Int = 5, auth: ClientAuth = null)
 
 object ClientConnection {
-  def fromEnvironment(): ClientConnection = new ClientConnection(sys.env("VERTA_HOST"), auth = ClientAuth.fromEnvironment())
+  private val DefaultScheme = "https"
+
+  private def hasScheme(url: String) = (new URI(url)).getScheme() != null
+
+  private def urlWithScheme(url: String) =
+    if (hasScheme(url)) url else (new URI(DefaultScheme, url, null, null, null)).toString()
+
+  def fromEnvironment(): ClientConnection = new ClientConnection(urlWithScheme(sys.env("VERTA_HOST")), auth = ClientAuth.fromEnvironment())
 }
