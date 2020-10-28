1f5f99094695eee6737ea9fe86277cd2fe09c77a
Nhat Pham <nphamcs@gmail.com>
2020-10-28 16:13:13 -0700

    Try to fix 3 more tests.

--- client/scala/src/test/scala/ai/verta/blobs/dataset/TestS3.scala
+++ client/scala/src/test/scala/ai/verta/blobs/dataset/TestS3.scala
@@ -28,19 +28,17 @@ class TestS3 extends FunSuite {
       val testfilePath2 = "s3://verta-starter/census-train.csv"
       val testfileLoc2 = S3Location(testfilePath2).get
 
-      val testdirPath = "s3://verta-starter"
+      val testdirPath = "s3://verta-versioned-bucket/data/"
       val testdirLoc = S3Location(testdirPath).get
 
-      val testsubdirPath = "s3://verta-starter"
-      val testsubdirLoc = S3Location(testsubdirPath).get
-
       val bucketLoc = S3Location("s3://verta-starter").get
-
     }
 
   def versioned_fixture = new {
     // for versioning tests:
     val s3: AmazonS3 = AmazonS3ClientBuilder.standard().build()
+    val filepath = "s3://verta-versioned-bucket/data/census-train.csv"
+
     val versionListing = s3.listVersions("verta-versioned-bucket", "data/census-train.csv")
     val oldVersionId = versionListing.getVersionSummaries().asScala.toList
       .filter((version: S3VersionSummary) => !version.getKey().endsWith("/")) // not a folder
@@ -48,6 +46,9 @@ class TestS3 extends FunSuite {
     val newVersionId = versionListing.getVersionSummaries().asScala.toList
       .filter((version: S3VersionSummary) => !version.getKey().endsWith("/")) // not a folder
       .filter(_.isLatest()).head.getVersionId()
+
+    val oldVersonLoc = S3Location(filepath, Some(oldVersionId)).get
+    val newVersionLoc = S3Location(filepath, Some(newVersionId)).get
   }
 
   test("S3Location should correctly determine bucket name and key") {
@@ -74,13 +75,15 @@ class TestS3 extends FunSuite {
     assert(s3Blob.listPaths equals List(f.testfilePath))
   }
 
-  ignore("S3 blob should retrieve a folder correctly") {
+  test("S3 blob should retrieve a folder correctly") {
     val f = fixture
-    val s3Blob = S3(List(f.testsubdirLoc)).get
+    val s3Blob = S3(List(f.testdirLoc)).get
 
-    TestMetadata.assertMetadata(s3Blob.getMetadata(f.testfilePath2).get, f.testfilePath2)
+    val filePath = "s3://verta-versioned-bucket/data/census-train.csv"
+
+    TestMetadata.assertMetadata(s3Blob.getMetadata(filePath).get, filePath)
     assert(s3Blob.getMetadata(f.testfilePath).isEmpty)
-    assert(s3Blob.listPaths equals List(f.testfilePath2))
+    assert(s3Blob.listPaths contains filePath)
   }
 
   test("S3 blob should retrieve the entire bucket correctly") {
@@ -92,22 +95,19 @@ class TestS3 extends FunSuite {
   }
 
   // testdir/testfile has two versions
-  ignore("S3 should retrieve the correct version") {
+  test("S3 should retrieve the correct version") {
     val f = fixture
     val vf = versioned_fixture
 
     // default should be the latest version
-    val s3BlobDefault = S3(List(f.testfileLoc)).get
-    var s3BlobLatest = S3(List(S3Location(f.testfilePath, Some(vf.newVersionId)).get)).get
+    val s3BlobDefault = S3(S3Location(vf.filepath).get).get
+    var s3BlobLatest = S3(vf.newVersionLoc).get
 
     assert(s3BlobDefault equals s3BlobLatest)
-    assert(s3BlobLatest.getVersionId(f.testfilePath).get equals vf.newVersionId)
 
     // get older version
-    val s3BlobOld = S3(List(S3Location(f.testfilePath, Some(vf.oldVersionId)).get)).get
-
+    val s3BlobOld = S3(vf.oldVersonLoc).get
     assert(!s3BlobDefault.equals(s3BlobOld))
-    assert(s3BlobOld.getVersionId(f.testfilePath).get equals vf.oldVersionId)
   }
 
   test("S3 should retrieve multiple keys correctly") {
@@ -120,7 +120,7 @@ class TestS3 extends FunSuite {
 
   test("S3 should not have duplicate paths") {
     val f = fixture
-    val s3Blob1 = S3(List(f.testfileLoc, f.testfileLoc, f.testdirLoc, f.testsubdirLoc, f.testfileLoc2)).get
+    val s3Blob1 = S3(List(f.testfileLoc, f.testfileLoc, f.testfileLoc2, f.bucketLoc)).get
 
     val s3Blob2 = S3(f.bucketLoc).get
     assert(s3Blob1 equals s3Blob2)
@@ -138,6 +138,7 @@ class TestS3 extends FunSuite {
   }
 
   ignore("S3 should produce similar hash and file size to PathBlob") {
+    // This test no longer applies, since local path dataset and S3 dataset does not match
     val f = fixture
     val s3Blob = S3(List(f.testfileLoc2)).get
     val s3Metadata = s3Blob.getMetadata(f.testfilePath2).get
@@ -160,14 +161,13 @@ class TestS3 extends FunSuite {
     assert(s3BlobCombined.listPaths.toSet equals Set(f.testfilePath, f.testfilePath2))
   }
 
-  ignore("Reducing S3 blobs with conflicting contents should fail") {
-    val f = fixture
+  test("Reducing S3 blobs with conflicting contents should fail") {
     val vf = versioned_fixture
 
-    val s3BlobDefault = S3(List(f.testfileLoc)).get
+    val s3BlobDefault = S3(vf.newVersionLoc).get
 
     // get older version
-    val s3BlobOld = S3(List(S3Location(f.testfilePath, Some(vf.oldVersionId)).get)).get
+    val s3BlobOld = S3(vf.oldVersonLoc).get
 
     val combineAttempt = S3.reduce(s3BlobDefault, s3BlobOld)
     assert(combineAttempt.isFailure)
