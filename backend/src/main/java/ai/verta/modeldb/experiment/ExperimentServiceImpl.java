fb49c89e52b9cf68979aedc2730a4e83dd285970
Anand Jakhaniya <anand.jakhaniya@gmail.com>
2020-11-13 10:38:21 +0530

    Fix review comment

--- backend/src/main/java/ai/verta/modeldb/experiment/ExperimentServiceImpl.java
+++ backend/src/main/java/ai/verta/modeldb/experiment/ExperimentServiceImpl.java
@@ -65,6 +65,7 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
+import java.util.stream.Collectors;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -97,19 +98,20 @@ public class ExperimentServiceImpl extends ExperimentServiceImplBase {
 
   private void saveAuditLogs(
       UserInfo userInfo, String action, List<String> resourceIds, String metadataBlob) {
-    List<AuditLogLocalEntity> auditLogLocalEntities = new ArrayList<>();
-    resourceIds.forEach(
-        resourceId ->
-            auditLogLocalEntities.add(
-                new AuditLogLocalEntity(
-                    SERVICE_NAME,
-                    authService.getVertaIdFromUserInfo(
-                        userInfo == null ? authService.getCurrentLoginUserInfo() : userInfo),
-                    action,
-                    resourceId,
-                    ModelDBConstants.EXPERIMENT,
-                    Service.MODELDB_SERVICE.name(),
-                    metadataBlob)));
+    List<AuditLogLocalEntity> auditLogLocalEntities =
+        resourceIds.stream()
+            .map(
+                resourceId ->
+                    new AuditLogLocalEntity(
+                        SERVICE_NAME,
+                        authService.getVertaIdFromUserInfo(
+                            userInfo == null ? authService.getCurrentLoginUserInfo() : userInfo),
+                        action,
+                        resourceId,
+                        ModelDBConstants.EXPERIMENT,
+                        Service.MODELDB_SERVICE.name(),
+                        metadataBlob))
+            .collect(Collectors.toList());
     if (!auditLogLocalEntities.isEmpty()) {
       auditLogLocalDAO.saveAuditLogs(auditLogLocalEntities);
     }
