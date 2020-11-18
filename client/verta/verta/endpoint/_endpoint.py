99580b4969328d834fa01b7a9f6d68c0062796fa
Michael Liu <miliu@protonmail.com>
2020-11-17 16:37:57 -0800

    Create stage when creating endpoint

--- client/verta/verta/endpoint/_endpoint.py
+++ client/verta/verta/endpoint/_endpoint.py
@@ -91,7 +91,9 @@ class Endpoint(object):
     def _create(cls, conn, conf, workspace, path, description=None):
         endpoint_json = cls._create_json(conn, workspace, path, description)
         if endpoint_json:
-            return cls(conn, conf, workspace, endpoint_json['id'])
+            endpoint = cls(conn, conf, workspace, endpoint_json['id'])
+            endpoint._create_stage()  # an endpoint needs a stage to function, so might as well create it here
+            return endpoint
         else:
             return None
 
