c9fa0f42d7c172caeb220375924bfd4fbdeb3904
Michael Liu <miliu@protonmail.com>
2020-11-23 18:58:19 -0800

    Swap over client methods

--- client/verta/tests/test_dataset_versioning/test_dataset_version.py
+++ client/verta/tests/test_dataset_versioning/test_dataset_version.py
@@ -6,18 +6,18 @@ import verta
 class TestDatasetVersion:
     def test_create_get_path(self, client, created_datasets):
         name = verta._internal_utils._utils.generate_default_name()
-        dataset = client._set_dataset2(name)
+        dataset = client.set_dataset(name)
         created_datasets.append(dataset)
         dataset_version = dataset.create_path_version(paths=["modelapi_hypothesis/"])
         dataset_version2 = dataset.create_path_version(paths=["modelapi_hypothesis/api_generator.py"])
 
-        assert dataset_version.id == client._get_dataset_version2(dataset_version.id).id
-        assert dataset_version2.id == client._get_dataset_version2(id=dataset_version2.id).id
+        assert dataset_version.id == client.get_dataset_version(dataset_version.id).id
+        assert dataset_version2.id == client.get_dataset_version(id=dataset_version2.id).id
         assert dataset_version2.id == dataset.get_latest_version().id
 
     def test_get_latest_printing(self, client, created_datasets, capsys):
         name = verta._internal_utils._utils.generate_default_name()
-        dataset = client._set_dataset2(name)
+        dataset = client.set_dataset(name)
         created_datasets.append(dataset)
 
         dataset_version = dataset.create_path_version(paths=["modelapi_hypothesis/"])
@@ -30,15 +30,15 @@ class TestDatasetVersion:
         pytest.importorskip("boto3")
 
         name = verta._internal_utils._utils.generate_default_name()
-        dataset = client._set_dataset2(name)
+        dataset = client.set_dataset(name)
         created_datasets.append(dataset)
         dataset_version = dataset.create_s3_version(paths=["s3://verta-starter/", "s3://verta-starter/census-test.csv"])
 
-        assert dataset_version.id == client._get_dataset_version2(dataset_version.id).id
+        assert dataset_version.id == client.get_dataset_version(dataset_version.id).id
 
     def test_description(self, client, created_datasets):
         name = verta._internal_utils._utils.generate_default_name()
-        dataset = client._set_dataset2(name)
+        dataset = client.set_dataset(name)
         created_datasets.append(dataset)
         original_description = "original description"
         dataset_version = dataset.create_path_version(paths=["modelapi_hypothesis/",
@@ -50,11 +50,11 @@ class TestDatasetVersion:
         dataset_version.set_description(updated_description)
         assert dataset_version.get_description() == updated_description
 
-        assert client._get_dataset_version2(id=dataset_version.id).get_description() == updated_description
+        assert client.get_dataset_version(id=dataset_version.id).get_description() == updated_description
 
     def test_tags(self, client, created_datasets):
         name = verta._internal_utils._utils.generate_default_name()
-        dataset = client._set_dataset2(name)
+        dataset = client.set_dataset(name)
         dataset_version = dataset.create_path_version(paths=["modelapi_hypothesis/",
                                                              "modelapi_hypothesis/api_generator.py"],
                                                       tags=["tag1", "tag2"])
@@ -74,7 +74,7 @@ class TestDatasetVersion:
 
     def test_attributes(self, client, created_datasets):
         name = verta._internal_utils._utils.generate_default_name()
-        dataset = client._set_dataset2(name)
+        dataset = client.set_dataset(name)
         created_datasets.append(dataset)
         dataset_version = dataset.create_path_version(paths=["modelapi_hypothesis/",
                                                              "modelapi_hypothesis/api_generator.py"],
