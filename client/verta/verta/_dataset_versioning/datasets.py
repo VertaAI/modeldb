e1f1d2fa7afadb61f1e8b283fce8b8350bf87dc2
Michael Liu <miliu@protonmail.com>
2020-11-25 11:43:52 -0800

    Fix Datasets docstrings

--- client/verta/verta/_dataset_versioning/datasets.py
+++ client/verta/verta/_dataset_versioning/datasets.py
@@ -9,6 +9,22 @@ from . import dataset
 
 
 class Datasets(_utils.LazyList):
+    """
+    ``list``-like object containing :class:`~verta._dataset_versioning.dataset.Dataset`s.
+
+    This class provides functionality for filtering and sorting its contents.
+
+    There should not be a need to instantiate this class directly; please use
+    :class:`Client.datasets <verta.client.Client>`.
+
+    Examples
+    --------
+    .. code-block:: python
+
+        datasets = client.datasets.find("tags ~= census")
+        dataset = datasets.sort("time_created", descending=True)[0]  # most recent
+
+    """
     # keys that yield predictable, sensible results
     _VALID_QUERY_KEYS = {
         'id',
@@ -37,11 +53,39 @@ class Datasets(_utils.LazyList):
         return dataset.Dataset(self._conn, self._conf, msg)
 
     def with_workspace(self, workspace_name=None):
+        """
+        Returns datasets in the specified workspace.
+
+        Parameters
+        ----------
+        workspace_name : str or None, default None
+            Workspace name. If ``None``, uses personal workspace.
+
+        Returns
+        -------
+        :class:`Datasets`
+            Filtered datasets.
+
+        """
         new_list = copy.deepcopy(self)
         new_list._msg.workspace_name = workspace_name
         return new_list
 
-    def with_ids(self, ids=None):
+    def with_ids(self, ids):
+        """
+        Returns datasets with the specified IDs.
+
+        Parameters
+        ----------
+        ids : list of str
+            Dataset IDs.
+
+        Returns
+        -------
+        :class:`Datasets`
+            Filtered datasets.
+
+        """
         new_list = copy.deepcopy(self)
         del new_list._msg.dataset_ids[:]
         if ids:
