7ee046a7024a3ef774541c9358e5fb692501add2
Cory Johannsen <73903791+coryverta@users.noreply.github.com>
2020-11-30 20:04:39 -0800

    Revert "Add workspace_id to ProjectEntity and create Liquibase migration (#1682)" (#1728)
    This reverts commit 842c2b758851a5a30e51fb8a2fa90dd003ee65c0.

--- backend/src/main/java/ai/verta/modeldb/ModelDBConstants.java
+++ backend/src/main/java/ai/verta/modeldb/ModelDBConstants.java
@@ -118,7 +118,6 @@ public interface ModelDBConstants {
   String TAGS = "tags";
   String VALUE = "value";
   String WORKSPACE = "workspace";
-  String LEGACY_WORKSPACE_ID = "legacy_workspace_id";
   String WORKSPACE_ID = "workspace_id";
   String WORKSPACE_NAME = "workspace_name";
   String WORKSPACE_TYPE = "workspace_type";
