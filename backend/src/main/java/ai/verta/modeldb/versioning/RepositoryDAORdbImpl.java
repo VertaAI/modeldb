65cd5db5feae93d5c0e6b14157537ef1af62b3c5
Cory Johannsen <cory@verta.ai>
2020-11-30 18:03:14 -0800

    Try WORKSPACE

--- backend/src/main/java/ai/verta/modeldb/versioning/RepositoryDAORdbImpl.java
+++ backend/src/main/java/ai/verta/modeldb/versioning/RepositoryDAORdbImpl.java
@@ -1399,7 +1399,7 @@ public class RepositoryDAORdbImpl implements RepositoryDAO {
           if (workspacePredicates.size() > 0) {
             Predicate privateWorkspacePredicate =
                 builder.equal(
-                    repositoryRoot.get(ModelDBConstants.LEGACY_WORKSPACE_ID),
+                    repositoryRoot.get(ModelDBConstants.WORKSPACE),
                     workspacePredicates.get(0).getValue().getStringValue());
             Predicate privateWorkspaceTypePredicate =
                 builder.equal(
