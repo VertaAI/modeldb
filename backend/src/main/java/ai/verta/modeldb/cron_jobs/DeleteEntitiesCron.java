6e0964fdbac34264784abfd23c46b6cd998c59a3
Cory Johannsen <73903791+coryverta@users.noreply.github.com>
2020-11-30 20:03:30 -0800

    Revert "Add workspace id to dataset entity (#1683)"
    This reverts commit 13a1c14c0b191c457f1231cecc94053fd2f18e78.

--- backend/src/main/java/ai/verta/modeldb/cron_jobs/DeleteEntitiesCron.java
+++ backend/src/main/java/ai/verta/modeldb/cron_jobs/DeleteEntitiesCron.java
@@ -536,7 +536,7 @@ public class DeleteEntitiesCron extends TimerTask {
       // Delete workspace based roleBindings
       List<String> workspaceRoleBindingNames =
           getWorkspaceRoleBindingsForDataset(
-              datasetEntity.getLegacy_workspace_id(),
+              datasetEntity.getWorkspace(),
               WorkspaceTypeEnum.WorkspaceType.forNumber(datasetEntity.getWorkspace_type()),
               datasetEntity.getId(),
               DatasetVisibilityEnum.DatasetVisibility.forNumber(
