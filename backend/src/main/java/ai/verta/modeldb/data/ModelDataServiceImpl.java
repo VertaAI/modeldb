0e4388b2b4a3a0290ff31b797dbafc047f38ebdc
Jenkins <jenkins@verta.ai>
2020-12-15 22:36:38 +0000

    https://jenkins.dev.verta.ai/job/build/job/autoformat/job/modeldb-backend/89/

--- backend/src/main/java/ai/verta/modeldb/data/ModelDataServiceImpl.java
+++ backend/src/main/java/ai/verta/modeldb/data/ModelDataServiceImpl.java
@@ -4,18 +4,22 @@ import io.grpc.stub.StreamObserver;
 
 public class ModelDataServiceImpl extends ModelDataServiceGrpc.ModelDataServiceImplBase {
 
-    @Override
-    public void storeModelData(StoreModelDataRequest request, StreamObserver<StoreModelDataRequest.Response> responseObserver) {
-        super.storeModelData(request, responseObserver);
-    }
+  @Override
+  public void storeModelData(
+      StoreModelDataRequest request,
+      StreamObserver<StoreModelDataRequest.Response> responseObserver) {
+    super.storeModelData(request, responseObserver);
+  }
 
-    @Override
-    public void getModelData(GetModelDataRequest request, StreamObserver<GetModelDataRequest.Response> responseObserver) {
-        super.getModelData(request, responseObserver);
-    }
+  @Override
+  public void getModelData(
+      GetModelDataRequest request, StreamObserver<GetModelDataRequest.Response> responseObserver) {
+    super.getModelData(request, responseObserver);
+  }
 
-    @Override
-    public void getModelDataDiff(GetModelDataRequest request, StreamObserver<GetModelDataRequest.Response> responseObserver) {
-        super.getModelDataDiff(request, responseObserver);
-    }
+  @Override
+  public void getModelDataDiff(
+      GetModelDataRequest request, StreamObserver<GetModelDataRequest.Response> responseObserver) {
+    super.getModelDataDiff(request, responseObserver);
+  }
 }
