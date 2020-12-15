bc6d92abe278a2fc529a16bc4f998308d9cf6bbf
Cory Johannsen <cory@verta.ai>
2020-12-15 15:35:16 -0800

    Initial implementation of getModelData

--- backend/src/main/java/ai/verta/modeldb/data/ModelDataServiceImpl.java
+++ backend/src/main/java/ai/verta/modeldb/data/ModelDataServiceImpl.java
@@ -1,12 +1,20 @@
 package ai.verta.modeldb.data;
 
+import ai.verta.modeldb.CreateDataset;
+import com.google.gson.Gson;
 import io.grpc.stub.StreamObserver;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.io.FileOutputStream;
+import java.io.File;
 import java.io.FileWriter;
+import java.io.FilenameFilter;
 import java.io.IOException;
+import java.time.Instant;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 public class ModelDataServiceImpl extends ModelDataServiceGrpc.ModelDataServiceImplBase {
   private static final Logger LOGGER = LogManager.getLogger(ModelDataServiceImpl.class);
@@ -17,38 +25,51 @@ public class ModelDataServiceImpl extends ModelDataServiceGrpc.ModelDataServiceI
     this.modelDataStoragePath = modelDataStoragePath;
   }
 
+  private String buildFileName(String modelId, Long timestampMillis, String endpoint) {
+    return modelDataStoragePath + "/" + modelId + "-" + endpoint + "-" + timestampMillis;
+  }
+
+  private String buildFileName(ModelDataMetadata metadata) {
+      final String modelId = metadata.getModelId();
+      final Long timestampMillis = metadata.getTimestampMillis();
+      final String endpoint = metadata.getEndpoint();
+      return buildFileName(modelId, timestampMillis, endpoint);
+  }
+
   @Override
   public void storeModelData(
       StoreModelDataRequest request,
       StreamObserver<StoreModelDataRequest.Response> responseObserver) {
     LOGGER.info("StoreModelData: " + request);
-    super.storeModelData(request, responseObserver);
+    final ModelDataMetadata metadata = request.getModelData().getMetadata();
+    final String data = request.getModelData().getData();
+    final String fileName = buildFileName(metadata);
+    try (FileWriter writer = new FileWriter(fileName)) {
+      writer.write(data);
+    } catch (IOException ex) {
+      LOGGER.error(ex);
+    }
   }
-    private String buildFileName(ModelDataMetadata metadata) {
-        final String modelId = metadata.getModelId();
-        final Long timestampMillis = metadata.getTimestampMillis();
-        final String endpoint = metadata.getEndpoint();
-        return modelDataStoragePath + "/" + modelId + "-" + endpoint + "-" + timestampMillis;
-    }
-
-    @Override
-    public void storeModelData(StoreModelDataRequest request, StreamObserver<StoreModelDataRequest.Response> responseObserver) {
-        LOGGER.info("StoreModelData: " + request);
-        final ModelDataMetadata metadata = request.getModelData().getMetadata();
-        final String data = request.getModelData().getData();
-        final String fileName = buildFileName(metadata);
-        try (FileWriter writer = new FileWriter(fileName)) {
-            writer.write(data);
-        } catch (IOException ex) {
-            LOGGER.error(ex);
-        }
-    }
 
   @Override
   public void getModelData(
       GetModelDataRequest request, StreamObserver<GetModelDataRequest.Response> responseObserver) {
     LOGGER.info("GetModelData: " + request);
-    super.getModelData(request, responseObserver);
+    final Instant startAt = Instant.ofEpochMilli(request.getStartTimeMillis());
+    final Instant endAt = Instant.ofEpochMilli(request.getEndTimeMillis());
+    final File fileRoot = new File(modelDataStoragePath);
+
+    final File[] filteredToModel = fileRoot.listFiles((dir, name) -> name.startsWith(request.getModelId() + "-"));
+    final List<File> filteredToTimespan = Arrays.stream(filteredToModel)
+      .filter(file -> {
+        final Instant fileTimestamp = Instant.ofEpochMilli(file.lastModified());
+        return startAt.isAfter(fileTimestamp) && endAt.isBefore(fileTimestamp);
+      })
+      .collect(Collectors.toList());
+    String json = new Gson().toJson(filteredToTimespan);
+    responseObserver.onNext(
+      GetModelDataRequest.Response.newBuilder().setData(json).build());
+    responseObserver.onCompleted();
   }
 
   @Override
