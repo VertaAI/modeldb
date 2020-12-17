fd255e3982f6530a3ec7a1ae74635b9107f21842
Jenkins <jenkins@verta.ai>
2020-12-17 19:49:08 +0000

    https://jenkins.dev.verta.ai/job/build/job/autoformat/job/modeldb-backend/176/

--- backend/src/main/java/ai/verta/modeldb/data/ModelDataServiceImpl.java
+++ backend/src/main/java/ai/verta/modeldb/data/ModelDataServiceImpl.java
@@ -76,14 +76,10 @@ public class ModelDataServiceImpl extends ModelDataServiceGrpc.ModelDataServiceI
     final Instant endAt = Instant.ofEpochMilli(request.getEndTimeMillis());
     LOGGER.info("Time window end at: " + endAt);
     final Optional<String> endpoint = resolveEndpoint(request.getEndpoint());
-    final Optional<Long> nNess = request.getNNess() > 0 ? Optional.of(request.getNNess()) : Optional.empty();
+    final Optional<Long> nNess =
+        request.getNNess() > 0 ? Optional.of(request.getNNess()) : Optional.empty();
     final List<NGramData> filteredToTimespan =
-        fetchNGramData(
-            request.getModelId(),
-            endpoint,
-            nNess,
-            startAt,
-            endAt);
+        fetchNGramData(request.getModelId(), endpoint, nNess, startAt, endAt);
     LOGGER.info("Found " + filteredToTimespan.size() + " predictions in the time window.");
     LOGGER.info("Aggregating predictions.");
     Map<String, Object> aggregate = aggregateTimespan(filteredToTimespan);
@@ -105,25 +101,16 @@ public class ModelDataServiceImpl extends ModelDataServiceGrpc.ModelDataServiceI
     final Instant endAt = Instant.ofEpochMilli(request.getEndTimeMillis());
     LOGGER.info("Time window end at: " + endAt);
     final Optional<String> endpoint = resolveEndpoint(request.getEndpoint());
-    final Optional<Long> nNess = request.getNNess() > 0 ? Optional.of(request.getNNess()) : Optional.empty();
+    final Optional<Long> nNess =
+        request.getNNess() > 0 ? Optional.of(request.getNNess()) : Optional.empty();
     final List<NGramData> aFilteredToTimespan =
-        fetchNGramData(
-            request.getModelIdA(),
-            endpoint,
-            nNess,
-            startAt,
-            endAt);
+        fetchNGramData(request.getModelIdA(), endpoint, nNess, startAt, endAt);
     LOGGER.info(
         "Found "
             + aFilteredToTimespan.size()
             + " predictions in the time window for model data A.");
     final List<NGramData> bFilteredToTimespan =
-        fetchNGramData(
-            request.getModelIdB(),
-            endpoint,
-            nNess,
-            startAt,
-            endAt);
+        fetchNGramData(request.getModelIdB(), endpoint, nNess, startAt, endAt);
     LOGGER.info(
         "Found "
             + bFilteredToTimespan.size()
