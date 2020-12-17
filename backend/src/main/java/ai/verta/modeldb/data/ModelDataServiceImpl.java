c6f4e621a3ac2f2fa54690f9ccfc4347b79d000d
Cory Johannsen <cory@verta.ai>
2020-12-17 13:46:05 -0800

    Extract n-ness as double then convert to long

--- backend/src/main/java/ai/verta/modeldb/data/ModelDataServiceImpl.java
+++ backend/src/main/java/ai/verta/modeldb/data/ModelDataServiceImpl.java
@@ -331,10 +331,10 @@ public class ModelDataServiceImpl extends ModelDataServiceGrpc.ModelDataServiceI
                 final Long populationSize = populationSizeDouble.longValue();
                 //                final Long predictionCount =
                 //                    Long.parseLong((String) rootObject.get("prediction_count"));
-                final String nString = (String) rootObject.get("n");
+                final Double nDouble = (Double) rootObject.get("n");
                 final Optional<Long> n =
-                    nString != null && !nString.isEmpty()
-                        ? Optional.of(Long.parseLong(nString))
+                    nDouble != null
+                        ? Optional.of(nDouble.longValue())
                         : Optional.empty();
                 if (nNess.isPresent() && n.isPresent()) {
                   if (n.get() != nNess.get()) {
