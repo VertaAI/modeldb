6b28cc3995dd940c24e78adb420898578f20db44
Jenkins <jenkins@verta.ai>
2020-12-23 06:57:45 +0000

    Update protos from https://github.com/VertaAI/protos-all/pull/115

--- protos/gen/go/protos/public/uac/Collaborator.pb.gw.go
+++ protos/gen/go/protos/public/uac/Collaborator.pb.gw.go
@@ -101,42 +101,6 @@ func local_request_CollaboratorService_SetResources_0(ctx context.Context, marsh
 
 }
 
-var (
-	filter_CollaboratorService_DeleteResources_0 = &utilities.DoubleArray{Encoding: map[string]int{}, Base: []int(nil), Check: []int(nil)}
-)
-
-func request_CollaboratorService_DeleteResources_0(ctx context.Context, marshaler runtime.Marshaler, client CollaboratorServiceClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
-	var protoReq DeleteResources
-	var metadata runtime.ServerMetadata
-
-	if err := req.ParseForm(); err != nil {
-		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
-	}
-	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_CollaboratorService_DeleteResources_0); err != nil {
-		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
-	}
-
-	msg, err := client.DeleteResources(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
-	return msg, metadata, err
-
-}
-
-func local_request_CollaboratorService_DeleteResources_0(ctx context.Context, marshaler runtime.Marshaler, server CollaboratorServiceServer, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
-	var protoReq DeleteResources
-	var metadata runtime.ServerMetadata
-
-	if err := req.ParseForm(); err != nil {
-		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
-	}
-	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_CollaboratorService_DeleteResources_0); err != nil {
-		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
-	}
-
-	msg, err := server.DeleteResources(ctx, &protoReq)
-	return msg, metadata, err
-
-}
-
 func request_CollaboratorService_AddOrUpdateProjectCollaborator_0(ctx context.Context, marshaler runtime.Marshaler, client CollaboratorServiceClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
 	var protoReq AddCollaboratorRequest
 	var metadata runtime.ServerMetadata
@@ -712,26 +676,6 @@ func RegisterCollaboratorServiceHandlerServer(ctx context.Context, mux *runtime.
 
 	})
 
-	mux.Handle("DELETE", pattern_CollaboratorService_DeleteResources_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
-		ctx, cancel := context.WithCancel(req.Context())
-		defer cancel()
-		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
-		rctx, err := runtime.AnnotateIncomingContext(ctx, mux, req)
-		if err != nil {
-			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
-			return
-		}
-		resp, md, err := local_request_CollaboratorService_DeleteResources_0(rctx, inboundMarshaler, server, req, pathParams)
-		ctx = runtime.NewServerMetadataContext(ctx, md)
-		if err != nil {
-			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
-			return
-		}
-
-		forward_CollaboratorService_DeleteResources_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
-
-	})
-
 	mux.Handle("POST", pattern_CollaboratorService_AddOrUpdateProjectCollaborator_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
 		ctx, cancel := context.WithCancel(req.Context())
 		defer cancel()
@@ -1113,26 +1057,6 @@ func RegisterCollaboratorServiceHandlerClient(ctx context.Context, mux *runtime.
 
 	})
 
-	mux.Handle("DELETE", pattern_CollaboratorService_DeleteResources_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
-		ctx, cancel := context.WithCancel(req.Context())
-		defer cancel()
-		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
-		rctx, err := runtime.AnnotateContext(ctx, mux, req)
-		if err != nil {
-			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
-			return
-		}
-		resp, md, err := request_CollaboratorService_DeleteResources_0(rctx, inboundMarshaler, client, req, pathParams)
-		ctx = runtime.NewServerMetadataContext(ctx, md)
-		if err != nil {
-			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
-			return
-		}
-
-		forward_CollaboratorService_DeleteResources_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
-
-	})
-
 	mux.Handle("POST", pattern_CollaboratorService_AddOrUpdateProjectCollaborator_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
 		ctx, cancel := context.WithCancel(req.Context())
 		defer cancel()
@@ -1441,8 +1365,6 @@ var (
 
 	pattern_CollaboratorService_SetResources_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"v1", "collaborator", "setResources"}, "", runtime.AssumeColonVerbOpt(true)))
 
-	pattern_CollaboratorService_DeleteResources_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"v1", "collaborator", "deleteResources"}, "", runtime.AssumeColonVerbOpt(true)))
-
 	pattern_CollaboratorService_AddOrUpdateProjectCollaborator_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"v1", "collaborator", "addOrUpdateProjectCollaborator"}, "", runtime.AssumeColonVerbOpt(true)))
 
 	pattern_CollaboratorService_RemoveProjectCollaborator_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"v1", "collaborator", "removeProjectCollaborator"}, "", runtime.AssumeColonVerbOpt(true)))
@@ -1478,8 +1400,6 @@ var (
 	forward_CollaboratorService_GetResources_0 = runtime.ForwardResponseMessage
 
 	forward_CollaboratorService_SetResources_0 = runtime.ForwardResponseMessage
-
-	forward_CollaboratorService_DeleteResources_0 = runtime.ForwardResponseMessage
 
 	forward_CollaboratorService_AddOrUpdateProjectCollaborator_0 = runtime.ForwardResponseMessage
 
