f78e6bc304e889037bc7aaca3b613a79e5c56f16
Conrado Miranda <conrado@verta.ai>
2021-01-04 11:24:08 -0800

    Add script to start telepresence

---
+++ backend/telepresence.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+set -eo pipefail
+
+mkdir -p ~/telepresence_mounts
+telepresence --swap-deployment modeldb--backend --env-json telepresence-env.json --expose 8085 --expose 8086 --also-proxy 10.0.0.0/8 --mount ~/telepresence_mounts/modeldb_backend
