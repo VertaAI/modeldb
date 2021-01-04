b7f3e6788ec348c56c0b32ab100121b5e98a6d0c
conradoverta <53195685+conradoverta@users.noreply.github.com>
2021-01-03 20:35:15 -0800

    Fix copy and paste
    Co-authored-by: Anand Jakhaniya <anand.jakhaniya@gmail.com>

--- backend/src/main/java/ai/verta/modeldb/config/NFSEndpointConfig.java
+++ backend/src/main/java/ai/verta/modeldb/config/NFSEndpointConfig.java
@@ -6,7 +6,7 @@ public class NFSEndpointConfig {
 
   public void Validate(String base) throws InvalidConfigException {
     if (getArtifact == null || getArtifact.isEmpty())
-      throw new InvalidConfigException(base + ".nfsUrlProtocol", Config.MISSING_REQUIRED);
+      throw new InvalidConfigException(base + ".getArtifact", Config.MISSING_REQUIRED);
     if (storeArtifact == null || storeArtifact.isEmpty())
       throw new InvalidConfigException(base + ".storeArtifact", Config.MISSING_REQUIRED);
   }
