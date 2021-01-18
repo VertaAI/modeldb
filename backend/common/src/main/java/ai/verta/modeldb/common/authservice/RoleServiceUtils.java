9602146b26f93caafe0f62e93dc136bcebb586b1
Anand Jakhaniya <anand.jakhaniya@gmail.com>
2021-01-18 16:12:45 +0530

    Fix import

--- backend/common/src/main/java/ai/verta/modeldb/common/authservice/RoleServiceUtils.java
+++ backend/common/src/main/java/ai/verta/modeldb/common/authservice/RoleServiceUtils.java
@@ -7,7 +7,7 @@ import ai.verta.modeldb.common.CommonUtils;
 import ai.verta.modeldb.common.collaborator.CollaboratorBase;
 import ai.verta.modeldb.common.collaborator.CollaboratorOrg;
 import ai.verta.modeldb.common.collaborator.CollaboratorUser;
-import ai.verta.modeldb.exceptions.PermissionDeniedException;
+import ai.verta.modeldb.common.exceptions.PermissionDeniedException;
 import ai.verta.uac.Action;
 import ai.verta.uac.Actions;
 import ai.verta.uac.CollaboratorPermissions;
