e8799ef739afb39874ca8026f2e95eb605b6cbe8
Evgenii Lezhnin <eugene.lezhnin@quantumsoft.ru>
2021-01-18 15:34:43 +0700

    VR-8155 delete bindings for a single entity

--- backend/src/main/java/ai/verta/modeldb/batchProcess/CollaboratorResourceMigration.java
+++ backend/src/main/java/ai/verta/modeldb/batchProcess/CollaboratorResourceMigration.java
@@ -246,7 +246,6 @@ public class CollaboratorResourceMigration {
           Map<String, GetResourcesResponseItem> responseItemMap =
               responseItems.stream()
                   .collect(Collectors.toMap(GetResourcesResponseItem::getResourceId, item -> item));
-          List<RepositoryEntity> migratedRepositoryEntities = new LinkedList<>();
           for (RepositoryEntity repository : repositoryEntities) {
             boolean migrated = false;
             if (repository.getOwner() != null && !repository.getOwner().isEmpty()) {
@@ -287,13 +286,13 @@ public class CollaboratorResourceMigration {
               migrated = true;
             }
             if (migrated) {
-              migratedRepositoryEntities.add(repository);
               Transaction transaction = null;
               try {
                 transaction = session.beginTransaction();
                 repository.setVisibility_migration(true);
                 session.update(repository);
                 transaction.commit();
+                deleteRoleBindingsOfRepositories(Collections.singletonList(repository));
               } catch (Exception ex) {
                 if (transaction != null && transaction.getStatus().canRollback()) {
                   transaction.rollback();
@@ -301,7 +300,6 @@ public class CollaboratorResourceMigration {
               }
             }
           }
-          deleteRoleBindingsOfRepositories(migratedRepositoryEntities);
         } else {
           LOGGER.debug("Total repositories count 0");
         }
