3391606e6e86428cf2c8e8bb8f10fbe29f2431d7
Michael Liu <miliu@protonmail.com>
2021-01-22 17:10:34 -0800

    Add is_dirty as param to Git()

--- client/verta/verta/code/_git.py
+++ client/verta/verta/code/_git.py
@@ -22,6 +22,9 @@ class Git(_code._Code):
         Commit tag. If not provided, it will automatically be determined.
     commit_hash : str, optional
         Commit hash. If not provided, it will automatically be determined.
+    is_dirty : bool, optional
+        Whether git status is dirty relative to `commit_hash`. If not provided,
+        it will automatically be determined.
     autocapture : bool, default True
         Whether to enable the automatic capturing behavior of parameters above.
 
@@ -58,7 +61,7 @@ class Git(_code._Code):
         )
 
     """
-    def __init__(self, repo_url=None, branch=None, tag=None, commit_hash=None, autocapture=True, _autocapture=True):
+    def __init__(self, repo_url=None, branch=None, tag=None, commit_hash=None, is_dirty=None, autocapture=True, _autocapture=True):
         # TODO: switch all similar blobs from _autocapture to autocapture so they have the same API
         if _autocapture is False:
             autocapture = False
@@ -83,13 +86,13 @@ class Git(_code._Code):
             self._msg.git.branch = branch or _git_utils.get_git_branch_name(ref)
             self._msg.git.tag = tag or _git_utils.get_git_commit_tag(ref)
             self._msg.git.hash = _git_utils.get_git_commit_hash(ref)  # use full commit hash
-            self._msg.git.is_dirty = _git_utils.get_git_commit_dirtiness(ref)
+            self._msg.git.is_dirty = is_dirty if is_dirty is not None else _git_utils.get_git_commit_dirtiness(ref)
         else:
             self._msg.git.repo = repo_url or ""
             self._msg.git.branch = branch or ""
             self._msg.git.tag = tag or ""
             self._msg.git.hash = commit_hash or ""
-            # self._msg.git.is_dirty  # TODO: expose as param
+            self._msg.git.is_dirty = is_dirty or False
 
     def __repr__(self):
         lines = ["Git Version"]
