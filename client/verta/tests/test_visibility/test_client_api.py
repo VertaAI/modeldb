b95ac6a9ebb31309813ab33e7f90bd81c17628b2
Michael Liu <miliu@protonmail.com>
2021-01-26 12:13:03 -0800

    Add remaining pub_within_org tests

--- client/verta/tests/test_visibility/test_client_api.py
+++ client/verta/tests/test_visibility/test_client_api.py
@@ -6,8 +6,12 @@ Basic tests to make sure the client passes `visibility` without errors.
 
 import pytest
 
-from verta._internal_utils import _utils
+from verta._protos.public.common import CommonService_pb2 as _CommonCommonService
 from verta._protos.public.modeldb import DatasetService_pb2 as _DatasetService
+from verta._protos.public.modeldb import ProjectService_pb2 as _ProjectService
+from verta._protos.public.modeldb.versioning import VersioningService_pb2 as _VersioningService
+
+from verta._internal_utils import _utils
 from verta.visibility import (
     OrgCustom,
     Private,
@@ -92,4 +96,44 @@ class TestPublicWithinOrg:
                 assert entity._msg.dataset_visibility == _DatasetService.DatasetVisibilityEnum.PRIVATE
         finally:
             entity.delete()
-    # TODO: the other entities
+
+    @pytest.mark.skip(reason="not implemented yet")
+    def test_endpoint(self, client, organization):
+        raise NotImplementedError
+
+    def test_project(self, client, organization):
+        visibility = OrgCustom(write=True)
+        entity = client.set_project(workspace=organization.name, visibility=visibility)
+        try:
+            if visibility._to_public_within_org():
+                assert entity._msg.project_visibility == _ProjectService.ORG_SCOPED_PUBLIC
+            else:
+                assert entity._msg.project_visibility == _ProjectService.PRIVATE
+        finally:
+            entity.delete()
+            client._ctx.proj = None  # otherwise client teardown tries to delete
+
+    def test_registered_model(self, client, organization):
+        visibility = OrgCustom(write=True)
+        entity = client.set_registered_model(workspace=organization.name, visibility=visibility)
+        try:
+            if visibility._to_public_within_org():
+                assert entity._msg.visibility == _CommonCommonService.VisibilityEnum.ORG_SCOPED_PUBLIC
+            else:
+                assert entity._msg.visibility == _CommonCommonService.VisibilityEnum.PRIVATE
+        finally:
+            entity.delete()
+
+    def test_repository(self, client, organization):
+        visibility = OrgCustom(write=True)
+        entity = client.set_repository(
+            name=_utils.generate_default_name(),
+            workspace=organization.name, visibility=visibility,
+        )
+        try:
+            if visibility._to_public_within_org():
+                assert entity._msg.repository_visibility == _VersioningService.RepositoryVisibilityEnum.ORG_SCOPED_PUBLIC
+            else:
+                assert entity._msg.repository_visibility == _VersioningService.RepositoryVisibilityEnum.PRIVATE
+        finally:
+            entity.delete()
