baf2e60890b536ae83e3b33344dbb7237ed156d3
Michael Liu <miliu@protonmail.com>
2021-01-28 16:47:00 -0800

    Add client.get_workspace() test

--- client/verta/tests/test_permissions/test_workspace.py
+++ client/verta/tests/test_permissions/test_workspace.py
@@ -1,13 +1,18 @@
+import json
+
 import pytest
 
-from verta._internal_utils import _utils
+from verta._internal_utils import (
+    _config_utils,
+    _utils,
+)
 
 
 pytestmark = pytest.mark.not_oss
 
 
 class TestAttr:
-    """`project.workspace`, etc."""
+    """`workspace` attribute of entity objects."""
     @pytest.mark.parametrize(
         "entity_name",
         ["dataset", "project", "repository", "registered_model"],
@@ -49,3 +54,39 @@ class TestAttr:
         reg_model = client.create_registered_model(workspace=organization.name)
         created_entities.append(reg_model)
         assert reg_model.create_version().workspace == organization.name
+
+
+class TestClientGetWorkspace:
+    """Order of precedence for `client.get_workspace()`."""
+    def test_client_get_workspace(self, create_client, create_organization, created_entities, in_tempdir):
+        client = create_client()
+
+        WEBAPP_WORKSPACE = client._conn.get_default_workspace()
+        CONFIG_WORKSPACE = create_organization().name
+        CLIENT_WORKSPACE = create_organization().name
+        PARAM_WORKSPACE = create_organization().name
+
+        # default workspace
+        dataset = client.create_dataset()
+        created_entities.append(dataset)
+        assert client.get_workspace() == WEBAPP_WORKSPACE
+        assert dataset.workspace == WEBAPP_WORKSPACE
+
+        # client config file
+        with open(_config_utils.CONFIG_JSON_FILENAME, 'w') as f:
+            json.dump({'workspace': CONFIG_WORKSPACE}, f)
+        client = create_client()  # init new client to load config
+        dataset = client.create_dataset()
+        assert client.get_workspace() == CONFIG_WORKSPACE
+        assert dataset.workspace == CONFIG_WORKSPACE
+
+        # client.set_workspace()
+        client.set_workspace(CLIENT_WORKSPACE)
+        dataset = client.create_dataset()
+        assert client.get_workspace() == CLIENT_WORKSPACE
+        assert dataset.workspace == CLIENT_WORKSPACE
+
+        # workspace parameter
+        dataset = client.create_dataset(workspace=PARAM_WORKSPACE)
+        assert client.get_workspace != PARAM_WORKSPACE
+        assert dataset.workspace == PARAM_WORKSPACE
