25a058075fb6a6082139a8eeda2589b32178bc83
Heather MacDonald (she/her) <80712961+hmacdonald-verta@users.noreply.github.com>
2021-03-19 09:54:12 -0700

    [VR-10252] Add getters that do NOT block for the UAC and Workspace services (#2041)

--- backend/common/src/main/java/ai/verta/modeldb/common/connections/UAC.java
+++ backend/common/src/main/java/ai/verta/modeldb/common/connections/UAC.java
@@ -6,6 +6,8 @@ import ai.verta.modeldb.common.authservice.AuthInterceptor;
 import ai.verta.modeldb.common.config.Config;
 import ai.verta.modeldb.common.exceptions.UnavailableException;
 import ai.verta.uac.CollaboratorServiceGrpc;
+import ai.verta.uac.UACServiceGrpc;
+import ai.verta.uac.WorkspaceServiceGrpc;
 import io.grpc.ClientInterceptor;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
@@ -24,6 +26,8 @@ public class UAC {
   private ClientInterceptor clientInterceptor = null;
 
   private final CollaboratorServiceGrpc.CollaboratorServiceFutureStub collaboratorServiceFutureStub;
+  private final UACServiceGrpc.UACServiceFutureStub uacServiceFutureStub;
+  private final WorkspaceServiceGrpc.WorkspaceServiceFutureStub workspaceServiceFutureStub;
 
   public static UAC FromConfig(Config config) {
     if (!config.hasAuth()) return null;
@@ -32,28 +36,30 @@ public class UAC {
 
   private UAC(Config config) {
     this(
-        config.authService.host,
-        config.authService.port,
-        config.service_user.email,
-        config.service_user.devKey);
+            config.authService.host,
+            config.authService.port,
+            config.service_user.email,
+            config.service_user.devKey);
   }
 
   public UAC(String host, Integer port, String serviceUserEmail, String serviceUserDevKey) {
     LOGGER.trace(CommonMessages.HOST_PORT_INFO_STR, host, port);
     if (host != null && port != null) { // AuthService not available.
       authServiceChannel =
-          ManagedChannelBuilder.forTarget(host + CommonConstants.STRING_COLON + port)
-              .usePlaintext()
-              .build();
+              ManagedChannelBuilder.forTarget(host + CommonConstants.STRING_COLON + port)
+                      .usePlaintext()
+                      .build();
 
       this.serviceUserEmail = serviceUserEmail;
       this.serviceUserDevKey = serviceUserDevKey;
     } else {
       throw new UnavailableException(
-          "Host OR Port not found for contacting authentication service");
+              "Host OR Port not found for contacting authentication service");
     }
 
     collaboratorServiceFutureStub = CollaboratorServiceGrpc.newFutureStub(authServiceChannel);
+    uacServiceFutureStub = UACServiceGrpc.newFutureStub(authServiceChannel);
+    workspaceServiceFutureStub = WorkspaceServiceGrpc.newFutureStub(authServiceChannel);
   }
 
   private UAC(UAC other) {
@@ -62,6 +68,8 @@ public class UAC {
     serviceUserEmail = other.serviceUserEmail;
 
     collaboratorServiceFutureStub = other.collaboratorServiceFutureStub;
+    uacServiceFutureStub = other.uacServiceFutureStub;
+    workspaceServiceFutureStub = other.workspaceServiceFutureStub;
   }
 
   public UAC withServiceAccount() {
@@ -69,9 +77,9 @@ public class UAC {
     Metadata requestHeaders = new Metadata();
     Metadata.Key<String> email_key = Metadata.Key.of("email", Metadata.ASCII_STRING_MARSHALLER);
     Metadata.Key<String> dev_key =
-        Metadata.Key.of("developer_key", Metadata.ASCII_STRING_MARSHALLER);
+            Metadata.Key.of("developer_key", Metadata.ASCII_STRING_MARSHALLER);
     Metadata.Key<String> dev_key_hyphen =
-        Metadata.Key.of("developer-key", Metadata.ASCII_STRING_MARSHALLER);
+            Metadata.Key.of("developer-key", Metadata.ASCII_STRING_MARSHALLER);
     Metadata.Key<String> source_key = Metadata.Key.of("source", Metadata.ASCII_STRING_MARSHALLER);
 
     requestHeaders.put(email_key, this.serviceUserEmail);
@@ -89,6 +97,22 @@ public class UAC {
       return collaboratorServiceFutureStub.withInterceptors(clientInterceptor);
     }
     return collaboratorServiceFutureStub.withInterceptors(
-        MetadataUtils.newAttachHeadersInterceptor(AuthInterceptor.METADATA_INFO.get()));
+            MetadataUtils.newAttachHeadersInterceptor(AuthInterceptor.METADATA_INFO.get()));
+  }
+
+  public UACServiceGrpc.UACServiceFutureStub getUACService() {
+    if (clientInterceptor != null) {
+      return uacServiceFutureStub.withInterceptors(clientInterceptor);
+    }
+    return uacServiceFutureStub.withInterceptors(
+            MetadataUtils.newAttachHeadersInterceptor(AuthInterceptor.METADATA_INFO.get()));
+  }
+
+  public WorkspaceServiceGrpc.WorkspaceServiceFutureStub getWorkspaceService() {
+    if (clientInterceptor != null) {
+      return workspaceServiceFutureStub.withInterceptors(clientInterceptor);
+    }
+    return workspaceServiceFutureStub.withInterceptors(
+            MetadataUtils.newAttachHeadersInterceptor(AuthInterceptor.METADATA_INFO.get()));
   }
 }
