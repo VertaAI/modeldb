176357bb7bfdd82485eadbc90212b509d7426870
Michael Liu <miliu@protonmail.com>
2021-03-25 18:58:40 -0700

    Remove Series (#2060)

--- client/verta/verta/data_types/__init__.py
+++ client/verta/verta/data_types/__init__.py
@@ -5,5 +5,4 @@ from ._discrete_histogram import DiscreteHistogram
 from ._float_histogram import FloatHistogram
 from ._line import Line
 from ._matrix import Matrix
-from ._series import Series
 from ._table import Table
