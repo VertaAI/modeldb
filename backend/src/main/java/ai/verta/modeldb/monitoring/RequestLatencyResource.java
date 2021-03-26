62183d5ae53811bc8911166fc28d637410c98c9f
Jenkins <jenkins@verta.ai>
2021-03-26 04:19:03 +0000

    https://jenkins.dev.verta.ai/job/build/job/autoformat/job/modeldb-backend/1302/

--- backend/src/main/java/ai/verta/modeldb/monitoring/RequestLatencyResource.java
+++ backend/src/main/java/ai/verta/modeldb/monitoring/RequestLatencyResource.java
@@ -9,10 +9,9 @@ public class RequestLatencyResource implements Closeable {
           .labelNames("grpc_method")
           .name("verta_backend_requests_latency_seconds")
           .help("Request latency in seconds.")
-          .buckets(0.2, 0.4, 0.6, 0.8, 1.0,
-              1.2, 1.4, 1.6, 1.8, 2.0,
-              2.2, 2.4, 2.6, 2.8, 3.0,
-              3.2, 3.4, 3.6, 3.8, 4.0)
+          .buckets(
+              0.2, 0.4, 0.6, 0.8, 1.0, 1.2, 1.4, 1.6, 1.8, 2.0, 2.2, 2.4, 2.6, 2.8, 3.0, 3.2, 3.4,
+              3.6, 3.8, 4.0)
           .register();
   private final Histogram.Timer timer;
 
