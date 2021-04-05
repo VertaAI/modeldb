0c9c69660f73269d9523c9160e6c8a2fe3f7728e
Michael Liu <miliu@protonmail.com>
2021-04-05 11:53:46 -0700

    Support NumPy

--- client/verta/verta/data_types/_numeric_value.py
+++ client/verta/verta/data_types/_numeric_value.py
@@ -2,7 +2,7 @@ import numbers
 
 from ..external import six
 
-from .._internal_utils import _utils
+from .._internal_utils import arg_handler
 
 from . import _VertaDataType
 
@@ -11,14 +11,15 @@ class NumericValue(_VertaDataType):
     _TYPE_NAME = "numericValue"
     _VERSION = "v1"
 
+    @arg_handler.args_to_builtin(ignore_self=True)
     def __init__(self, value, unit=None):
         if not isinstance(value, numbers.Real):
             raise TypeError("`value` must be a number, not {}".format(type(value)))
         if unit and not isinstance(unit, six.string_types):
             raise TypeError("`unit` must be a string, not {}".format(type(unit)))
 
-        self._value = _utils.to_builtin(value)
-        self._unit = _utils.to_builtin(unit)
+        self._value = value
+        self._unit = unit
 
     def _as_dict(self):
         data = {"value": self._value}
--- client/verta/verta/data_types/_string_value.py
+++ client/verta/verta/data_types/_string_value.py
@@ -1,6 +1,6 @@
 from ..external import six
 
-from .._internal_utils import _utils
+from .._internal_utils import arg_handler
 
 from . import _VertaDataType
 
@@ -9,11 +9,12 @@ class StringValue(_VertaDataType):
     _TYPE_NAME = "stringValue"
     _VERSION = "v1"
 
+    @arg_handler.args_to_builtin(ignore_self=True)
     def __init__(self, value):
         if not isinstance(value, six.string_types):
             raise TypeError("`value` must be a string, not {}".format(type(value)))
 
-        self._value = _utils.to_builtin(value)
+        self._value = value
 
     def _as_dict(self):
         return self._as_dict_inner({
