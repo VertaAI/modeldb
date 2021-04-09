1a0a6e7d0d0fa901ddd516e3eec8bf614ef061b7
Michael Liu <miliu@protonmail.com>
2021-04-08 19:28:44 -0700

    Impl _AlertStatus.__eq__()

--- client/verta/verta/operations/monitoring/alert/status/_status.py
+++ client/verta/verta/operations/monitoring/alert/status/_status.py
@@ -12,6 +12,12 @@ from ....._protos.public.monitoring import Alert_pb2 as _AlertService
 class _AlertStatus(object):
     _ALERT_STATUS = _AlertService.AlertStatusEnum.UNKNOWN
 
+    def __eq__(self, other):
+        if not isinstance(other, _AlertStatus):
+            return NotImplemented
+
+        return self._ALERT_STATUS == other._ALERT_STATUS
+
     def __repr__(self):
         return "<{} alert status>".format(
             _AlertService.AlertStatusEnum.AlertStatus.Name(self._ALERT_STATUS).lower()
