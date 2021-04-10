50d9e39490e9f28e7648d0d4f7ae82ec5df842bf
Conrado Miranda <conrado@verta.ai>
2021-04-10 11:34:42 -0700

    Add futures package

---
+++ backend/common/src/main/java/ai/verta/modeldb/common/futures/FutureJdbi.java
@@ -0,0 +1,54 @@
+package ai.verta.modeldb.common.futures;
+
+import org.jdbi.v3.core.HandleCallback;
+import org.jdbi.v3.core.HandleConsumer;
+import org.jdbi.v3.core.Jdbi;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executor;
+
+public class FutureJdbi {
+  private final Executor executor;
+  private final Jdbi jdbi;
+
+  public FutureJdbi(Jdbi jdbi, Executor executor) {
+    this.executor = executor;
+    this.jdbi = jdbi;
+  }
+
+  public <R, T extends Exception> InternalFuture<R> withHandle(HandleCallback<R, T> callback) {
+    CompletableFuture<R> promise = new CompletableFuture<R>();
+
+    executor.execute(
+        () -> {
+          try {
+            promise.complete(jdbi.withHandle(callback));
+          } catch (Throwable e) {
+            promise.completeExceptionally(e);
+          }
+        });
+
+    return InternalFuture.from(promise);
+  }
+
+  public <R, T extends Exception> InternalFuture<R> withHandleCompose(
+      HandleCallback<InternalFuture<R>, T> callback) {
+    return withHandle(callback).thenCompose(x -> x, this.executor);
+  }
+
+  public <T extends Exception> InternalFuture<Void> useHandle(final HandleConsumer<T> consumer) {
+    CompletableFuture<Void> promise = new CompletableFuture<Void>();
+
+    executor.execute(
+        () -> {
+          try {
+            jdbi.useHandle(consumer);
+            promise.complete(null);
+          } catch (Throwable e) {
+            promise.completeExceptionally(e);
+          }
+        });
+
+    return InternalFuture.from(promise);
+  }
+}
