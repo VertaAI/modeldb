5443da0bb752fa5c4734eda7b49a817bc165162a
Conrado Miranda <conrado@verta.ai>
2021-04-13 20:37:32 -0700

    Add entity name as constructor parameter

--- backend/src/main/java/ai/verta/modeldb/experimentRun/subtypes/AttributeHandler.java
+++ backend/src/main/java/ai/verta/modeldb/experimentRun/subtypes/AttributeHandler.java
@@ -5,8 +5,8 @@ import ai.verta.modeldb.common.futures.FutureJdbi;
 import java.util.concurrent.Executor;
 
 public class AttributeHandler extends KeyValueHandler {
-  public AttributeHandler(Executor executor, FutureJdbi jdbi) {
-    super(executor, jdbi, "attributes");
+  public AttributeHandler(Executor executor, FutureJdbi jdbi, String entityName) {
+    super(executor, jdbi, "attributes", entityName);
   }
 
   @Override
