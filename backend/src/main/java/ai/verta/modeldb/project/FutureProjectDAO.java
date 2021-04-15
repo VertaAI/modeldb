87e8698f85169d5609fa99976be6ef702c87a7fc
Anand Jakhaniya <anand.jakhaniya@gmail.com>
2021-04-15 09:17:52 +0530

    FIx review comment https://github.com/VertaAI/modeldb/pull/2117#discussion_r613719639

--- backend/src/main/java/ai/verta/modeldb/project/FutureProjectDAO.java
+++ backend/src/main/java/ai/verta/modeldb/project/FutureProjectDAO.java
@@ -117,7 +117,7 @@ public class FutureProjectDAO {
                 .execute());
   }
 
-  private InternalFuture<Void> checkProjectPermission(
+  public InternalFuture<Void> checkProjectPermission(
       String projId, ModelDBActionEnum.ModelDBServiceActions action) {
     return FutureGrpc.ClientRequest(
             uac.getAuthzService()
