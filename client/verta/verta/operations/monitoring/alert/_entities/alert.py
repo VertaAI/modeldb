1e0094bd5c8bfccba8b584c36599efe8bd653370
Michael Liu <miliu@protonmail.com>
2021-04-15 15:57:01 -0700

    Take datetime in set_status()

--- client/verta/verta/operations/monitoring/alert/_entities/alert.py
+++ client/verta/verta/operations/monitoring/alert/_entities/alert.py
@@ -192,10 +192,10 @@ class Alert(entity._ModelDBEntity):
 
         self._update(alert_msg)
 
-    def set_status(self, status, event_time_millis=None):
+    def set_status(self, status, event_time=None):
         msg = _AlertService.UpdateAlertStatusRequest(
             alert_id=self.id,
-            event_time_millis=event_time_millis,
+            event_time_millis=time_utils.epoch_millis(event_time) if event_time else None,
             status=status._ALERT_STATUS,
             violating_summary_sample_ids=status._sample_ids,
         )
