1c4d450b5471c6da3ce450cd3b3abcbdde12e283
Michael Liu <miliu@protonmail.com>
2021-04-19 17:57:52 -0700

    Update config class tests

--- client/verta/tests/operations/monitoring/alerts/test_alerter.py
+++ client/verta/tests/operations/monitoring/alerts/test_alerter.py
@@ -38,26 +38,27 @@ class TestFixed:
 
 class TestReference:
     @hypothesis.given(
+        comparison=st.sampled_from(_VertaComparison.__subclasses__()),
         threshold=st.floats(allow_nan=False),
         reference_sample_id=st.integers(min_value=1, max_value=2 ** 64 - 1),
     )
-    def test_create(self, threshold, reference_sample_id):
-        alerter = ReferenceAlerter(threshold, reference_sample_id)
-        assert alerter._threshold == threshold
-        assert alerter._reference_sample_id == reference_sample_id
+    def test_create(self, comparison, threshold, reference_sample_id):
+        alerter = ReferenceAlerter(comparison(threshold), reference_sample_id)
 
         msg = _AlertService.AlertReference(
             threshold=threshold,
             reference_sample_id=reference_sample_id,
+            operator=comparison._OPERATOR,
         )
         assert alerter._as_proto() == msg
 
     @hypothesis.given(
+        comparison=st.sampled_from(_VertaComparison.__subclasses__()),
         threshold=st.floats(allow_nan=False),
         reference_sample_id=st.integers(min_value=1, max_value=2 ** 64 - 1),
     )
-    def test_repr(self, threshold, reference_sample_id):
+    def test_repr(self, comparison, threshold, reference_sample_id):
         """__repr__() does not raise exceptions"""
-        alerter = ReferenceAlerter(threshold, reference_sample_id)
+        alerter = ReferenceAlerter(comparison(threshold), reference_sample_id)
 
         assert repr(alerter)
