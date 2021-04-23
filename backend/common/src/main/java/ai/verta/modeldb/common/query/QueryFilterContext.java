b2aac2256ca9803f8ae7dea4ab0cbb6afd39ec4b
conradoverta <53195685+conradoverta@users.noreply.github.com>
2021-04-23 09:09:43 -0700

    Add pagination info to filter query (#2220)
    * Add pagination info to filter query
    
    * Add limit string

--- backend/common/src/main/java/ai/verta/modeldb/common/query/QueryFilterContext.java
+++ backend/common/src/main/java/ai/verta/modeldb/common/query/QueryFilterContext.java
@@ -4,17 +4,22 @@ import org.jdbi.v3.core.statement.Query;
 
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Optional;
 import java.util.function.Consumer;
 
 public class QueryFilterContext {
   private final List<String> conditions;
   private final List<Consumer<Query>> binds;
   private final List<OrderItem> orderItems;
+  private Optional<Long> pageNumber;
+  private Optional<Long> pageSize;
 
   public QueryFilterContext() {
     conditions = new LinkedList<>();
     binds = new LinkedList<>();
     orderItems = new LinkedList<>();
+    pageNumber = Optional.empty();
+    pageSize = Optional.empty();
   }
 
   public List<String> getConditions() {
@@ -27,6 +32,14 @@ public class QueryFilterContext {
 
   public List<OrderItem> getOrderItems() {
     return orderItems;
+  }
+
+  public Optional<Long> getPageNumber() {
+    return pageNumber;
+  }
+
+  public Optional<Long> getPageSize() {
+    return pageSize;
   }
 
   public QueryFilterContext addCondition(String condition) {
@@ -44,10 +57,22 @@ public class QueryFilterContext {
     return this;
   }
 
+  public QueryFilterContext setPageSize(Long pageSize) {
+    this.pageSize = Optional.ofNullable(pageSize);
+    return this;
+  }
+
+  public QueryFilterContext setPageNumber(Long pageNumber) {
+    this.pageNumber = Optional.ofNullable(pageNumber);
+    return this;
+  }
+
   public QueryFilterContext combine(QueryFilterContext other) {
     this.conditions.addAll(other.conditions);
     this.binds.addAll(other.binds);
     this.orderItems.addAll(other.orderItems);
+    this.pageNumber = this.pageNumber.or(() -> other.pageNumber);
+    this.pageSize = this.pageSize.or(() -> other.pageSize);
     return this;
   }
 
@@ -57,5 +82,27 @@ public class QueryFilterContext {
       ret = ret.combine(context);
     }
     return ret;
+  }
+
+  private static long calculatePageIndex(long pageNumber) {
+    // page numbers are 1-based, SQL offset is 0-based
+    return pageNumber == 0 ? 0L : pageNumber - 1;
+  }
+
+  private static long calculateOffset(long pageIndex, long pageLimit) {
+    final var offset = pageLimit * pageIndex;
+    return offset;
+  }
+
+  public String getLimitString() {
+    return pageSize.map(
+        size -> {
+          var ret = " LIMIT " + size;
+          return ret + pageNumber.map(number -> {
+            final var pageIndex = calculatePageIndex(number);
+            final var offset = calculateOffset(pageIndex, size);
+            return " OFFSET " + offset;
+          }).orElse("");
+        }).orElse("");
   }
 }
