5fbf3cfb367026e667a1748ed3ab9e678589ab10
Conrado Miranda <conrado@verta.ai>
2021-04-23 08:40:03 -0700

    Add pagination info to filter query

--- backend/common/src/main/java/ai/verta/modeldb/common/query/QueryFilterContext.java
+++ backend/common/src/main/java/ai/verta/modeldb/common/query/QueryFilterContext.java
@@ -4,17 +4,22 @@ import org.jdbi.v3.core.statement.Query;
 
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Optional;
 import java.util.function.Consumer;
 
 public class QueryFilterContext {
   private final List<String> conditions;
   private final List<Consumer<Query>> binds;
   private final List<OrderItem> orderItems;
+  private Optional<Long> pageNumber;
+  private Optional<Long> pageSize;
 
   public QueryFilterContext() {
     conditions = new LinkedList<>();
     binds = new LinkedList<>();
     orderItems = new LinkedList<>();
+    pageNumber = Optional.empty();
+    pageSize = Optional.empty();
   }
 
   public List<String> getConditions() {
@@ -27,6 +32,14 @@ public class QueryFilterContext {
 
   public List<OrderItem> getOrderItems() {
     return orderItems;
+  }
+
+  public Optional<Long> getPageNumber() {
+    return pageNumber;
+  }
+
+  public Optional<Long> getPageSize() {
+    return pageSize;
   }
 
   public QueryFilterContext addCondition(String condition) {
@@ -44,10 +57,22 @@ public class QueryFilterContext {
     return this;
   }
 
+  public QueryFilterContext setPageSize(Long pageSize) {
+    this.pageSize = Optional.ofNullable(pageSize);
+    return this;
+  }
+
+  public QueryFilterContext setPageNumber(Long pageNumber) {
+    this.pageNumber = Optional.ofNullable(pageNumber);
+    return this;
+  }
+
   public QueryFilterContext combine(QueryFilterContext other) {
     this.conditions.addAll(other.conditions);
     this.binds.addAll(other.binds);
     this.orderItems.addAll(other.orderItems);
+    this.pageNumber = this.pageNumber.or(() -> other.pageNumber);
+    this.pageSize = this.pageSize.or(() -> other.pageSize);
     return this;
   }
 
