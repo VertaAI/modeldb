0d1290c34fa77bdd707c46294e72d7cd68dd4b6d
Michael Liu <miliu@protonmail.com>
2021-05-04 11:13:40 -0700

    Deprecate log_training_data()

--- client/verta/verta/_tracking/deployable_entity.py
+++ client/verta/verta/_tracking/deployable_entity.py
@@ -372,6 +372,10 @@ class _DeployableEntity(_ModelDBEntity):
            Instead of uploading the data itself as a CSV artifact ``'train_data'``, this method now
            generates a histogram for internal use by our deployment data monitoring system.
 
+        .. deprecated:: 0.17.7
+            This method is no longer supported. Please see our documentation
+            for information about our platform's data monitoring features.
+
         Parameters
         ----------
         train_features : pd.DataFrame
@@ -382,6 +386,12 @@ class _DeployableEntity(_ModelDBEntity):
             Whether to allow overwriting existing training data.
 
         """
+        warnings.warn(
+            "This method is no longer supported. Please see our documentation"
+            " for information about our platform's data monitoring features".,
+            category=FutureWarning,
+        )
+
         if train_features.__class__.__name__ != "DataFrame":
             raise TypeError("`train_features` must be a pandas DataFrame, not {}".format(type(train_features)))
         if train_targets.__class__.__name__ == "Series":
