ed7864cacd4dbd6a9945f09ca93f143af787829e
Cory Johannsen <cory@verta.ai>
2021-05-12 14:16:56 -0700

    Minor change to trigger build

--- backend/src/main/java/ai/verta/modeldb/telemetry/TelemetryUtils.java
+++ backend/src/main/java/ai/verta/modeldb/telemetry/TelemetryUtils.java
@@ -50,9 +50,9 @@ public class TelemetryUtils {
           LOGGER.warn("modeldb_deployment_info table not found");
           LOGGER.info("Table modeldb_deployment_info creating");
 
-          String createModelDBDeploymentInfoQuery =
+          final var createModelDBDeploymentInfoQuery =
               "create table modeldb_deployment_info (md_key varchar(50),md_value varchar(255), creation_timestamp BIGINT) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci";
-          String createTelemetryInformationQuery =
+          final var createTelemetryInformationQuery =
               "Create table telemetry_information (tel_key varchar(50),tel_value varchar(255), collection_timestamp BIGINT, transfer_timestamp BIGINT, telemetry_consumer varchar(256)) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci";
 
           try (Statement statement = connection.createStatement()) {
