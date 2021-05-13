af93d8da52a04d97cccb174c658464ae3a788b94
Anand Jakhaniya <anand.jakhaniya@gmail.com>
2021-05-13 13:44:27 +0530

    Fix keyvalue handler bugs

--- backend/src/main/java/ai/verta/modeldb/experimentRun/subtypes/KeyValueHandler.java
+++ backend/src/main/java/ai/verta/modeldb/experimentRun/subtypes/KeyValueHandler.java
@@ -12,6 +12,7 @@ import ai.verta.modeldb.utils.ModelDBUtils;
 import com.google.protobuf.InvalidProtocolBufferException;
 import com.google.protobuf.Value;
 import java.util.AbstractMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
@@ -50,36 +51,46 @@ public class KeyValueHandler {
     }
   }
 
-  public InternalFuture<List<KeyValue>> getKeyValues(String entityId) {
+  public InternalFuture<List<KeyValue>> getKeyValues(
+      String entityId, List<String> attrKeys, boolean getAll) {
     return jdbi.withHandle(
-        handle ->
-            handle
-                .createQuery(
-                    "select kv_key as k, kv_value as v, value_type as t from "
-                        + getTableName()
-                        + " where entity_name=:entity_name and field_type=:field_type and "
-                        + entityIdReferenceColumn
-                        + "=:entity_id")
-                .bind("entity_id", entityId)
-                .bind("field_type", fieldType)
-                .bind("entity_name", entityName)
-                .map(
-                    (rs, ctx) -> {
-                      try {
-                        return KeyValue.newBuilder()
-                            .setKey(rs.getString("k"))
-                            .setValue(
-                                (Value.Builder)
-                                    CommonUtils.getProtoObjectFromString(
-                                        rs.getString("v"), Value.newBuilder()))
-                            .setValueTypeValue(rs.getInt("t"))
-                            .build();
-                      } catch (InvalidProtocolBufferException e) {
-                        LOGGER.error("Error generating builder for {}", rs.getString("v"));
-                        throw new ModelDBException(e);
-                      }
-                    })
-                .list());
+        handle -> {
+          var queryString =
+              "select kv_key as k, kv_value as v, value_type as t from "
+                  + getTableName()
+                  + " where entity_name=:entity_name and field_type=:field_type and "
+                  + entityIdReferenceColumn
+                  + "=:entity_id";
+
+          if (!attrKeys.isEmpty() && !getAll) {
+            queryString += " AND kv_key IN (<keys>)";
+          }
+          var query = handle.createQuery(queryString);
+          if (!attrKeys.isEmpty() && !getAll) {
+            query.bindList("keys", attrKeys);
+          }
+          return query
+              .bind("entity_id", entityId)
+              .bind("field_type", fieldType)
+              .bind("entity_name", entityName)
+              .map(
+                  (rs, ctx) -> {
+                    try {
+                      return KeyValue.newBuilder()
+                          .setKey(rs.getString("k"))
+                          .setValue(
+                              (Value.Builder)
+                                  CommonUtils.getProtoObjectFromString(
+                                      rs.getString("v"), Value.newBuilder()))
+                          .setValueTypeValue(rs.getInt("t"))
+                          .build();
+                    } catch (InvalidProtocolBufferException e) {
+                      LOGGER.error("Error generating builder for {}", rs.getString("v"));
+                      throw new ModelDBException(e);
+                    }
+                  })
+              .list();
+        });
   }
 
   public InternalFuture<MapSubtypes<KeyValue>> getKeyValuesMap(Set<String> entityIds) {
@@ -135,7 +146,13 @@ public class KeyValueHandler {
                 // Check for conflicts
                 jdbi.useHandle(
                     handle -> {
+                      Set<String> keySet = new HashSet<>();
                       for (final var kv : kvs) {
+                        if (keySet.contains(kv.getKey())) {
+                          throw new InvalidArgumentException(
+                              "Multiple Key " + kv.getKey() + " found in " + getTableName());
+                        }
+                        keySet.add(kv.getKey());
                         handle
                             .createQuery(
                                 "select id from "
