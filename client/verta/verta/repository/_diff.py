ac6fef2df1dc62d9fcc4080e2042a745e7859312
Michael Liu <miliu@protonmail.com>
2021-05-19 13:53:15 -0700

    Add docstring to Diff

--- client/verta/verta/repository/_diff.py
+++ client/verta/verta/repository/_diff.py
@@ -6,6 +6,20 @@ from .._protos.public.modeldb.versioning import Enums_pb2 as _VersioningEnumsPro
 
 
 class Diff(object):
+    r"""A diff between :class:`~verta.repository.Commit`\ s.
+
+    There should not be a need to instantiate this class directly; please use
+    :meth:`Commit.diff_from() <verta.repository.Commit.diff_from>`.
+
+    Examples
+    --------
+    .. code-block:: python
+
+        diff = commit_b.diff_from(commit_a)
+        print(diff)
+
+    """
+
     def __init__(self, diffs):
         # TODO: order so that additions come after removal?
         self._diffs = diffs
