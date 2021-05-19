66ae0d1c330374b3afe925b51895481160ddde2c
Anand Jakhaniya <anand.jakhaniya@gmail.com>
2021-05-19 10:49:36 +0530

    Find ER based on config hyperparameters (#2299)
    * Find ER based on config hyperparameters
    
    * Fix double precision in hyperparameters predicate handler
    
    * Fix sorting for hyperparameters (keyvalue fields) (#2310)

--- backend/common/src/main/java/ai/verta/modeldb/common/query/OrderItem.java
+++ backend/common/src/main/java/ai/verta/modeldb/common/query/OrderItem.java
@@ -1,9 +1,12 @@
 package ai.verta.modeldb.common.query;
 
+import java.util.List;
+
 public interface OrderItem {
   boolean getAscending();
 
   String getTable();
 
   String getColumn();
+  List<OrderColumn> getColumns();
 }
--- backend/src/main/java/ai/verta/modeldb/experimentRun/subtypes/SortingHandler.java
+++ backend/src/main/java/ai/verta/modeldb/experimentRun/subtypes/SortingHandler.java
@@ -6,6 +6,9 @@ import ai.verta.modeldb.common.query.OrderColumn;
 import ai.verta.modeldb.common.query.OrderTable;
 import ai.verta.modeldb.common.query.QueryFilterContext;
 import ai.verta.modeldb.exceptions.InvalidArgumentException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class SortingHandler {
   public InternalFuture<QueryFilterContext> processSort(String key, boolean ascending) {
@@ -27,23 +30,50 @@ public class SortingHandler {
 
     switch (names[0]) {
       case ModelDBConstants.METRICS:
-        return processKeyValueSort(names[1], ascending, "metrics");
+        return InternalFuture.completedInternalFuture(
+            processKeyValueSort(names[1], ascending, "metrics"));
       case ModelDBConstants.HYPERPARAMETERS:
-        return processKeyValueSort(names[1], ascending, "hyperparameters");
+        final var hyperparameterSoryPredicate =
+            processKeyValueSort(names[1], ascending, "hyperparameters");
+        final var configHyperparameterSoryPredicate =
+            processConfigHyperparametersSort(names[1], ascending);
+        List<QueryFilterContext> queryFilterContexts = new ArrayList<>();
+        queryFilterContexts.add(hyperparameterSoryPredicate);
+        queryFilterContexts.add(configHyperparameterSoryPredicate);
+        return InternalFuture.completedInternalFuture(
+            QueryFilterContext.combine(queryFilterContexts));
     }
 
     return InternalFuture.failedStage(new InvalidArgumentException("Sort key cannot be handled"));
   }
 
-  private InternalFuture<QueryFilterContext> processKeyValueSort(
-      String key, boolean ascending, String fieldType) {
+  private QueryFilterContext processKeyValueSort(String key, boolean ascending, String fieldType) {
     var sql =
-        "select experiment_run_id as id, kv_value as value from keyvalue where entity_name=\"ExperimentRunEntity\" and field_type=:sort_field_type and kv_key=:sort_key";
+        "select experiment_run_id as id, kv_value as value from keyvalue where entity_name=:entityName and field_type=:sort_field_type and kv_key=:sort_key";
     var queryContext =
         new QueryFilterContext()
             .addBind(q -> q.bind("sort_field_type", fieldType))
+            .addBind(q -> q.bind("entityName", "ExperimentRunEntity"))
             .addBind(q -> q.bind("sort_key", key));
-    queryContext.addOrderItem(new OrderTable(sql, ascending));
-    return InternalFuture.completedInternalFuture(queryContext);
+    queryContext.addOrderItem(
+        new OrderTable(
+            sql, ascending, Collections.singletonList(new OrderColumn("value", ascending))));
+    return queryContext;
+  }
+
+  private QueryFilterContext processConfigHyperparametersSort(String key, boolean ascending) {
+    var hyperparameterFromBlobMappingSql =
+        "select distinct experiment_run_id as id, int_value, float_value, string_value from hyperparameter_element_mapping where entity_type=:entity_type and experiment_run_id IS NOT NULL and name=:name";
+    var queryContext =
+        new QueryFilterContext()
+            .addBind(q -> q.bind("entity_type", "ExperimentRunEntity"))
+            .addBind(q -> q.bind("name", key));
+    List<OrderColumn> orderColumnList = new ArrayList<>();
+    orderColumnList.add(new OrderColumn("int_value", ascending));
+    orderColumnList.add(new OrderColumn("float_value", ascending));
+    orderColumnList.add(new OrderColumn("string_value", ascending));
+    queryContext.addOrderItem(
+        new OrderTable(hyperparameterFromBlobMappingSql, ascending, orderColumnList));
+    return queryContext;
   }
 }
