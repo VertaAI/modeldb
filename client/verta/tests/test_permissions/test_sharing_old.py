15d25cbbec5fc7d0bad9e66a45399b2fffdfae3c
Michael Liu <miliu@protonmail.com>
2021-05-24 13:51:44 -0700

    Minor cleanup for test cleanups (#2339)
    * Add created_entities where missing
    
    * Group related lines

--- client/verta/tests/test_permissions/test_sharing_old.py
+++ client/verta/tests/test_permissions/test_sharing_old.py
@@ -61,26 +61,26 @@ class TestProject:
 
 
 class TestDataset:
-    def test_org_public_dataset(self, client, organization, client_2, email_2):
+    def test_org_public_dataset(self, client, organization, client_2, email_2, created_entities):
         """
         User 2 tries to access a org-public dataset created by a user in the same organization.
         """
         dataset_name = _utils.generate_default_name()
         dataset = client.create_dataset(dataset_name, workspace=organization.name, public_within_org=True)
+        created_entities.append(dataset)
 
         organization.add_member(email_2)
 
         assert client_2.get_dataset(id=dataset.id)
         assert client_2.get_dataset(name=dataset.name, workspace=organization.name)
 
-        dataset.delete()
-
-    def test_non_org_public_dataset_access_error(self, client, organization, client_2, email_2):
+    def test_non_org_public_dataset_access_error(self, client, organization, client_2, email_2, created_entities):
         """
         User 2 tries to access a non-org-public dataset created by a user in the same organization.
         """
         dataset_name = _utils.generate_default_name()
         dataset = client.create_dataset(dataset_name, workspace=organization.name, public_within_org=False)
+        created_entities.append(dataset)
 
         organization.add_member(email_2)
 
@@ -88,30 +88,28 @@ class TestDataset:
         with pytest.raises(ValueError, match="not found"):
             client_2.get_dataset(id=dataset.id)
 
-        dataset.delete()
-
 
 class TestRegisteredModel:
-    def test_org_public_registered_model(self, client, organization, client_2, email_2):
+    def test_org_public_registered_model(self, client, organization, client_2, email_2, created_entities):
         """
         User 2 tries to access a org-public registered_model created by a user in the same organization.
         """
         registered_model_name = _utils.generate_default_name()
         registered_model = client.create_registered_model(registered_model_name, workspace=organization.name, public_within_org=True)
+        created_entities.append(registered_model)
 
         organization.add_member(email_2)
 
         assert client_2.get_registered_model(id=registered_model.id)
         assert client_2.get_registered_model(name=registered_model.name, workspace=organization.name)
 
-        registered_model.delete()
-
-    def test_non_org_public_registered_model_access_error(self, client, organization, client_2, email_2):
+    def test_non_org_public_registered_model_access_error(self, client, organization, client_2, email_2, created_entities):
         """
         User 2 tries to access a non-org-public registered_model created by a user in the same organization.
         """
         registered_model_name = _utils.generate_default_name()
         registered_model = client.create_registered_model(registered_model_name, workspace=organization.name, public_within_org=False)
+        created_entities.append(registered_model)
 
         organization.add_member(email_2)
 
@@ -119,30 +117,28 @@ class TestRegisteredModel:
         with pytest.raises(ValueError, match="not found"):
             client_2.get_registered_model(id=registered_model.id)
 
-        registered_model.delete()
-
 
 class TestRepository:
-    def test_org_public_repository(self, client, organization, client_2, email_2):
+    def test_org_public_repository(self, client, organization, client_2, email_2, created_entities):
         """
         User 2 tries to access a org-public repository created by a user in the same organization.
         """
         repository_name = _utils.generate_default_name()
         repository = client.set_repository(repository_name, workspace=organization.name, public_within_org=True)
+        created_entities.append(repository)
 
         organization.add_member(email_2)
 
         assert client_2.get_or_create_repository(id=repository.id)
         assert client_2.get_or_create_repository(name=repository.name, workspace=organization.name).id == repository.id
 
-        repository.delete()
-
-    def test_non_org_public_repository_access_error(self, client, organization, client_2, email_2):
+    def test_non_org_public_repository_access_error(self, client, organization, client_2, email_2, created_entities):
         """
         User 2 tries to access a non-org-public repository created by a user in the same organization.
         """
         repository_name = _utils.generate_default_name()
         repository = client.set_repository(repository_name, workspace=organization.name, public_within_org=False)
+        created_entities.append(repository)
 
         organization.add_member(email_2)
 
@@ -150,11 +146,9 @@ class TestRepository:
         with pytest.raises(ValueError, match="no Repository found"):
             client_2.get_or_create_repository(id=repository.id)
 
-        repository.delete()
-
 
 class TestEndpoint:
-    def test_org_endpoint(self, client, organization, client_2, email_2):
+    def test_org_endpoint(self, client, organization, client_2, email_2, created_entities):
         """
         Non-owner access to org-public endpoint and private endpoint within an org.
         """
@@ -164,12 +158,12 @@ class TestEndpoint:
         # ORG_SCOPED_PUBLIC
         public_path = "public-{}".format(path)
         endpoint = client.create_endpoint(public_path, workspace=organization.name, public_within_org=True)
+        created_entities.append(endpoint)
         client_2.get_endpoint(public_path, workspace=organization.name)
-        endpoint.delete()
 
         # PRIVATE
         private_path = "private-{}".format(path)
         endpoint = client.create_endpoint(private_path, workspace=organization.name, public_within_org=False)
+        created_entities.append(endpoint)
         with pytest.raises(ValueError, match="Endpoint not found"):
             client_2.get_endpoint(private_path, workspace=organization.name)
-        endpoint.delete()
