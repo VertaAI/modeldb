675b447009e16b518c5578ff0e2f5b69d467d73c
Daniel Fennelly <daniel@verta.ai>
2021-05-24 16:32:30 -0700

    Add time utils for aggregation granularity

--- client/verta/verta/_internal_utils/time_utils/__init__.py
+++ client/verta/verta/_internal_utils/time_utils/__init__.py
@@ -3,7 +3,10 @@
 from datetime import datetime, timedelta
 import time
 
+import pytimeparse
+
 from ...external.six import PY2, PY3
+
 
 if PY3:
     from ._utc_py3 import utc
@@ -33,5 +36,32 @@ def epoch_millis(dt):
         raise ValueError("Cannot convert argument to epoch milliseconds")
 
 
+def timedelta_millis(delta):
+    if isinstance(delta, timedelta):
+        millis_from_seconds = delta.total_seconds() * 1000
+        millis_from_micros = delta.microseconds // 1000
+        return millis_from_seconds + millis_from_micros
+    elif type(delta) == int and delta > 0:
+        return delta
+    else:
+        raise ValueError("Cannot convert argument to duration milliseconds")
+
+
 def datetime_from_millis(millis):
     return UNIX_EPOCH + timedelta(milliseconds=millis)
+
+
+def parse_duration(value):
+    if type(value) is str:
+        try:
+            dur_seconds = pytimeparse.parse(value)
+            return timedelta(seconds=dur_seconds)
+        except:
+            raise ValueError("Cannot convert string argument to timedelta")
+    if type(value) is int:
+        if value < 0:
+            raise ValueError("Cannot accept negative integer as a millisecond duration")
+        return timedelta(milliseconds=value)
+    if type(value) is timedelta:
+        return value
+    raise ValueError("Cannot convert argument to a time duration")
