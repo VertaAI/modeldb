d61dc261664f8d39393c41ebe6797d713c799ce3
Michael Liu <miliu@protonmail.com>
2021-05-25 14:54:28 -0700

    Reset test accts' default workspace

--- client/verta/tests/conftest.py
+++ client/verta/tests/conftest.py
@@ -307,6 +307,9 @@ def mark_time():
 @pytest.fixture
 def client(host, port, email, dev_key, created_entities):
     client = Client(host, port, email, dev_key, debug=True)
+    client._conn._set_default_workspace(
+        client._conn.get_personal_workspace(),
+    )
 
     yield client
 
@@ -335,8 +338,16 @@ def client_2(host, port, email_2, dev_key_2, created_entities):
         pytest.skip("second account credentials not present")
 
     client = Client(host, port, email_2, dev_key_2, debug=True)
+    client._conn._set_default_workspace(
+        client._conn.get_personal_workspace(),
+    )
 
-    return client
+    yield client
+
+    proj = client._ctx.proj
+    if (proj is not None
+            and proj.id not in {entity.id for entity in created_entities}):
+        proj.delete()
 
 
 @pytest.fixture
@@ -346,8 +357,16 @@ def client_3(host, port, email_3, dev_key_3, created_entities):
         pytest.skip("second account credentials not present")
 
     client = Client(host, port, email_3, dev_key_3, debug=True)
+    client._conn._set_default_workspace(
+        client._conn.get_personal_workspace(),
+    )
 
-    return client
+    yield client
+
+    proj = client._ctx.proj
+    if (proj is not None
+            and proj.id not in {entity.id for entity in created_entities}):
+        proj.delete()
 
 
 @pytest.fixture
