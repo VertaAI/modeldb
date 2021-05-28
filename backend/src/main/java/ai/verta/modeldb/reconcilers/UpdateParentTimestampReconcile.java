5afd126de3792146cb47104bc38ffbd9eed24e7d
Anand Jakhaniya <anand.jakhaniya@gmail.com>
2021-05-28 12:49:55 +0530

    Fix review comment about max updated date and reduce one query

--- backend/src/main/java/ai/verta/modeldb/reconcilers/UpdateParentTimestampReconcile.java
+++ backend/src/main/java/ai/verta/modeldb/reconcilers/UpdateParentTimestampReconcile.java
@@ -5,6 +5,7 @@ import ai.verta.modeldb.common.reconcilers.Reconciler;
 import ai.verta.modeldb.common.reconcilers.ReconcilerConfig;
 import ai.verta.modeldb.utils.ModelDBHibernateUtil;
 import java.sql.PreparedStatement;
+import java.util.AbstractMap;
 import java.util.List;
 import java.util.Set;
 import org.apache.logging.log4j.LogManager;
@@ -12,7 +13,8 @@ import org.apache.logging.log4j.Logger;
 import org.hibernate.Session;
 import org.hibernate.query.Query;
 
-public class UpdateParentTimestampReconcile extends Reconciler<Long> {
+public class UpdateParentTimestampReconcile
+    extends Reconciler<AbstractMap.SimpleEntry<Long, Long>> {
   private static final Logger LOGGER = LogManager.getLogger(UpdateParentTimestampReconcile.class);
   private static final ModelDBHibernateUtil modelDBHibernateUtil =
       ModelDBHibernateUtil.getInstance();
@@ -24,7 +26,7 @@ public class UpdateParentTimestampReconcile extends Reconciler<Long> {
   @Override
   public void resync() {
     var fetchUpdatedDatasetIds =
-        new StringBuilder("SELECT rc.repository_id ")
+        new StringBuilder("SELECT rc.repository_id, MAX(cm.date_created) AS max_date ")
             .append(" FROM commit cm INNER JOIN repository_commit rc ")
             .append(" ON rc.commit_hash = cm.commit_hash ")
             .append(" INNER JOIN commit_parent cp ")
@@ -38,41 +40,31 @@ public class UpdateParentTimestampReconcile extends Reconciler<Long> {
     try (Session session = modelDBHibernateUtil.getSessionFactory().openSession()) {
       Query fetchUpdatedDatasetIdQuery = session.createSQLQuery(fetchUpdatedDatasetIds);
       fetchUpdatedDatasetIdQuery.setMaxResults(config.maxSync);
-      fetchUpdatedDatasetIdQuery.stream().forEach(id -> this.insert(Long.parseLong(id.toString())));
+      List<Object[]> selectedFieldsList = fetchUpdatedDatasetIdQuery.list();
+      selectedFieldsList.stream()
+          .forEach(
+              selectedFields -> {
+                Long datasetId = Long.parseLong(selectedFields[0].toString());
+                Long maxUpdatedDate = Long.parseLong(selectedFields[1].toString());
+                this.insert(new AbstractMap.SimpleEntry<>(datasetId, maxUpdatedDate));
+              });
     }
   }
 
   @Override
-  protected ReconcileResult reconcile(Set<Long> ids) {
-    LOGGER.debug("Reconciling projects " + ids.toString());
+  protected ReconcileResult reconcile(Set<AbstractMap.SimpleEntry<Long, Long>> updatedMaxDateMap) {
+    LOGGER.debug("Reconciling parent time update for repository" + updatedMaxDateMap.size());
 
     try (Session session = modelDBHibernateUtil.getSessionFactory().openSession()) {
-      var fetchUpdatedTimeQueryString =
-          new StringBuilder("SELECT rc.repository_id, MAX(cm.date_created) AS max_date ")
-              .append(" FROM commit cm INNER JOIN repository_commit rc ")
-              .append(" ON rc.commit_hash = cm.commit_hash ")
-              .append(" INNER JOIN commit_parent cp ")
-              .append(" ON cp.parent_hash IS NOT NULL ")
-              .append(" AND cp.child_hash = cm.commit_hash ")
-              .append(" INNER JOIN repository rp ")
-              .append(" ON rp.id = rc.repository_id AND rp.date_updated < cm.date_created ")
-              .append(" WHERE rp.id IN (:ids) ")
-              .append(" GROUP BY rc.repository_id")
-              .toString();
-
-      Query updateDeletedChildrenQuery = session.createSQLQuery(fetchUpdatedTimeQueryString);
-      updateDeletedChildrenQuery.setParameterList("ids", ids);
-      List<Object[]> fetchUpdatedTimeQuery = updateDeletedChildrenQuery.list();
-
       var updateDatasetTimestamp = "UPDATE repository SET date_updated = ? WHERE id = ?";
       session.doWork(
           connection -> {
             try (PreparedStatement preparedStatement =
                 connection.prepareStatement(updateDatasetTimestamp)) {
               int i = 1;
-              for (Object[] updatedRecord : fetchUpdatedTimeQuery) {
-                long id = Long.parseLong(String.valueOf(updatedRecord[0]));
-                long updatedDate = Long.parseLong(String.valueOf(updatedRecord[1]));
+              for (AbstractMap.SimpleEntry<Long, Long> updatedRecord : updatedMaxDateMap) {
+                long id = updatedRecord.getKey();
+                long updatedDate = updatedRecord.getValue();
                 preparedStatement.setLong(1, updatedDate);
                 preparedStatement.setLong(2, id);
                 preparedStatement.addBatch();
