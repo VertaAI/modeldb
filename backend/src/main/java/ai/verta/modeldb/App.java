6b91ce28cf295ea052a8cc4db06a5d781beefaef
Anand Jakhaniya <anand.jakhaniya@gmail.com>
2021-05-28 15:39:44 +0530

    Implement update timestamp reconcile util using jdbi Fix review comment about max updated date and reduce one query

--- backend/src/main/java/ai/verta/modeldb/App.java
+++ backend/src/main/java/ai/verta/modeldb/App.java
@@ -198,7 +198,7 @@ public class App implements ApplicationContextAware {
 
       // Initialize cron jobs
       CronJobUtils.initializeCronJobs(config, services);
-      ReconcilerInitializer.initialize(config, services);
+      ReconcilerInitializer.initialize(config, services, config.getJdbi());
 
       // Initialize grpc server
       ServerBuilder<?> serverBuilder =
