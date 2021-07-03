3e451a55dc9854552abe16aefc94d84fffdfd714
Conrado Miranda <conrado@verta.ai>
2021-07-02 23:05:46 -0700

    Fix backend tests

--- backend/src/test/java/ai/verta/modeldb/ExperimentTest.java
+++ backend/src/test/java/ai/verta/modeldb/ExperimentTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.*;
 
 import ai.verta.common.Artifact;
 import ai.verta.common.ArtifactTypeEnum.ArtifactType;
+import ai.verta.common.CodeVersion;
 import ai.verta.common.KeyValue;
 import ai.verta.common.ValueTypeEnum.ValueType;
 import com.google.protobuf.ListValue;
