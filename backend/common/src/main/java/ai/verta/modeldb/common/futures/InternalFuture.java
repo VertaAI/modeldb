ccc590884637cb37f909e9c08dc1dfd9b0d4acff
conradoverta <53195685+conradoverta@users.noreply.github.com>
2021-07-20 13:56:48 -0700

    Add tool for future tracing (#2502)

--- backend/common/src/main/java/ai/verta/modeldb/common/futures/InternalFuture.java
+++ backend/common/src/main/java/ai/verta/modeldb/common/futures/InternalFuture.java
@@ -1,10 +1,17 @@
 package ai.verta.modeldb.common.futures;
 
+import io.opentracing.Span;
+import io.opentracing.SpanContext;
+import io.opentracing.Tracer;
+import io.opentracing.contrib.grpc.ActiveSpanContextSource;
+import io.opentracing.contrib.grpc.ActiveSpanSource;
+import io.opentracing.tag.Tags;
 import org.apache.logging.log4j.util.TriConsumer;
 
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionStage;
 import java.util.concurrent.ExecutionException;
@@ -16,6 +23,52 @@ public class InternalFuture<T> {
     private CompletionStage<T> stage;
 
     private InternalFuture() {
+    }
+
+    static private SpanContext getActiveSpanContext(Tracer tracer) {
+        Span activeSpan = ActiveSpanSource.GRPC_CONTEXT.getActiveSpan();
+        if (activeSpan != null) {
+            return activeSpan.context();
+        }
+
+        SpanContext spanContext = ActiveSpanContextSource.GRPC_CONTEXT.getActiveSpanContext();
+        if (spanContext != null) {
+            return spanContext;
+        }
+
+        return tracer.activeSpan() != null ? tracer.activeSpan().context() : null;
+    }
+
+    static private Span createSpanFromParent(Tracer tracer, SpanContext parentSpanContext, String operationName, Map<String,String> tags) {
+        Tracer.SpanBuilder spanBuilder;
+        if (parentSpanContext == null) {
+            spanBuilder = tracer.buildSpan(operationName);
+        } else {
+            spanBuilder = tracer.buildSpan(operationName).asChildOf(parentSpanContext);
+        }
+
+        for (var entry : tags.entrySet()) {
+            spanBuilder = spanBuilder.withTag(entry.getKey(), entry.getValue());
+        }
+
+        return spanBuilder.start();
+    }
+
+    public static <T> InternalFuture<T> trace(Supplier<InternalFuture<T>> supplier, String operationName, Map<String,String> tags, Tracer tracer, Executor executor) {
+        final var spanContext = getActiveSpanContext(tracer);
+        final var span = createSpanFromParent(tracer, spanContext, operationName, tags);
+
+        final var promise = new CompletableFuture<T>();
+        supplier.get().stage.whenComplete((v, t) -> {
+            span.finish();
+            if (t != null) {
+                promise.completeExceptionally(t);
+            } else {
+                promise.complete(v);
+            }
+        });
+
+        return InternalFuture.from(promise);
     }
 
     // Convert a list of futures to a future of a list
