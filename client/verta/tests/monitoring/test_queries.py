4843e99508dd5e00cc0647a1ceb4cb377b903414
Michael Liu <miliu@protonmail.com>
2021-07-28 14:09:58 -0700

    Clarify comment

--- client/verta/tests/monitoring/test_queries.py
+++ client/verta/tests/monitoring/test_queries.py
@@ -10,7 +10,7 @@ class TestSummarySampleQuery:
         empty_from_proto = SummarySampleQuery._from_proto_request(empty_to_proto)
         assert empty_query == empty_from_proto
 
-        # ensure that the aggregation submessage isn't being set at all,
+        # ensure that the aggregation submessage isn't set when not provided,
         # which has caused backend errors in the past
         assert not empty_to_proto.HasField("aggregation")
         assert empty_from_proto.aggregation is None
