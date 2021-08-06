cb8694dc6370d3cac724cc896d1877bd75f068a0
Cory Johannsen <73903791+coryverta@users.noreply.github.com>
2021-08-06 14:55:38 -0700

    Add server thread count to SpringConfig (#2541)
    * Add threadCount property to SpringServerConfig
    
    * Add server thread count to SpringConfig

--- backend/common/src/main/java/ai/verta/modeldb/common/config/SpringServerConfig.java
+++ backend/common/src/main/java/ai/verta/modeldb/common/config/SpringServerConfig.java
@@ -3,6 +3,7 @@ package ai.verta.modeldb.common.config;
 public class SpringServerConfig {
   public int port;
   public Long shutdownTimeout = 30L;
+  public int threadCount = 32;
 
   public void Validate(String base) throws InvalidConfigException {
     if (port == 0) throw new InvalidConfigException(base + ".port", Config.MISSING_REQUIRED);
