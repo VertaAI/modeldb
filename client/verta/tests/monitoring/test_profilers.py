9fe6b17dca48948923934a58af5fe3cd5450c1a0
Michael Liu <miliu@protonmail.com>
2021-08-13 15:36:59 -0700

    Add tests

--- client/verta/tests/monitoring/test_profilers.py
+++ client/verta/tests/monitoring/test_profilers.py
@@ -16,12 +16,12 @@ from verta.monitoring._profilers import ProfilerReference
 from . import strategies
 
 
-class TestProfilers:
+class TestContinuousHistogramProfiler:
     @hypothesis.settings(deadline=None)  # building DataFrames can be slow
     @hypothesis.given(
         df=strategies.simple_dataframes(),  # pylint: disable=no-value-for-parameter
     )
-    def test_continuous(self, df):
+    def test_profile(self, df):
         np = pytest.importorskip("numpy")
 
         cols = ["continuous"]
@@ -40,16 +40,63 @@ class TestProfilers:
             # missing values omitted
             assert sum(data._data) == sum(~series.isna())
 
+    def test_profile_point(self):
+        np = pytest.importorskip("numpy")
+        pd = pytest.importorskip("pandas")
+        colname = "data"
+        df = pd.DataFrame(
+            {
+                colname: np.r_[
+                    0,
+                    np.abs(np.random.normal(loc=10, scale=5, size=50)),
+                    None,
+                ],
+            }
+        )
+
+        profiler = ContinuousHistogramProfiler([colname])
+        reference = profiler.profile(df)[colname + "_histogram"]
+
+        for value in df[colname].dropna():
+            profile = profiler.profile_point(value, reference)
+            assert profile._bucket_limits == reference._bucket_limits
+            assert sum(profile._data) == 1
+
+            # check value is in correct bucket
+            lower_bucket_limit = max(filter(
+                lambda bucket: bucket <= value,
+                profile._bucket_limits,
+            ))
+            data_index = min(
+                profile._bucket_limits.index(lower_bucket_limit),
+                len(profile._data) - 1,  # right-most bucket
+            )
+            assert profile._data[data_index] == 1
+
+        # value not in reference
+        value = -5
+        profile = profiler.profile_point(value, reference)
+        assert profile._bucket_limits == reference._bucket_limits
+        assert sum(profile._data) == 0
+
+        # missing value
+        value = None
+        profile = profiler.profile_point(value, reference)
+        assert profile._bucket_limits == reference._bucket_limits
+        assert sum(profile._data) == 0
+
+
+class TestBinaryHistogramProfiler:
     @hypothesis.settings(deadline=None)  # building DataFrames can be slow
     @hypothesis.given(
         df=strategies.simple_dataframes(),  # pylint: disable=no-value-for-parameter
     )
-    def test_discrete(self, df):
+    def test_profile(self, df):
         cols = ["discrete"]
 
         profiler = BinaryHistogramProfiler(cols)
-        profile = profiler.profile(df)
 
+        profile = profiler.profile(df)
         assert len(profile) == len(cols)
         for name, data in profile.items():
             assert isinstance(data, data_types.DiscreteHistogram)
@@ -61,16 +108,44 @@ class TestProfilers:
             # missing values omitted
             assert sum(data._data) == sum(~series.isna())
 
+    def test_profile_point(self):
+        pd = pytest.importorskip("pandas")
+        colname = "data"
+        df = pd.DataFrame({colname: [0, 1, None]})
+
+        profiler = BinaryHistogramProfiler([colname])
+        reference = profiler.profile(df)[colname + "_histogram"]
+
+        for value in [0, 1]:  # 0 (when in reference) should not be discarded
+            profile = profiler.profile_point(value, reference)
+            assert profile._buckets == reference._buckets
+            assert sum(profile._data) == 1
+            assert profile._data[profile._buckets.index(value)] == 1
+
+        # value not in reference
+        value = 2
+        profile = profiler.profile_point(value, reference)
+        assert profile._buckets == reference._buckets
+        assert sum(profile._data) == 0
+
+        # missing value
+        value = None
+        profile = profiler.profile_point(value, reference)
+        assert profile._buckets == reference._buckets
+        assert sum(profile._data) == 0
+
+
+class TestMissingValuesProfiler:
     @hypothesis.settings(deadline=None)  # building DataFrames can be slow
     @hypothesis.given(
         df=strategies.simple_dataframes(),  # pylint: disable=no-value-for-parameter
     )
-    def test_missing(self, df):
+    def test_profile(self, df):
         cols = ["continuous", "discrete"]
 
         profiler = MissingValuesProfiler(cols)
-        profile = profiler.profile(df)
 
+        profile = profiler.profile(df)
         assert len(profile) == len(cols)
         for name, data in profile.items():
             assert isinstance(data, data_types.DiscreteHistogram)
@@ -78,6 +153,27 @@ class TestProfilers:
             series = df[name.split("_missing")[0]]
             assert data._data[data._buckets.index("present")] == sum(~series.isna())
             assert data._data[data._buckets.index("missing")] == sum(series.isna())
+
+    def test_profile_point(self):
+        pd = pytest.importorskip("pandas")
+        colname = "data"
+        df = pd.DataFrame({colname: [0, 1, None]})
+
+        profiler = MissingValuesProfiler([colname])
+        reference = profiler.profile(df)[colname + "_missing"]
+
+        for value in [0, 1, 2]:
+            profile = profiler.profile_point(value, reference)
+            assert profile._buckets == reference._buckets
+            assert profile._data[profile._buckets.index("present")] == 1
+            assert profile._data[profile._buckets.index("missing")] == 0
+
+        # missing value
+        value = None
+        profile = profiler.profile_point(value, reference)
+        assert profile._buckets == reference._buckets
+        assert profile._data[profile._buckets.index("present")] == 0
+        assert profile._data[profile._buckets.index("missing")] == 1
 
 
 class TestLiveProfilers(object):
