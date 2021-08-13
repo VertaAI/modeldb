e3626e9d19c00b16fa8a082f164d774812dd35f7
Michael Liu <miliu@protonmail.com>
2021-08-13 13:10:45 -0700

    Allow 0 in BinHistProf.profile_point()

--- client/verta/verta/monitoring/profiler.py
+++ client/verta/verta/monitoring/profiler.py
@@ -176,7 +176,7 @@ class BinaryHistogramProfiler(Profiler):
         buckets = reference._buckets
         data = [0]*len(buckets)
 
-        if sample:
+        if sample is not None:
             data[buckets.index(sample)] = 1
         return DiscreteHistogram(buckets, data)
 
