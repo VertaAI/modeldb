c5e7df31338aaf377ff82218e669042bac5bb4df
Michael Liu <miliu@protonmail.com>
2021-09-16 17:01:39 -0700

    Add unit tests

--- client/verta/verta/registry/_docker_image.py
+++ client/verta/verta/registry/_docker_image.py
@@ -51,6 +51,23 @@ class DockerImage(object):
     env_vars : dict of str to str, or None
         Environment variables.
 
+    Examples
+    --------
+    .. code-block:: python
+
+        from verta.registry import DockerImage
+
+        docker_image = DockerImage(
+            port=5000,
+            request_path="/predict_json",
+            health_path="/health",
+
+            repository="012345678901.dkr.ecr.apne2-az1.amazonaws.com/models/example",
+            tag="example",
+
+            env_vars={"CUDA_VISIBLE_DEVICES": "0,1"},
+        )
+
     """
 
     def __init__(
