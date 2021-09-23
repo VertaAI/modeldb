bb958c6fcf7c91b4292eb0d9ed870ccadd802e53
Michael Liu <miliu@protonmail.com>
2021-09-23 12:24:39 -0700

    Add _StageChange base class

---
+++ client/verta/verta/registry/stage_change/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+"""Stage changes for registered model versions."""
+
+from verta._internal_utils import documentation
+
+
+documentation.reassign_module(
+    [],
+    module_name=__name__,
+)
