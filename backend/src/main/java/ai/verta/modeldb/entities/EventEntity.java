d99a920eda35144c266f5f407693fdb610fe7379
Anand Jakhaniya <anand.jakhaniya@gmail.com>
2021-09-27 16:07:21 +0530

    Event java entity

---
+++ backend/src/main/java/ai/verta/modeldb/entities/EventEntity.java
@@ -0,0 +1,26 @@
+package ai.verta.modeldb.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class EventEntity implements Serializable {
+    private EventEntity() {}
+
+    String eventUUID; // uuid for this event to handle deduplication
+    String eventType; // Service-specific name for this type of event
+    Long workspaceId; // Required
+    String eventMetadata;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof EventEntity)) return false;
+        EventEntity that = (EventEntity) o;
+        return eventUUID.equals(that.eventUUID) && eventType.equals(that.eventType) && workspaceId.equals(that.workspaceId) && eventMetadata.equals(that.eventMetadata);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(eventUUID, eventType, workspaceId, eventMetadata);
+    }
+}
