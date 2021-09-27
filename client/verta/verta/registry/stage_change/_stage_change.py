edba7f48d68e230182e6b7dd27c4c2a08850d151
Michael Liu <miliu@protonmail.com>
2021-09-27 08:40:44 -0700

    [VR-12990] Enable RMV stage change (#2654)
    * Add stage property
    
    * Add _StageChange base class
    
    * Add stage change objects
    
    * Impl model_ver.change_stage()
    
    * Add tests
    
    * Add unit tests

---
+++ client/verta/verta/registry/stage_change/_stage_change.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+
+import abc
+
+from verta.external import six
+
+from verta._protos.public.registry import StageService_pb2
+
+
+@six.add_metaclass(abc.ABCMeta)
+class _StageChange(object):
+    """Base class for registry stage changes. Not for external use."""
+
+    # StageService_pb2.StageEnum variant
+    _STAGE = None
+
+    def __init__(self, comment=None):
+        if comment and not isinstance(comment, six.string_types):
+            raise TypeError("`comment` must be type str, not {}".format(type(comment)))
+
+        self._comment = comment
+
+    def __repr__(self):
+        lines = [type(self).__name__]
+
+        if self._comment:
+            lines.append("comment: {}".format(self._comment))
+
+        return "\t\n".join(lines)
+
+    @property
+    def comment(self):
+        return self._comment
+
+    def _to_proto_request(self, model_version_id):
+        return StageService_pb2.UpdateStageRequest(
+            model_version_id=model_version_id,
+            stage=self._STAGE,
+            comment=self._comment,
+        )
