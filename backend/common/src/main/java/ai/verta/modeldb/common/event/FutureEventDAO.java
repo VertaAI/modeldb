e2373a6e881d0b3c57dfa04afaeac51f8eea87d5
Anand Jakhaniya <anand.jakhaniya@gmail.com>
2021-09-28 15:52:38 +0530

    - Updated deleted event DAO for batch deletion

--- backend/common/src/main/java/ai/verta/modeldb/common/event/FutureEventDAO.java
+++ backend/common/src/main/java/ai/verta/modeldb/common/event/FutureEventDAO.java
@@ -1,10 +1,17 @@
 package ai.verta.modeldb.common.event;
 
 import ai.verta.modeldb.common.config.Config;
+import ai.verta.modeldb.common.exceptions.ModelDBException;
 import ai.verta.modeldb.common.futures.FutureJdbi;
 import ai.verta.modeldb.common.futures.InternalFuture;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 import java.util.UUID;
 import java.util.concurrent.Executor;
+import java.util.stream.Collectors;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -21,20 +28,25 @@ public class FutureEventDAO {
   }
 
   public Boolean addLocalEventWithBlocking(
-      String eventType, Long workspaceId, String eventMetadata) {
+      String eventType, long workspaceId, String eventMetadata) {
     return addLocalEvent(eventType, workspaceId, eventMetadata).get();
   }
 
   public InternalFuture<Boolean> addLocalEventWithAsync(
-      String eventType, Long workspaceId, String eventMetadata) {
+      String eventType, long workspaceId, String eventMetadata) {
     return addLocalEvent(eventType, workspaceId, eventMetadata);
   }
 
   private InternalFuture<Boolean> addLocalEvent(
-      String eventType, Long workspaceId, String eventMetadata) {
+      String eventType, long workspaceId, String eventMetadata) {
     if (!config.isEvent_system_enabled()) {
       LOGGER.info("Event system is not enabled");
       return InternalFuture.completedInternalFuture(false);
+    }
+
+    if (workspaceId == 0){
+      LOGGER.info("workspaceId not found while logging event");
+      return InternalFuture.failedStage(new ModelDBException("Workspace id should not be empty"));
     }
 
     return jdbi.withHandle(
@@ -55,25 +67,31 @@ public class FutureEventDAO {
             executor);
   }
 
-  public Boolean deleteLocalEventWithBlocking(String eventUUID) {
-    return deleteLocalEvent(eventUUID).get();
+  public Boolean deleteLocalEventWithBlocking(List<String> eventUUIDs) {
+    return deleteLocalEvent(eventUUIDs).get();
   }
 
-  public InternalFuture<Boolean> deleteLocalEventWithAsync(String eventUUID) {
-    return deleteLocalEvent(eventUUID);
+  public InternalFuture<Boolean> deleteLocalEventWithAsync(List<String> eventUUIDs) {
+    return deleteLocalEvent(eventUUIDs);
   }
 
-  private InternalFuture<Boolean> deleteLocalEvent(String eventUUID) {
+  private InternalFuture<Boolean> deleteLocalEvent(List<String> eventUUIDList) {
     if (!config.isEvent_system_enabled()) {
       LOGGER.info("Event system is not enabled");
       return InternalFuture.completedInternalFuture(false);
     }
 
+    Set<String> eventUUIDs = eventUUIDList.stream().filter(eventUUID -> !eventUUID.isEmpty()).collect(Collectors.toSet());
+
+    if (eventUUIDs.isEmpty()){
+      return InternalFuture.failedStage(new ModelDBException("Event UUID not found for deletion"));
+    }
+
     return jdbi.withHandle(
             handle ->
                 handle
-                    .createUpdate("DELETE FROM event WHERE event_uuid = :eventUUID ")
-                    .bind("eventUUID", eventUUID)
+                    .createUpdate("DELETE FROM event WHERE event_uuid IN (<eventUUIDs>) ")
+                    .bindList("eventUUIDs", eventUUIDs)
                     .execute())
         .thenApply(
             insertedRowCount -> {
