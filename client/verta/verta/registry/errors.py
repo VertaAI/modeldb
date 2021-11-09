95ce309e918eceb12e9f175c54aefe8b5a74ea12
Daniel Fennelly <daniel@verta.ai>
2021-11-09 10:39:24 -0800

    Add CustomModelError class to be caught in model wrapping infra

---
+++ client/verta/verta/registry/errors.py
@@ -0,0 +1,18 @@
+class CustomModelError(Exception):
+    """Exception to be raised in user-defined models.
+
+    Messages of this class are caught in model containers and converted to
+    HTTP errors with the specified HTTP code and message in the response body.
+
+    Attributes
+    ----------
+    http_code : int
+        The http code to be returned by a wrapping service.
+    message : str
+        The response message.
+    """
+
+    def __init__(self, http_code, message):
+        self.http_code = http_code
+        self.message = message
+        super().__init__(self.message)
