68ee936ce657efaef5ffab934bdf09fb75c40eef
Daniel Fennelly <daniel@verta.ai>
2021-11-09 12:06:53 -0800

    Change order of args and rename to ModelError

--- client/verta/verta/registry/errors.py
+++ client/verta/verta/registry/errors.py
@@ -1,4 +1,4 @@
-class CustomModelError(Exception):
+class ModelError(Exception):
     """Exception to be raised in user-defined models.
 
     Messages of this class are caught in model containers and converted to
@@ -6,13 +6,13 @@ class CustomModelError(Exception):
 
     Attributes
     ----------
-    http_code : int
-        The http code to be returned by a wrapping service.
     message : str
         The response message.
+    http_code : int
+        The http code to be returned by a wrapping service.
     """
 
-    def __init__(self, http_code, message):
-        self.http_code = http_code
+    def __init__(self, message, http_code):
         self.message = message
-        super(CustomModelError, self).__init__(self.message)
+        self.http_code = http_code
+        super(ModelError, self).__init__(self.message)
