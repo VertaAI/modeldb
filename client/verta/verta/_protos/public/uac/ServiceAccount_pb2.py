d2e2f0ae55dece4c29e2c0dad693342d6719944e
Jenkins <jenkins@verta.ai>
2021-11-16 07:10:14 +0000

    Update protos from el/VR-13439_find_service_account_by_username

--- client/verta/verta/_protos/public/uac/ServiceAccount_pb2.py
+++ client/verta/verta/_protos/public/uac/ServiceAccount_pb2.py
@@ -20,7 +20,7 @@ DESCRIPTOR = _descriptor.FileDescriptor(
   package='ai.verta.uac',
   syntax='proto3',
   serialized_options=b'P\001Z:github.com/VertaAI/modeldb/protos/gen/go/protos/public/uac',
-  serialized_pb=b'\n\x18uac/ServiceAccount.proto\x12\x0c\x61i.verta.uac\x1a\x1cgoogle/api/annotations.proto\x1a\x14uac/UACService.proto\"\x7f\n\x0eServiceAccount\x12\n\n\x02id\x18\x01 \x01(\x04\x12\x0f\n\x07user_id\x18\x02 \x01(\t\x12\x1f\n\x17\x61ssociated_workspace_id\x18\x03 \x01(\x04\x12\x1a\n\x12\x63reation_timestamp\x18\x04 \x01(\x04\x12\x13\n\x0b\x64\x65scription\x18\x05 \x01(\t\"e\n\x1b\x43reateServiceAccountRequest\x12\x10\n\x08username\x18\x01 \x01(\t\x12\x1f\n\x17\x61ssociated_workspace_id\x18\x02 \x01(\x04\x12\x13\n\x0b\x64\x65scription\x18\x04 \x01(\t\">\n\x1bUpdateServiceAccountRequest\x12\n\n\x02id\x18\x01 \x01(\x04\x12\x13\n\x0b\x64\x65scription\x18\x04 \x01(\t\"*\n\x1b\x44\x65leteServiceAccountRequest\x12\x0b\n\x03ids\x18\x01 \x03(\x04\"\x8e\x01\n\x19\x46indServiceAccountRequest\x12\x0b\n\x03ids\x18\x01 \x03(\x04\x12 \n\x18\x61ssociated_workspace_ids\x18\x02 \x03(\x04\x1a\x42\n\x08Response\x12\x36\n\x10service_accounts\x18\x01 \x03(\x0b\x32\x1c.ai.verta.uac.ServiceAccount2\xf8\x04\n\x15ServiceAccountService\x12\x94\x01\n\x14\x63reateServiceAccount\x12).ai.verta.uac.CreateServiceAccountRequest\x1a\x1c.ai.verta.uac.ServiceAccount\"3\x82\xd3\xe4\x93\x02-\"(/v1/service_account/createServiceAccount:\x01*\x12\xa2\x01\n\x12\x66indServiceAccount\x12\'.ai.verta.uac.FindServiceAccountRequest\x1a\x30.ai.verta.uac.FindServiceAccountRequest.Response\"1\x82\xd3\xe4\x93\x02+\"&/v1/service_account/findServiceAccount:\x01*\x12\x8b\x01\n\x14\x64\x65leteServiceAccount\x12).ai.verta.uac.DeleteServiceAccountRequest\x1a\x13.ai.verta.uac.Empty\"3\x82\xd3\xe4\x93\x02-*(/v1/service_account/deleteServiceAccount:\x01*\x12\x94\x01\n\x14updateServiceAccount\x12).ai.verta.uac.UpdateServiceAccountRequest\x1a\x1c.ai.verta.uac.ServiceAccount\"3\x82\xd3\xe4\x93\x02-\"(/v1/service_account/updateServiceAccount:\x01*B>P\x01Z:github.com/VertaAI/modeldb/protos/gen/go/protos/public/uacb\x06proto3'
+  serialized_pb=b'\n\x18uac/ServiceAccount.proto\x12\x0c\x61i.verta.uac\x1a\x1cgoogle/api/annotations.proto\x1a\x14uac/UACService.proto\"\x7f\n\x0eServiceAccount\x12\n\n\x02id\x18\x01 \x01(\x04\x12\x0f\n\x07user_id\x18\x02 \x01(\t\x12\x1f\n\x17\x61ssociated_workspace_id\x18\x03 \x01(\x04\x12\x1a\n\x12\x63reation_timestamp\x18\x04 \x01(\x04\x12\x13\n\x0b\x64\x65scription\x18\x05 \x01(\t\"e\n\x1b\x43reateServiceAccountRequest\x12\x10\n\x08username\x18\x01 \x01(\t\x12\x1f\n\x17\x61ssociated_workspace_id\x18\x02 \x01(\x04\x12\x13\n\x0b\x64\x65scription\x18\x04 \x01(\t\">\n\x1bUpdateServiceAccountRequest\x12\n\n\x02id\x18\x01 \x01(\x04\x12\x13\n\x0b\x64\x65scription\x18\x04 \x01(\t\"*\n\x1b\x44\x65leteServiceAccountRequest\x12\x0b\n\x03ids\x18\x01 \x03(\x04\"\xa1\x01\n\x19\x46indServiceAccountRequest\x12\x0b\n\x03ids\x18\x01 \x03(\x04\x12 \n\x18\x61ssociated_workspace_ids\x18\x02 \x03(\x04\x12\x11\n\tusernames\x18\x03 \x03(\t\x1a\x42\n\x08Response\x12\x36\n\x10service_accounts\x18\x01 \x03(\x0b\x32\x1c.ai.verta.uac.ServiceAccount2\xf8\x04\n\x15ServiceAccountService\x12\x94\x01\n\x14\x63reateServiceAccount\x12).ai.verta.uac.CreateServiceAccountRequest\x1a\x1c.ai.verta.uac.ServiceAccount\"3\x82\xd3\xe4\x93\x02-\"(/v1/service_account/createServiceAccount:\x01*\x12\xa2\x01\n\x12\x66indServiceAccount\x12\'.ai.verta.uac.FindServiceAccountRequest\x1a\x30.ai.verta.uac.FindServiceAccountRequest.Response\"1\x82\xd3\xe4\x93\x02+\"&/v1/service_account/findServiceAccount:\x01*\x12\x8b\x01\n\x14\x64\x65leteServiceAccount\x12).ai.verta.uac.DeleteServiceAccountRequest\x1a\x13.ai.verta.uac.Empty\"3\x82\xd3\xe4\x93\x02-*(/v1/service_account/deleteServiceAccount:\x01*\x12\x94\x01\n\x14updateServiceAccount\x12).ai.verta.uac.UpdateServiceAccountRequest\x1a\x1c.ai.verta.uac.ServiceAccount\"3\x82\xd3\xe4\x93\x02-\"(/v1/service_account/updateServiceAccount:\x01*B>P\x01Z:github.com/VertaAI/modeldb/protos/gen/go/protos/public/uacb\x06proto3'
   ,
   dependencies=[google_dot_api_dot_annotations__pb2.DESCRIPTOR,uac_dot_UACService__pb2.DESCRIPTOR,])
 
@@ -226,8 +226,8 @@ _FINDSERVICEACCOUNTREQUEST_RESPONSE = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=511,
-  serialized_end=577,
+  serialized_start=530,
+  serialized_end=596,
 )
 
 _FINDSERVICEACCOUNTREQUEST = _descriptor.Descriptor(
@@ -251,6 +251,13 @@ _FINDSERVICEACCOUNTREQUEST = _descriptor.Descriptor(
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       serialized_options=None, file=DESCRIPTOR),
+    _descriptor.FieldDescriptor(
+      name='usernames', full_name='ai.verta.uac.FindServiceAccountRequest.usernames', index=2,
+      number=3, type=9, cpp_type=9, label=3,
+      has_default_value=False, default_value=[],
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR),
   ],
   extensions=[
   ],
@@ -264,7 +271,7 @@ _FINDSERVICEACCOUNTREQUEST = _descriptor.Descriptor(
   oneofs=[
   ],
   serialized_start=435,
-  serialized_end=577,
+  serialized_end=596,
 )
 
 _FINDSERVICEACCOUNTREQUEST_RESPONSE.fields_by_name['service_accounts'].message_type = _SERVICEACCOUNT
@@ -328,8 +335,8 @@ _SERVICEACCOUNTSERVICE = _descriptor.ServiceDescriptor(
   file=DESCRIPTOR,
   index=0,
   serialized_options=None,
-  serialized_start=580,
-  serialized_end=1212,
+  serialized_start=599,
+  serialized_end=1231,
   methods=[
   _descriptor.MethodDescriptor(
     name='createServiceAccount',
