e903a6eb3495a899eba409717e0be16fe3f4dd80
Daniel Fennelly <daniel@verta.ai>
2021-12-03 15:00:11 -0800

    Add `include_stacktrace` param in ModelError

--- client/verta/verta/registry/errors.py
+++ client/verta/verta/registry/errors.py
@@ -10,9 +10,14 @@ class ModelError(Exception):
         The response message.
     http_code : int
         The http code to be returned by a wrapping service.
+    include_stacktrace : bool
+        Whether or not to include the stacktrace in HTTP response contents.
     """
 
-    def __init__(self, message, http_code):
+    def __init__(self, message, http_code, include_stacktrace=False):
+        if http_code >= 500 or http_code < 400:
+            raise ValueError("error codes must fall in the 4XX range")
         self.message = message
         self.http_code = http_code
+        self.include_stacktrace = include_stacktrace
         super(ModelError, self).__init__(self.message)
