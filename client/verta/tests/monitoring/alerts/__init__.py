8634b76b6a5f4c09386089e661fa1064c75c059b
Liu <96442646+liuverta@users.noreply.github.com>
2022-01-27 10:28:56 -0800

    test: [VMM-272] Remove monitoring subclient tests (#2813)
    * Remove monitoring tests
    
    * Remove references to monitoring in tests README

--- client/verta/tests/monitoring/alerts/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-import pytest
-
-pytest.skip(
-    "alert functionality has breaking backend changes (https://github.com/VertaAI/services/pull/1537)",
-    allow_module_level=True,
-)
