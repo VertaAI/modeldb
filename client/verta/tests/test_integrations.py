053bd48b0708358e635b618dd9075b3b3e9a531b
Liu <96442646+liuverta@users.noreply.github.com>
2022-02-10 14:42:08 -0800

    test: Fix skip marker

--- client/verta/tests/test_integrations.py
+++ client/verta/tests/test_integrations.py
@@ -325,7 +325,7 @@ class TestTensorFlow:
 class TestXGBoost:
 
     # TODO: re-enable with VR-11963
-    @pytest.mark.skip(six.PY2, reason="XGBoost causes a segfault in Python 2")
+    @pytest.mark.skipif(six.PY2, reason="XGBoost causes a segfault in Python 2")
     def test_callback(self, experiment_run):
         verta_integrations_xgboost = pytest.importorskip("verta.integrations.xgboost")
         verta_callback = verta_integrations_xgboost.verta_callback
