c4130585743fc7409e36c85f9390910b96bec3ec
Cory Johannsen <cory@verta.ai>
2022-03-23 10:14:30 -0700

    feat: [VMM-400] Add monitoring client patch extensions

--- client/verta/verta/registry/entities/__init__.py
+++ client/verta/verta/registry/entities/__init__.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 "Entities for registering ML models to the Verta backend."
 
+from tempfile import NamedTemporaryFile
+
 from verta._internal_utils import documentation
 
 from ._model import RegisteredModel
@@ -18,3 +20,28 @@ documentation.reassign_module(
     ],
     module_name=__name__,
 )
+
+
+
+def log_reference_data(self, X, Y, *args, **kwargs):
+    if isinstance(X, pd.Series):
+        X = X.to_frame()
+    if isinstance(Y, pd.Series):
+        Y = Y.to_frame()
+
+    df = pd.DataFrame()
+
+    for c in X.columns:
+        df['input.'+c] = X[c]
+    for c in Y.columns:
+        df['output.'+c] = Y[c]
+
+    df['source'] = "reference"
+    df['model_version_id'] = self.id
+
+    with NamedTemporaryFile() as f:
+        df.to_csv(f.name, encoding='utf-8', index=False)
+        self.log_artifact("reference_data", f.name, *args, **kwargs)
+
+
+RegisteredModelVersion.log_reference_data = log_reference_data
