b32d590b9c41e52c1d3dd1244bc0ec5b95d0b314
Michael Liu <miliu@verta.ai>
2022-04-08 14:13:15 -0700

    Move log_reference_data() into RegisteredModelVersion

--- client/verta/verta/registry/entities/__init__.py
+++ client/verta/verta/registry/entities/__init__.py
@@ -1,12 +1,7 @@
 # -*- coding: utf-8 -*-
 "Entities for registering ML models to the Verta backend."
 
-from tempfile import NamedTemporaryFile
-
-import pandas as pd
-
 from verta._internal_utils import documentation
-import pandas as pd
 
 from ._model import RegisteredModel
 from ._models import RegisteredModels
@@ -23,28 +18,3 @@ documentation.reassign_module(
     ],
     module_name=__name__,
 )
-
-
-
-def log_reference_data(self, X, Y, *args, **kwargs):
-    if isinstance(X, pd.Series):
-        X = X.to_frame()
-    if isinstance(Y, pd.Series):
-        Y = Y.to_frame()
-
-    df = pd.DataFrame()
-
-    for c in X.columns:
-        df['input.'+c] = X[c]
-    for c in Y.columns:
-        df['output.'+c] = Y[c]
-
-    df['source'] = "reference"
-    df['model_version_id'] = self.id
-
-    with NamedTemporaryFile() as f:
-        df.to_csv(f.name, encoding='utf-8', index=False)
-        self.log_artifact("reference_data", f.name, *args, **kwargs)
-
-
-RegisteredModelVersion.log_reference_data = log_reference_data
