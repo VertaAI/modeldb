5a1fe2ee5b0025c56a5bdb41e73bd77c4eeac77d
Jenkins <jenkins@verta.ai>
2022-04-20 08:35:19 +0000

    https://jenkins.dev.verta.ai/job/build/job/autoformat/job/modeldb-backend/922/

--- backend/src/main/java/ai/verta/modeldb/App.java
+++ backend/src/main/java/ai/verta/modeldb/App.java
@@ -49,7 +49,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.ApplicationPidFileWriter;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
