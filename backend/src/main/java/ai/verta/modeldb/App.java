a54a64b93a28b23eb5fb32b3da3d32dba0527dd9
Jenkins <jenkins@verta.ai>
2022-05-17 21:13:33 +0000

    https://jenkins.dev.verta.ai/job/build/job/autoformat/job/modeldb-backend/1007/

--- backend/src/main/java/ai/verta/modeldb/App.java
+++ backend/src/main/java/ai/verta/modeldb/App.java
@@ -60,12 +60,10 @@ import java.util.logging.Level;
 import javax.annotation.PreDestroy;
 import javax.management.MalformedObjectNameException;
 import javax.servlet.Filter;
-
 import liquibase.exception.LiquibaseException;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
