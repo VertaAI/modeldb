60276d8386103b2c260c322e01746c5ce1f70288
Liam Newman <96086065+liam-verta@users.noreply.github.com>
2022-05-24 14:57:26 -0700

    Update backend/telepresence.sh

--- backend/telepresence.sh
+++ backend/telepresence.sh
@@ -4,8 +4,7 @@ set -eo pipefail
 
 mkdir -p ~/telepresence_mounts
 
-# As of 5/20/2022 telepresence only supports a single port.  The last one listed is the one that is used.
-# HTTP:
-#telepresence intercept modeldb--backend --env-json telepresence-env.json --port 8086:8086 --mount ~/telepresence_mounts/modeldb_backend --context engine-dev-admin
-# GRPC:
-telepresence intercept modeldb--backend --env-json telepresence-env.json --port 8085:8085 --mount ~/telepresence_mounts/modeldb_backend --context engine-dev-admin
+PORT_TO_INTERCEPT="${1:-8085}"
+
+#TP2 only lets you do one port at a time; choose one. 8085 is GRPC and 8086 is HTTP
+telepresence intercept modeldb--backend --env-json telepresence-env.json --port $PORT_TO_INTERCEPT:$PORT_TO_INTERCEPT --mount ~/telepresence_mounts/modeldb_backend --context engine-dev-admin
