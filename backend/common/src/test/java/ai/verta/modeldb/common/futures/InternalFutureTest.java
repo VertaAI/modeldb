2a610d7db7d9f75f12180893ca9f4905d9ea66a7
John K. Watson <john@verta.ai>
2022-05-27 15:37:07 -0700

    chore: Make it possible to write unit tests in commons

---
+++ backend/common/src/test/java/ai/verta/modeldb/common/futures/InternalFutureTest.java
@@ -0,0 +1,30 @@
+package ai.verta.modeldb.common.futures;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import com.google.common.util.concurrent.MoreExecutors;
+import java.util.concurrent.Executor;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class InternalFutureTest {
+  @Test
+  void composition_failsFast() {
+    Executor executor = MoreExecutors.directExecutor();
+    InternalFuture<String> testFuture =
+        InternalFuture.completedInternalFuture("cheese")
+            .thenApply(
+                s1 -> {
+                  throw new RuntimeException("borken");
+                },
+                executor)
+            .thenCompose(
+                s -> {
+                  Assertions.fail("should never execute the next stage");
+                  return InternalFuture.failedStage(new RuntimeException("broken"));
+                },
+                executor);
+
+    assertThatThrownBy(testFuture::get).getRootCause().hasMessage("borken");
+  }
+}
