2c4c50f772c46d03e2439499536ba8e6041b89c0
Jenkins <jenkins@verta.ai>
2022-05-27 08:00:28 +0000

    https://jenkins.dev.verta.ai/job/build/job/autoformat/job/modeldb-backend/1055/

--- backend/common/src/main/java/ai/verta/modeldb/common/futures/FutureGrpc.java
+++ backend/common/src/main/java/ai/verta/modeldb/common/futures/FutureGrpc.java
@@ -1,16 +1,9 @@
 package ai.verta.modeldb.common.futures;
 
 import ai.verta.modeldb.common.CommonUtils;
-import com.google.common.util.concurrent.Futures;
-import com.google.common.util.concurrent.ListenableFuture;
 import com.google.protobuf.GeneratedMessageV3;
-import io.grpc.Context;
 import io.grpc.stub.StreamObserver;
-import io.opentracing.Scope;
-import io.opentracing.util.GlobalTracer;
-import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Executor;
-import java.util.concurrent.ForkJoinPool;
 
 @SuppressWarnings({"squid:S100"})
 public class FutureGrpc extends FutureUtil {
