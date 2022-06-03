b5bc98ef4b3b75b372767a59ba61b6a6e0fe70e7
Jenkins <jenkins@verta.ai>
2022-06-03 05:14:04 +0000

    https://jenkins.dev.verta.ai/job/build/job/autoformat/job/modeldb-backend/1074/

--- backend/common/src/main/java/ai/verta/modeldb/common/config/Config.java
+++ backend/common/src/main/java/ai/verta/modeldb/common/config/Config.java
@@ -2,7 +2,6 @@ package ai.verta.modeldb.common.config;
 
 import static io.opentelemetry.api.common.AttributeKey.stringKey;
 
-import ai.verta.modeldb.common.CommonConstants;
 import ai.verta.modeldb.common.CommonUtils;
 import ai.verta.modeldb.common.exceptions.InternalErrorException;
 import ai.verta.modeldb.common.exceptions.ModelDBException;
@@ -242,8 +241,6 @@ public abstract class Config {
   }
 
   public boolean isMigration() {
-    return Boolean.parseBoolean(
-        Optional.ofNullable("false")
-            .orElse("false"));
+    return Boolean.parseBoolean(Optional.ofNullable("false").orElse("false"));
   }
 }
