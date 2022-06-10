d2db91ae9ab87383e3cde81ac935570da425ad81
Jenkins <jenkins@verta.ai>
2022-06-10 21:50:36 +0000

    https://jenkins.dev.verta.ai/job/build/job/autoformat/job/modeldb-backend/1092/

--- backend/src/main/java/ai/verta/modeldb/configuration/ReconcilerInitializer.java
+++ backend/src/main/java/ai/verta/modeldb/configuration/ReconcilerInitializer.java
@@ -18,7 +18,6 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
