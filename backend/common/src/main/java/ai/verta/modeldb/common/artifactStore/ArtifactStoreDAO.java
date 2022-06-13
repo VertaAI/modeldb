a3765df5acda197abd8edf6d33f5d883f3a1e73d
Jenkins <jenkins@verta.ai>
2022-06-13 11:23:30 +0000

    https://jenkins.dev.verta.ai/job/build/job/autoformat/job/modeldb-backend/1099/

--- backend/common/src/main/java/ai/verta/modeldb/common/artifactStore/ArtifactStoreDAO.java
+++ backend/common/src/main/java/ai/verta/modeldb/common/artifactStore/ArtifactStoreDAO.java
@@ -10,8 +10,8 @@ import java.util.Optional;
 public interface ArtifactStoreDAO {
   GetUrlForArtifact.Response getUrlForArtifact(String path, String method) throws ModelDBException;
 
-  GetUrlForArtifact.Response getUrlForArtifactMultipart(String s3Key, String method, long partNumber, String uploadId)
-      throws ModelDBException;
+  GetUrlForArtifact.Response getUrlForArtifactMultipart(
+      String s3Key, String method, long partNumber, String uploadId) throws ModelDBException;
 
   Optional<String> initializeMultipart(String s3Path) throws ModelDBException;
 
