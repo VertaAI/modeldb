9dd405a967657af2ace698da174aad3908806b44
Anand Jakhaniya <anand.jakhaniya@gmail.com>
2022-06-24 21:05:54 +0530

    Added conditions as per suggestions

--- backend/src/main/java/ai/verta/modeldb/configuration/EnableReconcilers.java
+++ backend/src/main/java/ai/verta/modeldb/configuration/EnableReconcilers.java
@@ -1,5 +1,6 @@
 package ai.verta.modeldb.configuration;
 
+import ai.verta.modeldb.common.CommonConstants;
 import org.springframework.context.annotation.Condition;
 import org.springframework.context.annotation.ConditionContext;
 import org.springframework.core.type.AnnotatedTypeMetadata;
@@ -7,10 +8,15 @@ import org.springframework.core.type.AnnotatedTypeMetadata;
 public class EnableReconcilers implements Condition {
   @Override
   public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
-    return !liquibaseRunSeparately();
+    return liquibaseMigrationDisabled() || !liquibaseRunSeparately();
+  }
+
+  boolean liquibaseMigrationDisabled() {
+    return "false"
+        .equalsIgnoreCase(System.getenv(CommonConstants.ENABLE_LIQUIBASE_MIGRATION_ENV_VAR));
   }
 
   boolean liquibaseRunSeparately() {
-    return "true".equalsIgnoreCase("true");
+    return "true".equalsIgnoreCase(System.getenv(CommonConstants.RUN_LIQUIBASE_SEPARATE));
   }
 }
