cd9a594df6b36b162e04eb017a8d28d13168b539
Anand Jakhaniya <anand.jakhaniya@gmail.com>
2022-06-27 11:33:00 +0530

    Added logs

--- backend/src/main/java/ai/verta/modeldb/configuration/EnableReconcilers.java
+++ backend/src/main/java/ai/verta/modeldb/configuration/EnableReconcilers.java
@@ -1,22 +1,31 @@
 package ai.verta.modeldb.configuration;
 
 import ai.verta.modeldb.common.CommonConstants;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.context.annotation.Condition;
 import org.springframework.context.annotation.ConditionContext;
 import org.springframework.core.type.AnnotatedTypeMetadata;
 
 public class EnableReconcilers implements Condition {
+  private static final Logger LOGGER = LogManager.getLogger(EnableReconcilers.class);
+
   @Override
   public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
     return liquibaseMigrationDisabled() || !liquibaseRunSeparately();
   }
 
   boolean liquibaseMigrationDisabled() {
-    return "false"
-        .equalsIgnoreCase(System.getenv(CommonConstants.ENABLE_LIQUIBASE_MIGRATION_ENV_VAR));
+    var liquibaseMigrationDisabled =
+        "false".equalsIgnoreCase(System.getenv(CommonConstants.ENABLE_LIQUIBASE_MIGRATION_ENV_VAR));
+    LOGGER.info("liquibase migration disabled: {}", liquibaseMigrationDisabled);
+    return liquibaseMigrationDisabled;
   }
 
   boolean liquibaseRunSeparately() {
-    return "true".equalsIgnoreCase(System.getenv(CommonConstants.RUN_LIQUIBASE_SEPARATE));
+    var liquibaseRunSeparately =
+        "true".equalsIgnoreCase(System.getenv(CommonConstants.RUN_LIQUIBASE_SEPARATE));
+    LOGGER.info("liquibase run separately: {}", liquibaseRunSeparately);
+    return liquibaseRunSeparately;
   }
 }
--- backend/src/main/java/ai/verta/modeldb/configuration/MigrationSetupConfig.java
+++ backend/src/main/java/ai/verta/modeldb/configuration/MigrationSetupConfig.java
@@ -7,11 +7,15 @@ import ai.verta.modeldb.config.MDBConfig;
 import ai.verta.modeldb.config.MigrationConfig;
 import java.util.List;
 import java.util.Optional;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.context.annotation.Condition;
 import org.springframework.context.annotation.ConditionContext;
 import org.springframework.core.type.AnnotatedTypeMetadata;
 
 public class MigrationSetupConfig implements Condition {
+  private static final Logger LOGGER = LogManager.getLogger(MigrationSetupConfig.class);
+
   public MigrationSetupConfig() {}
 
   private MDBConfig mdbConfig;
@@ -37,14 +41,21 @@ public class MigrationSetupConfig implements Condition {
   }
 
   public boolean isMigration() {
-    return Boolean.parseBoolean(
-        Optional.ofNullable(System.getenv(CommonConstants.ENABLE_LIQUIBASE_MIGRATION_ENV_VAR))
-            .orElse("false"));
+    var enableLiquibaseMigrationEnvVar =
+        Boolean.parseBoolean(
+            Optional.ofNullable(System.getenv(CommonConstants.ENABLE_LIQUIBASE_MIGRATION_ENV_VAR))
+                .orElse("false"));
+    LOGGER.info("ENABLE_LIQUIBASE_MIGRATION_ENV_VAR: {}", enableLiquibaseMigrationEnvVar);
+    return enableLiquibaseMigrationEnvVar;
   }
 
   public boolean liquibaseRunSeparately() {
-    return Boolean.parseBoolean(
-        Optional.ofNullable(System.getenv(CommonConstants.RUN_LIQUIBASE_SEPARATE)).orElse("false"));
+    var liquibaseRunSeparately =
+        Boolean.parseBoolean(
+            Optional.ofNullable(System.getenv(CommonConstants.RUN_LIQUIBASE_SEPARATE))
+                .orElse("false"));
+    LOGGER.info("RUN_LIQUIBASE_SEPARATE: {}", liquibaseRunSeparately);
+    return liquibaseRunSeparately;
   }
 
   @Override
