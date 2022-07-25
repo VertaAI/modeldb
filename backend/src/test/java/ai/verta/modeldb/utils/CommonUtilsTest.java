f13d94e9eb0a11ad40d47211db10f1ba1bf02725
Jenkins <jenkins@verta.ai>
2022-07-25 11:30:34 +0000

    https://jenkins.dev.verta.ai/job/build/job/autoformat/job/modeldb-backend/98/

--- backend/src/test/java/ai/verta/modeldb/utils/CommonUtilsTest.java
+++ backend/src/test/java/ai/verta/modeldb/utils/CommonUtilsTest.java
@@ -4,11 +4,9 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import ai.verta.modeldb.common.CommonUtils;
 import ai.verta.modeldb.common.exceptions.ModelDBException;
-import ai.verta.modeldb.experimentRun.FutureExperimentRunDAO;
 import com.google.rpc.Code;
 import io.grpc.Status;
 import io.grpc.StatusRuntimeException;
-import java.util.concurrent.ExecutionException;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.junit.Test;
@@ -23,45 +21,58 @@ public class CommonUtilsTest {
   @Test
   public void getHydratedCollaboratorUserInfo() {
     var responseStatusException =
-        CommonUtils.observeErrorWithResponse(new ModelDBException("internal exception"), "internal exception message", LOGGER);
-    assertThat(responseStatusException.getStatus())
-        .isEqualTo(HttpStatus.INTERNAL_SERVER_ERROR);
-    assertThat(responseStatusException.getReason())
-        .isEqualTo("Internal Server Error");
+        CommonUtils.observeErrorWithResponse(
+            new ModelDBException("internal exception"), "internal exception message", LOGGER);
+    assertThat(responseStatusException.getStatus()).isEqualTo(HttpStatus.INTERNAL_SERVER_ERROR);
+    assertThat(responseStatusException.getReason()).isEqualTo("Internal Server Error");
 
     responseStatusException =
-        CommonUtils.observeErrorWithResponse(new ModelDBException("not found exception", Code.NOT_FOUND), "not found exception message", LOGGER);
-    assertThat(responseStatusException.getStatus())
-        .isEqualTo(HttpStatus.NOT_FOUND);
+        CommonUtils.observeErrorWithResponse(
+            new ModelDBException("not found exception", Code.NOT_FOUND),
+            "not found exception message",
+            LOGGER);
+    assertThat(responseStatusException.getStatus()).isEqualTo(HttpStatus.NOT_FOUND);
 
     responseStatusException =
-        CommonUtils.observeErrorWithResponse(new StatusRuntimeException(Status.UNIMPLEMENTED), "unimplemented exception message", LOGGER);
-    assertThat(responseStatusException.getStatus())
-        .isEqualTo(HttpStatus.NOT_IMPLEMENTED);
+        CommonUtils.observeErrorWithResponse(
+            new StatusRuntimeException(Status.UNIMPLEMENTED),
+            "unimplemented exception message",
+            LOGGER);
+    assertThat(responseStatusException.getStatus()).isEqualTo(HttpStatus.NOT_IMPLEMENTED);
 
     responseStatusException =
-        CommonUtils.observeErrorWithResponse(new StatusRuntimeException(Status.FAILED_PRECONDITION), "bad request exception message", LOGGER);
-    assertThat(responseStatusException.getStatus())
-        .isEqualTo(HttpStatus.BAD_REQUEST);
+        CommonUtils.observeErrorWithResponse(
+            new StatusRuntimeException(Status.FAILED_PRECONDITION),
+            "bad request exception message",
+            LOGGER);
+    assertThat(responseStatusException.getStatus()).isEqualTo(HttpStatus.BAD_REQUEST);
 
     responseStatusException =
-        CommonUtils.observeErrorWithResponse(new StatusRuntimeException(Status.ALREADY_EXISTS), "already exists exception message", LOGGER);
-    assertThat(responseStatusException.getStatus())
-        .isEqualTo(HttpStatus.CONFLICT);
+        CommonUtils.observeErrorWithResponse(
+            new StatusRuntimeException(Status.ALREADY_EXISTS),
+            "already exists exception message",
+            LOGGER);
+    assertThat(responseStatusException.getStatus()).isEqualTo(HttpStatus.CONFLICT);
 
     responseStatusException =
-        CommonUtils.observeErrorWithResponse(new StatusRuntimeException(Status.PERMISSION_DENIED), "permission denied exception message", LOGGER);
-    assertThat(responseStatusException.getStatus())
-        .isEqualTo(HttpStatus.FORBIDDEN);
+        CommonUtils.observeErrorWithResponse(
+            new StatusRuntimeException(Status.PERMISSION_DENIED),
+            "permission denied exception message",
+            LOGGER);
+    assertThat(responseStatusException.getStatus()).isEqualTo(HttpStatus.FORBIDDEN);
 
     responseStatusException =
-        CommonUtils.observeErrorWithResponse(new StatusRuntimeException(Status.UNAVAILABLE), "permission denied exception message", LOGGER);
-    assertThat(responseStatusException.getStatus())
-        .isEqualTo(HttpStatus.SERVICE_UNAVAILABLE);
+        CommonUtils.observeErrorWithResponse(
+            new StatusRuntimeException(Status.UNAVAILABLE),
+            "permission denied exception message",
+            LOGGER);
+    assertThat(responseStatusException.getStatus()).isEqualTo(HttpStatus.SERVICE_UNAVAILABLE);
 
     responseStatusException =
-        CommonUtils.observeErrorWithResponse(new StatusRuntimeException(Status.INVALID_ARGUMENT), "permission denied exception message", LOGGER);
-    assertThat(responseStatusException.getStatus())
-        .isEqualTo(HttpStatus.BAD_REQUEST);
+        CommonUtils.observeErrorWithResponse(
+            new StatusRuntimeException(Status.INVALID_ARGUMENT),
+            "permission denied exception message",
+            LOGGER);
+    assertThat(responseStatusException.getStatus()).isEqualTo(HttpStatus.BAD_REQUEST);
   }
 }
