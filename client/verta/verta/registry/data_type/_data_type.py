495d35601478a5af56553ab6f48e06854a4efe58
Marcelo Goncalves <marcelo@verta.ai>
2022-08-15 18:19:04 -0300

    feat: new data type class

---
+++ client/verta/verta/registry/data_type/_data_type.py
@@ -0,0 +1,81 @@
+import abc
+
+from ...external import six
+
+
+@six.add_metaclass(abc.ABCMeta)
+class _DataType(object):
+    """
+    Base class for data type. Not for external use.
+
+    """
+
+    _DATA_TYPE = None
+
+    def __repr__(self):
+        return "<{} data type>".format(self.__class__.__name__)
+
+    def _as_proto(self):
+        return self._DATA_TYPE
+
+    @staticmethod
+    def _from_proto(data_type):
+        """
+        Parameters
+        ----------
+        data_type : ``RegistryService_pb2.DataTypeEnum.DataType.OTHER``
+
+        Returns
+        -------
+        :class:`_DataType`
+
+        """
+        # imports here to avoid circular import in Python 2
+        from . import (
+            Other,
+            Audio,
+            Image,
+            Tabular,
+            Text,
+            Video,
+            Unknown,
+        )
+
+        for data_type_cls in (Other, Audio, Image, Tabular, Text, Video, Unknown):
+            if data_type == data_type_cls._DATA_TYPE:
+                return data_type_cls()
+        else:
+            raise ValueError(
+                "unrecognized data type {}".format(data_type)
+            )
+
+    @staticmethod
+    def _from_str(data_type_str):
+        """
+        Parameters
+        ----------
+        data_type : Data type name.
+
+        Returns
+        -------
+        :class:`_DataType`
+
+        """
+        # imports here to avoid circular import in Python 2
+        from . import (
+            Other,
+            Audio,
+            Image,
+            Tabular,
+            Text,
+            Video,
+            Unknown,
+        )
+        
+        for data_type_cls in (Other, Audio, Image, Tabular, Text, Video, Unknown):
+            if data_type_str.lower() == data_type_cls.__qualname__.lower():
+                return data_type_cls()
+        else:
+            raise ValueError(
+                "unrecognized data type {}".format(data_type_str)
+            )
