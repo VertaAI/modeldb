28b7980e4d11b27d74fc4e4f894702f6bf6ef118
marcelo-verta-ai <108302696+marcelo-verta-ai@users.noreply.github.com>
2022-08-17 12:19:14 -0300

    Update client/verta/verta/registry/action_type/_unknown.py
    Co-authored-by: Liu <96442646+liuverta@users.noreply.github.com>

--- client/verta/verta/registry/action_type/_unknown.py
+++ client/verta/verta/registry/action_type/_unknown.py
@@ -3,16 +3,9 @@ from ..._protos.public.registry import RegistryService_pb2
 from . import _ActionType
 
 
-class Unknown(_ActionType):
+class _Unknown(_ActionType):
     """
-    Unknown action of the registered model.
-
-    Examples
-    --------
-    .. code-block:: python
-
-        from verta.registry import action_type
-        reg_model = client.create_registered_model("My Model", workspace="my-org", action_type=action_type.Unknown())
+    Unknown action of the registered model. Not for external use.
 
     """
 
