0c618488a3c1c0f95babc72d6162ac647d01017e
John K. Watson <john@verta.ai>
2022-08-31 15:29:50 -0700

    chore: Add a new spring bean Condition for the servers being enabled.

---
+++ backend/common/src/main/java/ai/verta/modeldb/common/configuration/ServerEnabled.java
@@ -0,0 +1,31 @@
+package ai.verta.modeldb.common.configuration;
+
+import ai.verta.modeldb.common.CommonConstants;
+import org.springframework.context.annotation.Condition;
+import org.springframework.context.annotation.ConditionContext;
+import org.springframework.core.type.AnnotatedTypeMetadata;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+/**
+ * Condition that will allow spring beans to only be created if the web/grpc server(s) are enabled.
+ */
+@Component
+public class ServerEnabled implements Condition {
+  @Override
+  public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
+    var enableLiquibase =
+        Boolean.parseBoolean(
+            Optional.ofNullable(System.getenv(CommonConstants.ENABLE_LIQUIBASE_MIGRATION_ENV_VAR))
+                .orElse("false"));
+    var runLiquibaseSeparate =
+        Boolean.parseBoolean(
+            Optional.ofNullable(System.getenv(CommonConstants.RUN_LIQUIBASE_SEPARATE))
+                .orElse("false"));
+
+    // we run the web/grpc servers in every case where don't both have liquibase enabled and run it
+    // separately.
+    return !(enableLiquibase && runLiquibaseSeparate);
+  }
+}
