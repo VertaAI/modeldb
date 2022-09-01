737381f07871012318917d6452499d5b080dadb0
John K Watson <104948055+jkwatson-verta@users.noreply.github.com>
2022-09-01 08:00:11 -0700

    chore: Add a new spring bean Condition for the servers being enabled. (#3142)
    * chore: Add a new spring bean Condition for the servers being enabled.
    
    * https://jenkins.dev.verta.ai/job/build/job/autoformat/job/modeldb-backend/325/
    
    * tiny fix in comment
    
    Co-authored-by: Jenkins <jenkins@verta.ai>

--- backend/common/src/main/java/ai/verta/modeldb/common/configuration/GracefulShutdownBeans.java
+++ backend/common/src/main/java/ai/verta/modeldb/common/configuration/GracefulShutdownBeans.java
@@ -5,12 +5,14 @@ import ai.verta.modeldb.common.config.SpringServerConfig;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class GracefulShutdownBeans {
 
   @Bean
+  @Conditional(ServerEnabled.class)
   public GracefulShutdown gracefulShutdown(SpringServerConfig springServerConfig) {
     if (springServerConfig.getShutdownTimeout() == null) {
       return new GracefulShutdown(30L);
@@ -19,6 +21,7 @@ public class GracefulShutdownBeans {
   }
 
   @Bean
+  @Conditional(ServerEnabled.class)
   public ServletWebServerFactory servletContainer(final GracefulShutdown gracefulShutdown) {
     var factory = new TomcatServletWebServerFactory();
     factory.addConnectorCustomizers(gracefulShutdown);
