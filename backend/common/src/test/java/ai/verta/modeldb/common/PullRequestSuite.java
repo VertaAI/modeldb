8be5e526f6b040f16831a02667d527a100722163
John K. Watson <john@verta.ai>
2022-09-06 12:56:09 -0700

    chore: clean up junit dependencies, and create test suites for PRs

---
+++ backend/common/src/test/java/ai/verta/modeldb/common/PullRequestSuite.java
@@ -0,0 +1,8 @@
+package ai.verta.modeldb.common;
+
+import org.junit.platform.suite.api.SelectPackages;
+import org.junit.platform.suite.api.Suite;
+
+@Suite
+@SelectPackages("ai.verta.modeldb.common")
+public class PullRequestSuite {}
