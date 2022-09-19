7f8da1b2a7d9609e1aca610a4292624e11366886
John K Watson <104948055+jkwatson-verta@users.noreply.github.com>
2022-09-19 14:16:21 -0700

    chore: Allow config subclasses to provide a custom sampler. (#3232)

--- backend/common/src/main/java/ai/verta/modeldb/common/config/Config.java
+++ backend/common/src/main/java/ai/verta/modeldb/common/config/Config.java
@@ -25,6 +25,7 @@ import io.opentelemetry.sdk.extension.resources.HostResource;
 import io.opentelemetry.sdk.resources.Resource;
 import io.opentelemetry.sdk.trace.SdkTracerProvider;
 import io.opentelemetry.sdk.trace.export.BatchSpanProcessor;
+import io.opentelemetry.sdk.trace.samplers.Sampler;
 import io.opentracing.Tracer;
 import io.opentracing.contrib.grpc.ActiveSpanContextSource;
 import io.opentracing.contrib.grpc.ActiveSpanSource;
@@ -153,6 +154,7 @@ public abstract class Config {
     SdkTracerProvider tracerProvider =
         SdkTracerProvider.builder()
             .addSpanProcessor(BatchSpanProcessor.builder(spanExporter).build())
+            .setSampler(createOpenTelemetrySampler())
             .setResource(
                 Resource.getDefault()
                     .merge(
@@ -172,6 +174,15 @@ public abstract class Config {
                 TextMapPropagator.composite(
                     W3CTraceContextPropagator.getInstance(), JaegerPropagator.getInstance())))
         .buildAndRegisterGlobal();
+  }
+
+  /**
+   * Override this method to provide a custom sampler implementation for spans.
+   *
+   * <p>todo: figure out how to do this without requiring a subclass.
+   */
+  protected Sampler createOpenTelemetrySampler() {
+    return Sampler.alwaysOn();
   }
 
   public Optional<TracingServerInterceptor> getTracingServerInterceptor() {
@@ -263,5 +274,9 @@ public abstract class Config {
 
   public void setGrpcServer(GrpcServerConfig grpcServer) {
     this.grpcServer = grpcServer;
+  }
+
+  public boolean tracingEnabled() {
+    return enableTrace;
   }
 }
