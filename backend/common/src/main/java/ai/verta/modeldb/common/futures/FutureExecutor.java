9739c741df5299164432aa7754526db31cf2a5d9
conradoverta <53195685+conradoverta@users.noreply.github.com>
2022-11-02 14:23:38 -0700

    fix: Make context propagation deterministic (#3311)
    * fix: Make context propagation deterministic
    
    * https://jenkins.dev.verta.ai/job/build/job/autoformat/job/modeldb-backend/844/
    
    * avoid wrapping and add test
    
    * https://jenkins.dev.verta.ai/job/build/job/autoformat/job/modeldb-backend/916/
    
    * cleanup and add a test for trace propagation
    
    Co-authored-by: Verta SRE bot <sre-bot@verta.ai>
    Co-authored-by: John K. Watson <john@verta.ai>

--- backend/common/src/main/java/ai/verta/modeldb/common/futures/FutureExecutor.java
+++ backend/common/src/main/java/ai/verta/modeldb/common/futures/FutureExecutor.java
@@ -1,23 +1,27 @@
 package ai.verta.modeldb.common.futures;
 
-import io.grpc.Context;
-import io.opentracing.Scope;
-import io.opentracing.util.GlobalTracer;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ForkJoinPool;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.With;
 import org.springframework.lang.NonNull;
 
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
 public class FutureExecutor implements Executor {
-  final Executor other;
+  private final Executor other;
+  @With private final io.opentelemetry.context.Context otelContext;
+  @With private final io.grpc.Context grpcContext;
 
-  FutureExecutor(Executor other) {
-    this.other = io.opentelemetry.context.Context.taskWrapping(other);
+  public FutureExecutor captureContext() {
+    return withOtelContext(io.opentelemetry.context.Context.current())
+        .withGrpcContext(io.grpc.Context.current());
   }
 
   // Wraps an Executor and make it compatible with grpc's context
   public static FutureExecutor makeCompatibleExecutor(Executor ex) {
-    return new FutureExecutor(ex);
+    return new FutureExecutor(ex, null, null);
   }
 
   public static FutureExecutor initializeExecutor(Integer threadCount) {
@@ -35,19 +39,13 @@ public class FutureExecutor implements Executor {
 
   @Override
   public void execute(@NonNull Runnable r) {
-    if (GlobalTracer.isRegistered()) {
-      final var tracer = GlobalTracer.get();
-      final var span = tracer.scopeManager().activeSpan();
-      other.execute(
-          Context.current()
-              .wrap(
-                  () -> {
-                    try (Scope s = tracer.scopeManager().activate(span)) {
-                      r.run();
-                    }
-                  }));
-    } else {
-      other.execute(Context.current().wrap(r));
+    if (otelContext != null) {
+      r = otelContext.wrap(r);
     }
+    if (grpcContext != null) {
+      r = grpcContext.wrap(r);
+    }
+
+    other.execute(r);
   }
 }
