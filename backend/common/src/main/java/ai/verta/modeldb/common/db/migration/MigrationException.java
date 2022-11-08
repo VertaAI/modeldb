e3f78f3d42f78b49de0ed36360632ae88b71c237
John K Watson <104948055+jkwatson-verta@users.noreply.github.com>
2022-11-08 10:51:28 -0800

    chore: [VR-14170] Implement database locking during migrations. (#3341)
    * chore: [VR-14168] Create the basic classes for the new tool and verify multiline SQL works
    
    * cleanup method names
    
    * chore: [VR-14170] Implement database locking during migrations.
    
    * fix the RdbConfig with the builder added
    
    * merge from upstream PR

---
+++ backend/common/src/main/java/ai/verta/modeldb/common/db/migration/MigrationException.java
@@ -0,0 +1,7 @@
+package ai.verta.modeldb.common.db.migration;
+
+public class MigrationException extends Exception {
+  public MigrationException(String message) {
+    super(message);
+  }
+}
---
+++ backend/common/src/main/java/ai/verta/modeldb/common/db/migration/MigrationTools.java
@@ -0,0 +1,35 @@
+package ai.verta.modeldb.common.db.migration;
+
+import java.nio.charset.StandardCharsets;
+import java.sql.SQLException;
+import java.util.zip.CRC32;
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2
+public class MigrationTools {
+  public static String generateLockId(String database, String otherLockInfo) {
+    CRC32 crc32 = new CRC32();
+    crc32.update((database + ":" + otherLockInfo).getBytes(StandardCharsets.UTF_8));
+    return String.valueOf(crc32.getValue());
+  }
+
+  public static void lockDatabase(MigrationDatastore datastore) throws MigrationException {
+    int tries = 0;
+    while (tries++ < 15) {
+      try {
+        datastore.lock();
+        return;
+      } catch (SQLException e) {
+        log.warn("failed to lock db. sleeping 1s then trying again. message: " + e.getMessage());
+        try {
+          Thread.sleep(1000);
+        } catch (InterruptedException ex) {
+          Thread.currentThread().interrupt();
+          log.warn("Interrupted while trying to lock the database. Returning");
+          return;
+        }
+      }
+    }
+    throw new MigrationException("Failed to lock the database for migrations.");
+  }
+}
