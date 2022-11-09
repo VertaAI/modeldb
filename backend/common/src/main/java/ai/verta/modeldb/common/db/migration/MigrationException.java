3b153fb6c7080d336737836a0029776d67839e4a
John K Watson <104948055+jkwatson-verta@users.noreply.github.com>
2022-11-09 14:02:31 -0800

    chore: [VR-14175] Implement migration orchestration (#3346)
    * chore: [VR-14168] Create the basic classes for the new tool and verify multiline SQL works
    
    * cleanup method names
    
    * chore: [VR-14170] Implement database locking during migrations.
    
    * WIP on creating migration tables
    
    * fix the RdbConfig with the builder added
    
    * merge from upstream PR
    
    * implement table creation for mysql, verify table was created in tests.
    
    * Implement and test the full orchestration of migrations
    This does not yet include handling of the dirty state
    
    * cleanup some logic to make a bit less clever
    
    * clean up a bit
    
    * tighten up the access modifiers on some internal classes
    
    * fix test from merge mangling
    
    * Tweak a test to verify ordering when matching numbers but different up/down
    
    * https://jenkins.dev.verta.ai/job/build/job/autoformat/job/modeldb-backend/1013/
    
    * throw an explicit IllegalStateException when there is nothing in the schema_migrations table
    
    * Add javadoc for isUp/isDown for clarity
    
    * changes from code review
    
    Co-authored-by: Verta SRE bot <sre-bot@verta.ai>

--- backend/common/src/main/java/ai/verta/modeldb/common/db/migration/MigrationException.java
+++ backend/common/src/main/java/ai/verta/modeldb/common/db/migration/MigrationException.java
@@ -4,4 +4,8 @@ public class MigrationException extends Exception {
   public MigrationException(String message) {
     super(message);
   }
+
+  public MigrationException(String message, Throwable cause) {
+    super(message, cause);
+  }
 }
