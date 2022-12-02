1855ad3254a0b0885cfea68419bf6cf96263603b
Liu <96442646+liuverta@users.noreply.github.com>
2022-12-02 13:35:56 -0800

    Say Keras instead of TensorFlow

--- client/verta/verta/integrations/keras/__init__.py
+++ client/verta/verta/integrations/keras/__init__.py
@@ -58,7 +58,7 @@ class VertaCallback(keras.callbacks.Callback):
         try:
             self.run.log_hyperparameter("optimizer", model.optimizer._name)
         except:
-            try:  # TensorFlow 2.11.0 makes the optimizer name public
+            try:  # Keras 2.11.0 makes the optimizer name public
                 self.run.log_hyperparameter("optimizer", model.optimizer.name)
             except:
                 pass  # don't halt execution
