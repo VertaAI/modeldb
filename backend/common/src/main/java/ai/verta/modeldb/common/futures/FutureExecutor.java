1e4dd7a6678151a4b3654fa23a55e083ba2c1156
John K Watson <104948055+jkwatson-verta@users.noreply.github.com>
2022-12-12 15:05:20 -0800

    chore: Add a metric that measures the time a Future waits before being executed (#3441)
    * Measure the amount of time that Futures are waiting to be executed
    
    * https://jenkins.dev.verta.ai/job/build/job/autoformat/job/modeldb-backend/1270/
    
    * fully qualify the otel context for clarity
    
    * Update backend/common/src/main/java/ai/verta/modeldb/common/futures/FutureExecutor.java
    
    Co-authored-by: Liam Newman <96086065+liam-verta@users.noreply.github.com>
    
    * add some comments to clarify what's happening
    
    * https://jenkins.dev.verta.ai/job/build/job/autoformat/job/modeldb-backend/1273/
    
    Co-authored-by: Verta SRE bot <sre-bot@verta.ai>
    Co-authored-by: Liam Newman <96086065+liam-verta@users.noreply.github.com>

--- backend/common/src/main/java/ai/verta/modeldb/common/futures/FutureExecutor.java
+++ backend/common/src/main/java/ai/verta/modeldb/common/futures/FutureExecutor.java
@@ -1,5 +1,9 @@
 package ai.verta.modeldb.common.futures;
 
+import io.opentelemetry.api.GlobalOpenTelemetry;
+import io.opentelemetry.api.metrics.LongHistogram;
+import io.opentelemetry.context.Context;
+import io.opentelemetry.context.ContextKey;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ForkJoinPool;
@@ -10,13 +14,32 @@ import org.springframework.lang.NonNull;
 
 @AllArgsConstructor(access = AccessLevel.PROTECTED)
 public class FutureExecutor implements Executor {
+  private static final ContextKey<Long> EXECUTION_TIMER_KEY = ContextKey.named("execution timer");
+  private static volatile LongHistogram schedulingDelayHistogram;
   private final Executor delegate;
   @With private final io.opentelemetry.context.Context otelContext;
   @With private final io.grpc.Context grpcContext;
 
   public FutureExecutor captureContext() {
-    return withOtelContext(io.opentelemetry.context.Context.current())
-        .withGrpcContext(io.grpc.Context.current());
+    // set the current nano time into the context, so we can measure how long it takes to actually
+    // execute the future.
+    io.opentelemetry.context.Context otelContext = Context.current();
+    otelContext = otelContext.with(EXECUTION_TIMER_KEY, System.nanoTime());
+    return withOtelContext(otelContext).withGrpcContext(io.grpc.Context.current());
+  }
+
+  private static LongHistogram getSchedulingDelayHistogram() {
+    if (schedulingDelayHistogram != null) {
+      return schedulingDelayHistogram;
+    }
+    schedulingDelayHistogram =
+        GlobalOpenTelemetry.get()
+            .getMeter("verta.future")
+            .histogramBuilder("future_exec_delay")
+            .setDescription("The number of microseconds between creating and executing a Future")
+            .ofLongs()
+            .build();
+    return schedulingDelayHistogram;
   }
 
   // Wraps an Executor and make it compatible with grpc's context
@@ -37,8 +60,23 @@ public class FutureExecutor implements Executor {
     return makeCompatibleExecutor(Executors.newSingleThreadExecutor());
   }
 
+  private Runnable wrapWithTimer(Runnable r) {
+    return () -> {
+      // pull the start time out of the context, and record how much time has elapsed.
+      Long startTime = io.opentelemetry.context.Context.current().get(EXECUTION_TIMER_KEY);
+      if (startTime != null) {
+        long endTime = System.nanoTime();
+        long microsDelay = (endTime - startTime) / 1_000L;
+        getSchedulingDelayHistogram().record(microsDelay);
+      }
+      r.run();
+    };
+  }
+
   @Override
   public void execute(@NonNull Runnable r) {
+    r = wrapWithTimer(r);
+
     if (otelContext != null) {
       r = otelContext.wrap(r);
     }
