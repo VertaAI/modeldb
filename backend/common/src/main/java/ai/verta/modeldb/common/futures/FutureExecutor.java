421dfaa6cf40a78a56ab5ae1ab0f0484339a7b55
John K Watson <104948055+jkwatson-verta@users.noreply.github.com>
2022-12-13 08:28:09 -0800

    chore: change the start of the future execution delay timer to right before submission (#3443)
    * Tweak when the timer starts for measuring future execution delay.
    Allow assigning an OpenTelemetry instance, rather than relying on the global.
    
    * https://jenkins.dev.verta.ai/job/build/job/autoformat/job/modeldb-backend/1278/
    
    Co-authored-by: Verta SRE bot <sre-bot@verta.ai>

--- backend/common/src/main/java/ai/verta/modeldb/common/futures/FutureExecutor.java
+++ backend/common/src/main/java/ai/verta/modeldb/common/futures/FutureExecutor.java
@@ -1,6 +1,6 @@
 package ai.verta.modeldb.common.futures;
 
-import io.opentelemetry.api.GlobalOpenTelemetry;
+import io.opentelemetry.api.OpenTelemetry;
 import io.opentelemetry.api.metrics.LongHistogram;
 import io.opentelemetry.context.Context;
 import io.opentelemetry.context.ContextKey;
@@ -21,25 +21,24 @@ public class FutureExecutor implements Executor {
   @With private final io.grpc.Context grpcContext;
 
   public FutureExecutor captureContext() {
-    // set the current nano time into the context, so we can measure how long it takes to actually
-    // execute the future.
     io.opentelemetry.context.Context otelContext = Context.current();
-    otelContext = otelContext.with(EXECUTION_TIMER_KEY, System.nanoTime());
     return withOtelContext(otelContext).withGrpcContext(io.grpc.Context.current());
   }
 
-  private static LongHistogram getSchedulingDelayHistogram() {
-    if (schedulingDelayHistogram != null) {
-      return schedulingDelayHistogram;
-    }
+  /**
+   * Allow the FutureExecutors to have instrumentation via OpenTelemetry. Set this as early as
+   * possible after creation, in order not to miss any metrics that might be recorded.
+   */
+  public static void setOpenTelemetry(OpenTelemetry openTelemetry) {
+    // it's ok to do this more than once, since creation of meters/instruments is idempotent in the
+    // OTel SDK.
     schedulingDelayHistogram =
-        GlobalOpenTelemetry.get()
+        openTelemetry
             .getMeter("verta.future")
             .histogramBuilder("future_exec_delay")
             .setDescription("The number of microseconds between creating and executing a Future")
             .ofLongs()
             .build();
-    return schedulingDelayHistogram;
   }
 
   // Wraps an Executor and make it compatible with grpc's context
@@ -62,12 +61,14 @@ public class FutureExecutor implements Executor {
 
   private Runnable wrapWithTimer(Runnable r) {
     return () -> {
-      // pull the start time out of the context, and record how much time has elapsed.
-      Long startTime = io.opentelemetry.context.Context.current().get(EXECUTION_TIMER_KEY);
-      if (startTime != null) {
-        long endTime = System.nanoTime();
-        long microsDelay = (endTime - startTime) / 1_000L;
-        getSchedulingDelayHistogram().record(microsDelay);
+      if (schedulingDelayHistogram != null) {
+        // pull the start time out of the context, and record how much time has elapsed.
+        Long startTime = io.opentelemetry.context.Context.current().get(EXECUTION_TIMER_KEY);
+        if (startTime != null) {
+          long endTime = System.nanoTime();
+          long microsDelay = (endTime - startTime) / 1_000L;
+          schedulingDelayHistogram.record(microsDelay);
+        }
       }
       r.run();
     };
@@ -78,7 +79,9 @@ public class FutureExecutor implements Executor {
     r = wrapWithTimer(r);
 
     if (otelContext != null) {
-      r = otelContext.wrap(r);
+      // set the current nano time into the context, so we can measure how long it takes to actually
+      // start the future execution after submission to the executor.
+      r = otelContext.with(EXECUTION_TIMER_KEY, System.nanoTime()).wrap(r);
     }
     if (grpcContext != null) {
       r = grpcContext.wrap(r);
