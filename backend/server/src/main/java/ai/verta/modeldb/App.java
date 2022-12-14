e4a1fa3733806630a0bbfd640bc3fc6f2b82cc20
John K Watson <104948055+jkwatson-verta@users.noreply.github.com>
2022-12-14 09:48:21 -0800

    chore: remove the broken PID management code (#3449)

--- backend/server/src/main/java/ai/verta/modeldb/App.java
+++ backend/server/src/main/java/ai/verta/modeldb/App.java
@@ -1,14 +1,11 @@
 package ai.verta.modeldb;
 
-import ai.verta.modeldb.common.CommonConstants;
-import ai.verta.modeldb.common.CommonUtils;
 import ai.verta.modeldb.common.configuration.ServerEnabled;
 import ai.verta.modeldb.config.MDBConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.context.ApplicationPidFileWriter;
 import org.springframework.context.annotation.ComponentScan;
 
 /** This class is entry point of modeldb server. */
@@ -32,10 +29,7 @@ public class App {
     return app;
   }
 
-  public static void main(String[] args) throws Exception {
-    var pathToPidFile =
-        System.getProperty(CommonConstants.USER_DIR) + "/" + CommonConstants.BACKEND_PID_FILENAME;
-    CommonUtils.resolvePortCollisionIfExists(pathToPidFile);
+  public static void main(String[] args) {
     SpringApplicationBuilder springApplicationBuilder = new SpringApplicationBuilder(App.class);
     if (ServerEnabled.serverIsEnabled()) {
       springApplicationBuilder.web(WebApplicationType.SERVLET);
@@ -43,7 +37,6 @@ public class App {
       springApplicationBuilder.web(WebApplicationType.NONE);
     }
     SpringApplication application = springApplicationBuilder.build();
-    application.addListeners(new ApplicationPidFileWriter(pathToPidFile));
     application.run(args);
   }
 }
