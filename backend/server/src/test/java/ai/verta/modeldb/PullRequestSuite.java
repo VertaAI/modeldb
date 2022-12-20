fe5e6034ca301a3731f43bcddf14433fe189f039
Anand Jakhaniya <anand.jakhaniya@gmail.com>
2022-12-20 21:38:06 +0530

    [VUMM-653] Modify DiffTest.java to run in each PR (#3328)
    * Added lombok dependency
    
    * fix compilation due to lombok
    
    * Modify ProjectTest.java to run in each PR
    
    * Improve code
    
    * remove comments
    
    * Fix PullRequestSuite for updated test class
    
    * Remove unused function
    
    * [VUMM-653] Modify DatasetTest.java to run in each PR
    
    * fix relavent test cases for the dataset test
    
    * [VUMM-653] Modify DiffTest.java to run in each PR
    
    * Reverted unwanted changes

--- backend/server/src/test/java/ai/verta/modeldb/PullRequestSuite.java
+++ backend/server/src/test/java/ai/verta/modeldb/PullRequestSuite.java
@@ -10,7 +10,6 @@ import org.junit.platform.suite.api.Suite;
   "ai.verta.modeldb.BranchTest",
   "ai.verta.modeldb.CommitTest",
   "ai.verta.modeldb.DatasetVersionTest",
-  "ai.verta.modeldb.DiffTest",
   "ai.verta.modeldb.FindDatasetEntitiesTest",
   "ai.verta.modeldb.GlobalSharingTest",
   "ai.verta.modeldb.LineageTest",
