d27518228d93a00e96fe94ff719afa57029b1b2f
Anand Jakhaniya <anand.jakhaniya@gmail.com>
2023-01-02 11:58:03 +0530

    [VUMM-653] Modify BranchTest.java to run in each PR (#3293)
    * Added lombok dependency
    
    * fix compilation due to lombok
    
    * Modify ProjectTest.java to run in each PR
    
    * Improve code
    
    * remove comments
    
    * Fix PullRequestSuite for updated test class
    
    * Remove unused function
    
    * [VUMM-653] Modify DatasetTest.java to run in each PR
    
    * fix relavent test cases for the dataset test
    
    * [VUMM-653] Modify RepositoryTest.java to run in each PR
    
    * [VUMM-653] Modify BranchTest.java to run in each PR
    
    * https://jenkins.dev.verta.ai/job/build/job/autoformat/job/modeldb-backend/801/
    
    * Remove unwanted changes
    
    * Remove branch test name frome exclude list
    
    Co-authored-by: Verta SRE bot <sre-bot@verta.ai>

--- backend/server/src/test/java/ai/verta/modeldb/PullRequestSuite.java
+++ backend/server/src/test/java/ai/verta/modeldb/PullRequestSuite.java
@@ -7,7 +7,6 @@ import org.junit.platform.suite.api.Suite;
 @Suite
 @ExcludeClassNamePatterns({
   "ai.verta.modeldb.metadata.MetadataTest",
-  "ai.verta.modeldb.BranchTest",
   ".*TestSequenceSuite",
 })
 @SelectPackages({"ai.verta.modeldb"})
