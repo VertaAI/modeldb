072a78de7e7131fccb46f895b5a7ba7af48c0b28
Anand Jakhaniya <anand.jakhaniya@gmail.com>
2023-01-23 21:43:51 +0530

    Removed DatasetToRepositoryMigration (#1822)
    * Removed DatasetToRepositoryMigration
    
    * Remove other relavent code which not in use
    
    * Remove unused code

--- backend/server/src/main/java/ai/verta/modeldb/authservice/MDBRoleService.java
+++ backend/server/src/main/java/ai/verta/modeldb/authservice/MDBRoleService.java
@@ -2,25 +2,15 @@ package ai.verta.modeldb.authservice;
 
 import ai.verta.common.ModelDBResourceEnum.ModelDBServiceResourceTypes;
 import ai.verta.modeldb.common.authservice.RoleService;
-import ai.verta.modeldb.common.collaborator.CollaboratorBase;
 import ai.verta.modeldb.dto.WorkspaceDTO;
-import ai.verta.uac.GetCollaboratorResponseItem;
 import ai.verta.uac.ModelDBActionEnum.ModelDBServiceActions;
 import ai.verta.uac.UserInfo;
 import ai.verta.uac.Workspace;
 import com.google.protobuf.GeneratedMessageV3;
-import io.grpc.Metadata;
-import java.util.List;
 
 public interface MDBRoleService extends RoleService {
 
   boolean IsImplemented();
-
-  List<GetCollaboratorResponseItem> getResourceCollaborators(
-      ModelDBServiceResourceTypes modelDBServiceResourceTypes,
-      String resourceId,
-      String resourceOwnerId,
-      Metadata requestHeaders);
 
   /**
    * Checks permissions of the user wrt the Entity
@@ -33,13 +23,6 @@ public interface MDBRoleService extends RoleService {
       ModelDBServiceResourceTypes modelDBServiceResourceTypes,
       String resourceId,
       ModelDBServiceActions modelDBServiceActions);
-
-  String buildReadOnlyRoleBindingName(
-      String resourceId,
-      CollaboratorBase collaborator,
-      ModelDBServiceResourceTypes modelDBServiceResourceTypes);
-
-  GeneratedMessageV3 getTeamById(String teamId);
 
   GeneratedMessageV3 getOrgById(String orgId);
 
@@ -55,9 +38,4 @@ public interface MDBRoleService extends RoleService {
 
   WorkspaceDTO getWorkspaceDTOByWorkspaceIdForServiceUser(
       UserInfo currentLoginUserInfo, String workspaceId, Integer workspaceType);
-
-  boolean checkConnectionsBasedOnPrivileges(
-      ModelDBServiceResourceTypes serviceResourceTypes,
-      ModelDBServiceActions serviceActions,
-      String resourceId);
 }
