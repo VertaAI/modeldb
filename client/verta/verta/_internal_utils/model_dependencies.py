fd7929271972c54f4efebb70720986d71e86d6d9
ewagner-verta <ewagner@verta.ai>
2023-02-28 16:27:27 -0700

    refactor: handle return type hints, included nested hints, along with the rest of the signature all at once

--- client/verta/verta/_internal_utils/model_dependencies.py
+++ client/verta/verta/_internal_utils/model_dependencies.py
@@ -1,10 +1,7 @@
 # -*- coding: utf-8 -*-
 
-import ast
 import inspect
-import re
-from importlib.metadata import packages_distributions
-from typing import Callable, List, Set, Tuple, Type, get_type_hints
+from typing import Callable, List, Tuple, Type, get_type_hints
 from types import ModuleType
 from verta.registry import VertaModelBase
 
@@ -16,11 +13,50 @@ def list_class_functions(model_class: Type[VertaModelBase]) -> List[Tuple[str, C
 
 def list_modules_in_function_definition(func: Callable) -> List[ModuleType]:
     """List all modules called within the body of the provided function."""
-    try:
-        _func = inspect.unwrap(func)  # returns last object in chain
-    except ValueError:  # if a cycle is encountered
-        _func = func  # give it a shot anyway
+    _func = unwrap(func)
     return [
         value for key, value in inspect.getclosurevars(_func).globals.items()
         if isinstance(value, ModuleType)
     ]
+
+
+def list_modules_in_function_signature(func: Callable) -> List[ModuleType]:
+    """List all modules used in type hints in the provided function's arguments."""
+    _func = unwrap(func)
+    hints = get_type_hints(_func)
+    arg_hints = { k: v for k, v in hints.items() if k != 'return' }
+    return_hint = { k: v for k, v in hints.items() if k == 'return' }
+
+    modules = [ inspect.getmodule(value) for key, value in arg_hints.items() ]
+
+    if return_hint:
+        mod = inspect.getmodule(return_hint['return'])
+        if mod.__name__ == 'typing':
+            ret_ann = inspect.signature(_func).return_annotation
+            nested_args = ret_ann.__args__
+            for a in nested_args:
+                if inspect.isclass(a):
+                    modules.append(inspect.getmodule(a))
+        else:
+            modules.append(inspect.getmodule(return_hint['return']))
+    return modules
+
+
+def unwrap(func: Callable) -> Callable:
+    """Unwraps a function to get the original function object in a non-blocking manner."""
+    try:
+        return inspect.unwrap(func)  # returns last object in chain
+    except ValueError:  # if a cycle is encountered
+        return func
+
+
+def modules_in_class(model_class: Type[VertaModelBase]) -> List[ModuleType]:
+    funcs = list_class_functions(model_class)
+    modules_found = list()
+    for function in funcs:
+        _func = function[1]
+        mods_in_def = list_modules_in_function_definition(_func)
+        mods_in_args = list_modules_in_function_signature(_func)
+        total_mods = mods_in_def + mods_in_args
+        modules_found += total_mods
+    return list(set(modules_found))
