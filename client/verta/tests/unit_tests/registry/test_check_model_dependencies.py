7bcdff53092aada3a340e456e311a27460111e8e
ewagner-verta <ewagner@verta.ai>
2023-03-03 13:24:37 -0700

    test: unit tests for check_model_dependencies

---
+++ client/verta/tests/unit_tests/registry/test_check_model_dependencies.py
@@ -0,0 +1,117 @@
+# -*- coding: utf-8 -*-
+""" Unit tests for verta.registry.check_model_dependencies """
+
+import pytest
+from typing import Set
+import warnings
+
+from verta.registry.check_model_dependencies import check_model_dependencies
+from verta.environment import Python
+
+
+# TODO VRD-682 convert module names to pkg names and update this
+@pytest.fixture(scope='session')
+def complete_env() -> Python:
+    """ Environment with all expected packages from dependency_testing_model
+    fixture.  For completeness, some are versioned and some are not.
+    """
+    return Python([
+        'calendar',
+        'click',
+        'cloudpickle==0.0.1',
+        'collections==0.0.1',
+        'datetime==0.0.1',
+        'google==0.0.1',
+        'json==0.0.1',
+        'numpy==0.0.1',
+        'pandas==0.0.1',
+        'PIL==0.0.1',
+        'requests==0.0.1',
+        'requests==0.0.1',
+        'sklearn==0.0.1',
+        'torch==0.0.1',
+        'urllib3==0.0.1',
+        'verta==0.0.1',
+        'yaml==0.0.1',
+    ])
+
+
+def test_check_model_dependencies_complete(dependency_testing_model, complete_env) -> None:
+    """ Verify that check_model_dependencies returns all the expected packages from
+    the test model class (dependency_testing_model fixture).
+    """
+    assert check_model_dependencies(
+        model=dependency_testing_model,
+        environment=complete_env,
+        raise_for_missing=False,
+    )
+
+
+def test_check_model_dependencies_missing_raise(dependency_testing_model, complete_env) -> None:
+    """ Verify that check_model_dependencies raises an exception for missing
+    packages when `raise_for_missing`` is True.
+    """
+    incomplete_env = Python(
+        [ r for r in complete_env.requirements if r != 'click' ]
+    )  # drop a single dependency to be caught
+    with pytest.raises(RuntimeError) as err:
+        check_model_dependencies(
+            model=dependency_testing_model,
+            environment=incomplete_env,
+            raise_for_missing=True,
+        )
+    assert err.value.args[0] == "the following packages are required by the model " \
+                                "but missing from the environment: {'click'}"
+
+
+def test_check_model_dependencies_missing_warning(dependency_testing_model, complete_env) -> None:
+    """ Verify that check_model_dependencies does not raise an exception for
+    missing packages when `raise_for_missing`` is False.
+    """
+    incomplete_env = Python(
+        [ r for r in complete_env.requirements if r != 'pandas==0.0.1' ]
+    )  # drop a single dependency to be caught
+    with warnings.catch_warnings(record=True) as caught_warnings:
+        assert not check_model_dependencies(
+            model=dependency_testing_model,
+            environment=incomplete_env,
+            raise_for_missing=False,
+        )
+    warn_msg = caught_warnings[0].message.args[0]
+    assert warn_msg == "the following packages are required by the model but " \
+                       "missing from the environment: {'pandas'}"
+
+
+def test_check_model_dependencies_extra_raise(dependency_testing_model, complete_env) -> None:
+    """ Verify that check_model_dependencies does not raise an exception for
+    extra packages in the environment.
+    """
+    extra_env = Python(
+        complete_env.requirements + ['boto3==0.0.1']
+    )  # add an unnecessary dependency in the env to be caught
+    with pytest.raises(RuntimeError) as err:
+        assert not check_model_dependencies(
+            model=dependency_testing_model,
+            environment=extra_env,
+            raise_for_missing=True,
+        )
+    assert err.value.args[0] == "the following packages are not required by the model but " \
+                                "are present in the environment: {'boto3'}"
+
+
+def test_check_model_dependencies_extra_warning(dependency_testing_model, complete_env) -> None:
+    """ Verify that check_model_dependencies does not raise an exception for
+    extra packages in the environment.
+    """
+    extra_env = Python(
+        complete_env.requirements + ['pytest==0.0.1']
+    )  # add an unnecessary dependency in the env to be caught
+    with warnings.catch_warnings(record=True) as caught_warnings:
+        assert not check_model_dependencies(
+            model=dependency_testing_model,
+            environment=extra_env,
+            raise_for_missing=False,
+        )
+    warn_msg = caught_warnings[0].message.args[0]
+    assert warn_msg == "the following packages are not required by the model but " \
+                        "are present in the environment: {'pytest'}"
