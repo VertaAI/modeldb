9125d54657c6b7dfb28fdfec5143d03e293188de
Baasit Sharief <125391539+baasitsh-verta@users.noreply.github.com>
2023-03-07 17:08:48 -0500

    fix: [VUMM-629] Update client's XGBoost callback (and test_integrations.py::TestXGBoost::test_callback) (#3649)

--- client/verta/verta/integrations/xgboost/__init__.py
+++ client/verta/verta/integrations/xgboost/__init__.py
@@ -8,11 +8,11 @@ import xgboost as xgb  # pylint: disable=import-error
 from ..._internal_utils import _utils
 
 
-def verta_callback(run):
+class VertaCallback(xgb.callback.TrainingCallback):
     """
     XGBoost callback that automates logging to Verta during booster training.
 
-    This callback logs ``eval_metric``\ s passed into ``xgb.train()``.
+    This callback logs ``eval_metrics``\ s passed into ``xgb.train()``.
 
     See our `GitHub repository
     <https://github.com/VertaAI/examples/blob/main/experiment-management/xgboost/xgboost-integration.ipynb>`__
@@ -29,23 +29,24 @@ def verta_callback(run):
     --------
     .. code-block:: python
 
-        from verta.integrations.xgboost import verta_callback
+        from verta.integrations.xgboost import VertaCallback
         run = client.set_experiment_run()
         run.log_hyperparameters(params)
         bst = xgb.train(
             params, dtrain,
             evals=[(dtrain, "train")],
-            callbacks=[verta_callback(run)],
+            callbacks=[VertaCallback(run)],
         )
 
     """
+    def __init__(self, run):
+        self.run = run
 
-    def callback(env):
-        for metric, val in env.evaluation_result_list:
-            try:
-                run.log_observation(metric, val)
-            except:
-                pass  # don't halt execution
+    def after_iteration(self, model, epoch, evals_log):
+        for data, metric in evals_log.items():
+            for metric_name, log in metric.items():
+                try:
+                    self.run.log_observation(f"{data}-{metric_name}", log[-1])  # don't halt execution
+                except:
+                    pass
         # TODO: support `xgb.cv()`, which gives `(metric, val, std_dev)` across folds
-
-    return callback
