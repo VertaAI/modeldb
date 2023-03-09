52debe55017825a30eb4faa1e534e5d4660342d0
Michael Liu <miliu@verta.ai>
2023-03-09 15:10:06 -0800

    Add utilities for cross-referencing custom modules

--- client/verta/verta/_internal_utils/custom_modules.py
+++ client/verta/verta/_internal_utils/custom_modules.py
@@ -1,33 +1,27 @@
 # -*- coding: utf-8 -*-
 
 import importlib
+import logging
+import os
 import pkgutil
+from types import ModuleType
+from typing import List, Set
+
+logger = logging.getLogger(__name__)
 
 
 class CustomModules(object):
     @staticmethod
-    def is_importable(module_name):
-        """Return whether `module_name` can be imported.
-
-        Returns
-        -------
-        bool
-
-        """
+    def is_importable(module_name: str) -> bool:
+        """Return whether `module_name` can be imported."""
         try:
             return True if pkgutil.find_loader(module_name) else False
         except ImportError:
             return False
 
     @staticmethod
-    def get_module_path(module_name):
-        """Return root path to `module`.
-
-        Returns
-        -------
-        str
-
-        """
+    def get_module_path(module_name: str) -> str:
+        """Return root path to `module`."""
         module_spec = importlib.util.find_spec(module_name)  # pylint: disable=no-member
 
         module_path = module_spec.submodule_search_locations  # module.__path__
@@ -36,3 +30,49 @@ class CustomModules(object):
 
         # single-file module
         return module_spec.origin  # module.__file__
+
+    @staticmethod
+    def categorize_custom_modules(custom_modules: List[str]):
+        # TODO: docstring
+        module_names, filepaths, dirpaths = set(), set(), set()
+        for custom_module in custom_modules:
+            if CustomModules.is_importable(custom_module):
+                _bucket = module_names
+            elif os.path.isfile(custom_module):
+                _bucket = filepaths
+            elif os.path.isdir(custom_module):
+                _bucket = dirpaths
+            else:
+                raise ValueError(f"custom module {custom_module} not found")
+            _bucket.add(custom_module)
+        return module_names, filepaths, dirpaths
+
+    @staticmethod
+    def in_named_custom_modules(module: ModuleType, module_names: Set[str]) -> bool:
+        """Return whether `module` is supplied by a named module."""
+        if module.__name__ in module_names:
+            logger.debug("found %s in modules", module)
+            return True
+        return False
+
+    @staticmethod
+    def in_file_custom_modules(module: ModuleType, filepaths: Set[str]) -> bool:
+        """Return whether `module` is potentially supplied by a Python file within it."""
+        for filepath in map(os.path.abspath, filepaths):
+            if any(
+                # TODO: check that all base modules have __path__
+                map(filepath.startswith, module.__path__)
+            ):
+                logger.debug("found %s in filepaths", module)
+                return True
+        return False
+
+    @staticmethod
+    def in_directory_custom_modules(module: ModuleType, dirpaths: Set[str]) -> bool:
+        """Return whether `module` is potentially supplied by a directory that contains it."""
+        for dirpath in map(os.path.abspath, dirpaths):
+            dirpath = dirpath.rstrip("/")
+            if any(module_path.startswith(dirpath) for module_path in module.__path__):
+                logger.debug("found %s in dirpaths", module)
+                return True
+        return False
