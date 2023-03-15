7ab21b2a6ca886baf64667300b0a4dbcfc17e4ce
Michael Liu <miliu@verta.ai>
2023-03-15 14:54:25 -0700

    Cover existing CustomModules methods

---
+++ client/verta/tests/unit_tests/custom_modules/test_custom_modules_util.py
@@ -0,0 +1,79 @@
+# -*- coding: utf-8 -*-
+
+import importlib
+
+from hypothesis import (
+    HealthCheck,
+    assume,
+    given,
+    settings,
+    strategies as st,
+)
+import pytest
+
+from verta._internal_utils.custom_modules import CustomModules
+from verta.registry import VertaModelBase, verify_io
+
+
+@st.composite
+def custom_module_name(draw, max_depth: int = 8) -> str:
+    """Generate dot-delimited Python module names."""
+    depth = draw(st.integers(min_value=0, max_value=max_depth))
+
+    # Python 2 rules, for simplicity
+    module_name = st.from_regex("[a-zA-Z_][a-zA-Z0-9_]{0,31}", fullmatch=True)
+
+    return ".".join(draw(module_name) for _ in range(depth + 1))
+
+
+class TestIsImportable:
+    """Test ``CustomModules.is_importable()``."""
+
+    @settings(suppress_health_check=[HealthCheck.function_scoped_fixture])
+    @given(data=st.data())
+    def test_is_importable(self, make_custom_module, data):
+        module = make_custom_module(data.draw(custom_module_name()))
+        assert CustomModules.is_importable(module.__name__)
+
+    @settings(suppress_health_check=[HealthCheck.function_scoped_fixture])
+    @given(data=st.data())
+    def test_is_not_importable(self, make_custom_module, data):
+        nonexistant_module_name = data.draw(custom_module_name())
+        assert not CustomModules.is_importable(nonexistant_module_name)
+
+
+class TestGetModulePath:
+    """Test ``CustomModules.get_module_path()``."""
+
+    @settings(
+        suppress_health_check=[
+            HealthCheck.filter_too_much,
+            HealthCheck.function_scoped_fixture,
+        ]
+    )
+    @given(data=st.data())
+    def test_get_single_file_module_path(self, make_custom_module, data):
+        """Test that we can find the path to a ``.py`` file module."""
+        _module_name = data.draw(custom_module_name())
+        assume("." not in _module_name)
+        module = make_custom_module(_module_name)
+
+        assert CustomModules().get_module_path(module.__name__) == module.__file__
+
+    @settings(
+        suppress_health_check=[
+            HealthCheck.filter_too_much,
+            HealthCheck.function_scoped_fixture,
+        ]
+    )
+    @given(data=st.data())
+    def test_get_directory_module_path(self, make_custom_module, data):
+        """Test that we can find the path to a directory module."""
+        _module_name = data.draw(custom_module_name())
+        assume("." in _module_name)
+        module = importlib.import_module(
+            name="..",
+            package=make_custom_module(_module_name).__name__,
+        )
+
+        assert CustomModules().get_module_path(module.__name__) == module.__path__[0]
