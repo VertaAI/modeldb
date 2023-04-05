aa4aed7d5f86c39b61a92fb9899160ab5157799d
ewagner-verta <ewagner@verta.ai>
2023-04-05 11:33:21 -0600

    test: add fixtures for mocking a Client and a RegisteredModelVersion

--- client/verta/tests/unit_tests/conftest.py
+++ client/verta/tests/unit_tests/conftest.py
@@ -8,25 +8,40 @@ from unittest.mock import patch
 import pytest
 import responses
 
-from verta._internal_utils._utils import Connection, Configuration
+from verta._internal_utils._utils import Configuration, Connection
+from verta._protos.public.registry import RegistryService_pb2 as _RegistryService
+from verta.client import Client
 from verta.credentials import EmailCredentials
+from verta.registry.entities import RegisteredModelVersion
+
+
+@pytest.fixture(scope="session")
+def mock_client(mock_conn) -> Client:
+    """Return a mocked object of the Client class for use in tests"""
+    # with patch.dict(os.environ, {'VERTA_EMAIL': 'test_email@verta.ai', 'VERTA_DEV_KEY':'123test1232dev1232key123'}):
+    client = Client(ignore_conn_err=True, _connect=False)
+    client._conn = mock_conn
+    return client
 
 
 @pytest.fixture(scope="session")
 def mock_conn() -> Connection:
-    """ Return a mocked object of the _internal_utils._utils.Connection class for use in tests """
-    with patch.dict(os.environ, {'VERTA_EMAIL': 'test_email@verta.ai', 'VERTA_DEV_KEY':'123test1232dev1232key123'}):
+    """Return a mocked object of the _internal_utils._utils.Connection class for use in tests"""
+    with patch.dict(
+        os.environ,
+        {
+            "VERTA_EMAIL": "test_email@verta.ai",
+            "VERTA_DEV_KEY": "123test1232dev1232key123",
+        },
+    ):
         credentials = EmailCredentials.load_from_os_env()
 
-    return Connection(
-        scheme='https',
-        socket='test_socket',
-        credentials=credentials,
-    )
+    return Connection(scheme="https", socket="test_socket", credentials=credentials)
+
 
 @pytest.fixture(scope="session")
 def mock_config() -> Configuration:
-    """ Return a mocked object of the _internal_utils._utils.Configuration class for use in tests """
+    """Return a mocked object of the _internal_utils._utils.Configuration class for use in tests"""
     return Configuration(use_git=False, debug=False)
 
 
@@ -34,3 +49,9 @@ def mock_config() -> Configuration:
 def mocked_responses():
     with responses.RequestsMock() as rsps:
         yield rsps
+
+
+@pytest.fixture(scope="session")
+def mock_registered_model_version(mock_conn, mock_config):
+    Message = _RegistryService.RegisteredModel
+    return RegisteredModelVersion(mock_conn, mock_config, Message(id=654))
