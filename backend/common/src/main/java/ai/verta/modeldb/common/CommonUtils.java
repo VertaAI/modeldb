5042a53be767f5e8b914a97c16486b870e6185d3
John K. Watson <john@verta.ai>
2023-04-10 11:28:05 -0700

    chore: backport scheduled tasks being daemon threads

--- backend/common/src/main/java/ai/verta/modeldb/common/CommonUtils.java
+++ backend/common/src/main/java/ai/verta/modeldb/common/CommonUtils.java
@@ -78,7 +78,15 @@ public class CommonUtils {
   public static void scheduleTask(
       TimerTask task, long initialDelay, long frequency, TimeUnit timeUnit) {
     // scheduling the timer instance
-    ScheduledExecutorService executor = Executors.newSingleThreadScheduledExecutor();
+    ScheduledExecutorService executor =
+            Executors.newSingleThreadScheduledExecutor(
+                    r -> {
+                      Thread t = new Thread(r);
+                      t.setName(task.getClass().getSimpleName());
+                      t.setDaemon(true);
+                      return t;
+                    });
+
     executor.scheduleAtFixedRate(task, initialDelay, frequency, timeUnit);
   }
 
