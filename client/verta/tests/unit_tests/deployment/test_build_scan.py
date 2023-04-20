c08275c82b9907ec78ce7fa2a0acbe62440e5281
Michael Liu <miliu@verta.ai>
2023-04-20 12:08:46 -0700

    Consolidate assertions

--- client/verta/tests/unit_tests/deployment/test_build_scan.py
+++ client/verta/tests/unit_tests/deployment/test_build_scan.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
+from typing import Any, Dict
+
 from hypothesis import given, HealthCheck, settings
 import pytest
 
@@ -9,11 +11,10 @@ from verta._internal_utils import time_utils
 from verta.endpoint.build import Build, BuildScan, ScanProgressEnum, ScanStatusEnum
 
 
-@given(build_scan_dict=build_scan_dict())
-def test_instantiation(build_scan_dict):
-    """Verify a BuildScan object can be instantated from a dict."""
-    build_scan = BuildScan(build_scan_dict)
-
+def assert_build_scan_fields(
+    build_scan: BuildScan,
+    build_scan_dict: Dict[str, Any],
+) -> None:
     assert build_scan.date_updated == time_utils.datetime_from_iso(
         build_scan_dict["date_updated"],
     )
@@ -24,6 +25,14 @@ def test_instantiation(build_scan_dict):
     else:
         assert build_scan.passed is False
         assert build_scan.status is None
+
+
+@given(build_scan_dict=build_scan_dict())
+def test_instantiation(build_scan_dict):
+    """Verify a BuildScan object can be instantated from a dict."""
+    build_scan = BuildScan(build_scan_dict)
+
+    assert_build_scan_fields(build_scan, build_scan_dict)
 
 
 @settings(suppress_health_check=[HealthCheck.function_scoped_fixture])
@@ -40,13 +49,4 @@ def test_get_scan(mock_conn, mocked_responses, build_dict, build_scan_dict):
 
         build_scan = build.get_scan()
 
-    assert build_scan.date_updated == time_utils.datetime_from_iso(
-        build_scan_dict["date_updated"],
-    )
-    assert build_scan.progress == ScanProgressEnum(build_scan_dict["scan_status"])
-    if build_scan.progress == ScanProgressEnum.SCANNED:
-        assert build_scan.passed == (build_scan.status == ScanStatusEnum.SAFE)
-        assert build_scan.status == ScanStatusEnum(build_scan_dict["safety_status"])
-    else:
-        assert build_scan.passed is False
-        assert build_scan.status is None
+    assert_build_scan_fields(build_scan, build_scan_dict)
