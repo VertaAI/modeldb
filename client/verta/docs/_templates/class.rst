ab71768176a8b099a537c5d44a75176bd292e5af
Liu <96442646+liuverta@users.noreply.github.com>
2023-04-25 09:52:30 -0700

    feat: [VRD-818] Implement `BuildScan` class (#3757)

--- client/verta/docs/_templates/class.rst
+++ client/verta/docs/_templates/class.rst
@@ -5,6 +5,10 @@
 .. autoclass:: {{ objname }}
     :members:
     :inherited-members:
+    {% if objname.endswith("Enum") -%}
+    :undoc-members:
+    :member-order: bysource
+    {% endif -%}
     {% if 'ExperimentRun' != objname -%}
     :exclude-members: log_code, get_code
-    {%- endif %}
+    {% endif -%}
---
+++ client/verta/tests/unit_tests/deployment/test_build_scan.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+from hypothesis import given, HealthCheck, settings
+import pytest
+
+from tests.unit_tests.strategies import build_scan_dict
+
+from verta._internal_utils import time_utils
+from verta.endpoint.build import BuildScan, ScanProgressEnum, ScanResultEnum
+
+
+@given(build_scan_dict=build_scan_dict())
+def test_instantiation(build_scan_dict):
+    """Verify a BuildScan object can be instantated from a dict."""
+    build_scan = BuildScan(build_scan_dict)
+
+    assert build_scan.date_updated == time_utils.datetime_from_iso(
+        build_scan_dict["date_updated"],
+    )
+    assert build_scan.progress == ScanProgressEnum(build_scan_dict["scan_status"])
+    if build_scan.progress == ScanProgressEnum.SCANNED:
+        assert build_scan.passed == (build_scan.result == ScanResultEnum.SAFE)
+        assert build_scan.result == ScanResultEnum(build_scan_dict["safety_status"])
+    else:
+        assert build_scan.passed is False
+        assert build_scan.result is None
