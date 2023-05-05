4940ca5f16175a3bc275a11fefe851a201ebd673
Heather MacDonald <80712961+hmacdonald-verta@users.noreply.github.com>
2023-05-05 09:33:46 -0700

    fix: Replace `method_whitelist` with `allowed_methods` for urllib3 v2 (#3792)

--- client/verta/verta/_internal_utils/_utils.py
+++ client/verta/verta/_internal_utils/_utils.py
@@ -89,7 +89,7 @@ class Connection(object):
         self.retry = Retry(
             total=max_retries,
             backoff_factor=1,  # each retry waits (2**retry_num) seconds
-            method_whitelist=False,  # retry on all HTTP methods
+            allowed_methods=False,  # retry on all HTTP methods
             status_forcelist=(
                 requests.codes.bad_gateway,
                 requests.codes.unavailable,
