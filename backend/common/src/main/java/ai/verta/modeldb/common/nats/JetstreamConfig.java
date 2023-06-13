8902413e67f8d07b1547e8477c7b216832dfa7c4
John K. Watson <john@verta.ai>
2023-06-13 10:27:19 -0700

    feat: Create a JetstreamConnector for managing Jetstream connectivity

---
+++ backend/common/src/main/java/ai/verta/modeldb/common/nats/JetstreamConfig.java
@@ -0,0 +1,14 @@
+package ai.verta.modeldb.common.nats;
+
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter(AccessLevel.PRIVATE)
+@With
+public class JetstreamConfig {
+  private boolean enabled = false;
+  private String host = "";
+  private int port = 0;
+}
