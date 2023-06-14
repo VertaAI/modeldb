30f60b54837850edce418484eb005c5b94c0dbc9
Michael Liu <miliu@verta.ai>
2023-06-13 18:00:36 -0700

    Make runnable linear orchestrator

--- client/verta/verta/_pipeline_orchestrator/_orchestrator.py
+++ client/verta/verta/_pipeline_orchestrator/_orchestrator.py
@@ -31,14 +31,30 @@ class _OrchestratorBase(abc.ABC):
         self,
         input: Any,
     ):
-        raise NotImplementedError
         dag = self.get_dag()
+
+        # run input node
+        # NOTE: assumes only one input node
+        model_version_id = dag.get_ready()[0]
+        step_handler = self._step_handlers[model_version_id]
+        outputs = {model_version_id: step_handler.run(input)}
+        dag.done(model_version_id)
+
         while dag.is_active():
             for model_version_id in dag.get_ready():
                 step_handler = self._step_handlers[model_version_id]
-                output = step_handler.run(input)  # TODO: async?
-                # TODO: track outputs
+                outputs[model_version_id] = step_handler.run(  # TODO: async?
+                    outputs[step_handler.predecessors[0]]  # TEST
+                    # {
+                    #     predecessor_id: outputs[predecessor_id]
+                    #     for predecessor_id in step_handler.predecessors
+                    # }
+                )
                 dag.done(model_version_id)  # TODO: callback?
+
+        # return output from final node
+        # NOTE: assumes only one leaf node
+        return outputs[step_handler.model_version_id]
 
 
 class DeployedOrchestrator(_OrchestratorBase):
