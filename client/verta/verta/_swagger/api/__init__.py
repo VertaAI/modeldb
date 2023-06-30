534dd82638f40552455ea9db2b6f1151bd5539bf
Michael Liu <miliu@verta.ai>
2023-06-30 15:32:23 -0700

    Fix imports and remove Py2 pieces

--- client/verta/verta/_swagger/api/__init__.py
+++ client/verta/verta/_swagger/api/__init__.py
@@ -1,17 +1,15 @@
-from __future__ import absolute_import
-
 # flake8: noqa
 
 # import apis into api package
-from swagger_client.api.builds_api import BuildsApi
-from swagger_client.api.builds_docker_api import BuildsDockerApi
-from swagger_client.api.builds_scan_api import BuildsScanApi
-from swagger_client.api.endpoints_api import EndpointsApi
-from swagger_client.api.endpoints_labels_api import EndpointsLabelsApi
-from swagger_client.api.endpoints_monitoring_api import EndpointsMonitoringApi
-from swagger_client.api.endpoints_operations_api import EndpointsOperationsApi
-from swagger_client.api.endpoints_stages_api import EndpointsStagesApi
-from swagger_client.api.endpoints_stages_kafka_api import EndpointsStagesKafkaApi
-from swagger_client.api.endpoints_tokens_api import EndpointsTokensApi
-from swagger_client.api.list_api import ListApi
-from swagger_client.api.validation_api import ValidationApi
+from .builds_api import BuildsApi
+from .builds_docker_api import BuildsDockerApi
+from .builds_scan_api import BuildsScanApi
+from .endpoints_api import EndpointsApi
+from .endpoints_labels_api import EndpointsLabelsApi
+from .endpoints_monitoring_api import EndpointsMonitoringApi
+from .endpoints_operations_api import EndpointsOperationsApi
+from .endpoints_stages_api import EndpointsStagesApi
+from .endpoints_stages_kafka_api import EndpointsStagesKafkaApi
+from .endpoints_tokens_api import EndpointsTokensApi
+from .list_api import ListApi
+from .validation_api import ValidationApi
