cf050f88901aa5399d2792626a416a56b2f1cb58
Heather MacDonald <hmacdonald@verta.ai>
2023-07-05 14:22:43 -0700

    testing is good now

--- client/verta/tests/registry/conftest.py
+++ client/verta/tests/registry/conftest.py
@@ -3,7 +3,7 @@ import json
 
 import pytest
 
-from tests.registry.pydantic_models import AClass
+from tests.registry.pydantic_models import AClass, AnotherClass
 from verta.registry import DockerImage
 from verta.registry._validate_schema import _MODEL_SCHEMA_PATH_ENV_VAR
 
@@ -23,6 +23,6 @@ def docker_image():
 @pytest.fixture
 def make_model_schema_file(tmp_path, monkeypatch):
     path = tmp_path / "model_schema.json"
-    schema = {"input": AClass.schema()}
+    schema = {"input": AClass.schema(), "output": AnotherClass.schema()}
     path.write_text(json.dumps(schema))
     monkeypatch.setenv(_MODEL_SCHEMA_PATH_ENV_VAR, str(path))
--- client/verta/verta/registry/_validate_schema.py
+++ client/verta/verta/registry/_validate_schema.py
@@ -1,6 +1,7 @@
 import functools
 import json
 import os
+import warnings
 
 import jsonschema
 
@@ -16,6 +17,7 @@ def validate_schema(f):
     def wrapper(self, *args, **kwargs):
         # accepts dict input
         # TODO: validate length of args and type
+        # Fetch schema
         prediction_input = args[0]
         model_schema_path = os.environ.get(_MODEL_SCHEMA_PATH_ENV_VAR, "/app/model_schema.json")
         try:
@@ -27,14 +29,23 @@ def validate_schema(f):
                 "no schema found for model. Did you remember to `set_schema`?"
             ) from e
         input_schema = schema["input"]
+        output_schema = schema["output"]
+
+        # Validate input
         try:
             jsonschema.validate(instance=prediction_input, schema=input_schema)
         except jsonschema.exceptions.ValidationError as e:
             raise jsonschema.exceptions.ValidationError(
-                "failed schema validation"
+                "input failed schema validation"
             ) from e
 
+        # Run function and validate output
         output = f(self, *args, **kwargs)
+        try:
+            jsonschema.validate(instance=output, schema=output_schema)
+        except jsonschema.exceptions.ValidationError as e:
+            warnings.warn("output failed schema validation: " + str(e))
+
         return output
 
     setattr(wrapper, _VALIDATE_DECORATED_FLAG, True)
