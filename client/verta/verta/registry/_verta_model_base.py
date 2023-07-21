f8cc2661103f229d1185ed19b5e31e60d5a9e271
Heather MacDonald <80712961+hmacdonald-verta@users.noreply.github.com>
2023-07-21 15:28:56 -0700

    Update client/verta/verta/registry/_verta_model_base.py
    Co-authored-by: Liu <96442646+liuverta@users.noreply.github.com>

--- client/verta/verta/registry/_verta_model_base.py
+++ client/verta/verta/registry/_verta_model_base.py
@@ -56,7 +56,7 @@ class VertaModelBase(object):
         raise NotImplementedError
 
     @abc.abstractmethod
-    def predict(self, input, headers=Dict[str, str]):
+    def predict(self, input, headers: Dict[str, str]):
         """Produce an output from `input`.
 
         This method is called when requests are made against a Verta endpoint.
