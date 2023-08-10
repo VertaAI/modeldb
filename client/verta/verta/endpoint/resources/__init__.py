627d8100f8def956857eda44b0ac3fd0ce14ca84
Heather MacDonald <hmacdonald@verta.ai>
2023-08-10 16:00:37 -0700

    forgot this

---
+++ client/verta/verta/endpoint/resources/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+from verta._internal_utils import documentation
+
+from .resources import Resources
+
+
+documentation.reassign_module(
+    [
+        Resources,
+    ],
+    module_name=__name__,
+)
