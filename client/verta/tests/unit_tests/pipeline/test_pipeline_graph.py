1fb8e1e26f88bb79d4b5fad15d7a0fac300c8a28
ewagner-verta <114943931+ewagner-verta@users.noreply.github.com>
2023-08-29 20:44:24 +0200

    Update client/verta/tests/unit_tests/pipeline/test_pipeline_graph.py
    Co-authored-by: Liu <96442646+liuverta@users.noreply.github.com>

--- client/verta/tests/unit_tests/pipeline/test_pipeline_graph.py
+++ client/verta/tests/unit_tests/pipeline/test_pipeline_graph.py
@@ -175,7 +175,7 @@ def test_bad_mutation_of_step_predecessors_exception(
     """
     mocked_rmv = make_mock_registered_model_version()
     mocked_rm = make_mock_registered_model(
-        id=mocked_rmv.registered_model_id, name="test_rmv"
+        id=mocked_rmv.registered_model_id, name="test_rm"
     )
     with patch.object(
         verta.pipeline.PipelineStep, "_get_registered_model", return_value=mocked_rm
