e43e5c707a6ff1d5fb61b25540363ecc2958c2d7
Liu <96442646+liuverta@users.noreply.github.com>
2023-09-26 10:32:11 -0700

    test: Reduce external library use in unit test suite (#4103)

--- client/verta/tests/unit_tests/registry/conftest.py
+++ client/verta/tests/unit_tests/registry/conftest.py
@@ -8,7 +8,9 @@ from typing import Type, Union
 
 import click
 import cloudpickle as cp
+import jsonschema
 import pytest
+import pytimeparse
 import requests
 from requests import post
 import urllib3
@@ -22,17 +24,13 @@ from verta.registry import verify_io, VertaModelBase
 @pytest.fixture(scope="session")
 def dependency_testing_model() -> Type[VertaModelBase]:
     """Returns a model class that imports and calls external dependencies."""
-    numpy = pytest.importorskip("numpy")
-    pd =pytest.importorskip("pandas")
-    sklearn = pytest.importorskip("sklearn")
-    torch = pytest.importorskip("torch")
-    PIL = pytest.importorskip("PIL")
 
     class DependencyTestingModel(VertaModelBase):
         """
         Model class that imports and calls external dependencies in a variety of ways
         for the purpose of testing our model environment validation logic.
         """
+
         def __init__(self, artifacts):
             pass
 
@@ -40,53 +38,56 @@ def dependency_testing_model() -> Type[VertaModelBase]:
         # extracted, thus we explicitly test the same scenarios with and without.
         @verify_io
         def predict(
-                self,
-                w: calendar.Calendar,            # standard library in function arg
-                x: dt.datetime,                  # standard library in function arg via alias
-                y: numpy.ndarray,                # 3rd-party module in function arg
-                z: Message,                      # 3rd-party module in function arg via class
-        ) -> pd.DataFrame:                       # 3rd-party module in return type hint
-            hour = x.hour                        # standard library usage in function body
-            runtime.log('error', 'Error')        # 3rd-party module in function body (VERTA)
-            yaml_con = yaml.constructor          # 3rd party module in function body
-            z = self.make_dataframe(y)           # 3rd party module called indirectly
+            self,
+            w: calendar.Calendar,  # standard library in function arg
+            x: dt.datetime,  # standard library in function arg via alias
+            y: click.File,  # 3rd-party module in function arg
+            z: Message,  # 3rd-party module in function arg via class
+        ) -> cp.CloudPickler:  # 3rd-party module in return type hint
+            hour = x.hour  # standard library usage in function body
+            runtime.log("error", "Error")  # 3rd-party module in function body (VERTA)
+            yaml_con = yaml.constructor  # 3rd party module in function body
+            z = self.do_something(y)  # 3rd party module called indirectly
             return z
-
 
         def unwrapped_predict(
-                self,
-                a: json.JSONEncoder,             # standard library in function arg
-                b: collecs.OrderedDict,          # standard library in function arg via alias
-                c: sklearn.base.BaseEstimator,   # 3rd-party module in function arg
-                d: cp.CloudPickler,              # 3rd-party module in function arg via alias
-        ) -> requests.Timeout:                   # 3rd-party module in return type hint
-            _json = a.encode({'x':'y'})          # standard library usage in function body
-            with runtime.context():              # 3rd-party module in function body (VERTA)
-                runtime.log('error', 'Error')    # 3rd-party module in function body (VERTA)
-            click_exc = click.ClickException     # 3rd party module in function body
-            z = self.make_timeout()              # 3rd party module called indirectly
+            self,
+            a: json.JSONEncoder,  # standard library in function arg
+            b: collecs.OrderedDict,  # standard library in function arg via alias
+            c: yaml.SafeLoader,  # 3rd-party module in function arg
+            d: cp.CloudPickler,  # 3rd-party module in function arg via alias
+        ) -> requests.Timeout:  # 3rd-party module in return type hint
+            _json = a.encode({"x": "y"})  # standard library usage in function body
+            with runtime.context():  # 3rd-party module in function body (VERTA)
+                runtime.log(
+                    "error", "Error"
+                )  # 3rd-party module in function body (VERTA)
+            click_exc = click.ClickException  # 3rd party module in function body
+            z = self.make_timeout()  # 3rd party module called indirectly
             return z
 
-        def make_dataframe(self, input):         # No modules in function signature
-            return pd.DataFrame(input)           # 3rd party module in function body
+        def make_dataframe(self, input):  # No modules in function signature
+            return pytimeparse.parse(input)  # 3rd party module in function body
 
         def make_message(self, input: str):
             msg = Message(input)
             return msg
 
         @staticmethod
-        def make_timeout():                      # No modules in function signature
-            return requests.Timeout()            # 3rd party module in function body
+        def make_timeout():  # No modules in function signature
+            return requests.Timeout()  # 3rd party module in function body
 
         def post_request(self) -> None:
             post("https://www.verta.ai")
 
         # 3rd-party modules nested inside type constructs should still be extracted
-        def nested_multiple_returns_hint(self) -> Union[urllib3.Retry, PIL.UnidentifiedImageError]:
-            return urllib3.Retry or PIL.UnidentifiedImageError
+        def nested_multiple_returns_hint(
+            self,
+        ) -> Union[urllib3.Retry, jsonschema.exceptions.ValidationError]:
+            return urllib3.Retry or jsonschema.exceptions.ValidationError
 
         # 3rd-party modules nested inside type constructs should still be extracted
-        def nested_type_hint(self) -> Type[torch.NoneType]:
-            return torch.NoneType
+        def nested_type_hint(self) -> Type[jsonschema.TypeChecker]:
+            return jsonschema.TypeChecker
 
     return DependencyTestingModel
--- client/verta/tests/unit_tests/registry/test_check_model_dependencies.py
+++ client/verta/tests/unit_tests/registry/test_check_model_dependencies.py
@@ -9,27 +9,28 @@ from verta.environment import Python
 from verta.registry import check_model_dependencies
 
 
-@pytest.fixture(scope='session')
+@pytest.fixture(scope="session")
 def complete_env() -> Python:
-    """ Environment with all 3rd-party packages expected to be extracted
+    """Environment with all 3rd-party packages expected to be extracted
     from the dependency_testing_model fixture.
     """
-    return Python([
-        'click==0.0.1',
-        'googleapis-common-protos==0.0.1',
-        'numpy==0.0.1',
-        'pandas==0.0.1',
-        'Pillow==0.0.1',
-        'requests==0.0.1',
-        'scikit-learn==0.0.1',
-        'torch==0.0.1',
-        'urllib3==0.0.1',
-        'PyYAML==0.0.1',
-    ])  # `verta` and `cloudpickle` included by default
+    return Python(
+        [
+            "click",
+            "jsonschema",
+            "googleapis-common-protos",
+            "pytimeparse",
+            "requests",
+            "urllib3",
+            "PyYAML",
+        ]
+    )  # `verta` and `cloudpickle` included by default
 
 
-def test_check_model_dependencies_complete(dependency_testing_model, complete_env) -> None:
-    """ Verify that check_model_dependencies extracts all the expected packages from
+def test_check_model_dependencies_complete(
+    dependency_testing_model, complete_env
+) -> None:
+    """Verify that check_model_dependencies extracts all the expected packages from
     the test model class (dependency_testing_model fixture) and correctly reconciles
     them against the provided environment (complete_env fixture).
     """
@@ -40,12 +41,14 @@ def test_check_model_dependencies_complete(dependency_testing_model, complete_en
     )
 
 
-def test_check_model_dependencies_missing_raise(dependency_testing_model, complete_env) -> None:
-    """ Verify that check_model_dependencies raises an exception, with the
+def test_check_model_dependencies_missing_raise(
+    dependency_testing_model, complete_env
+) -> None:
+    """Verify that check_model_dependencies raises an exception, with the
     correct message, for missing packages when `raise_for_missing` is True.
     """
     incomplete_env = Python(
-        [r for r in complete_env.requirements if r != 'click==0.0.1']
+        [r for r in complete_env.requirements if not r.startswith("click==")]
     )  # drop a single dependency to be caught
     with pytest.raises(RuntimeError) as err:
         check_model_dependencies(
@@ -53,16 +56,25 @@ def test_check_model_dependencies_missing_raise(dependency_testing_model, comple
             environment=incomplete_env,
             raise_for_missing=True,
         )
-    assert err.value.args[0] == "the following packages are required by the model but missing " \
-                                "from the environment:\nclick (installed via ['click'])"
+    assert (
+        err.value.args[0]
+        == "the following packages are required by the model but missing "
+        "from the environment:\nclick (installed via ['click'])"
+    )
 
 
-def test_check_model_dependencies_missing_warning(dependency_testing_model, complete_env) -> None:
-    """ Verify that check_model_dependencies defaults to raising a warning, with
+def test_check_model_dependencies_missing_warning(
+    dependency_testing_model, complete_env
+) -> None:
+    """Verify that check_model_dependencies defaults to raising a warning, with
     the correct message, for missing packages when `raise_for_missing` is False.
     """
     incomplete_env = Python(
-        [r for r in complete_env.requirements if r not in ['PyYAML==0.0.1', 'pandas==0.0.1']]
+        [
+            r
+            for r in complete_env.requirements
+            if not r.startswith(("PyYAML==", "requests=="))
+        ]
     )  # drop a single dependency to be caught
     with warnings.catch_warnings(record=True) as caught_warnings:
         assert not check_model_dependencies(
@@ -71,6 +83,8 @@ def test_check_model_dependencies_missing_warning(dependency_testing_model, comp
             raise_for_missing=False,
         )
     warn_msg = caught_warnings[0].message.args[0]
-    assert warn_msg == "the following packages are required by the model but missing " \
-                       "from the environment:\npandas (installed via ['pandas'])" \
-                       "\nyaml (installed via ['PyYAML'])"
+    assert (
+        warn_msg == "the following packages are required by the model but missing "
+        "from the environment:\nrequests (installed via ['requests'])"
+        "\nyaml (installed via ['PyYAML'])"
+    )
