1aeea3e7bab962cf9a5f324b8d03cd9325e251c6
Michael Liu <miliu@verta.ai>
2023-10-06 18:50:23 -0700

    Add org ID tests

--- client/verta/tests/unit_tests/client/test_arguments.py
+++ client/verta/tests/unit_tests/client/test_arguments.py
@@ -32,3 +32,30 @@ def test_organization_id_and_name_error(make_mock_client, org_id, org_name):
         monkeypatch.setenv("VERTA_ORG_NAME", org_name)
         with raises_expected_error:
             make_mock_client(organization_id=org_id)
+
+
+class TestOrganizationId:
+    @hypothesis.given(org_id=st.uuids())
+    def test_arg(self, make_mock_client, org_id):
+        org_id = str(org_id)
+
+        client = make_mock_client(organization_id=org_id)
+        assert client._conn._credentials.organization_id == org_id
+
+    @hypothesis.given(org_id=st.uuids())
+    def test_env_var(self, make_mock_client, org_id):
+        org_id = str(org_id)
+
+        with pytest.MonkeyPatch.context() as monkeypatch:
+            monkeypatch.setenv("VERTA_ORG_ID", org_id)
+            client = make_mock_client()
+        assert client._conn._credentials.organization_id == org_id
+
+    @hypothesis.given(org_ids=st.lists(st.uuids(), min_size=2, max_size=2))
+    def test_arg_overrides_env_var(self, make_mock_client, org_ids):
+        org_id1, org_id2 = map(str, org_ids)
+
+        with pytest.MonkeyPatch.context() as monkeypatch:
+            monkeypatch.setenv("VERTA_ORG_ID", org_id1)
+            client = make_mock_client(organization_id=org_id2)
+        assert client._conn._credentials.organization_id == org_id2
