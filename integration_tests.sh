005bda844dacdc4c98cbd0aeb4e6a719f317a27e
John K. Watson <john@verta.ai>
2023-12-19 09:33:30 -0800

    fix: more toolchain usage across all of the submodules

--- integration_tests.sh
+++ integration_tests.sh
@@ -5,5 +5,5 @@ export LOG4J_FORMAT_MSG_NO_LOOKUPS=true
 export VERTA_MODELDB_TEST_CONFIG=itconfig/config-test-h2.yaml
 export LOG4J_CONFIGURATION_FILE=itconfig/log4j2.yaml
 
-#mvn -B verify -Dtest=PullRequestSuite -Dmaven.compiler.useIncrementalCompilation=false -Dsurefire.rerunFailingTestsCount=2
-mvn -B verify -Dtest=PullRequestSuite -Dsurefire.rerunFailingTestsCount=2 jacoco:report
+mvn -B verify -Dtest=PullRequestSuite -Dmaven.compiler.useIncrementalCompilation=false -Dsurefire.rerunFailingTestsCount=2
+#mvn -B test -Dtest=PullRequestSuite -Dsurefire.rerunFailingTestsCount=2 -Dtoolchain.skip=false -Dspotless.check.skip=true -Dspotless.apply.skip=true jacoco:report
